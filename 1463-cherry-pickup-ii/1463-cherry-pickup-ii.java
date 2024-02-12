c415b3b1e4828990d7d602a75fb092344fdb26bf
team-hungrian <def.hunger@gmail.com>
2024-02-12 18:02:34 +0900

    Time: 12 ms (99.33%), Space: 44.6 MB (93.57%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,6 +1,35 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        return try_bottomup(grid);
+        return try_bottomup_spaceopt(grid);
+    }
+    
+    public int try_bottomup_spaceopt(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        int[][] dp = new int[n + 1][n + 1];
+        
+        for (int r = m - 1; r >= 0; r--) {
+            int[][] currentDp = new int[n + 1][n + 1];
+            for (int c1 = 0; c1 < n; c1 ++) {
+                for (int c2 = n - 1; c2 > c1; c2--) {
+                    int sum = 0;
+                    
+                    for (int i = -1; i <= 1; i++) {
+                        for (int j = -1; j <= 1; j++) {
+                            if (c1 + i >= 0 && c2 + j >= 0) {
+                                sum = Math.max(sum, dp[c1 + i][c2 + j]);
+                            }
+                        }
+                    }
+                    
+                    currentDp[c1][c2] = sum + grid[r][c1] + grid[r][c2];
+                }
+            }
+            dp = currentDp;
+        }
+        
+        return dp[0][n - 1];
     }
     
     public int try_bottomup(int[][] grid) {
