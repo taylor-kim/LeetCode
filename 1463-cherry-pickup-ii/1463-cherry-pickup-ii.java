5bd6e7cf77c3354af4ef828bfaf35de66394477b
team-hungrian <def.hunger@gmail.com>
2024-02-12 17:25:58 +0900

    Time: 26 ms (59.42%), Space: 47.3 MB (10.64%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -8,29 +8,29 @@ class Solution {
         int n = grid[0].length;
         
         Integer[][][] dp = new Integer[m][n][n];
-        return dfs(grid, 0, 0, 0, n - 1, dp);
+        return dfs(grid, 0, 0, n - 1, dp);
     }
     
-    private int dfs(int[][] grid, int y1, int x1, int y2, int x2, Integer[][][] dp) {
-        if (!isValid(grid, y1, x1) || !isValid(grid, y2, x2)) {
+    private int dfs(int[][] grid, int y, int x1, int x2, Integer[][][] dp) {
+        if (!isValid(grid, y, x1) || !isValid(grid, y, x2)) {
             return 0;
         }
         
-        if (dp[y1][x1][x2] != null) {
-            return dp[y1][x1][x2];
+        if (dp[y][x1][x2] != null) {
+            return dp[y][x1][x2];
         }
         
-        int sum = (y1 == y2 && x1 == x2) ? grid[y1][x1] : grid[y1][x1] + grid[y2][x2];
+        int sum = (x1 == x2) ? grid[y][x1] : grid[y][x1] + grid[y][x2];
         
         int next = 0;
         
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
-                next = Math.max(next, dfs(grid, y1 + 1, x1 + i, y2 + 1, x2 + j, dp));
+                next = Math.max(next, dfs(grid, y + 1, x1 + i, x2 + j, dp));
             }
         }
         
-        return dp[y1][x1][x2] = sum + next;
+        return dp[y][x1][x2] = sum + next;
     }
     
     private boolean isValid(int[][] grid, int y, int x) {
