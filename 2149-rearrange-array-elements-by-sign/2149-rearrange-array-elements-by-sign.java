e9be1e8fd2bfbd37c5a1ef699e882d43fb28c958
team-hungrian <def.hunger@gmail.com>
2024-02-14 16:29:31 +0900

    Time: 6 ms (30.79%), Space: 76.7 MB (80.13%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,6 +1,39 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        return mySimple(nums);
+        return mySimple2(nums);
+    }
+    
+    public int[] try_spaceopt(int[] nums) {
+        return nums;
+    }
+    
+    public int[] mySimple2(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        
+        int index = 0;
+        int pos = 0;
+        int neg = 0;
+        
+        while (index < n) {
+            while (nums[pos] < 0) pos++;
+            
+            ret[index] = nums[pos++];
+            
+            index += 2;
+        }
+        
+        index = 1;
+        
+        while (index < n) {
+            while (nums[neg] > 0) neg++;
+            
+            ret[index] = nums[neg++];
+            
+            index += 2;
+        }
+        
+        return ret;
     }
     
     public int[] mySimple(int[] nums) {
