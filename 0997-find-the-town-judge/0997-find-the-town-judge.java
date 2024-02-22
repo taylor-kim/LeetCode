bbce24f6c41b3590a07cad9d81a9f5b4286da8ef
team-hungrian <def.hunger@gmail.com>
2024-02-22 14:13:09 +0900

    Time: 3 ms (71.04%), Space: 53.9 MB (35.74%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        return mySol(n, trust);
+    }
+    
+    public int mySol(int n, int[][] trust) {
+        int[] count = new int[n];
+        
+        for (int[] t : trust) {
+            count[t[0] - 1]--;
+            count[t[1] - 1]++;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (count[i] == n - 1) return i + 1;
+        }
+        
+        return -1;
+    }
+}
