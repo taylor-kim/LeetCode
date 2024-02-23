b4a52f2ee00159eba4e3da158b9a6c37d1154b92
team-hungrian <def.hunger@gmail.com>
2024-02-23 18:22:06 +0900

    Time: 12 ms (21.74%), Space: 46.6 MB (5.39%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -61,7 +61,6 @@ class Solution {
         
         while (!queue.isEmpty() && k-- > 0) {
             int size = queue.size();
-            int minCost = Integer.MAX_VALUE;
             
             while (size-- > 0) {
                 int city = queue.peek()[0];
@@ -73,16 +72,58 @@ class Solution {
                     continue;
                 }
                 
-                if (city == dst) {
-                    minCost = Math.min(minCost, cost);
-                }
-                
                 costs[city] = cost;
                 
                 if (edges.get(city) == null) continue;
                 
                 for (int[] next : edges.get(city)) {
                     queue.add(new int[] {next[0], cost + next[1]});
+                }
+            }
+        }
+        
+        // System.out.println(Arrays.toString(costs));
+        
+        return costs[dst] == Integer.MAX_VALUE ? -1 : costs[dst];
+    }
+    
+    public int mySol_bfs_improved(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> edges = new HashMap();
+        
+        for (int[] f : flights) {
+            edges.computeIfAbsent(f[0], key -> new ArrayList());
+            edges.get(f[0]).add(new int[] {f[1], f[2]});
+        }
+        
+        Queue<int[]> queue = new LinkedList();
+        queue.add(new int[] {src, 0});
+        
+        int[] costs = new int[n];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        
+        k += 2;
+        
+        while (!queue.isEmpty() && k-- > 0) {
+            int size = queue.size();
+            int minCost = Integer.MAX_VALUE;
+            
+            while (size-- > 0) {
+                int city = queue.peek()[0];
+                int cost = queue.poll()[1];
+                
+                // System.out.println(String.format("city:%d, cost:%d, k:%d", city, cost, k + 1));
+                
+                costs[city] = cost;
+                
+                if (edges.get(city) == null) continue;
+                
+                for (int[] next : edges.get(city)) {
+                    int nextNode = next[0];
+                    int nextCost = cost + next[1];
+                    
+                    if (costs[nextNode] > nextCost) {
+                        queue.add(new int[] {next[0], cost + next[1]});
+                    }
                 }
             }
         }
