56f908c9940fd3e6249ab831382d1feeb458afe4
team-hungrian <def.hunger@gmail.com>
2024-02-25 19:04:52 +0900

    Time: 46 ms (83.00%), Space: 107.7 MB (49.00%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.java
@@ -1,53 +1,94 @@
 class Solution {
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        return official_bfs(n, meetings, firstPerson);
+        return try_official_bfs(n, meetings, firstPerson);
     }
     
-    public List<Integer> official_bfs(int n, int[][] meetings, int firstPerson) {
-        // For every person, we store the meeting time and label of the person met.
-        Map<Integer, List<int[]>> graph = new HashMap<>();
-        for (int[] meeting : meetings) {
-            int x = meeting[0], y = meeting[1], t = meeting[2];
-            graph.computeIfAbsent(x, k -> new ArrayList<>()).add(new int[]{t, y});
-            graph.computeIfAbsent(y, k -> new ArrayList<>()).add(new int[]{t, x});
+    public List<Integer> try_official_bfs(int n, int[][] meetings, int first) {
+        Map<Integer, List<int[]>> edges = new HashMap();
+        
+        for (int[] m : meetings) {
+            edges.computeIfAbsent(m[0], k -> new ArrayList()).add(new int[] {m[1], m[2]});
+            edges.computeIfAbsent(m[1], k -> new ArrayList()).add(new int[] {m[0], m[2]});
         }
-
-        // Earliest time at which a person learned the secret 
-        // as per current state of knowledge. If due to some new information, 
-        // the earliest time of knowing the secret changes, we will update it
-        // and again process all the people whom he/she meets after the time
-        // at which he/she learned the secret.
+        
+        Queue<int[]> queue = new LinkedList();
+        queue.add(new int[] {0, 0});
+        queue.add(new int[] {first, 0});
+        
+        Set<Integer> set = new HashSet();
+        // set.add(0);
+        // set.add(first);
+        
         int[] earliest = new int[n];
         Arrays.fill(earliest, Integer.MAX_VALUE);
         earliest[0] = 0;
-        earliest[firstPerson] = 0;
+        earliest[first] = 0;
         
-        // Queue for BFS. It will store (person, time of knowing the secret)
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
-        q.offer(new int[]{firstPerson, 0});
-
-        // Do BFS
-        while (!q.isEmpty()) {
-            int[] personTime = q.poll();
-            int person = personTime[0], time = personTime[1];
-            for (int[] nextPersonTime : graph.getOrDefault(person, new ArrayList<>())) {
-                int t = nextPersonTime[0], nextPerson = nextPersonTime[1];
-                if (t >= time && earliest[nextPerson] > t) {
-                    earliest[nextPerson] = t;
-                    q.offer(new int[]{nextPerson, t});
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            
+            while (size-- > 0) {
+                int a = queue.peek()[0];
+                int t = queue.poll()[1];
+                
+                // if (!set.add(a)) {
+                //     continue;
+                // }
+                
+                for (int[] next : edges.getOrDefault(a, Collections.emptyList())) {
+                    if (next[1] >= t && earliest[next[0]] > next[1]) {
+                        earliest[next[0]] = next[1];
+                        queue.add(next);
+                    }
                 }
             }
         }
         
-        // Since we visited only those people who know the secret,
-        // we need to return indices of all visited people.
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
+        // return new ArrayList(set);
+        
+        List<Integer> ans = new ArrayList();
+        
+        for (int i = 0; i < n; i++) {
             if (earliest[i] != Integer.MAX_VALUE) {
                 ans.add(i);
             }
         }
+        
+        return ans;
+    }
+    
+    public List<Integer> mySol3_fail(int n, int[][] meetings, int first) {
+        Map<Integer, Set<int[]>> map = new TreeMap();
+        
+        UnionFind uf = new UnionFind(n);
+        uf.merge(0, first);
+        
+        // uf.print();
+        
+        for (int[] m : meetings) {
+            map.computeIfAbsent(m[2], k -> new HashSet());
+            map.get(m[2]).add(new int[] {m[0], m[1]});
+            uf.merge(m[0], m[1]);
+        }
+        
+        for (Set<int[]> parts : map.values()) {
+            for (int[] each : parts) {
+                if (uf.find(each[0]) == 0) {
+                    uf.merge(0, each[1]);
+                } else if (uf.find(each[1]) == 0) {
+                    uf.merge(0, each[0]);
+                }
+            }
+        }
+        
+        List<Integer> ans = new ArrayList();
+        
+        for (int i = 0; i < n; i++) {
+            if (uf.find(i) == 0) {
+                ans.add(i);
+            }
+        }
+        
         return ans;
     }
     
@@ -121,6 +162,13 @@ class Solution {
         
         public void merge(int a, int b) {
             a = find(a);
+            b = find(b);
+            
+            if (a > b) {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
             
             parents[b] = a;
         }
