5af505ef3b68ff0aa12199f4be36b82ed68275ab
team-hungrian <def.hunger@gmail.com>
2024-03-01 17:40:02 +0900

    Time: 1 ms (100.00%), Space: 42.7 MB (99.46%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -1,6 +1,38 @@
 class Solution {
     public String maximumOddBinaryNumber(String s) {
-        return mySol(s);
+        return official_onepass(s);
+    }
+    
+    public String official_onepass(String s) {
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left <= right) {
+            if (arr[left] == '1') {
+                left++;
+                continue;
+            }
+            
+            if (arr[right] == '0') {
+                right--;
+                continue;
+            }
+            
+            arr[left++] = '1';
+            arr[right--] = '0';
+        }
+        
+        arr[left - 1] = '0';
+        arr[arr.length - 1] = '1';
+        
+        return String.valueOf(arr);
+    }
+    
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     
     public String mySol(String s) {
