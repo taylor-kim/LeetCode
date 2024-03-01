79bad0feb650b2f81748d1394e2041888e52419b
team-hungrian <def.hunger@gmail.com>
2024-03-01 17:16:30 +0900

    Time: 2 ms (83.47%), Space: 43.2 MB (69.38%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        return mySol(s);
+    }
+    
+    public String mySol(String s) {
+        int n = s.length();
+        int numberOfOne = 0;
+        
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                numberOfOne++;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("");
+        sb.append("1".repeat(numberOfOne - 1));
+        sb.append("0".repeat(n - numberOfOne));
+        sb.append("1");
+        
+        return sb.toString();
+    }
+}
