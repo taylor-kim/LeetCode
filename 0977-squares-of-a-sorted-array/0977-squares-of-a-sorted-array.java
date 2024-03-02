bdbae904e04a3771010b570af7e17c127fdec4a9
team-hungrian <def.hunger@gmail.com>
2024-03-02 19:41:58 +0900

    Time: 1 ms (100.00%), Space: 47.4 MB (18.17%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,6 +1,25 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        return mySol(nums);
+        return others_two_pointer(nums);
+    }
+    
+    public int[] others_two_pointer(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        
+        int[] ans = new int[n];
+        int index = n - 1;
+        
+        while (left <= right) {
+            if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+                ans[index--] = nums[right] * nums[right--];
+            } else {
+                ans[index--] = nums[left] * nums[left++];
+            }
+        }
+        
+        return ans;
     }
     
     public int[] mySol(int[] nums) {
