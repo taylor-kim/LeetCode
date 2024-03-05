2122dd4c90328e5d1f4cc4acee6c7af5ae7a9456
team-hungrian <def.hunger@gmail.com>
2024-03-05 17:32:32 +0900

    Time: 3 ms (100.00%), Space: 44.9 MB (87.94%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,6 +1,25 @@
 class Solution {
     public int minimumLength(String s) {
-        return mySol(s);
+        return official_two_pointer(s);
+    }
+    
+    public int official_two_pointer(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            char c = s.charAt(left);
+            
+            while (left <= right && s.charAt(left) == c) {
+                left++;
+            }
+            
+            while (left < right && s.charAt(right) == c) {
+                right--;
+            }
+        }
+        
+        return right - left + 1;
     }
     
     public int mySol(String s) {
