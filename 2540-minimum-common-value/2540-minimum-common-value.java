b4c4cbe4b8cfed5f9639a906f45f623cf1fef956
team-hungrian <def.hunger@gmail.com>
2024-03-09 14:20:46 +0900

    Time: 7 ms (34.44%), Space: 63.1 MB (26.00%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,6 +1,35 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        return mySol(nums1, nums2);
+        return official_binarySearch(nums1, nums2);
+    }
+    
+    public int official_binarySearch(int[] nums1, int[] nums2) {
+        for (int num : nums1) {
+            if (search(nums2, num)) {
+                return num;
+            }
+        }
+        
+        return -1;
+    }
+    
+    private boolean search(int[] arr, int target) {
+        int lo = 0;
+        int hi = arr.length - 1;
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        
+        return false;
     }
     
     public int mySol(int[] nums1, int[] nums2) {
