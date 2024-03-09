f8ee81bbe0e75103b2e48833c6d7d0a0f3e27dad
team-hungrian <def.hunger@gmail.com>
2024-03-09 18:00:09 +0900

    Time: 231 ms (55.59%), Space: 54.9 MB (99.74%) - LeetHub

--- 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.java
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.java
@@ -1,85 +1,188 @@
 class Solution {
     public boolean canTraverseAllPairs(int[] nums) {
-        return test_leethub3(nums);
+        return others_sol(nums);
     }
 
-    public boolean test_leethub3(int[] nums) {
-        int MAX = 100000;
-        int N = nums.length;
-        boolean[] has = new boolean[MAX + 1];
-        for (int x: nums) {
-            has[x] = true;
+    public boolean others_sol(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) return true;
+
+        Graph g = new UnionFind(n);
+        Map<Integer, Integer> seen = new HashMap();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) return false;
+
+            List<Integer> primes = getPrimeFactors(nums[i]);
+
+            // System.out.println(String.format("num:%d, primeFactors:%s", nums[i], primes));
+
+            for (int p : primes) {
+                if (seen.containsKey(p)) {
+                    g.addEdge(seen.get(p), i);
+                } else {
+                    seen.put(p, i);
+                }
+            }
         }
 
-        // edge cases
-        if (N == 1) {
+        return g.isConnected();
+    }
+
+    private List<Integer> getPrimeFactors(int x) {
+        List<Integer> primeFactors = new ArrayList();
+
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) {
+                primeFactors.add(i);
+
+                while (x % i == 0) {
+                    x /= i;
+                }
+            }
+        }
+        if (x != 1) {
+            primeFactors.add(x);
+        }
+
+        return primeFactors;
+    }
+
+    public interface Graph {
+        void addEdge(int i, int j);
+        boolean isConnected();
+    }
+
+    public class DFS implements Graph {
+        protected int n;
+        protected Map<Integer, List<Integer>> edges;
+        protected boolean[] visited;
+
+        public DFS(int n) {
+            this.n = n;
+            edges = new HashMap();
+            visited = new boolean[n];
+        }
+
+        public void addEdge(int i, int j) {
+            edges.computeIfAbsent(i, k -> new ArrayList()).add(j);
+            edges.computeIfAbsent(j, k -> new ArrayList()).add(i);
+        }
+
+        public boolean isConnected() {
+            traverse(0);
+
+            for (boolean b : visited) {
+                if (!b) return false;
+            }
+
             return true;
         }
-        if (has[1]) {
-            return false;
+
+        void traverse(int index) {
+            if (index >= n || visited[index]) return;
+
+            visited[index] = true;
+
+            if (!edges.containsKey(index)) return;
+
+            for (int next : edges.get(index)) {
+                traverse(next);
+            }
+        }
+    }
+
+    public class BFS extends DFS {
+        public BFS(int n) {
+            super(n);
         }
 
-        // the general solution
-        int[] sieve = new int[MAX + 1];
-        for (int d = 2; d <= MAX; d++) {
-            if (sieve[d] == 0) {
-                for (int v = d; v <= MAX; v += d) {
-                    sieve[v] = d;
+        void traverse(int index) {
+            Queue<Integer> queue = new LinkedList();
+            queue.add(index);
+
+            while (!queue.isEmpty()) {
+                int node = queue.poll();
+                // visited[node] = true;
+
+                if (!edges.containsKey(node)) {
+                    continue;
+                }
+
+                for (int next : edges.get(node)) {
+                    if (!visited[next]) {
+                        queue.add(next);
+                        visited[next] = true;
+                    }
                 }
             }
         }
 
-        DSU union = new DSU(2 * MAX + 1);
-        for (int x: nums) {
-            int val = x;
-            while (val > 1) {
-                int prime = sieve[val];
-                int root = prime+MAX;
-                if (union.find(root) != union.find(x)) {
-                    union.merge(root, x);
+        void traverse_others(int x) {
+            Queue<Integer> q = new LinkedList<>();
+            q.offer(x);
+            while (!q.isEmpty()) {
+                x = q.poll();
+                if (!edges.containsKey(x)) {
+                    continue;
                 }
-                while (val % prime == 0) {
-                    val /= prime;
+                for (int y: edges.get(x)) {
+                    if (!visited[y]) {
+                        q.add(y);
+                        visited[y] = true;
+                    }
                 }
             }
         }
+    }
 
-        int cnt = 0;
-        for (int i=2; i <= MAX; i++) {
-            if (has[i] && union.find(i) == i) {
-                cnt++;
+    public class UnionFind implements Graph {
+        private int n;
+        private int[] parents;
+        private int[] ranks;
+
+        public UnionFind(int n) {
+            this.n = n;
+            parents = new int[n];
+            ranks = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+                ranks[i] = 1;
             }
         }
-        return cnt == 1;
-    }
-}
-class DSU {
-    public int[] dsu;
-    public int[] size;
 
-    public DSU(int N) {
-        dsu = new int[N + 1];
-        size = new int[N + 1];
-        for (int i = 0; i <= N; i++) {
-            dsu[i] = i;
-            size[i] = 1;
+        private int find(int num) {
+            if (parents[num] != num) {
+                parents[num] = find(parents[num]);
+            }
+
+            return parents[num];
         }
-    }
-    public int find(int x) {
-        return dsu[x] == x ? x : (dsu[x] = find(dsu[x]));
-    }
-    public void merge(int x, int y) {
-        int fx = find(x);
-        int fy = find(y);
-        if (fx == fy){
-            return;
+
+        private void merge(int a, int b) {
+            a = find(a);
+            b = find(b);
+
+            if (a != b) {
+                if (ranks[a] < ranks[b]) {
+                    int temp = a;
+                    a = b;
+                    b = temp;
+                }
+
+                parents[b] = a;
+                ranks[a] += ranks[b];
+            }
         }
-        if (size[fx] > size[fy]) {
-            int temp = fx;
-            fx = fy;
-            fy = temp;
+
+        public void addEdge(int i, int j) {
+            merge(i, j);
         }
-        dsu[fx] = fy;
-        size[fy] += size[fx];
+
+        public boolean isConnected() {
+            return ranks[find(0)] == n;
+        }
     }
 }
