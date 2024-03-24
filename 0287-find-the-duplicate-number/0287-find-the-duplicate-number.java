512f045f66f694243ede6f8d5c8d804663707832
team-hungrian <def.hunger@gmail.com>
2024-03-24 15:50:05 +0900

    Time: 5 ms (68.61%), Space: 57.8 MB (73.33%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        return mySol(nums);
+    }
+
+    public int mySol(int[] nums) {
+        int ans = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]);
+
+            nums[index - 1] *= -1;
+
+            if (nums[index - 1] > 0) {
+                ans = index;
+                break;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Math.abs(nums[i]);
+        }
+
+        return ans;
+    }
+}
