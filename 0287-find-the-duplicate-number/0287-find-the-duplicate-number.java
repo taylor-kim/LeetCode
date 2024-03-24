346567b14a0db31e031930ae75e3a22b5b7d053b
team-hungrian <def.hunger@gmail.com>
2024-03-24 17:28:45 +0900

    Time: 40 ms (5.07%), Space: 58.2 MB (55.1%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,39 +1,36 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        return try_bit(nums);
+        return study_bit(nums);
     }
 
-    public int try_bit(int[] nums) {
-        int max = 0;
-        for (int num : nums) {
-            max = Math.max(max, num);
-        }
-
+    public int study_bit(int[] nums) {
         int bitLimit = 0;
+        int ans = 0;
+        int max = 0;
+
+        for (int num : nums) max = Math.max(max, num);
 
         while (max > 0) {
             max >>= 1;
             bitLimit++;
         }
 
-        int ans = 0;
-
         for (int bit = 0; bit < bitLimit; bit++) {
             int mask = 1 << bit;
-            int countOfDistinct = 0;
-            int countOfNums = 0;
+            int countWithoutDup = 0;
+            int countNum = 0;
 
-            for (int i = 0; i <= nums.length - 1; i++) {
-                if ((i & mask) > 0) {
-                    countOfDistinct++;
+            for (int i = 0; i < nums.length; i++) {
+                if ((i & mask) != 0) {
+                    countWithoutDup++;
                 }
 
-                if ((nums[i] & mask) > 0) {
-                    countOfNums++;
+                if ((nums[i] & mask) != 0) {
+                    countNum++;
                 }
             }
 
-            if (countOfNums > countOfDistinct) {
+            if (countNum > countWithoutDup) {
                 ans |= mask;
             }
         }
@@ -41,121 +38,11 @@ class Solution {
         return ans;
     }
 
-    public int officialBit(int[] nums) {
-        int n = nums.length - 1;
-        int ans = 0;
-
-        int bitLimit = getPositionOfSignificant(getMax(nums));
-
-        for (int bit = 0; bit < bitLimit; bit++) {
-            int mask = 1 << bit;
-            int countOfDistinct = 0;
-            int countOfNums = 0;
-
-            for (int i = 0; i <= n; i++) {
-                if ((i & mask) > 0) {
-                    countOfDistinct++;
-                }
-
-                if ((nums[i] & mask) > 0) {
-                    countOfNums++;
-                }
-            }
-
-            if (countOfNums > countOfDistinct) {
-                ans |= mask;
-            }
-        }
-
-        return ans;
-    }
-
-    private int getPositionOfSignificant(int num) {
-        int pos = 0;
-
-        while (num > 0) {
-            num >>= 1;
-            pos++;
-        }
-
-        return pos;
-    }
-
-    private int getMax(int[] nums) {
-        int max = 0;
-
-        for (int num : nums) {
-            max = Math.max(max, num);
-        }
-
-        return max;
-    }
-
-    // https://www.youtube.com/watch?v=wjYnzkAhcNk
-    public int studyAgain_slow_fast(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        slow = nums[0];
-
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-
-        return slow;
-    }
-
-    public int mySol(int[] nums) {
-        int ans = -1;
-
-        for (int i = 0; i < nums.length; i++) {
-            int num = Math.abs(nums[i]);
-
-            if (nums[num] < 0) {
-                ans = num;
-                break;
-            } else {
-                nums[num] *= -1;
-            }
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = Math.abs(nums[i]);
-        }
-
-        return ans;
-    }
-
-    public int officialTortoiseAndHare(int[] nums) {
-        int tor = nums[0];
-        int hare = nums[0];
-
-        do {
-            tor = nums[tor];
-            hare = nums[nums[hare]];
-        } while (tor != hare);
-
-        tor = nums[0];
-
-        while (tor != hare) {
-            tor = nums[tor];
-            hare = nums[hare];
-        }
-
-        return tor;
-    }
-
-    public int officialBinarySearch(int[] nums) {
+    public int try_binarysearch(int[] nums) {
         int lo = 1;
-        int hi = nums.length - 1;
+        int hi = nums.length;
 
-        while (lo <= hi) {
+        while (lo < hi) {
             int cand = lo + (hi - lo) / 2;
 
             int count = 0;
@@ -166,13 +53,40 @@ class Solution {
                 }
             }
 
-            if (count > cand) {
-                hi = cand - 1;
-            } else {
+            if (count == cand) {
+                lo = cand + 1;
+            } else if (count > cand) {
+                hi = cand;
+            } else if (count < cand) {
                 lo = cand + 1;
             }
         }
 
         return lo;
+    }
+
+    public int mySol(int[] nums) {
+        int n = nums.length;
+        int slow = nums[0];
+        int fast = slow;
+
+        // 1 -> 3 -> 2 -> 4 -> 2
+        // 1 -> 3 -> 2 <-> 4
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast) break;
+        }
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
     }
 }
