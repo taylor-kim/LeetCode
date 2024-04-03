6c0524ddbe496708b3c46de6787adda914b413d5
team-hungrian <def.hunger@gmail.com>
2024-04-03 17:44:20 +0900

    Time: 176 ms (36.11%), Space: 41.6 MB (49.08%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -4,37 +4,35 @@ class Solution {
     }
 
     public boolean mySol(char[][] board, String word) {
-        boolean[][] visit = new boolean[board.length][board[0].length];
-
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (mySol(board, word, i, j, 0, visit)) return true;
+                if (mySol(board, word, i, j, 0)) return true;
             }
         }
 
         return false;
     }
 
-    public boolean mySol(char[][] board, String word, int y, int x, int index, boolean[][] visit) {
-        // System.out.println(String.format("y:%d, x:%d, index:%d, length:%d", y, x, index, word.length()));
-
+    public boolean mySol(char[][] board, String word, int y, int x, int index) {
         if (index >= word.length()) return true;
 
         if (y >= board.length || y < 0 || x >= board[0].length || x < 0) return false;
 
-        if (visit[y][x] || board[y][x] != word.charAt(index)) return false;
+        if (board[y][x] != word.charAt(index)) return false;
 
-        visit[y][x] = true;
+        char origin = board[y][x];
+
+        board[y][x] = '1';
 
         for (int delta = -1; delta <= 1; delta += 2) {
-            if (mySol(board, word, y + delta, x, index + 1, visit)) return true;
+            if (mySol(board, word, y + delta, x, index + 1)) return true;
         }
 
         for (int delta = -1; delta <= 1; delta += 2) {
-            if (mySol(board, word, y, x + delta, index + 1, visit)) return true;
+            if (mySol(board, word, y, x + delta, index + 1)) return true;
         }
 
-        visit[y][x] = false;
+        board[y][x] = origin;
 
         return false;
     }
