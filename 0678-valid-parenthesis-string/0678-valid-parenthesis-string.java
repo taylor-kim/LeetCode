4143f1454dbb2f9062ff3b6ea692f3555cb14d5d
team-hungrian <def.hunger@gmail.com>
2024-04-08 00:52:43 +0900

    Time: 1 ms (38.41%), Space: 41.2 MB (57.11%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,6 +1,31 @@
 class Solution {
     public boolean checkValidString(String s) {
-        return mySol(s);
+        return officialTwoStack(s);
+    }
+
+    public boolean officialTwoStack(String s) {
+        Stack<Integer> open = new Stack();
+        Stack<Integer> star = new Stack();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                open.push(i);
+            } else if (c == '*') {
+                star.push(i);
+            } else {
+                if (!open.isEmpty()) open.pop();
+                else if (!star.isEmpty()) star.pop();
+                else return false;
+            }
+        }
+
+        while (!open.isEmpty() && !star.isEmpty()) {
+            if (open.pop() > star.pop()) return false;
+        }
+
+        return open.isEmpty();
     }
 
     public boolean mySol(String s) {
