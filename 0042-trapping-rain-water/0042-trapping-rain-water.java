b224d27e7abc7afa3a2508b1200a91f4015ba045
team-hungrian <def.hunger@gmail.com>
2024-04-13 18:16:45 +0900

    Time: 1 ms (71.11%), Space: 45.5 MB (89.58%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,6 +1,6 @@
 class Solution {
     public int trap(int[] height) {
-        return sol_stack(height);
+        return sol_dp(height);
     }
 
     public int sol_stack(int[] height) {
@@ -23,6 +23,31 @@ class Solution {
             }
 
             stack.push(cur);
+        }
+
+        return ans;
+    }
+
+    public int sol_dp(int[] height) {
+        int n = height.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        int lMax = height[0];
+        int rMax = height[n - 1];
+
+        for (int i = 1; i < n; i++) {
+            lMax = Math.max(lMax, height[i]);
+            rMax = Math.max(rMax, height[n - i - 1]);
+
+            left[i] = lMax - height[i];
+            right[n - i - 1] = rMax - height[n - i - 1];
+        }
+
+        int ans = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+            ans += Math.min(left[i], right[i]);
         }
 
         return ans;
