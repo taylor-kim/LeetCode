6144599e3d9ef687f8b64dc311f90744324d3e37
team-hungrian <def.hunger@gmail.com>
2024-04-21 17:19:56 +0900

    Time: 85 ms (72.9%), Space: 156.8 MB (34.89%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -11,7 +11,30 @@ class Solution {
             graph.computeIfAbsent(edge[1], k -> new ArrayList()).add(edge[0]);
         }
 
-        return dfs(source, dest, graph, new boolean[n]);
+        return bfs(source, dest, graph, new boolean[n]);
+    }
+
+    private boolean bfs(int source, int dest, Map<Integer, List<Integer>> graph, boolean[] visit) {
+        Queue<Integer> queue = new LinkedList();
+        queue.add(source);
+        visit[source] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+
+            if (node == dest) return true;
+
+            if (!graph.containsKey(node)) return false;
+
+            for (int next : graph.get(node)) {
+                if (!visit[next]) {
+                    queue.add(next);
+                    visit[next] = true;
+                }
+            }
+        }
+
+        return false;
     }
 
     private boolean dfs(int node, int dest, Map<Integer, List<Integer>> graph, boolean[] visit) {
