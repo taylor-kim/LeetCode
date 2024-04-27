6ce41d35c8329a91acb09288afb7df202edc8088
team-hungrian <def.hunger@gmail.com>
2024-04-27 16:41:50 +0900

    Time: 29 ms (74.24%), Space: 51.1 MB (53.56%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -1,23 +1,37 @@
 class Solution {
     public int minFallingPathSum(int[][] grid) {
-        return mySol_dfs_tle(grid);
+        return official_bottomup(grid);
     }
 
-    public int mySol_sort(int[][] grid) {
+    public int official_bottomup(int[][] grid) {
         int n = grid.length;
-        int[][][] mat = new int[n][n][2];
+        int[][] dp = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                mat[i][j] = new int[] {grid[i][j], j};
-            }
-
-            Arrays.sort(mat[i], (a, b) -> {
-                return a[0] - b[0];
-            });
+        for (int col = 0; col < n; col++) {
+            dp[n - 1][col] = grid[n - 1][col];
         }
 
-        return -1;
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col < n; col++) {
+                int min = Integer.MAX_VALUE;
+
+                for (int nextCol = 0; nextCol < n; nextCol++) {
+                    if (col != nextCol) {
+                        min = Math.min(min, dp[row + 1][nextCol]);
+                    }
+                }
+
+                dp[row][col] = grid[row][col] + min;
+            }
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int cand : dp[0]) {
+            ans = Math.min(ans, cand);
+        }
+
+        return ans;
     }
 
     public int mySol_dfs_tle(int[][] grid) {
