057161efc6c569259d3a2540afa5d7b0841f4c75
team-hungrian <def.hunger@gmail.com>
2024-04-27 16:25:00 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -1,36 +1,43 @@
 class Solution {
-    public int minFallingPathSum(int[][] grid) {
-        // Initialize a two-dimensional array to cache the result of each sub-problem
-        int[][] memo = new int[grid.length][grid.length];
+    // Initialize a hash map to cache the result of each sub-problem
+    Map<Pair<Integer, Integer>, Integer> memo = new HashMap<>();
 
-        // Fill the base case
+    public int minFallingPathSum(int[][] grid) {
+        // We can select any element from the first row. We will select
+        // the element which leads to minimum sum.
+        int answer = Integer.MAX_VALUE;
         for (int col = 0; col < grid.length; col++) {
-            memo[grid.length - 1][col] = grid[grid.length - 1][col];
+            answer = Math.min(answer, optimal(0, col, grid));
         }
 
-        // Fill the recursive cases
-        for (int row = grid.length - 2; row >= 0; row--) {
-            for (int col = 0; col < grid.length; col++) {
-                // Select minimum from valid cells of next row
-                int nextMinimum = Integer.MAX_VALUE;
-                for (int nextRowCol = 0; nextRowCol < grid.length; nextRowCol++) {
-                    if (nextRowCol != col) {
-                        nextMinimum = Math.min(nextMinimum, memo[row + 1][nextRowCol]);
-                    }
-                }
+        // Return the minimum sum
+        return answer;
+    }
 
-                // Minimum cost from this cell
-                memo[row][col] = grid[row][col] + nextMinimum;
+    // The optimal(row, col) function returns the minimum sum of a
+    // falling path with non-zero shifts, starting from grid[row][col]
+    int optimal(int row, int col, int[][] grid) {
+        // If the last row, then return the value of the cell itself
+        if (row == grid.length - 1) {
+            return grid[row][col];
+        }
+
+        // If the result of this sub-problem is already cached
+        if (memo.containsKey(new Pair<>(row, col))) {
+            return memo.get(new Pair<>(row, col));
+        }
+
+        // Select grid[row][col], and move on to next row. For next
+        // row, choose the cell that leads to the minimum sum
+        int nextMinimum = Integer.MAX_VALUE;
+        for (int nextRowCol = 0; nextRowCol < grid.length; nextRowCol++) {
+            if (nextRowCol != col) {
+                nextMinimum = Math.min(nextMinimum, optimal(row + 1, nextRowCol, grid));
             }
         }
 
-        // Find the minimum from the first row
-        int answer = Integer.MAX_VALUE;
-        for (int col = 0; col < grid.length; col++) {
-            answer = Math.min(answer, memo[0][col]);
-        }
-
-        // Return the answer
-        return answer;
+        // Minimum cost from this cell
+        memo.put(new Pair<>(row, col), grid[row][col] + nextMinimum);
+        return memo.get(new Pair<>(row, col));
     }
 }
