21da51adfcfb5c69cc43b1047fd108ce02116009
team-hungrian <def.hunger@gmail.com>
2024-05-01 16:16:13 +0900

    Time: 0 ms (100%), Space: 41.6 MB (56.56%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,9 +1,27 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
-        return mySol_stack(word, ch);
+        return do_simple(word, ch);
     }
 
-    public String mySol_stack(String word, char ch) {
+    public String do_simple(String word, char ch) {
+        StringBuilder sb = new StringBuilder();
+
+        int i = 0;
+
+        while (i < word.length()) {
+            if (word.charAt(i++) == ch) {
+                sb.append(word.substring(0, i));
+                sb.reverse();
+                break;
+            }
+        }
+
+        sb.append(word.substring(i, word.length()));
+
+        return sb.length() == 0 ? word : sb.toString();
+    }
+
+    public String mySol_stack_fail(String word, char ch) {
         Stack<Character> stack = new Stack();
 
         StringBuilder sb = new StringBuilder();
