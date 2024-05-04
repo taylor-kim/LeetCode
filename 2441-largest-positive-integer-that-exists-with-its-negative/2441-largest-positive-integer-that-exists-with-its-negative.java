38887680450028fecc7e5c6ce3dcf9d3ab0a8f51
team-hungrian <def.hunger@gmail.com>
2024-05-04 18:17:55 +0900

    Time: 2 ms (97.22%), Space: 44.6 MB (72.15%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
@@ -1,6 +1,23 @@
 class Solution {
     public int findMaxK(int[] nums) {
-        return official_two_pointers(nums);
+        return official_bitset(nums);
+    }
+
+    public int official_bitset(int[] nums) {
+        int ans = -1;
+        BitSet seen = new BitSet(2048);
+
+        for (int num : nums) {
+            int abs = Math.abs(num);
+
+            if (abs > ans && seen.get(-num + 1024)) {
+                ans = abs;
+            }
+
+            seen.set(num + 1024);
+        }
+
+        return ans;
     }
 
     public int official_two_pointers(int[] nums) {
