44410985748ae090b9bdee375bf0d5e5e32d24bf
team-hungrian <def.hunger@gmail.com>
2024-05-05 15:25:48 +0900

    Time: 0 ms (100%), Space: 43.5 MB (84.53%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,17 +8,22 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        mySol(node);
+        genious(node);
+    }
+
+    public void genious(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 
     public void mySol(ListNode node) {
         ListNode prev = null;
+        ListNode current = node;
 
-        while (node.next != null) {
-            int nextValue = node.next.val;
-            node.val = nextValue;
-            prev = node;
-            node = node.next;
+        while (current.next!= null) {
+            prev = current;
+            current.val = current.next.val;
+            current = current.next;
         }
 
         prev.next = null;
