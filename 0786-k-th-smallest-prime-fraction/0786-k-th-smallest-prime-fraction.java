00c0743df13e9e29ce254a16f13228fc37bdb755
team-hungrian <def.hunger@gmail.com>
2024-05-10 18:37:26 +0900

    Time: 4 ms (90.83%), Space: 42.2 MB (89.52%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,6 +1,50 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        return mySol(arr, k);
+        return official_bs(arr, k);
+    }
+
+    public int[] official_bs(int[] arr, int k) {
+        int n = arr.length;
+        double left = 0.0;
+        double right = 1.0;
+
+        while (left <= right) {
+            double mid = left + (right - left) / 2;
+
+            double max = 0.0;
+            int count = 0;
+            int numeratorIdx = 0;
+            int denominatorIdx = 0;
+            int j = 1;
+
+            for (int i = 0; i < n - 1; i++) {
+                while (j < n && arr[i] > mid * arr[j]) {
+                    j++;
+                }
+
+                count += (n - j);
+
+                if (j == n) break;
+
+                double fraction = (double) arr[i] / arr[j];
+
+                if (fraction > max) {
+                    numeratorIdx = i;
+                    denominatorIdx = j;
+                    max = fraction;
+                }
+            }
+
+            if (count == k) {
+                return new int[] {arr[numeratorIdx], arr[denominatorIdx]};
+            } else if (count > k) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        return new int[] {};
     }
 
     public int[] mySol(int[] arr, int k) {
