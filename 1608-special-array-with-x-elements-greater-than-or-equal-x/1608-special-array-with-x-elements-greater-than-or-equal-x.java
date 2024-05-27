ab4dad2af1c19a736c0aa7cd5e1c0e6ddeb34cef
team-hungrian <def.hunger@gmail.com>
2024-05-27 15:28:47 +0900

    Time: 2 ms (47.7%), Space: 41 MB (83.2%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        return mySol(nums);
+    }
+
+    public int mySol(int[] nums) {
+        int ans = -1;
+
+        int left = 0;
+        int right = nums.length;
+
+        Arrays.sort(nums);
+
+        while (left <= right) {
+            int x = left + (right - left) / 2;
+
+            int posX = leftmost(nums, x);
+            int count = 0;
+
+            if (posX < 0) {
+                count = nums.length;
+            } else if (posX == nums.length) {
+                count = 0;
+            } else {
+                count = nums.length - posX;
+            }
+
+            if (count == x) {
+                return x;
+            } else if (count < x) {
+                right = x - 1;
+            } else {
+                left = x + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int leftmost(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        if (left == 0 && nums[left] != target) return -1;
+
+        return left;
+    }
+}
