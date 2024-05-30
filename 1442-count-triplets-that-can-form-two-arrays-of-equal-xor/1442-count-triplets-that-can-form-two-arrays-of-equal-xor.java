5fbc4ba2217900c7cc5d7bb48845f0396d9f598b
team-hungrian <def.hunger@gmail.com>
2024-05-30 18:58:04 +0900

    Time: 18 ms (33.33%), Space: 40.7 MB (95.9%) - LeetHub

---
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int countTriplets(int[] arr) {
+        return mySol(arr);
+    }
+
+    public int mySol(int[] arr) {
+        int n = arr.length;
+
+        int ans = 0;
+
+        int[] pSum = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            pSum[i + 1] = arr[i] ^ pSum[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int a = pSum[j] ^ pSum[i];
+                for (int k = j; k < n; k++) {
+                    int b = pSum[k + 1] ^ pSum[j];
+                    // System.out.print(String.format("i:%d, j:%d, k:%d", i, j, k));
+
+                    if (a == b) {
+                        // System.out.println(" hit!!");
+                        ans++;
+                    } else {
+                        // System.out.println("");
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
