31362f79c89ef3683c5a2dff3d44b78c1ef12c58
team-hungrian <def.hunger@gmail.com>
2024-05-30 18:33:47 +0900

    Time: 8 ms (45.32%), Space: 43.4 MB (17.5%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -1,24 +1,45 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
-        return mySol(s, t, maxCost);
+        return official(s, t, maxCost);
+    }
+
+    public int official(String s, String t, int maxCost) {
+        int n = s.length();
+        int length = 0;
+
+        int cost = 0;
+        int left = 0;
+
+        for (int i = 0; i < n; i++) {
+            cost += Math.abs(s.charAt(i) - t.charAt(i));
+
+            while (left <= i && cost > maxCost) {
+                cost -= Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
+            }
+
+            length = Math.max(length, i - left + 1);
+        }
+
+        return length;
     }
 
     public int mySol(String s, String t, int maxCost) {
         int n = s.length();
-
         int[] dp = new int[n + 1];
 
-        int left = 0;
         int cost = 0;
+        int left = 0;
 
-        for (int right = 0; right < n; right++) {
-            cost += Math.abs(s.charAt(right) - t.charAt(right));
+        for (int i = 0; i < n; i++) {
+            cost += Math.abs(s.charAt(i) - t.charAt(i));
 
-            while (left <= right && cost > maxCost) {
-                cost -= Math.abs(s.charAt(left) - t.charAt(left++));
+            while (left <= i && cost > maxCost) {
+                cost -= Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
             }
 
-            dp[right + 1] = Math.max(dp[right], right - left + 1);
+            dp[i + 1] = Math.max(dp[i], i - left + 1);
         }
 
         return dp[n];
