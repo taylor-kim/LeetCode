0173954821b19a7cadc7f0d92ee8fc7e42cf22b9
team-hungrian <def.hunger@gmail.com>
2024-06-02 19:13:19 +0900

    Time: 3 ms (20.02%), Space: 41 MB (85.1%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,6 +1,34 @@
 class Solution {
     public int numSteps(String s) {
-        return mySol(s);
+        return official_simulation(s);
+    }
+
+    public int official_simulation(String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        int ans = 0;
+
+        while (sb.length() > 1) {
+            if (sb.charAt(sb.length() - 1) == '0') {
+                sb.setLength(sb.length() - 1);
+            } else {
+                int index = sb.length() - 1;
+
+                while (index >= 0 && sb.charAt(index) == '1') {
+                    sb.setCharAt(index--, '0');
+                }
+
+                if (index < 0) {
+                    sb.insert(0, '1');
+                } else {
+                    sb.setCharAt(index, '1');
+                }
+            }
+
+            ans++;
+        }
+
+        return ans;
     }
 
     public int mySol(String s) {
