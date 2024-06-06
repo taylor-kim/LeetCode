0568a952fc0a52f475aa34b0f297236c17cfa7d4
team-hungrian <def.hunger@gmail.com>
2024-06-06 20:12:34 +0900

    Time: 66 ms (12.94%), Space: 45.5 MB (52.37%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,9 +1,45 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        return mySol(hand, groupSize);
+        return mySol_freq(hand, groupSize);
     }
 
-    public boolean mySol(int[] hand, int groupSize) {
+    public boolean mySol_freq(int[] hand, int groupSize) {
+        TreeMap<Integer, Integer> freq = new TreeMap();
+
+        for (int num : hand) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        int made = 0;
+
+        while (freq.size() > 0) {
+            int start = freq.firstKey();
+
+            if (freq.get(start) == 0) {
+                freq.remove(start);
+                continue;
+            }
+
+            int end = start;
+
+            while (end < start + groupSize) {
+                if (freq.getOrDefault(end, 0) == 0) break;
+
+                freq.put(end, freq.get(end) - 1);
+                end++;
+            }
+
+            if (end == start + groupSize) {
+                made++;
+            } else {
+                return false;
+            }
+        }
+
+        return hand.length == groupSize * made;
+    }
+
+    public boolean mySol_bucket_MLE(int[] hand, int groupSize) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
@@ -12,17 +48,17 @@ class Solution {
             max = Math.max(max, num);
         }
 
-        int[] bucket = new int[max + 1];
+        int[] bucket = new int[max + 1 - min];
 
         for (int num : hand) {
-            bucket[num]++;
+            bucket[num - min]++;
         }
 
         int num = min;
         int made = 0;
 
         while (num <= max - groupSize + 1) {
-            if (bucket[num] <= 0) {
+            if (bucket[num - min] <= 0) {
                 num++;
                 continue;
             }
@@ -32,7 +68,7 @@ class Solution {
 
             while (seq < groupSize + num) {
                 // System.out.print(String.format("bucket[%d]:%d, ", seq, bucket[seq]));
-                if (bucket[seq]-- <= 0) break;
+                if (bucket[seq - min]-- <= 0) break;
 
                 seq++;
             }
