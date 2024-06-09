551a28d32ede55e2c45e1a47a49d163a885f3ac4
team-hungrian <def.hunger@gmail.com>
2024-06-09 18:38:12 +0900

    Time: 23 ms (56.17%), Space: 56.7 MB (99.46%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,16 +1,28 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-    Map<Integer, Integer> map = new HashMap<>(){{put(0,-1);}};;
-    int runningSum = 0;
-    for (int i=0;i<nums.length;i++) {
-        runningSum += nums[i];
-        if (k != 0) runningSum %= k; 
-        Integer prev = map.get(runningSum);
-        if (prev != null) {
-            if (i - prev > 1) return true;
-        }
-        else map.put(runningSum, i);
+        return mySol(nums, k);
     }
-    return false;
-}
+
+    public boolean mySol(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap();
+        map.put(0, -1);
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = (sum + nums[i]) % k;
+
+            if (map.containsKey(sum)) {
+                int before = map.getOrDefault(sum, -1);
+
+                if (i - before > 1) {
+                    return true;
+                }
+            } else {
+                map.put(sum, i);
+            }
+        }
+
+        return false;
+    }
 }
