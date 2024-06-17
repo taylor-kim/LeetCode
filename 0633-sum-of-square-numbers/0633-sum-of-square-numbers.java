5686110cea415c59e05fd2229f280e456f35be83
team-hungrian <def.hunger@gmail.com>
2024-06-17 20:28:48 +0900

    Time: 0 ms (100%), Space: 40.7 MB (9.53%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,16 +1,45 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        return mySol_fail(c);
+        return official_Fermat_Theorem(c);
+    }
+
+    public boolean official_Fermat_Theorem(int c) {
+        for (int i = 2; i * i <= c; i++) {
+            int count = 0;
+            if (c % i == 0) {
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0)
+                    return false;
+            }
+        }
+        return c % 4 != 3;
     }
 
     public boolean official_bs(int c) {
-        for (int a = 0; a * a <= c; a++) {
-            int bb = c - (a * a);
+        for (long a = 0; a * a <= c; a++) {
+            int bb = c - (int)(a * a);
 
-            // if (bs(0, bb, bb)) return true;
+            if (bs(0, bb, bb)) return true;
         }
 
         return false;
+    }
+
+    private boolean bs(long lo, long hi, int num) {
+        if (lo > hi) return false;
+
+        long mid = lo + (hi - lo) / 2;
+
+        if (mid * mid == num) {
+            return true;
+        } else if (mid * mid < num) {
+            return bs(mid + 1, hi, num);
+        } else {
+            return bs(lo, mid - 1, num);
+        }
     }
 
     public boolean official_sqrt(int c) {
