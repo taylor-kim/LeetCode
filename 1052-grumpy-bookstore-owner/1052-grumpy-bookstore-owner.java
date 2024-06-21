347d9d8d130f68c211b90f11a894a91adfb8c84c
team-hungrian <def.hunger@gmail.com>
2024-06-21 16:18:00 +0900

    Time: 11 ms (13.37%), Space: 44.8 MB (99.83%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -1,6 +1,40 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        return official_better_than_me(customers, grumpy, minutes);
+        return improve_mySol_onepass(customers, grumpy, minutes);
+    }
+
+    public int improve_mySol_onepass(int[] customers, int[] grumpy, int minutes) {
+        int n = customers.length;
+        int maxUnsatisfied = 0;
+        int currentUnsatisfied = 0;
+        int[] pos = {-1, -1};
+
+        Queue<Integer> queue = new LinkedList();
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            queue.add(i);
+
+            if (grumpy[i] == 1) {
+                currentUnsatisfied += customers[i];
+            } else {
+                ans += customers[i];
+            }
+
+            if (!queue.isEmpty() && queue.size() > minutes) {
+                int index = queue.poll();
+                if (grumpy[index] == 1) {
+                    currentUnsatisfied -= customers[index];
+                }
+            }
+
+            if (maxUnsatisfied < currentUnsatisfied) {
+                maxUnsatisfied = currentUnsatisfied;
+            }
+        }
+
+        return ans + maxUnsatisfied;
     }
 
     public int official_better_than_me(int[] customers, int[] grumpy, int minutes) {
