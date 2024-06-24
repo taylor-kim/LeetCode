aa3b5a86b97345488336d6be2b4fb6b61521c2bd
team-hungrian <def.hunger@gmail.com>
2024-06-24 19:29:29 +0900

    Time: 23 ms (54.23%), Space: 45.7 MB (66.7%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,6 +1,51 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        return mySol_bs(difficulty, profit, worker);
+        return official_bs_profit_base(difficulty, profit, worker);
+    }
+
+    public int official_bs_profit_base(int[] difficulty, int[] profit, int[] worker) {
+        int n = profit.length;
+        int[][] datas = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            datas[i][0] = profit[i];
+            datas[i][1] = difficulty[i];
+        }
+
+        Arrays.sort(datas, (a, b) -> {
+            return a[0] != b[0] ? b[0] - a[0] : a[1] - b[1];
+        });
+
+        for (int i = 0; i < n - 1; i ++) {
+            datas[i + 1][1] = Math.min(datas[i][1], datas[i + 1][1]);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < worker.length; i++) {
+            int lo = 0;
+            int hi = n - 1;
+
+            int max = 0;
+
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+
+                int p = datas[mid][0];
+                int diff = datas[mid][1];
+
+                if (worker[i] >= diff) {
+                    max = Math.max(max, p);
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+
+            ans += max;
+        }
+
+        return ans;
     }
 
     public int mySol_bs(int[] difficulty, int[] profit, int[] worker) {
