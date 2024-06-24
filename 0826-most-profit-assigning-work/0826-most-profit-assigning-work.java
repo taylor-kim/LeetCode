17a00149d48d836b65bb6632f6c4d9cae757802a
team-hungrian <def.hunger@gmail.com>
2024-06-24 22:27:04 +0900

    Time: 24 ms (50.32%), Space: 45.1 MB (95.73%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,54 +1,9 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        return official_bs_profit_base(difficulty, profit, worker);
+        return diffbase_bs(difficulty, profit, worker);
     }
 
-    public int official_bs_profit_base(int[] difficulty, int[] profit, int[] worker) {
-        int n = profit.length;
-        int[][] datas = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            datas[i][0] = profit[i];
-            datas[i][1] = difficulty[i];
-        }
-
-        Arrays.sort(datas, (a, b) -> {
-            return a[0] != b[0] ? b[0] - a[0] : a[1] - b[1];
-        });
-
-        for (int i = 0; i < n - 1; i ++) {
-            datas[i + 1][1] = Math.min(datas[i][1], datas[i + 1][1]);
-        }
-
-        int ans = 0;
-
-        for (int i = 0; i < worker.length; i++) {
-            int lo = 0;
-            int hi = n - 1;
-
-            int max = 0;
-
-            while (lo <= hi) {
-                int mid = lo + (hi - lo) / 2;
-
-                int p = datas[mid][0];
-                int diff = datas[mid][1];
-
-                if (worker[i] >= diff) {
-                    max = Math.max(max, p);
-                    hi = mid - 1;
-                } else {
-                    lo = mid + 1;
-                }
-            }
-
-            ans += max;
-        }
-
-        return ans;
-    }
-
-    public int mySol_bs(int[] difficulty, int[] profit, int[] worker) {
+    public int diffbase_bs(int[] difficulty, int[] profit, int[] worker) {
         int n = profit.length;
         int[][] datas = new int[n][2];
 
@@ -58,11 +13,11 @@ class Solution {
         }
 
         Arrays.sort(datas, (a, b) -> {
-            return a[0] != b[0] ? a[0] - b[0] : b[1] - a[1];
+            return a[0] - b[0];
         });
 
-        for (int i = 0; i < n - 1; i ++) {
-            datas[i + 1][1] = Math.max(datas[i][1], datas[i + 1][1]);
+        for (int i = 0; i < n - 1; i++) {
+            datas[i + 1][1] = Math.max(datas[i + 1][1], datas[i][1]);
         }
 
         int ans = 0;
@@ -70,12 +25,10 @@ class Solution {
         for (int i = 0; i < worker.length; i++) {
             int lo = 0;
             int hi = n - 1;
-
             int max = 0;
 
             while (lo <= hi) {
                 int mid = lo + (hi - lo) / 2;
-
                 int diff = datas[mid][0];
                 int p = datas[mid][1];
 
@@ -93,7 +46,9 @@ class Solution {
         return ans;
     }
 
-    public int mySol_pq_fail(int[] difficulty, int[] profit, int[] worker) {
+    public int mySol(int[] difficulty, int[] profit, int[] worker) {
+        
+
         Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
             return a[1] != b[1] ? b[1] - a[1] : a[0] - b[0];
         });
@@ -105,22 +60,30 @@ class Solution {
             });
         }
 
-        Arrays.sort(worker);
-
         int ans = 0;
 
-        for (int i = worker.length - 1; i >= 0; i--) {
-            while (pq.size() > 0) {
+        for (int i = 0; i < worker.length; i++) {
+            Queue<int[]> next = new PriorityQueue<>((a, b) -> {
+                return a[1] != b[1] ? b[1] - a[1] : a[0] - b[0];
+            });
+
+            int size = pq.size();
+
+            while (size-- > 0) {
                 int[] data = pq.poll();
                 int diff = data[0];
                 int p = data[1];
 
+                next.add(data);
+
                 if (worker[i] >= diff) {
-                    System.out.println(p);
+                    // System.out.println(String.format("worker:%d, data:%s", worker[i], Arrays.toString(data)));
                     ans += p;
                     break;
                 }
             }
+
+            pq.addAll(next);
         }
 
         return ans;
