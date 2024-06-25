297277142564ce9a71b917d4a8bfc63176e4ae30
team-hungrian <def.hunger@gmail.com>
2024-06-26 00:50:28 +0900

    Time: 7 ms (69.34%), Space: 55 MB (35.85%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,20 +1,54 @@
 class Solution {
-
     public int numberOfSubarrays(int[] nums, int k) {
-        int currSum = 0, subarrays = 0;
-        Map<Integer, Integer> prefixSum = new HashMap<>();
-        prefixSum.put(currSum, 1);
+        return mySol(nums, k);
+    }
 
-        for (int i = 0; i < nums.length; i++) {
-            currSum += nums[i] % 2;
-            // Find subarrays with sum k ending at i
-            if (prefixSum.containsKey(currSum - k)) {
-                subarrays = subarrays + prefixSum.get(currSum - k);
+    public int mySol(int[] nums, int k) {
+        int n = nums.length;
+
+        int leftOuter = 0;
+        int rightOuter = 0;
+        
+        int left = 0;
+
+        int count = 0;
+
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            count += nums[right] % 2;
+
+            if (count < k) continue;
+
+            while (left <= right) {
+                if (nums[left] % 2 == 1) break;
+                left++;
             }
-            // Increment the current prefix sum in hashmap
-            prefixSum.put(currSum, prefixSum.getOrDefault(currSum, 0) + 1);
+
+            // if (left > right) continue;
+
+            while (leftOuter < left && nums[leftOuter] % 2 == 1) {
+                leftOuter++;
+            }
+
+            rightOuter = right;
+
+            while (rightOuter + 1 < n && nums[rightOuter + 1] % 2 == 0) {
+                rightOuter++;
+            }
+
+            // rightOuter = Math.min(rightOuter, n - 1);
+
+            int subAns = (left - leftOuter + 1) * (rightOuter - right + 1);
+
+            // System.out.println(String.format("left:%d, leftOuter:%d, rightOuter:%d, right:%d, count:%d, subAns:%d", left, leftOuter, rightOuter, right, count, subAns));
+
+            ans += subAns;
+            
+            leftOuter = left;
+            count -= nums[left++] % 2;
         }
 
-        return subarrays;
+        return ans;
     }
 }
