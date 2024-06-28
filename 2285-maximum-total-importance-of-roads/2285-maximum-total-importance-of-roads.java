e102b7e139cd97517613410b10e90e22b32be0cf
team-hungrian <def.hunger@gmail.com>
2024-06-28 23:42:09 +0900

    Time: 48 ms (24.32%), Space: 72 MB (13.51%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -1,22 +1,57 @@
 class Solution {
-
     public long maximumImportance(int n, int[][] roads) {
-        long[] degree = new long[n];
+        return mySol(n, roads);
+    }
 
-        for (int[] edge : roads) {
-            degree[edge[0]]++;
-            degree[edge[1]]++;
+    public long mySol(int n, int[][] roads) {
+        int[][] iFreq = new int[n][2];
+
+        for (int i = 0; i < roads.length; i++) {
+            int[] road = roads[i];
+            int a = road[0];
+            int b = road[1];
+
+            iFreq[a][0] = a;
+            iFreq[a][1]++;
+
+            iFreq[b][0] = b;
+            iFreq[b][1]++;
         }
 
-        Arrays.sort(degree);
+        Arrays.sort(iFreq, (a, b) -> {
+            return a[1] - b[1];
+        });
 
-        long value = 1;
-        long totalImportance = 0;
-        for (long d : degree) {
-            totalImportance += (value * d);
-            value++;
+        long sum = 0;
+
+        for (int i = 0; i < iFreq.length; i++) {
+            iFreq[i][1] = i + 1;
         }
 
-        return totalImportance;
+        Map<Integer, Integer> map = new HashMap();
+
+        for (int[] freq : iFreq) {
+            // System.out.println(String.format("city:%d, freq:%d", freq[0], freq[1]));
+
+            map.put(freq[0], freq[1]);
+        }
+
+        for (int[] road : roads) {
+            int r0 = road[0];
+            int r1 = road[1];
+
+            // int p0 = iFreq[r0][1];
+            // int p1 = iFreq[r1][1];
+
+            int p0 = map.get(r0);
+            int p1 = map.get(r1);
+
+            // System.out.println(String.format("road (%d, %d) has an importance of %d + %d = %d", r0, r1, p0, p1, p0 + p1));
+
+            // sum += iFreq[r0][0] + iFreq[r1][0] + 2;
+            sum += p0 + p1;
+        }
+
+        return sum;
     }
 }
