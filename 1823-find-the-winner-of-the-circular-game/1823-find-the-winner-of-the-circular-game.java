7134f9de7779860e86ce17cb21dfd9130a2d51ee
team-hungrian <def.hunger@gmail.com>
2024-07-08 19:53:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,6 +1,16 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        return official_queue(n, k);
+        return official_recur(n, k);
+    }
+
+    public int official_recur(int n, int k) {
+        return helper(n, k) + 1;
+    }
+
+    public int helper(int n, int k) {
+        if (n == 1) return 0;
+
+        return (helper(n - 1, k) + k) % n;
     }
 
     public int official_queue(int n, int k) {
