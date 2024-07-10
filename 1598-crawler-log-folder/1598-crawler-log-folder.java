1f3dcc0c06590125c50720501c681f2401d5dac5
team-hungrian <def.hunger@gmail.com>
2024-07-10 23:36:16 +0900

    Time: 1 ms (99.42%), Space: 42.6 MB (5.9%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,6 +1,18 @@
 class Solution {
     public int minOperations(String[] logs) {
-        return mySol(logs);
+        return official_stack(logs);
+    }
+
+    public int official_stack(String[] logs) {
+        Stack<String> stack = new Stack();
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (stack.size() > 0) stack.pop();
+            } else if (log.charAt(0) != '.') stack.push(log);
+        }
+
+        return stack.size();
     }
 
     public int mySol(String[] logs) {
