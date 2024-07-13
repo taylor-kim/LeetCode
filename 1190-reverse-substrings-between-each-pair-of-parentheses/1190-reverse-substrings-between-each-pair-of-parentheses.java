e764b774c84c064a45a92d95f767402d977858ab
team-hungrian <def.hunger@gmail.com>
2024-07-13 18:02:19 +0900

    Time: 1 ms (88.24%), Space: 41.1 MB (98.7%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,6 +1,64 @@
 class Solution {
     public String reverseParentheses(String s) {
-        return try_third(s);
+        return official_wormhole_teleportation(s);
+    }
+
+    public String official_wormhole_teleportation(String s) {
+        int n = s.length();
+        int[] pairs = new int[n];
+        Stack<Integer> stack = new Stack();
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else if (s.charAt(i) == ')') {
+                int open = stack.pop();
+
+                pairs[i] = open;
+                pairs[open] = i;
+            }
+        }
+
+        int direction = 1;
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int index = 0; index < n; index += direction) {
+            char c = s.charAt(index);
+            if (c == '(' || c == ')') {
+                index = pairs[index];
+                direction = -direction;
+            } else {
+                ans.append(c);
+            }
+        }
+
+        return ans.toString();
+    }
+
+    public String official_stack(String s) {
+        Stack<Integer> stack = new Stack();
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(sb.length());
+            } else if (c == ')') {
+                reverse(sb, stack.pop(), sb.length() - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private void reverse(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char temp = sb.charAt(left);
+            sb.setCharAt(left++, sb.charAt(right));
+            sb.setCharAt(right--, temp);
+        }
     }
 
     public String try_third(String s) {
