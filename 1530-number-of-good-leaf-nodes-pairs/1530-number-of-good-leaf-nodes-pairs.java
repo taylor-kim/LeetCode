e7fb6eb54faeb56ae23954a95afb3da9bd99ecde
team-hungrian <def.hunger@gmail.com>
2024-07-18 20:09:41 +0900

    Time: 1550 ms (5.08%), Space: 45.2 MB (12.03%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.java
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.java
@@ -1,63 +1,103 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
     public int countPairs(TreeNode root, int distance) {
-        Map<TreeNode, List<TreeNode>> graph = new HashMap<>();
-        Set<TreeNode> leafNodes = new HashSet<>();
+        return mySol(root, distance);
+    }
 
-        traverseTree(root, null, graph, leafNodes);
+    public int mySol(TreeNode root, int distance) {
+        Map<TreeNode, List<TreeNode>> graph = buildGraph(root);
 
+        List<TreeNode> leaves = new ArrayList();
+
+        collectLeaves(root, leaves);
+
+        Map<TreeNode, TreeNode> pairs = new HashMap();
         int ans = 0;
 
-        for (TreeNode leaf : leafNodes) {
-            Queue<TreeNode> bfsQueue = new LinkedList<>();
-            Set<TreeNode> seen = new HashSet<>();
-            bfsQueue.add(leaf);
-            seen.add(leaf);
-            // Go through all nodes that are within the given distance of
-            // the current leaf node
-            for (int i = 0; i <= distance; i++) {
-                // Clear all nodes in the queue (distance i away from leaf node)
-                // Add the nodes' neighbors (distance i+1 away from leaf node)
-                int size = bfsQueue.size();
-                for (int j = 0; j < size; j++) {
-                    // If current node is a new leaf node, add the found pair to our count
-                    TreeNode currNode = bfsQueue.remove();
-                    if (leafNodes.contains(currNode) && currNode != leaf) {
+        for (TreeNode start : leaves) {
+            Queue<TreeNode> queue = new LinkedList();
+            Set<TreeNode> visit = new HashSet();
+            queue.add(start);
+            visit.add(start);
+
+            int level = 0;
+
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+
+                while (size-- > 0) {
+                    TreeNode node = queue.poll();
+
+                    if (node != start && node.left == null && node.right == null && level <= distance) {
+                        pairs.put(start, node);
+                        pairs.put(node, start);
                         ans++;
                     }
-                    if (graph.containsKey(currNode)) {
-                        for (TreeNode neighbor : graph.get(currNode)) {
-                            if (!seen.contains(neighbor)) {
-                                bfsQueue.add(neighbor);
-                                seen.add(neighbor);
-                            }
+
+                    for (TreeNode next : graph.getOrDefault(node, new ArrayList<>())) {
+                        if (visit.add(next)) {
+                            queue.add(next);
                         }
                     }
                 }
+
+                level++;
             }
         }
+
         return ans / 2;
     }
 
-    private void traverseTree(
-        TreeNode currNode,
-        TreeNode prevNode,
-        Map<TreeNode, List<TreeNode>> graph,
-        Set<TreeNode> leafNodes
-    ) {
-        if (currNode == null) {
-            return;
+    private void collectLeaves(TreeNode root, List<TreeNode> list) {
+        if (root == null) return;
+
+        if (root.left == null && root.right == null) {
+            list.add(root);
         }
-        if (currNode.left == null && currNode.right == null) {
-            leafNodes.add(currNode);
+
+        collectLeaves(root.left, list);
+        collectLeaves(root.right, list);
+    }
+
+    private Map<TreeNode, List<TreeNode>> buildGraph(TreeNode root) {
+        Map<TreeNode, List<TreeNode>> graph = new HashMap();
+
+        buildGraph(root, graph);
+
+        return graph;
+    }
+
+    private void buildGraph(TreeNode node, Map<TreeNode, List<TreeNode>> graph) {
+        if (node == null) return;
+
+        graph.computeIfAbsent(node, k -> new ArrayList());
+
+        if (node.left != null) {
+            graph.computeIfAbsent(node, k -> new ArrayList()).add(node.left);
+            graph.computeIfAbsent(node.left, k -> new ArrayList()).add(node);
+
+            buildGraph(node.left, graph);
         }
-        if (prevNode != null) {
-            graph.computeIfAbsent(prevNode, k -> new ArrayList<TreeNode>());
-            graph.get(prevNode).add(currNode);
-            graph.computeIfAbsent(currNode, k -> new ArrayList<TreeNode>());
-            graph.get(currNode).add(prevNode);
+
+        if (node.right != null) {
+            graph.computeIfAbsent(node, k -> new ArrayList()).add(node.right);
+            graph.computeIfAbsent(node.right, k -> new ArrayList()).add(node);
+
+            buildGraph(node.right, graph);
         }
-        traverseTree(currNode.left, currNode, graph, leafNodes);
-        traverseTree(currNode.right, currNode, graph, leafNodes);
     }
 }
