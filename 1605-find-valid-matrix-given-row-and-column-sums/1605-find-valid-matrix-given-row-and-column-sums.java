35784b18bb45f81e4d4e11c8e401287786c8f7e9
team-hungrian <def.hunger@gmail.com>
2024-07-20 17:01:21 +0900

    Time: 7 ms (52.59%), Space: 52.9 MB (41.43%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
@@ -1,6 +1,27 @@
 class Solution {
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        return official_greedy(rowSum, colSum);
+        return try_spaceopt_greedy(rowSum, colSum);
+    }
+
+    public int[][] try_spaceopt_greedy(int[] rowSum, int[] colSum) {
+        int m = rowSum.length;
+        int n = colSum.length;
+
+        int[][] ret = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ret[i][j] = Math.min(
+                    rowSum[i]
+                    , colSum[j]
+                );
+
+                rowSum[i] -= ret[i][j];
+                colSum[j] -= ret[i][j];
+            }
+        }
+
+        return ret;
     }
 
     public int[][] official_greedy(int[] rowSum, int[] colSum) {
