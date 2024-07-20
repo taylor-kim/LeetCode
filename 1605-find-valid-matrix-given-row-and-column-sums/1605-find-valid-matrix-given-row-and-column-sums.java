4fd13b3b2812a8d0b3a7ba50f8a7d4fef13fc298
team-hungrian <def.hunger@gmail.com>
2024-07-20 16:56:32 +0900

    Time: 8 ms (13.15%), Space: 52.8 MB (53.39%) - LeetHub

---
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.java
@@ -0,0 +1,85 @@
+class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        return official_greedy(rowSum, colSum);
+    }
+
+    public int[][] official_greedy(int[] rowSum, int[] colSum) {
+        int m = rowSum.length;
+        int n = colSum.length;
+
+        int[] curRowSum = new int[m];
+        int[] curColSum = new int[n];
+
+        int[][] ret = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ret[i][j] = Math.min(
+                    rowSum[i] - curRowSum[i]
+                    , colSum[j] - curColSum[j]
+                );
+
+                curRowSum[i] += ret[i][j];
+                curColSum[j] += ret[i][j];
+            }
+        }
+
+        return ret;
+    }
+
+    public int[][] bruteForce_fail(int[] rowSum, int[] colSum) {
+        int m = rowSum.length;
+        int n = colSum.length;
+
+        int[][] matrix = new int[m][n];
+
+        // for (int[] row : matrix) {
+        //     Arrays.fill(row, -1);
+        // }
+
+        int total = 0;
+
+        for (int num : rowSum) total += num;
+
+        build(matrix, rowSum, colSum, 0, 0, total);
+
+        return matrix;
+    }
+
+    private boolean build(int[][] mat, int[] rowSum, int[] colSum, int r, int c, int odd) {
+        if (r >= mat.length && c >= mat[0].length && odd == 0) {
+            return true;
+        }
+
+        if (r >= mat.length || c >= mat[0].length) return false;
+
+        int rMax = rowSum[r];
+        int cMax = colSum[c];
+
+        int delta = 0;
+        int limit = Math.max(rMax, cMax);
+
+        while (delta <= limit) {
+            mat[r][c] = delta;
+
+            rowSum[r] -= delta;
+            colSum[c] -= delta;
+
+            if (build(mat, rowSum, colSum, r, c + 1, odd - delta)
+                || build(mat, rowSum, colSum, r + 1, c, odd - delta)
+                || build(mat, rowSum, colSum, r + 1, c + 1, odd - delta)
+            ) {
+                return true;
+            }
+
+            rowSum[r] += delta;
+            colSum[c] += delta;
+
+            delta++;
+        }
+
+        mat[r][c] = 0;
+
+        return false;
+    }
+}
