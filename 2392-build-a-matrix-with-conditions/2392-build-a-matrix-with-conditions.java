b88dc6614fa32b0d18a3c59ce8afe1020fc23085
team-hungrian <def.hunger@gmail.com>
2024-07-21 19:00:12 +0900

    Time: 18 ms (32.28%), Space: 55.6 MB (25.98%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.java
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.java
@@ -1,9 +1,65 @@
 class Solution {
     public int[][] buildMatrix(int k, int[][] rowConditions, int[][] colConditions) {
-        return mySol(k, rowConditions, colConditions);
+        return official_topoSort(k, rowConditions, colConditions);
     }
 
-    public int[][] mySol(int k, int[][] rc, int[][] cc) {
+    public int[][] official_topoSort(int k, int[][] rc, int[][] cc) {
+
+        int[] rows = buildTopo(k, rc);
+        int[] cols = buildTopo(k, cc);
+
+        System.out.println(Arrays.toString(rows));
+        System.out.println(Arrays.toString(cols));
+
+        if (rows == null || cols == null) return new int[0][0];
+
+        int[][] ans = new int[k][k];
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < k; j++) {
+                if (rows[i] == cols[j]) {
+                    ans[i][j] = rows[i];
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private int[] buildTopo(int k, int[][] cond) {
+        int[] indegree = new int[k + 1];
+        Map<Integer, List<Integer>> graph = new HashMap();
+
+        for (int[] edge : cond) {
+            graph.computeIfAbsent(edge[0], key -> new ArrayList()).add(edge[1]);
+            indegree[edge[1]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList();
+
+        for (int i = 1; i <= k; i++) {
+            if (indegree[i] == 0) queue.add(i);
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            result[index++] = num;
+
+            for (int next : graph.getOrDefault(num, new ArrayList<>())) {
+                if (--indegree[next] == 0) queue.add(next);
+            }
+            k--;
+        }
+
+        if (k != 0) return null;
+
+        return result;
+    }
+
+    public int[][] mySol_fail(int k, int[][] rc, int[][] cc) {
         int[][] ret = new int[k][k];
 
         Map<Integer, Integer> rows = buildLine(k, rc);
@@ -45,7 +101,7 @@ class Solution {
             indegree.computeIfAbsent(from, key -> 0);
             indegree.put(to, indegree.getOrDefault(to, 0) + 1);
 
-            if (graph.getOrDefault(to, 0) == from) return null;
+            // if (graph.getOrDefault(to, 0) == from) return null;
         }
 
         int index = 0;
