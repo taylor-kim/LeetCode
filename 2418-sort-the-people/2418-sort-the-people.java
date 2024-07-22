6522bab5275814bda22fe8c719050863eb9ee398
team-hungrian <def.hunger@gmail.com>
2024-07-22 20:25:28 +0900

    Time: 7 ms (90.64%), Space: 45.6 MB (10.15%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,6 +1,24 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        return mySol(names, heights);
+        return official_map(names, heights);
+    }
+
+    public String[] official_map(String[] names, int[] heights) {
+        Map<Integer, String> map = new HashMap();
+
+        for (int i = 0; i < names.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+
+        Arrays.sort(heights);
+
+        String[] ans = new String[names.length];
+
+        for (int i = 0; i < names.length; i++) {
+            ans[names.length - i - 1] = map.get(heights[i]);
+        }
+
+        return ans;
     }
 
     public String[] mySol(String[] names, int[] heights) {
