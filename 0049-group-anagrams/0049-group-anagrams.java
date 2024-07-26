afa36ab02f0811d35f7e2d56f1ff75cc8af39b58
team-hungrian <def.hunger@gmail.com>
2024-07-26 18:13:06 +0900

    Time: 6 ms (97.97%), Space: 47.5 MB (85.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,6 +1,24 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        return try_20240726(strs);
+        return try_20240726_others(strs);
+    }
+
+    public List<List<String>> try_20240726_others(String[] strs) {
+        Map<String, List<String>> map = new HashMap();
+
+        for (String s : strs) {
+            char[] freq = new char[26];
+
+            for (char c : s.toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+            String key = new String(freq);
+
+            map.computeIfAbsent(key, k -> new ArrayList()).add(s);
+        }
+
+        return new ArrayList(map.values());
     }
 
     public List<List<String>> try_20240726(String[] strs) {
