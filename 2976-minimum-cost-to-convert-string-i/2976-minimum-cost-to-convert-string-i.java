cefb1ce72eaa3e3faf81d25e30d3eda0770bec13
team-hungrian <def.hunger@gmail.com>
2024-07-28 19:46:52 +0900

    Time: 25 ms (59.91%), Space: 46.2 MB (13.36%) - LeetHub

--- 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -3,6 +3,26 @@ class Solution {
         return mySol(source, target, original, changed, cost);
     }
 
+    public long try_dijkstra(String source, String target, char[] original, char[] changed, int[] cost) {
+        long MAX = (long)1e6 * source.length();
+
+        long[][] min = new long[26][26];
+
+        for (long[] arr : min) Arrays.fill(arr, MAX);
+
+        long ans = 0;
+
+        for (int i = 0; i < source.length(); i++) {
+            long val = min[source.charAt(i) - 'a'][target.charAt(i) - 'a'];
+
+            if (val == MAX) return -1;
+
+            ans += val;
+        }
+
+        return ans;
+    }
+
     public long mySol(String source, String target, char[] original, char[] changed, int[] cost) {
         long[][] min = new long[26][26];
 
@@ -13,7 +33,7 @@ class Solution {
         }
 
         for (int i = 0; i < original.length; i++) {
-            min[original[i] - 'a'][changed[i] - 'a'] = cost[i];
+            min[original[i] - 'a'][changed[i] - 'a'] = Math.min(min[original[i] - 'a'][changed[i] - 'a'], cost[i]);
         }
 
         for (int k = 0; k < 26; k++) {
