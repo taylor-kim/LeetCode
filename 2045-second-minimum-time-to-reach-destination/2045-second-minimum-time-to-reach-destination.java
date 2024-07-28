26340203995c1f0314f14a2909ccd61ec729891e
team-hungrian <def.hunger@gmail.com>
2024-07-28 22:50:17 +0900

    Time: 76 ms (74.67%), Space: 55.2 MB (96%) - LeetHub

--- 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.java
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.java
@@ -1,6 +1,57 @@
 class Solution {
     public int secondMinimum(int n, int[][] edges, int time, int change) {
-        return tune_mySol_with_editorial(n, edges, time, change);
+        return official_bfs(n, edges, time, change);
+    }
+
+    public int official_bfs(int n, int[][] edges, int time, int change) {
+        Map<Integer, List<Integer>> graph = new HashMap();
+        int[] dist1 = new int[n + 1];
+        int[] dist2 = new int[n + 1];
+
+        for (int[] edge : edges) {
+            graph.computeIfAbsent(edge[0], k -> new ArrayList()).add(edge[1]);
+            graph.computeIfAbsent(edge[1], k -> new ArrayList()).add(edge[0]);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            dist1[i] = dist2[i] = -1;
+        }
+
+        Queue<int[]> queue = new LinkedList();
+        queue.add(new int[] {1, 1});
+        dist1[1] = 0;
+
+        while (!queue.isEmpty()) {
+            int[] data = queue.poll();
+            int node = data[0];
+            int freq = data[1];
+
+            if (!graph.containsKey(node)) continue;
+
+            int timeTaken = freq == 1 ? dist1[node] : dist2[node];
+
+            int count = timeTaken / change;
+
+            if (count % 2 == 0) {
+                timeTaken += time;
+            } else {
+                timeTaken = (count + 1) * change + time;
+            }
+
+            for (int next : graph.get(node)) {
+                if (dist1[next] == -1) {
+                    dist1[next] = timeTaken;
+                    queue.add(new int[] {next, 1});
+                } else if (dist2[next] == -1 && dist1[next] != timeTaken) {
+                    if (next == n) return timeTaken;
+
+                    dist2[next] = timeTaken;
+                    queue.add(new int[] {next, 2});
+                }
+            }
+        }
+
+        return -1;
     }
 
     public int tune_mySol_with_editorial(int n, int[][] edges, int time, int change) {
