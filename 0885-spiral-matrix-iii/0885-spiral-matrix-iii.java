d5e135e8586a39f3105dc2a43411ea564c79b702
team-hungrian <def.hunger@gmail.com>
2024-08-10 21:13:17 +0900

    Time: 11 ms (15.55%), Space: 44.9 MB (92.42%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
@@ -6,7 +6,9 @@ class Solution {
     private int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
     public int[][] mySol(int rows, int cols, int rStart, int cStart) {
-        int[][] ans = new int[rows][cols];
+        int[][] ans = new int[rows * cols][];
+
+        int[][] mat = new int[rows][cols];
 
         int y = rStart;
         int x = cStart;
@@ -14,50 +16,37 @@ class Solution {
         int number = 1;
         int dist = 1;
         int direction = 0;
-
         int index = 0;
-
-        int loop = 0;
-
-        int[][] ans2 = new int[rows * cols][2];
-
         int changeDir = 0;
 
-        while (number <= rows * cols) {
-            if (ans[y][x] == 0) {
-                ans[y][x] = number++;
-
-                ans2[index++] = new int[] {y, x};
+        while (index < ans.length) {
+            if (mat[y][x] == 0) {
+                mat[y][x] = number++;
+                ans[index++] = new int[] {y, x};
             }
 
-            int ny = y + dirs[direction][0];
-            int nx = x + dirs[direction][1];
+            for (int k = 0; k < 4; k++) {
+                int ny = y + dirs[direction][0];
+                int nx = x + dirs[direction][1];
 
-            int diff = Math.max(Math.abs(ny - rStart), Math.abs(nx - cStart));
+                int diff = Math.max(Math.abs(ny - rStart), Math.abs(nx - cStart));
 
-            if (isIn(ny, nx, rows, cols) && diff <= dist) {
-                y = ny;
-                x = nx;
-
-                // if (diff == dist && ans[ny][nx] != 0) {
-                //     dist++;
-                // }
-            } else {
-                direction = (direction + 1) % 4;
-                changeDir++;
-            }
-
-            if (changeDir == 4) {
-                dist++;
-                changeDir = 0;
+                if (isIn(ny, nx, rows, cols) && diff <= dist) {
+                    y = ny;
+                    x = nx;
+                    break;
+                } else {
+                    direction = (direction + 1) % 4;
+                    changeDir++;
+                    if (changeDir == 4) {
+                        dist++;
+                        changeDir = 0;
+                    }
+                }
             }
         }
 
-        // for (int[] row : ans) {
-        //     System.out.println(Arrays.toString(row));
-        // }
-
-        return ans2;
+        return ans;
     }
 
     private boolean isIn(int y, int x, int rows, int cols) {
