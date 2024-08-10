5fef35cd590a35dd2a14bf6fb3b8dcfbbb0657bd
team-hungrian <def.hunger@gmail.com>
2024-08-10 20:39:34 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
@@ -1,37 +1,58 @@
 class Solution {
-
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        // Store all possible directions in an array.
-        int[][] dir = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-        int[][] traversed = new int[rows * cols][2];
-        int idx = 0;
+        return mySol(rows, cols, rStart, cStart);
+    }
 
-        // Initial step size is 1, value of d represents the current direction.
-        for (int step = 1, direction = 0; idx < rows * cols;) {
-            // direction = 0 -> East, direction = 1 -> South
-            // direction = 2 -> West, direction = 3 -> North
-            for (int i = 0; i < 2; ++i) {
-                for (int j = 0; j < step; ++j) {
-                    // Validate the current position
-                    if (
-                        rStart >= 0 &&
-                        rStart < rows &&
-                        cStart >= 0 &&
-                        cStart < cols
-                    ) {
-                        traversed[idx][0] = rStart;
-                        traversed[idx][1] = cStart;
-                        ++idx;
-                    }
-                    // Make changes to the current position.
-                    rStart += dir[direction][0];
-                    cStart += dir[direction][1];
+    private int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public int[][] mySol(int rows, int cols, int rStart, int cStart) {
+        int[][] ans = new int[rows][cols];
+
+        int y = rStart;
+        int x = cStart;
+
+        int number = 1;
+        int dist = 1;
+        int direction = 0;
+
+        int index = 0;
+
+        int loop = 0;
+
+        int[][] ans2 = new int[rows * cols][2];
+
+        while (number <= rows * cols) {
+            if (ans[y][x] == 0) {
+                ans[y][x] = number++;
+
+                ans2[index++] = new int[] {y, x};
+            }
+
+            int ny = y + dirs[direction][0];
+            int nx = x + dirs[direction][1];
+
+            int diff = Math.max(Math.abs(ny - rStart), Math.abs(nx - cStart));
+
+            if (isIn(ny, nx, rows, cols) && diff <= dist) {
+                y = ny;
+                x = nx;
+
+                if (diff == dist && ans[ny][nx] != 0) {
+                    dist++;
                 }
-
+            } else {
                 direction = (direction + 1) % 4;
             }
-            ++step;
         }
-        return traversed;
+
+        // for (int[] row : ans) {
+        //     System.out.println(Arrays.toString(row));
+        // }
+
+        return ans2;
+    }
+
+    private boolean isIn(int y, int x, int rows, int cols) {
+        return y >= 0 && x >= 0 && y < rows && x < cols;
     }
 }
