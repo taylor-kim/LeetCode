6559f77bdc68850100765e745b37606f387d0479
team-hungrian <def.hunger@gmail.com>
2024-08-16 14:54:15 +0900

    Time: 5 ms (100%), Space: 64.2 MB (77.64%) - LeetHub

---
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int maxDistance(List<List<Integer>> arrays) {
+        return mySol(arrays);
+    }
+    
+    public int mySol(List<List<Integer>> arrays) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        int secMin = Integer.MAX_VALUE;
+        int secMax = Integer.MIN_VALUE;
+
+        boolean isSameList = false;
+
+        for (List<Integer> list : arrays) {
+            int localMin = list.get(0);
+            int localMax = list.get(list.size() - 1);
+
+            boolean minChanged = false;
+
+            if (min > localMin) {
+                secMin = min;
+                min = localMin;
+                minChanged = true;
+            } else if (secMin > localMin) {
+                secMin = localMin;
+            }
+
+            boolean maxChanged = false;
+
+            if (max < localMax) {
+                secMax = max;
+                max = localMax;
+                maxChanged = true;
+            } else if (secMax < localMax) {
+                secMax = localMax;
+            }
+
+            if (minChanged || maxChanged) {
+                isSameList = minChanged && maxChanged;
+            }
+
+            // System.out.println(String.format("min:%d, max:%d, secMin:%d, secMax:%d, isSmameList:%b"
+            // , min, max, secMin, secMax, isSameList));
+        }
+
+        if (isSameList) {
+            return Math.max(max - secMin, secMax - min);
+        } else {
+            return max - min;
+        }
+    }
+}
