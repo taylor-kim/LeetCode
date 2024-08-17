c66fce5966f3f6ca8746a4497696d7f73d60b70b
team-hungrian <def.hunger@gmail.com>
2024-08-17 19:15:53 +0900

    Time: 9 ms (55.29%), Space: 83.5 MB (51.18%) - LeetHub

---
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
@@ -0,0 +1,67 @@
+class Solution {
+    public long maxPoints(int[][] points) {
+        return hint_bottomup(points);
+    }
+
+    public long hint_bottomup(int[][] points) {
+        int m = points.length;
+        int n = points[0].length;
+
+        long[][] dp = new long[m + 1][n];
+
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = 0; c < n; c++) {
+                dp[r][c] = dp[r + 1][c] + points[r][c];
+            }
+
+            for (int c = 1; c < n; c++) {
+                dp[r][c] = Math.max(dp[r][c], dp[r][c - 1] - 1);
+            }
+
+            for (int c = n - 2; c >= 0; c--) {
+                dp[r][c] = Math.max(dp[r][c], dp[r][c + 1] - 1);
+            }
+        }
+
+        long ans = 0;
+
+        for (long cand : dp[0]) {
+            ans = Math.max(ans, cand);
+        }
+
+        return ans;
+    }
+
+    public long mySol_topdown(int[][] points) {
+        int m = points.length;
+        int n = points[0].length;
+
+        long ans = 0;
+
+        Long[][] memo = new Long[m][n];
+
+        for (int c = 0; c < n; c++) {
+            ans = Math.max(ans, mySol_topdown(points, 0, c, memo));
+        }
+
+        return ans;
+    }
+
+    public long mySol_topdown(int[][] points, int r, int c, Long[][] memo) {
+        if (r >= points.length) return 0L;
+
+        if (memo[r][c] != null) {
+            return memo[r][c];
+        }
+
+        long ans = 0;
+
+        for (int i = 0; i < points[0].length; i++) {
+            int loosing = Math.abs(c - i);
+
+            ans = Math.max(ans, points[r][c] - loosing + mySol_topdown(points, r + 1, i, memo));
+        }
+
+        return memo[r][c] = ans;
+    }
+}
