bbc0357a53042592a46f0f9cb2607600f657dc3d
team-hungrian <def.hunger@gmail.com>
2024-08-19 23:36:57 +0900

    Time: 5 ms (64.07%), Space: 40.5 MB (45.92%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,6 +1,22 @@
 class Solution {
     public int minSteps(int n) {
-        return official_prime_factorizaion(n);
+        return others_good_bottomup(n);
+    }
+
+    public int others_good_bottomup(int n) {
+        int[] dp = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
+            for (int j = i / 2; j > 1; j--) {
+                if (i % j == 0) {
+                    dp[i] = dp[j] + i / j;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
     }
 
     public int official_prime_factorizaion(int n) {
