168388f8270aa0f47ca400b0556712956affb4c6
team-hungrian <def.hunger@gmail.com>
2024-08-23 00:15:18 +0900

    Time: 0 ms (100%), Space: 40.1 MB (80.96%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,6 +1,20 @@
 class Solution {
     public int findComplement(int num) {
-        return mySol(num);
+        return others_good(num);
+    }
+
+    public int others_good(int num) {
+        int sum = 0;
+        int pos = 0;
+
+        while (sum < num) {
+            sum += Math.pow(2, pos++);
+        }
+
+        // a + a's complement => 111111
+        // 111111 - a = a's complement
+
+        return sum - num;
     }
 
     public int mySol(int num) {
