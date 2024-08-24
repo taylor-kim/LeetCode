db2cfda5c6457faef986473a400e6a163166f8a0
team-hungrian <def.hunger@gmail.com>
2024-08-24 17:55:40 +0900

    Time: 7 ms (53.57%), Space: 41.9 MB (59.82%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
@@ -1,63 +1,64 @@
 class Solution {
-
     public String fractionAddition(String expression) {
-        int num = 0;
-        int denom = 1;
-
-        int i = 0;
-        while (i < expression.length()) {
-            int currNum = 0;
-            int currDenom = 0;
-
-            boolean isNegative = false;
-
-            // check for sign
-            if (expression.charAt(i) == '-' || expression.charAt(i) == '+') {
-                if (expression.charAt(i) == '-') {
-                    isNegative = true;
-                }
-                // move to next character
-                i++;
-            }
-
-            // build numerator
-            while (Character.isDigit(expression.charAt(i))) {
-                int val = expression.charAt(i) - '0';
-                currNum = currNum * 10 + val;
-                i++;
-            }
-
-            if (isNegative) currNum *= -1;
-
-            //skip divisor
-            i++;
-
-            // build denominator
-            while (
-                i < expression.length() &&
-                Character.isDigit(expression.charAt(i))
-            ) {
-                int val = expression.charAt(i) - '0';
-                currDenom = currDenom * 10 + val;
-                i++;
-            }
-
-            // add fractions together using common denominator
-            num = num * currDenom + currNum * denom;
-            denom = denom * currDenom;
-        }
-
-        int gcd = Math.abs(FindGCD(num, denom));
-
-        // reduce fractions
-        num /= gcd;
-        denom /= gcd;
-
-        return num + "/" + denom;
+        return mySol(expression);
     }
 
-    private int FindGCD(int a, int b) {
-        if (a == 0) return b;
-        return FindGCD(b % a, a);
+    public String mySol(String expression) {
+        int base = 1;
+
+        for (int i = 2; i <= 10; i++) {
+            base *= i;
+        }
+
+        if (expression.charAt(0) != '-') {
+            expression = "+" + expression;
+        }
+
+        List<String> fractions = new ArrayList();
+
+        for (String s : expression.split("\\+")) {
+            String[] ss = s.split("\\-");
+            for (int i = 0; i < ss.length; i++) {
+                // s - s
+                // i == 0 => s, i > 0 => -s
+                // -s -s
+                // i == 0 => continue, i > 0 => -s
+                if (i == 0 && ss[i].length() > 0) {
+                    fractions.add(ss[i]);
+                } else if (i > 0) {
+                    fractions.add("-" + ss[i]);
+                }
+            }
+        }
+
+        int a = 0;
+
+        for (String fraction : fractions) {
+            String[] f = fraction.split("/");
+
+            int numer = Integer.parseInt(f[0]);
+            int deno = Integer.parseInt(f[1]);
+
+            a += numer * (base / deno);
+        }
+
+        int[] ans = getIrreducible(a, base);
+
+        return ans[0] + "/" + ans[1];
+    }
+
+    private int[] getIrreducible(int a, int b) {
+        int factor = 2;
+
+        while (factor <= 10) {
+            while (a % factor == 0 && b % factor == 0) {
+                a /= factor;
+                b /= factor;
+            }
+
+            factor++;
+        }
+
+        return new int[] {a, b};
     }
 }
