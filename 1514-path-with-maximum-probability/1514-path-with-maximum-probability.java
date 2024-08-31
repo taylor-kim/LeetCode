4b6ce82b13439f26d229d950467b261f406f47f9
team-hungrian <def.hunger@gmail.com>
2024-08-31 20:44:31 +0900

    Time: 10 ms (92.38%), Space: 56.4 MB (72.32%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.java
@@ -7,7 +7,7 @@ class Solution {
         double[] scores = new double[n];
         scores[start] = 1;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             boolean hasUpdate = false;
 
             for (int j = 0; j < edges.length; j++) {
