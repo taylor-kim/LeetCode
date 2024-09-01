55b452e33c175a9ef7c2623b042d656879a4aceb
team-hungrian <def.hunger@gmail.com>
2024-09-01 18:19:55 +0900

    Time: 2 ms (89.14%), Space: 41.1 MB (75.02%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,35 +1,63 @@
 class Solution {
     public String countAndSay(int n) {
-        return mySol(n);
+        return try_20240901_topdown(n);
     }
 
-    public String mySol(int n) {
-        if (n == 1) {
-            return "1";
-        }
+    public String try_20240901_topdown(int n) {
+        if (n == 1) return "1";
 
-        String s = mySol(n - 1);
+        String s = try_20240901_topdown(n - 1);
+
+        char prev = '0';
+        int count = 0;
 
         StringBuilder sb = new StringBuilder();
 
-        int count = 0;
-        int before = -1;
-
-        for (char ch : s.toCharArray()) {
-            int num = (int)ch - '0';
-
-            if (before == -1 || before == num) {
+        for (char c : s.toCharArray()) {
+            if (prev == c) {
                 count++;
-                before = num;
             } else {
-                sb.append(count).append(before);
+                if (prev != '0') {
+                    sb.append(count).append(prev);
+                }
+
+                prev = c;
                 count = 1;
-                before = num;
             }
         }
 
-        sb.append(count).append(before);
+        sb.append(count).append(prev);
 
         return sb.toString();
+    }
+
+    public String try_20240901(int n) {
+        StringBuilder ans = new StringBuilder();
+        ans.append("1");
+
+        while (--n > 0) {
+            StringBuilder next = new StringBuilder();
+
+            char prev = '0';
+            int count = 1;
+
+            for (char c : ans.toString().toCharArray()) {
+                if (prev == c) {
+                    count++;
+                } else {
+                    if (prev != '0') {
+                        next.append(count).append(prev);
+                    }
+                    prev = c;
+                    count = 1;
+                }
+            }
+
+            next.append(count).append(prev);
+
+            ans = next;
+        }
+
+        return ans.toString();
     }
 }
