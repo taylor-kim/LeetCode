73f354a9b7cdbc3d77f9b128769c740654cf4c00
team-hungrian <def.hunger@gmail.com>
2024-09-02 20:48:45 +0900

    Time: 2 ms (72.59%), Space: 60.5 MB (8.89%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+        return mySol2(chalk, k);
+    }
+
+    public int mySol2(int[] chalk, int k) {
+        long sum = 0;
+
+        for (int c : chalk) {
+            sum += c;
+        }
+
+        long remainder = ((long)k) % sum;
+
+        int ans = 0;
+
+        while (chalk[ans] <= remainder) {
+            remainder -= chalk[ans++];
+            ans = ans % chalk.length;
+        }
+
+        return ans;
+    }
+
+    public int mySol_tle(int[] chalk, int k) {
+        int ans = 0;
+
+        while (chalk[ans] <= k) {
+            k -= chalk[ans++];
+            ans = ans % chalk.length;
+        }
+
+        return ans;
+    }
+}
