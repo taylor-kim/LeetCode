eae8edb82d59229da3af43ec8b76621cc235d0dc
team-hungrian <def.hunger@gmail.com>
2024-09-02 21:00:45 +0900

    Time: 2 ms (72.59%), Space: 56.5 MB (99.54%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -1,6 +1,34 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        return mySol2(chalk, k);
+        return official_prefixsum_binarySearch(chalk, k);
+    }
+
+    public int official_prefixsum_binarySearch(int[] chalk, int k) {
+        int n = chalk.length;
+        long[] pSum = new long[n];
+        pSum[0] = chalk[0];
+
+        for (int i = 1; i < n; i++) {
+            pSum[i] = pSum[i - 1] + chalk[i];
+        }
+
+        long sum = pSum[n - 1];
+        long remainder = k % sum;
+
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int mid = (right - left) / 2 + left;
+
+            if (pSum[mid] <= remainder) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
     }
 
     public int mySol2(int[] chalk, int k) {
