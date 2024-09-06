174881d004995f7dee980a5e9213542990397778
team-hungrian <def.hunger@gmail.com>
2024-09-06 12:34:00 +0900

    Time: 20 ms (87.14%), Space: 64.1 MB (89.04%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
@@ -10,7 +10,26 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        return mySol(nums, head);
+        return mySol_improved(nums, head);
+    }
+
+    public ListNode mySol_improved(int[] nums, ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode node = dummy;
+
+        Set<Integer> set = new HashSet();
+
+        for (int num : nums) set.add(num);
+
+        while (node.next != null) {
+            if (set.contains(node.next.val)) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+
+        return dummy.next;
     }
 
     public ListNode mySol(int[] nums, ListNode head) {
