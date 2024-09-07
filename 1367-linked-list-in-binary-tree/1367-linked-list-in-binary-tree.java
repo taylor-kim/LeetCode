3752b2a1859dd045c7abad1c1ece85fe19eb8948
team-hungrian <def.hunger@gmail.com>
2024-09-07 23:19:23 +0900

    Time: 1 ms (97.88%), Space: 44.8 MB (82.19%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.java
@@ -1,98 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
     public boolean isSubPath(ListNode head, TreeNode root) {
-        return mySol_simple(head, root);
-    }
-
-    public boolean mySol_simple(ListNode head, TreeNode root) {
-        return dfs(head, root);
-    }
-
-    private boolean dfs(ListNode head, TreeNode root) {
         if (root == null) return false;
-
-        return find(head, root) || dfs(head, root.left) || dfs(head, root.right);
+        // Check the current node and all its descendants
+        boolean result = dfs(root, head);
+        // Check all paths from the left and right children of the root
+        result = result || isSubPath(head, root.left);
+        result = result || isSubPath(head, root.right);
+        return result;
     }
 
-    private boolean find(ListNode head, TreeNode root) {
+    private boolean dfs(TreeNode node, ListNode head) {
         if (head == null) return true;
-
-        if (root == null) return false;
-
-        if (head.val != root.val) return false;
-
-        return find(head.next, root.left) || find(head.next, root.right);
-    }
-
-    public boolean mySol_mle(ListNode head, TreeNode root) {
-        Trie t = new Trie();
-
-        buildTrie(t, root);
-
-        return t.find(head);
-    }
-
-    private void buildTrie(Trie t, TreeNode root) {
-        if (root == null) return;
-
-        t.buildTrie(root);
-
-        buildTrie(t, root.left);
-        buildTrie(t, root.right);
-    }
-
-    private class Trie {
-        // Trie[] data = new Trie[101];
-        Map<Integer, Trie> data = new HashMap();
-
-
-        public void buildTrie(TreeNode node) {
-            if (node == null) {
-                return;
-            }
-
-            if (!data.containsKey(node.val)) {
-                // data[node.val] = new Trie();
-                data.put(node.val, new Trie());
-            }
-
-            data.get(node.val).buildTrie(node.left);
-            data.get(node.val).buildTrie(node.right);
+        if (node == null) return false;
+        boolean result = false;
+        if (node.val == head.val) {
+            // Continue searching in both left and right subtrees
+            result = dfs(node.left, head.next);
+            result = result || dfs(node.right, head.next);
         }
-
-        public boolean find(ListNode node) {
-            Trie t = this;
-
-            while (t != null && node != null) {
-                t = t.data.get(node.val);
-                node = node.next;
-            }
-
-            return t != null && node == null;
-        }
+        return result;
     }
 }
