a9dedf16728899b6521c7c1cb2fffcf1f4653cc1
team-hungrian <def.hunger@gmail.com>
2024-09-12 18:14:51 +0900

    Time: 6 ms (88.76%), Space: 45.4 MB (52.29%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.java
@@ -1,6 +1,30 @@
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
-        return mySol(allowed, words);
+        return official_bit(allowed, words);
+    }
+
+    public int official_bit(String allowed, String[] words) {
+        int bits = 0;
+
+        for (char c : allowed.toCharArray()) bits |= 1 << (c - 'a');
+
+        int ans = 0;
+
+        for (String word : words) {
+            int delta = 1;
+            for (char c : word.toCharArray()) {
+                int matched = (bits >> (c - 'a')) & 1;
+
+                if (matched == 0) {
+                    delta = 0;
+                    break;
+                }
+            }
+
+            ans += delta;
+        }
+
+        return ans;
     }
 
     public int mySol(String allowed, String[] words) {
