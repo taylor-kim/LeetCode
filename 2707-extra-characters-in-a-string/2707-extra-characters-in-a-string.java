fbd16b3c67be209546dd3aca172753280b4f2b2c
team-hungrian <def.hunger@gmail.com>
2024-09-23 19:29:36 +0900

    Time: 89 ms (8.16%), Space: 45.7 MB (6.8%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minExtraChar(String s, String[] dictionary) {
+        return mySol(s, dictionary);
+    }
+
+    public int mySol(String s, String[] dictionary) {
+        Set<String> dict = new HashSet();
+
+        for (String word : dictionary) {
+            dict.add(word);
+        }
+
+        return mySol(s, dict, 0, new Integer[s.length()]);
+    }
+
+    public int mySol(String s, Set<String> dict, int index, Integer[] memo) {
+        if (index >= s.length()) return 0;
+
+        if (memo[index] != null) {
+            return memo[index];
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = index; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+
+            if (dict.contains(sb.toString())) {
+                
+            }
+
+            int length = dict.contains(sb.toString()) ? 0 : sb.length();
+
+            ans = Math.min(ans, length + mySol(s, dict, i + 1, memo));
+        }
+
+        return memo[index] = ans;
+    }
+}
