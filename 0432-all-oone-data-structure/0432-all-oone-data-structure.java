53cd9b6219c84cf81649aa47881e9ec672f5e6e1
team-hungrian <def.hunger@gmail.com>
2024-09-29 17:14:44 +0900

    Time: 60 ms (20.96%), Space: 62.2 MB (5.24%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.java
@@ -0,0 +1,68 @@
+class AllOne {
+    Map<String, Integer> counter = new HashMap();
+    TreeMap<Integer, TreeSet<String>> map = new TreeMap();
+
+    public AllOne() {
+        
+    }
+    
+    public void inc(String key) {
+        int count = counter.getOrDefault(key, 0);
+
+        counter.put(key, count + 1);
+        map.computeIfAbsent(count + 1, k -> new TreeSet()).add(key);
+
+        remove(count, key);
+    }
+    
+    public void dec(String key) {
+        int count = counter.get(key);
+
+        counter.put(key, count - 1);
+
+        if (counter.get(key) == 0) counter.remove(key);
+
+        remove(count, key);
+
+        if (count - 1 > 0) {
+            map.computeIfAbsent(count - 1, k -> new TreeSet()).add(key);
+        }
+    }
+
+    private void remove(int count, String value) {
+        if (count > 0) {
+            map.get(count).remove(value);
+
+            if (map.get(count).size() == 0) {
+                map.remove(count);
+            }
+        } else {
+            map.remove(count);
+        }
+    }
+    
+    public String getMaxKey() {
+        if (map.isEmpty()) return "";
+
+        int key = map.lastKey();
+
+        return map.get(key).last();
+    }
+    
+    public String getMinKey() {
+        if (map.isEmpty()) return "";
+
+        int key = map.firstKey();
+
+        return map.get(key).first();
+    }
+}
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne obj = new AllOne();
+ * obj.inc(key);
+ * obj.dec(key);
+ * String param_3 = obj.getMaxKey();
+ * String param_4 = obj.getMinKey();
+ */
