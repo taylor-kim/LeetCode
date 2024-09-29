746ab39995e9dadcb5ed867f4a5f087b0a91aa31
team-hungrian <def.hunger@gmail.com>
2024-09-29 16:19:20 +0900

    Time: 47 ms (67.63%), Space: 45.4 MB (75.45%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.java
+++ 0731-my-calendar-ii/0731-my-calendar-ii.java
@@ -1,43 +1,42 @@
 class MyCalendarTwo {
-
-    private List<int[]> bookings;
-    private List<int[]> overlapBookings;
-
-    // Return true if the booking [start1, end1) & [start2, end2) overlaps.
-    private boolean doesOverlap(int start1, int end1, int start2, int end2) {
-        return Math.max(start1, start2) < Math.min(end1, end2);
-    }
-
-    // Return overlapping booking between [start1, end1) & [start2, end2).
-    private int[] getOverlapped(int start1, int end1, int start2, int end2) {
-        return new int[] { Math.max(start1, start2), Math.min(end1, end2) };
-    }
+    List<int[]> bookings = new ArrayList();
+    List<int[]> overlapBookings = new ArrayList();
 
     public MyCalendarTwo() {
-        bookings = new ArrayList<>();
-        overlapBookings = new ArrayList<>();
+        
     }
-
+    
     public boolean book(int start, int end) {
-        // Returns false if the new booking has an overlap
-        // with the existing double-booked bookings.
-        for (int[] booking : overlapBookings) {
-            if (doesOverlap(booking[0], booking[1], start, end)) {
+        for (int[] d : overlapBookings) {
+            if (isOverlap(start, end, d[0], d[1])) {
                 return false;
             }
         }
 
-        // Add the double overlapping bookings if any with the new booking.
-        for (int[] booking : bookings) {
-            if (doesOverlap(booking[0], booking[1], start, end)) {
-                overlapBookings.add(
-                    getOverlapped(booking[0], booking[1], start, end)
-                );
+        for (int[] d : bookings) {
+            if (isOverlap(start, end, d[0], d[1])) {
+                overlapBookings.add(getOverlap(start, end, d[0], d[1]));
             }
         }
 
-        // Add the new booking to the list of bookings.
-        bookings.add(new int[] { start, end });
+        bookings.add(new int[] {start, end});
+
         return true;
     }
+
+    private boolean isOverlap(int s1, int e1, int s2, int e2) {
+        return Math.max(s1, s2) < Math.min(e1, e2);
+    }
+
+    private int[] getOverlap(int s1, int e1, int s2, int e2) {
+        return new int[] {
+            Math.max(s1, s2), Math.min(e1, e2)
+        };
+    }
 }
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
