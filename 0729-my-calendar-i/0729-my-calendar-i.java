2d93fa8d6632aeb44367ece2cd3fd4c69c842ac9
team-hungrian <def.hunger@gmail.com>
2024-09-29 15:26:27 +0900

    Time: 24 ms (51.43%), Space: 45.4 MB (64.72%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,58 +1,21 @@
 class MyCalendar {
-    private List<int[]> schedules = new ArrayList();
-
-    private Comparator<int[]> comparator = (a, b) -> {
-        return a[0] - b[0];
-    };
+    TreeMap<Integer, Integer> map = new TreeMap();
 
     public MyCalendar() {
         
     }
     
     public boolean book(int start, int end) {
-        int[] d = {start, end};
-        int index = Collections.binarySearch(schedules, d, comparator);
+        Integer prev = map.floorKey(start);
+        Integer next = map.ceilingKey(start);
 
-        // System.out.println(String.format("size:%d, index:%d", schedules.size(), index));
+        if ((prev == null || map.get(prev) <= start) && (next == null || end <= next)) {
+            map.put(start, end);
 
-        // StringBuilder sb = new StringBuilder();
-        // sb.append("list: ");
-
-        // for (int[] each : schedules) {
-        //     sb.append(Arrays.toString(each)).append(", ");
-        // }
-
-        // sb.append("d: ").append(Arrays.toString(d));
-
-        // System.out.println(sb.toString());
-
-        if (index >= 0) return false;
-
-        index = -(index + 1);
-
-        if (index < schedules.size()) {
-            int[] next = schedules.get(index);
-
-            if (d[1] > next[0]) return false;
-        }
-
-        if (index - 1 >= 0) {
-            int[] prev = schedules.get(index - 1);
-
-            if (prev[1] > d[0]) {
-                return false;
-            }
-        }
-
-        if (schedules.size() == index) {
-            schedules.add(d);
-            // System.out.println("add last");
             return true;
-        } else {
-            schedules.add(index, d);
         }
 
-        return true;
+        return false;
     }
 }
 
