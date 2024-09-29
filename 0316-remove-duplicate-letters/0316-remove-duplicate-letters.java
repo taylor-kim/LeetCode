d62ce7b4402f0b7d46204053db863ed543e67698
team-hungrian <def.hunger@gmail.com>
2024-09-30 00:10:10 +0900

    Time: 2 ms (94.2%), Space: 42 MB (72.48%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,6 +1,37 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        return tryAgain_20240929(s);
+        return tryAgain_20240929_faster(s);
+    }
+
+    public String tryAgain_20240929_faster(String s) {
+        int[] last = new int[26];
+        boolean[] seen = new boolean[26];
+        Stack<Integer> stack = new Stack();
+
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            int val = s.charAt(i) - 'a';
+
+            if (seen[val]) continue;
+
+            while (!stack.isEmpty() && stack.peek() > val && last[stack.peek()] > i) {
+                seen[stack.pop()] = false;
+            }
+
+            stack.push(val);
+            seen[val] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            sb.append((char)(stack.pop() + 'a'));
+        }
+
+        return sb.reverse().toString();
     }
 
     public String tryAgain_20240929(String s) {
@@ -18,9 +49,10 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             while (!stack.isEmpty() 
                 && s.charAt(stack.peek()) >= s.charAt(i) 
-                && last[s.charAt(stack.peek()) - 'a'] > i) {
-                    set.remove(s.charAt(stack.peek()));
-                    System.out.println(String.format("pop:%c", s.charAt(stack.pop())));
+                && last[s.charAt(stack.peek()) - 'a'] > i
+                && !set.contains(s.charAt(i))) {
+                    // System.out.println(String.format("pop:%c", s.charAt(stack.peek())));
+                    set.remove(s.charAt(stack.pop()));
             }
 
             if (set.add(s.charAt(i))) {
