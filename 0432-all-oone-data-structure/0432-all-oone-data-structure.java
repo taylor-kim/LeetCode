01697daca056b112edeed4d082311276f14cb1f6
team-hungrian <def.hunger@gmail.com>
2024-09-29 17:58:33 +0900

    Time: 52 ms (71.32%), Space: 61 MB (35.94%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.java
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.java
@@ -1,64 +1,125 @@
 class AllOne {
-    Map<String, Integer> counter = new HashMap();
-    TreeMap<Integer, TreeSet<String>> map = new TreeMap();
+    MyList head;
+    MyList tail;
+    Map<String, MyList> map;
 
     public AllOne() {
-        
+        head = new MyList(0);
+        tail = new MyList(0);
+
+        head.next = tail;
+        tail.prev = head;
+
+        map = new HashMap();
     }
     
     public void inc(String key) {
-        int count = counter.getOrDefault(key, 0);
+        if (map.containsKey(key)) {
+            MyList node = map.get(key);
+            int freq = node.freq;
+            node.keys.remove(key);
 
-        counter.put(key, count + 1);
-        map.computeIfAbsent(count + 1, k -> new TreeSet()).add(key);
+            MyList nextNode = node.next;
 
-        remove(count, key);
+            if (nextNode == tail || nextNode.freq != freq + 1) {
+                MyList newNode = new MyList(freq + 1);
+
+                newNode.prev = node;
+                node.next = newNode;
+                newNode.next = nextNode;
+                nextNode.prev = newNode;
+
+                newNode.keys.add(key);
+                map.put(key, newNode);
+            } else {
+                nextNode.keys.add(key);
+                map.put(key, nextNode);
+            }
+
+            removeNodeIfNeed(node);
+        } else {
+            MyList node = head.next;
+
+            if (node == tail || node.freq != 1) {
+                MyList newNode = new MyList(1);
+                newNode.keys.add(key);
+
+                newNode.prev = node.prev;
+                node.prev = newNode;
+
+                newNode.prev.next = newNode;
+                newNode.next = node;
+
+                map.put(key, newNode);
+            } else {
+                node.keys.add(key);
+                map.put(key, node);
+            }
+        }
     }
     
     public void dec(String key) {
-        int count = counter.get(key);
+        if (!map.containsKey(key)) return;
 
-        counter.put(key, count - 1);
+        MyList node = map.get(key);
+        node.keys.remove(key);
+        int freq = node.freq;
 
-        if (counter.get(key) == 0) counter.remove(key);
-
-        remove(count, key);
-
-        if (count - 1 > 0) {
-            map.computeIfAbsent(count - 1, k -> new TreeSet()).add(key);
-        }
-    }
-
-    private void remove(int count, String value) {
-        if (count > 0) {
-            map.get(count).remove(value);
-
-            if (map.get(count).size() == 0) {
-                map.remove(count);
-            }
+        if (freq == 1) {
+            map.remove(key);
         } else {
-            map.remove(count);
+            MyList prevNode = node.prev;
+
+            if (prevNode == head || prevNode.freq != freq - 1) {
+                MyList newNode = new MyList(freq - 1);
+
+                newNode.prev = prevNode;
+                prevNode.next = newNode;
+                newNode.next = node;
+                node.prev = newNode;
+
+                newNode.keys.add(key);
+                map.put(key, newNode);
+            } else {
+                prevNode.keys.add(key);
+                map.put(key, prevNode);
+            }
         }
+
+        removeNodeIfNeed(node);
     }
     
     public String getMaxKey() {
-        if (map.isEmpty()) return "";
+        if (tail.prev == head) return "";
 
-        // int key = map.lastKey();
-
-        // return map.get(key).last();
-
-        return map.lastEntry().getValue().first();
+        return tail.prev.keys.iterator().next();
     }
     
     public String getMinKey() {
-        if (map.isEmpty()) return "";
+        if (head.next == tail) return "";
 
-        // int key = map.firstKey();
+        return head.next.keys.iterator().next();
+    }
 
-        // return map.get(key).first();
+    private void removeNodeIfNeed(MyList node) {
+        if (node.keys.size() == 0) {
+            MyList prev = node.prev;
+            MyList next = node.next;
 
-        return map.firstEntry().getValue().first();
+            prev.next = next;
+            next.prev = prev;
+        }
+    }
+
+    class MyList {
+        int freq = 0;
+        MyList prev;
+        MyList next;
+        Set<String> keys = new HashSet();
+
+        public MyList(int freq) {
+            this.freq = freq;
+        }
     }
 }
 
