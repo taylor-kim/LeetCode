3053f224d9d7c7400f2d44775f338165f2f6d92d
team-hungrian <def.hunger@gmail.com>
2024-10-02 20:21:40 +0900

    Time: 50 ms (15.79%), Space: 70.6 MB (5.29%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,6 +1,38 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        return mySol(arr);
+        return mySol2(arr);
+    }
+
+    public int[] mySol2(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        Map<Integer, List<Integer>> bucket = new TreeMap();
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+
+            bucket.computeIfAbsent(num - min, k -> new ArrayList()).add(i);
+        }
+
+        int[] ans = new int[arr.length];
+        
+        int rank = 1;
+        int bucketIndex = 0;
+
+        for (List<Integer> indices : bucket.values()) {
+            for (int originIndex : indices) {
+                ans[originIndex] = rank;
+            }
+            rank++;
+        }
+
+        return ans;
     }
 
     public int[] mySol(int[] arr) {
