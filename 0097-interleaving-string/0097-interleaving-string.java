9740b7840c0f3149315894bcbba077662475852b
team-hungrian <def.hunger@gmail.com>
2024-10-03 23:34:03 +0900

    Time: 4 ms (53.47%), Space: 41.6 MB (61.73%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,6 +1,32 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        return try_bottomup2(s1, s2, s3);
+        return try_bottomup_opt(s1, s2, s3);
+    }
+
+    public boolean try_bottomup_opt(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+
+        if (m + n != s3.length()) return false;
+
+        boolean[] dp = new boolean[n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+                } else if (j == 0) {
+                    dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                } else {
+                    dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1)
+                            || dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+                }
+            }
+        }
+
+        return dp[n];
     }
 
     public boolean try_bottomup2(String s1, String s2, String s3) {
