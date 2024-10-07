6781ad740c9f993383268d6e7931d8190abdce79
team-hungrian <def.hunger@gmail.com>
2024-10-07 18:08:28 +0900

    Time: 5 ms (65.3%), Space: 43.2 MB (71.14%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,6 +1,22 @@
 class Solution {
     public int minLength(String s) {
-        return mySol(s);
+        return mySol_stack(s);
+    }
+
+    public int mySol_stack(String s) {
+        Stack<Character> stack = new Stack();
+
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty()
+                 && ((stack.peek() == 'A' && c == 'B')
+                 || (stack.peek() == 'C' && c == 'D'))) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.size();
     }
 
     public int mySol(String s) {
