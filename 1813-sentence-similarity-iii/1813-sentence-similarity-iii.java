a34cb6550cb9d6c22570f1073f5c4dcb9ad426d5
team-hungrian <def.hunger@gmail.com>
2024-10-08 02:18:58 +0900

    Time: 2 ms (23.11%), Space: 41.7 MB (39.04%) - LeetHub

--- 1813-sentence-similarity-iii/1813-sentence-similarity-iii.java
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.java
@@ -1,26 +1,58 @@
 class Solution {
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        return official_deque(sentence1, sentence2);
+    }
 
-    public boolean areSentencesSimilar(String s1, String s2) {
-        Deque<String> deque1 = new ArrayDeque<>(Arrays.asList(s1.split(" ")));
-        Deque<String> deque2 = new ArrayDeque<>(Arrays.asList(s2.split(" ")));
-        //Compare the prefixes or beginning of the strings.
-        while (
-            !deque1.isEmpty() &&
-            !deque2.isEmpty() &&
-            deque1.peek().equals(deque2.peek())
-        ) {
-            deque1.poll();
-            deque2.poll();
+    public boolean official_deque(String s1, String s2) {
+        Deque<String> d1 = new LinkedList(Arrays.asList(s1.split(" ")));
+        Deque<String> d2 = new LinkedList(Arrays.asList(s2.split(" ")));
+
+        while (!d1.isEmpty() && !d2.isEmpty() && d1.peek().equals(d2.peek())) {
+            d1.poll();
+            d2.poll();
         }
-        // Compare the suffixes or ending of the strings.
-        while (
-            !deque1.isEmpty() &&
-            !deque2.isEmpty() &&
-            deque1.peekLast().equals(deque2.peekLast())
-        ) {
-            deque1.pollLast();
-            deque2.pollLast();
+
+        while (!d1.isEmpty() && !d2.isEmpty() && d1.peekLast().equals(d2.peekLast())) {
+            d1.pollLast();
+            d2.pollLast();
         }
-        return deque1.isEmpty() || deque2.isEmpty();
+
+        return d1.isEmpty() || d2.isEmpty();
+    }
+
+    public boolean mySol_fail(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            String temp = s1;
+            s1 = s2;
+            s2 = temp;
+        }
+
+        String[] arr1 = s1.split(" ");
+        String[] arr2 = s2.split(" ");
+
+        int i1 = 0;
+        int i2 = 0;
+
+        int skipFrom = -1;
+
+        while (i1 < arr1.length && i2 < arr2.length) {
+            if (arr1[i1].equals(arr2[i2])) {
+                i1++;
+                i2++;
+            } else {
+                if (skipFrom == -1 || skipFrom == i1) {
+                    if (skipFrom == -1) {
+                        skipFrom = i1;
+                    }
+                    i2++;
+                } else {
+                    if (i1 == 0) i2++;
+
+                    i1 = 0;
+                }
+            }
+        }
+
+        return i1 == arr1.length;
     }
 }
