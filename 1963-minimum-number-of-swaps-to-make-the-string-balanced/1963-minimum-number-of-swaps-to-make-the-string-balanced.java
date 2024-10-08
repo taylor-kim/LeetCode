e1f2517a18efbd4613b82c565a676ee1b9f014c7
team-hungrian <def.hunger@gmail.com>
2024-10-08 18:19:20 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minSwaps(String s) {
+        return mySol(s);
+    }
+
+    public int mySol(String s) {
+        Stack<Integer> stack = new Stack();
+        int open = 0;
+        int close = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (!stack.isEmpty() && s.charAt(stack.peek()) == '[' && c == ']') {
+                stack.pop();
+                open--;
+            } else {
+                stack.push(i);
+
+                if (c == '[') {
+                    open++;
+                } else {
+                    close++;
+                }
+            }
+        }
+
+        // System.out.println(String.format("open:%d, close:%d", open, close));
+
+        int ans = stack.size() / 2;
+
+        return ans > 1 ? ans - 1 : ans;
+    }
+}
