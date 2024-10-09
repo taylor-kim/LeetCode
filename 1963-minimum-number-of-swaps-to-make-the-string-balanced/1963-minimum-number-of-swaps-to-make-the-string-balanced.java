e93b887e71552c47a8f9e560170d6e09b69fa508
team-hungrian <def.hunger@gmail.com>
2024-10-10 00:54:59 +0900

    Time: 17 ms (51.26%), Space: 53.8 MB (95.24%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,26 +1,55 @@
 class Solution {
     public int minSwaps(String s) {
-        return official_stack(s);
+        return official_opt(s);
     }
 
-    public int official_stack(String s) {
-        Stack<Character> stack = new Stack();
+    public int official_opt(String s) {
+        int open = 0;
         int unmatched = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            if (c == '[') {
-                stack.push(c);
+            if (s.charAt(i) == '[') {
+                open++;
             } else {
-                if (stack.isEmpty()) {
-                    unmatched++;
+                if (open > 0) {
+                    open--;
                 } else {
-                    stack.pop();
+                    unmatched++;
                 }
             }
         }
 
         return (unmatched + 1) / 2;
+    }
+
+    public int mySol(String s) {
+        Stack<Integer> stack = new Stack();
+        int open = 0;
+        int close = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (!stack.isEmpty() && s.charAt(stack.peek()) == '[' && c == ']') {
+                stack.pop();
+                open--;
+            } else {
+                stack.push(i);
+
+                if (c == '[') {
+                    open++;
+                } else {
+                    close++;
+                }
+            }
+        }
+
+        // System.out.println(String.format("open:%d, close:%d", open, close));
+
+        int ans = stack.size() / 2;
+
+        // return ans > 1 ? ans - 1 : ans;
+
+        return (stack.size() / 2 + 1) / 2;
     }
 }
