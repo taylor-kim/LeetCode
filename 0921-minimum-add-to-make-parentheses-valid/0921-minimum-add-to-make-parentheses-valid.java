ebd3dde9f015a74633d94e545b72e168c424d64c
team-hungrian <def.hunger@gmail.com>
2024-10-09 17:01:20 +0900

    Time: 2 ms (26.85%), Space: 41.4 MB (64.62%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,6 +1,22 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        return mySol(s);
+        return mySol2(s);
+    }
+
+    public int mySol2(String s) {
+        Stack<Character> stack = new Stack();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (!stack.isEmpty() && stack.peek() == '(' && c == ')') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.size();
     }
 
     public int mySol(String s) {
