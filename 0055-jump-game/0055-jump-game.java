f00026fbcedf41df21bb9832b01e6b6215524ff7
team-hungrian <def.hunger@gmail.com>
2024-10-12 19:50:12 +0900

    Time: 2 ms (78.97%), Space: 45.5 MB (71.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,6 +1,39 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        return try_20241012(nums);
+        return good_greedy(nums);
+    }
+
+    public boolean good_greedy(int[] nums) {
+        int far = 0;
+
+        for (int i = 0; i <= far && i < nums.length; i++) {
+            far = Math.max(far, i + nums[i]);
+
+            if (far >= nums.length - 1) return true;
+        }
+
+        return false;
+    }
+
+    public boolean try_20241012_bottomup(int[] nums) {
+        int n = nums.length;
+        boolean[] dp = new boolean[n];
+        dp[n - 1] = true;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (i + nums[i] >= n - 1) {
+                dp[i] = true;
+            } else {
+                for (int j = nums[i]; j > 0; j--) {
+                    if (dp[i + j]) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return dp[0];
     }
 
     public boolean try_20241012(int[] nums) {
