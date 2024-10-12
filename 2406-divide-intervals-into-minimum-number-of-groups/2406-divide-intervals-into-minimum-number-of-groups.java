288f4ef6091cad562a39d9d00ff969c322bbf8d6
team-hungrian <def.hunger@gmail.com>
2024-10-12 17:37:59 +0900

    Time: 187 ms (11.14%), Space: 76.8 MB (58.7%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minGroups(int[][] intervals) {
+        return mySol(intervals);
+    }
+
+    public int mySol(int[][] intervals) {
+        TreeMap<Integer, List<int[]>> map = new TreeMap();
+
+        for (int[] d : intervals) {
+            map.computeIfAbsent(d[0], k -> new ArrayList()).add(d);
+        }
+
+        int ans = 0;
+
+        while (map.size() > 0) {
+            Integer key = map.firstKey();
+
+            while (key != null) {
+                int[] d = map.get(key).remove(0);
+
+                if (map.get(key).size() == 0) {
+                    map.remove(key);
+                }
+
+                key = map.higherKey(d[1]);
+            }
+
+            ans++;
+        }
+
+        return ans;
+    }
+}
