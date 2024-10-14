a428a36137e160eb060ba22a2c0edacc6852eede
team-hungrian <def.hunger@gmail.com>
2024-10-14 21:14:35 +0900

    Time: 124 ms (22.99%), Space: 78.9 MB (66.46%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -1,6 +1,85 @@
 class Solution {
     public int minGroups(int[][] intervals) {
-        return mySol(intervals);
+        return official_events(intervals);
+    }
+
+    public int official_events(int[][] intervals) {
+        List<int[]> events = new ArrayList();
+
+        for (int i = 0; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            events.add(new int[] {interval[0], 1});
+            events.add(new int[] {interval[1] + 1, -1});
+        }
+
+        Collections.sort(events, (a, b) -> {
+            return a[0] != b[0] ? a[0] - b[0] : a[1] - b[1];
+        });
+
+        int ans = 0;
+        int current = 0;
+
+        for (int[] event : events) {
+            current += event[1];
+
+            ans = Math.max(ans, current);
+        }
+
+        return ans;
+    }
+
+    public int mySol2(int[][] intervals) {
+        List<int[]> list = new ArrayList();
+
+        for (int[] d : intervals) list.add(d);
+
+        Collections.sort(list, (a, b) -> {
+            return a[0] - b[0];
+        });
+
+        int ans = 0;
+
+        while (list.size() > 0) {
+            int[] interval = list.remove(0);
+
+            while (interval != null) {
+                interval = removeNext(list, interval);
+            }
+
+            ans++;
+        }
+
+        return ans;
+    }
+
+    private int[] removeNext(List<int[]> list, int[] key) {
+        if (list.size() == 0) return null;
+
+        int left = 0;
+        int right = list.size();
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            int[] found = list.get(mid);
+
+            if (key[1] >= found[0]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        int index = Math.min(left, list.size() - 1);
+
+        int[] ret = list.get(index);
+
+        if (key[1] < ret[0]) {
+            list.remove(index);
+            return ret;
+        }
+
+        return null;
     }
 
     public int mySol(int[][] intervals) {
@@ -22,7 +101,7 @@ class Solution {
                     map.remove(key);
                 }
 
-                key = map.ceilingKey(d[1]);
+                key = map.higherKey(d[1]);
             }
 
             ans++;
