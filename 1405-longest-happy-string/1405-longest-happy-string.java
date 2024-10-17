ff00c359e90ec47904b705320c753c929e1f0e7c
team-hungrian <def.hunger@gmail.com>
2024-10-18 00:05:13 +0900

    Time: 2 ms (53.13%), Space: 41 MB (42.93%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.java
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -1,62 +1,60 @@
 class Solution {
-
     public String longestDiverseString(int a, int b, int c) {
-        PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) ->
-            (y.count - x.count)
-        );
-        // Add the counts of a, b and c in priority queue.
-        if (a > 0) {
-            pq.add(new Pair(a, 'a'));
-        }
-
-        if (b > 0) {
-            pq.add(new Pair(b, 'b'));
-        }
-
-        if (c > 0) {
-            pq.add(new Pair(c, 'c'));
-        }
-
-        StringBuilder ans = new StringBuilder();
-        while (!pq.isEmpty()) {
-            Pair p = pq.poll();
-            int count = p.count;
-            char character = p.character;
-            // If three consecutive characters exists, pick the second most
-            // frequent character.
-            if (
-                ans.length() >= 2 &&
-                ans.charAt(ans.length() - 1) == p.character &&
-                ans.charAt(ans.length() - 2) == p.character
-            ) {
-                if (pq.isEmpty()) break;
-
-                Pair temp = pq.poll();
-                ans.append(temp.character);
-                if (temp.count - 1 > 0) {
-                    pq.add(new Pair(temp.count - 1, temp.character));
-                }
-            } else {
-                count--;
-                ans.append(character);
-            }
-
-            // If count is greater than zero, add it to priority queue.
-            if (count > 0) {
-                pq.add(new Pair(count, character));
-            }
-        }
-        return ans.toString();
+        return mySol(a, b, c);
     }
 
-    class Pair {
+    public String mySol(int a, int b, int c) {
+        Queue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            return o2[0] - o1[0];
+        });
 
-        int count;
-        char character;
+        pq.add(new int[] {a, (int)'a'});
+        pq.add(new int[] {b, (int)'b'});
+        pq.add(new int[] {c, (int)'c'});
 
-        Pair(int count, char character) {
-            this.count = count;
-            this.character = character;
+        StringBuilder sb = new StringBuilder();
+
+        char lc = '1';
+        int ls = 0;
+
+        while (pq.size() > 0 && pq.peek()[0] > 0) {
+            int[] data = pq.poll();
+
+            int limit = 2;
+
+            if (lc == (char)data[1]) {
+                limit = 2 - ls;
+            }
+
+            if (limit == 0) continue;
+
+            append(data, sb, limit);
+
+            lc = (char)data[1];
+            ls = limit;
+
+            if (pq.size() > 0 && pq.peek()[0] > 0) {
+                int[] next = pq.poll();
+
+                append(next, sb, 1);
+
+                lc = (char)next[1];
+                ls = 1;
+
+                if (next[0] > 0) pq.add(next);
+            }
+
+            if (data[0] > 0) pq.add(data);
         }
+
+        return sb.toString();
+    }
+
+    private void append(int[] data, StringBuilder sb, int limit) {
+        int count = Math.min(limit, data[0]);
+
+        sb.append(String.valueOf((char)data[1]).repeat(count));
+
+        data[0] -= count;
     }
 }
