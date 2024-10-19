f2fac930bb27ab6656074bc48f4debd4005fcd2c
team-hungrian <def.hunger@gmail.com>
2024-10-19 20:17:56 +0900

    Time: 1 ms (98.66%), Space: 44.6 MB (96.79%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,6 +1,24 @@
 class Solution {
     public int jump(int[] nums) {
-        return bottomup(nums);
+        return greedy(nums);
+    }
+
+    public int greedy(int[] nums) {
+        int n = nums.length;
+        int far = 0;
+        int ans = 0;
+        int end = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            far = Math.max(far, i + nums[i]);
+
+            if (i == end) {
+                ans++;
+                end = far;
+            }
+        }
+
+        return ans;
     }
     
     public int bottomup(int[] nums) {
