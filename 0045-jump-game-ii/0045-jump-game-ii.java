6ab30e9105c46fc4517233cce9165e68c1408d1c
team-hungrian <def.hunger@gmail.com>
2024-10-19 20:05:33 +0900

    Time: 31 ms (21.53%), Space: 46 MB (6.28%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,8 +1,30 @@
 class Solution {
     public int jump(int[] nums) {
-        return topdown(nums);
+        return bottomup(nums);
     }
     
+    public int bottomup(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) return 0;
+
+        int[] dp = new int[n];
+
+        Arrays.fill(dp, n);
+        dp[n - 1] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + nums[i] >= n - 1) {
+                dp[i] = 1;
+            } else {
+                for (int j = nums[i]; j >= 0; j--) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i + j]);
+                }
+            }
+        }
+
+        return dp[0];
+    }
 
     public int topdown(int[] nums) {
         return topdown(nums, 0, new Integer[nums.length]);
