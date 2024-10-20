f7d4b56c6324b34b9781edd28ba877f733a8c82f
team-hungrian <def.hunger@gmail.com>
2024-10-20 16:58:23 +0900

    Time: 10 ms (69.27%), Space: 42.5 MB (68.77%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.java
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.java
@@ -1,6 +1,41 @@
 class Solution {
     public boolean parseBoolExpr(String expression) {
-        return mySol(expression);
+        return mySol_more_efficient(expression);
+    }
+
+    public boolean mySol_more_efficient(String s) {
+        Stack<Character> stack = new Stack();
+        Set<Character> opSet = new HashSet(Arrays.asList('&', '|', '!'));
+
+        for (char c : s.toCharArray()) {
+            if (c == ')') {
+                boolean and = true;
+                boolean or = false;
+
+                while (!opSet.contains(stack.peek())) {
+                    boolean b = stack.pop() == 't';
+
+                    and &= b;
+                    or |= b;
+                }
+
+                char op = stack.pop();
+
+                if (op == '&') {
+                    stack.push(and ? 't' : 'f');
+                } else if (op == '|') {
+                    stack.push(or ? 't' : 'f');
+                } else if (op == '!') {
+                    stack.push(and ? 'f' : 't');
+                }
+            } else if (c != ',' && c != '(') {
+                stack.push(c);
+            }
+        }
+
+        // System.out.println(stack);
+
+        return stack.pop() == 't';
     }
 
     public boolean mySol(String s) {
