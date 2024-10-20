516e94e933e0eca0080a13ebfbfe2e9efeb05ffa
team-hungrian <def.hunger@gmail.com>
2024-10-20 18:07:36 +0900

    Time: 2 ms (81.61%), Space: 44.7 MB (26.7%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.java
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.java
@@ -1,6 +1,98 @@
 class Solution {
     public boolean parseBoolExpr(String expression) {
-        return mySol_more_efficient(expression);
+        return official_recur(expression);
+    }
+
+    int index = 0;
+
+    public boolean official_recur(String s) {
+        char c = s.charAt(index++);
+
+        if (c == 't') return true;
+        if (c == 'f') return false;
+
+        if (c == '!') {
+            index++; // skip '('
+            boolean sub = !official_recur(s);
+            index++; // skip ')'
+            return sub;
+        }
+
+        List<Boolean> values = new ArrayList();
+        index++; // skip '('
+        while (s.charAt(index) != ')') {
+            if (s.charAt(index) != ',') {
+                values.add(official_recur(s));
+            } else {
+                index++; // skip ','
+            }
+        }
+        index++; // skip ')'
+
+        if (c == '&') {
+            for (Boolean b : values) {
+                if (!b) return false;
+            }
+            return true;
+        }
+
+        if (c == '|') {
+            for (Boolean b : values) {
+                if (b) return true;
+            }
+            return false;
+        }
+
+        throw new RuntimeException("Shouldn't be reached.");
+    }
+
+    public boolean try_recur_fail(String s) {
+        return try_recur(s, 0);
+    }
+
+    public boolean try_recur(String s, int index) {
+        char op = s.charAt(index++);
+        index++; // open
+
+        char next = s.charAt(index);
+
+        boolean and = true;
+        boolean or = false;
+
+        if (next == '&' || next == '|' || next == '!') {
+            boolean sub = try_recur(s, index);
+
+            if (op == '&') {
+                and &= sub;
+            } else if (op == '|') {
+                or |= sub;
+            } else {
+                and = !sub;
+            }
+        }
+
+        while (s.charAt(index) != ')') {
+            char c = s.charAt(index++);
+
+            if (c == ',') {
+                continue;
+            }
+
+            boolean b = c == 't';
+
+            and &= b;
+            or |= b;
+        }
+
+        if (op == '&') {
+            return and;
+        } else if (op == '|') {
+            return or;
+        } else {
+            return !and;
+        }
+
+        // return false;
     }
 
     public boolean mySol_more_efficient(String s) {
