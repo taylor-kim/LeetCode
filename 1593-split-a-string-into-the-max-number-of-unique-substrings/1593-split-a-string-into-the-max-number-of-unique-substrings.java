4a14cb4e84e2f352ca3c6f64679e241b1cc8d8ad
team-hungrian <def.hunger@gmail.com>
2024-10-21 20:22:53 +0900

    Time: 33 ms (73.86%), Space: 45.1 MB (31.05%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
@@ -1,6 +1,78 @@
 class Solution {
     public int maxUniqueSplit(String s) {
-        return topdown(s);
+        return official_topdown_pruning(s);
+    }
+
+    public int official_topdown_pruning(String s) {
+        Set<String> set = new HashSet();
+
+        return official_topdown_pruning(s, 0, 0, set);
+    }
+
+    public int official_topdown_pruning(String s, int index, int count, Set<String> set) {
+        if (count + (s.length() - index) <= set.size()) return set.size();
+
+        if (index >= s.length()) return set.size();
+
+        int ans = 0;
+
+        for (int i = index; i < s.length(); i++) {
+            String sub = s.substring(index, i + 1);
+
+            if (set.add(sub)) {
+                ans = Math.max(ans, official_topdown_pruning(s, i + 1, count + 1, set));
+
+                set.remove(sub);
+            }
+        }
+
+        return ans;
+    }
+
+    public int try_iter_fail(String s) {
+        Set<String> set = new HashSet();
+        Stack<String> stack = new Stack();
+
+        int ans = 0;
+
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            String sub = s.substring(left, right + 1);
+
+            System.out.println(sub);
+
+            if (set.add(sub)) {
+                ans++;
+                left = right + 1;
+
+                stack.add(sub);
+            } else if (right + 1 == s.length()) {
+                System.out.println(String.format("need to back. sub:%s", sub));
+
+                // while (set.contains(stack.peek() + sub)) {
+                //     String pop = stack.pop();
+                //     sub = pop + sub;
+                //     set.remove(pop);
+                // }
+
+                String pop = stack.pop();
+
+                set.remove(pop);
+                set.add(pop + sub);
+
+                stack.add(pop + sub);
+
+                System.out.println(String.format("pop:%s, pop + sub:%s", pop, pop + sub));
+
+                left = right + 1;
+            }
+        }
+
+        System.out.println(set);
+        System.out.println(stack);
+
+        return set.size();
     }
 
     public int topdown(String s) {
