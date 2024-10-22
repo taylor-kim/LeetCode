32b39059bd1822bb7f6a8dbee236e72ffc7a2988
team-hungrian <def.hunger@gmail.com>
2024-10-22 19:13:32 +0900

    Time: 31 ms (73.45%), Space: 63.7 MB (40.4%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.java
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.java
@@ -15,7 +15,62 @@
  */
 class Solution {
     public long kthLargestLevelSum(TreeNode root, int k) {
-        return bfs(root, k);
+        return official_min_heap(root, k);
+    }
+
+    public long official_min_heap(TreeNode root, int k) {
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+
+        Queue<Long> pq = new PriorityQueue();
+
+        while (!queue.isEmpty()) {
+            long levelSum = 0;
+
+            int size = queue.size();
+
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+
+                levelSum += node.val;
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            pq.add(levelSum);
+
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        if (pq.size() < k) return -1;
+        
+        return pq.poll();
+    }
+
+    public long dfs(TreeNode root, int k) {
+        List<Long> list = new ArrayList();
+
+        dfs(root, 0, list);
+
+        Collections.sort(list, Comparator.reverseOrder());
+
+        return list.size() < k ? -1 : list.get(k - 1);
+    }
+
+    public void dfs(TreeNode root, int level, List<Long> list) {
+        if (root == null) return;
+
+        if (list.size() == level) {
+            list.add((long)root.val);
+        } else {
+            list.set(level, list.get(level) + root.val);
+        }
+
+        dfs(root.left, level + 1, list);
+        dfs(root.right, level + 1, list);
     }
 
     public long bfs(TreeNode root, int k) {
