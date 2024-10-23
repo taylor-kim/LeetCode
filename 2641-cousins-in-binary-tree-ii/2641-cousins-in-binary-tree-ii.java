223834f16221670e32cf20ddb5e3aca83bedad2d
team-hungrian <def.hunger@gmail.com>
2024-10-23 20:22:00 +0900

    Time: 38 ms (38.98%), Space: 78.2 MB (53.39%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode replaceValueInTree(TreeNode root) {
+        return mySol(root);
+    }
+
+    public TreeNode mySol(TreeNode root) {
+        Map<Integer, Integer> levelSum = new HashMap();
+
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+
+                levelSum.put(level, levelSum.getOrDefault(level, 0) + node.val);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            level++;
+        }
+
+        queue.add(root);
+        level = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+
+                int childSum = 0;
+
+                if (node.left != null) {
+                    childSum += node.left.val;
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    childSum += node.right.val;
+                    queue.add(node.right);
+                }
+
+                if (level <= 1) {
+                    node.val = 0;
+                }
+
+                if (node.left != null && level > 0) {
+                    node.left.val = levelSum.getOrDefault(level + 1, 0) - childSum;
+                }
+                if (node.right != null && level > 0) {
+                    node.right.val = levelSum.getOrDefault(level + 1, 0) - childSum;
+                }
+            }
+
+            level++;
+        }
+
+        return root;
+    }
+}
