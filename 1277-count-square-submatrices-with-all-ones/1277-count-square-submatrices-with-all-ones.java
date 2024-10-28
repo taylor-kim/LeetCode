82eba0a09c6acf62617899cd8768c2e41620c562
team-hungrian <def.hunger@gmail.com>
2024-10-29 00:38:29 +0900

    Time: 6 ms (73.12%), Space: 54.8 MB (74.2%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,40 +1,23 @@
 class Solution {
-
-    public int solve(int i, int j, int[][] grid, int[][] dp) {
-        // If the cell lies outside the grid, return 0.
-        if (i >= grid.length || j >= grid[0].length) {
-            return 0;
-        }
-        if (grid[i][j] == 0) {
-            return 0;
-        }
-
-        // If we have already visited this cell, return the memoized value.
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        // Find the answer for the cell to the right of the current cell.
-        int right = solve(i, j + 1, grid, dp);
-        // Find the answer for the cell to the diagonal of the current cell.
-        int diagonal = solve(i + 1, j + 1, grid, dp);
-        // Find the answer for the cell below the current cell.
-        int below = solve(i + 1, j, grid, dp);
-
-        return dp[i][j] = 1 + Math.min(right, Math.min(diagonal, below));
+    public int countSquares(int[][] matrix) {
+        return mySol(matrix);
     }
 
-    public int countSquares(int[][] grid) {
+    public int mySol(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] dp = new int[m + 1][n + 1];
+
         int ans = 0;
-        int[][] dp = new int[grid.length][grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                Arrays.fill(dp[i], -1);
-            }
-        }
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                ans += solve(i, j, grid, dp);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    dp[i + 1][j + 1] = Math.min(dp[i][j], Math.min(dp[i + 1][j], dp[i][j + 1])) + 1;
+
+                    ans += dp[i + 1][j + 1];
+                }
             }
         }
 
