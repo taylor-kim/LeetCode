36d1a8977458dfaf87a898fad0476aaeaad7234e
team-hungrian <def.hunger@gmail.com>
2024-11-04 22:40:18 +0900

    Time: 17 ms (80.51%), Space: 45.7 MB (79.79%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.java
+++ 3163-string-compression-iii/3163-string-compression-iii.java
@@ -1,5 +1,64 @@
 class Solution {
     public String compressedString(String word) {
+        return my_oldSol(word);
+    }
+
+    public String official_clean(String word) {
+        int pos = 0;
+        int count = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (pos < word.length()) {
+            char c = word.charAt(pos);
+            count = 0;
+
+            while (pos < word.length()
+                && count < 9
+                && c == word.charAt(pos)) {
+                pos++;
+                count++;
+            }
+
+            sb.append(count).append(c);
+        }
+
+        return sb.toString();
+    }
+
+    public String mySol(String word) {
+        int n = word.length();
+
+        if (n == 0) return "";
+
+        StringBuilder sb = new StringBuilder();
+
+        char prev = word.charAt(0);
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            char c = word.charAt(i);
+
+            if (prev == c) {
+                count++;
+
+                if (count == 10) {
+                    sb.append(9).append(c);
+                    count = 1;
+                }
+            } else {
+                sb.append(count).append(prev);
+                prev = c;
+                count = 1;
+            }
+        }
+
+        sb.append(count).append(prev);
+
+        return sb.toString();
+    }
+
+    public String my_oldSol(String word) {
         char prev = word.charAt(0);
         
         StringBuilder sb = new StringBuilder();
