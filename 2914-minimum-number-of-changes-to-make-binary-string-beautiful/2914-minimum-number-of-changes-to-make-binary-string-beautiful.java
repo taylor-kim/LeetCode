3b4bc5002799ad5d9073f4995e150b5c5733e4c9
team-hungrian <def.hunger@gmail.com>
2024-11-05 18:10:20 +0900

    Time: 3 ms (97.78%), Space: 44.9 MB (17.19%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minChanges(String s) {
+        return mySol(s);
+    }
+
+    public int mySol(String s) {
+        int ans = 0;
+
+        for (int i = 0; i + 1 < s.length(); i += 2) {
+            if (s.charAt(i) != s.charAt(i + 1)) ans++;
+        }
+
+        return ans;
+    }
+}
