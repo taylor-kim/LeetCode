31f1bea453837d2ca5dc1423dc8f041a4a5e554c
team-hungrian <def.hunger@gmail.com>
2024-11-07 01:34:44 +0900

    Time: 2 ms (60%), Space: 44.6 MB (21.38%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
@@ -0,0 +1,76 @@
+class Solution {
+    public boolean canSortArray(int[] nums) {
+        return mySol3(nums);
+    }
+
+    public boolean mySol3(int[] nums) {
+        int lastMax = nums[0];
+        int groupMax = nums[0];
+
+        int index = 0;
+        List<int[]> minMaxList = new ArrayList();
+
+        while (index < nums.length) {
+            int min = nums[index];
+            int max = nums[index];
+
+            int start = index + 1;
+
+            while (start < nums.length && Integer.bitCount(nums[start - 1]) == Integer.bitCount(nums[start])) {
+                min = Math.min(min, nums[start]);
+                max = Math.max(max, nums[start++]);
+            }
+
+            minMaxList.add(new int[] {min, max});
+
+            index = start;
+        }
+
+        for (int i = 1; i < minMaxList.size(); i++) {
+            if (minMaxList.get(i - 1)[1] > minMaxList.get(i)[0]) return false;
+        }
+
+        return true;
+    }
+
+    public boolean mySol2_fail(int[] nums) {
+        Integer[] arr = new Integer[nums.length];
+
+        for (int i = 0; i < arr.length; i++) arr[i] = nums[i];
+
+        Arrays.sort(arr, (a, b) -> {
+            if (Integer.bitCount(a) == Integer.bitCount(b)) {
+                return a - b;
+            } else {
+                return 0;
+            }
+        });
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) return false;
+        }
+
+        return true;
+    }
+
+    public boolean mySol_fail(int[] nums) {
+        int min = 0;
+        int max = 1024;
+
+        int last = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                last = i - 1;
+                continue;
+            }
+            if (nums[i - 1] > nums[i] && Integer.bitCount(nums[i - 1]) != Integer.bitCount(nums[i])) {
+                return false;
+            }
+
+            // if ()
+        }
+
+        return true;
+    }
+}
