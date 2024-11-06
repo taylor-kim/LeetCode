275c9913f59d897f4a3d37892f26dd1fc2fe368f
team-hungrian <def.hunger@gmail.com>
2024-11-07 02:05:14 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.java
@@ -1,61 +1,9 @@
 class Solution {
     public boolean canSortArray(int[] nums) {
-        return try_onepass(nums);
+        return mySol2(nums);
     }
 
-    public boolean try_onepass(int[] nums) {
-        int lastMax = 0;
-        int min = nums[0];
-        int max = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            if (Integer.bitCount(nums[i - 1]) == Integer.bitCount(nums[i])) {
-                min = Math.min(min, nums[i]);
-                max = Math.max(max, nums[i]);
-            } else {
-                if (lastMax > min) return false;
-
-                lastMax = max;
-
-                min = nums[i];
-                max = nums[i];
-            }
-        }
-
-        return lastMax < min;
-    }
-
-    public boolean mySol3(int[] nums) {
-        int lastMax = nums[0];
-        int groupMax = nums[0];
-
-        int index = 0;
-        List<int[]> minMaxList = new ArrayList();
-
-        while (index < nums.length) {
-            int min = nums[index];
-            int max = nums[index];
-
-            int start = index + 1;
-
-            while (start < nums.length && Integer.bitCount(nums[start - 1]) == Integer.bitCount(nums[start])) {
-                min = Math.min(min, nums[start]);
-                max = Math.max(max, nums[start++]);
-            }
-
-            minMaxList.add(new int[] {min, max});
-
-            index = start;
-        }
-
-        for (int i = 1; i < minMaxList.size(); i++) {
-            if (minMaxList.get(i - 1)[1] > minMaxList.get(i)[0]) return false;
-        }
-
-        return true;
-    }
-
-    public boolean mySol2_fail(int[] nums) {
+    public boolean mySol2(int[] nums) {
         Integer[] arr = new Integer[nums.length];
 
         for (int i = 0; i < arr.length; i++) arr[i] = nums[i];
