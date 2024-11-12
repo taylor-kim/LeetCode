fc53da6d062d7402726a7b32b735918e8beaf8ad
team-hungrian <def.hunger@gmail.com>
2024-11-12 19:51:46 +0900

    Time: 146 ms (7.25%), Space: 80 MB (77.51%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
@@ -6,19 +6,10 @@ class Solution {
     public int[] mySol(int[][] items, int[] queries) {
         TreeMap<Integer, Integer> map = new TreeMap();
 
-        List<Integer> dp = new ArrayList();
-        dp.add(0);
-
         for (int i = 0; i < items.length; i++) {
             int key = items[i][0];
             int b = items[i][1];
             map.put(key, Math.max(map.getOrDefault(key, 0), b));
-
-            // Integer lower = map.lowerKey(key);
-
-            // if (lower != null && map.get(lower) > b) {
-            //     map.put(key, map.get(lower));
-            // }
         }
 
         Integer key = map.firstKey();
