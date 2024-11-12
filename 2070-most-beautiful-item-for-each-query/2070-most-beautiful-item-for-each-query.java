0fcb9c413e1005f79be2b427621426a3201ed77e
team-hungrian <def.hunger@gmail.com>
2024-11-13 00:49:28 +0900

    Time: 130 ms (7.25%), Space: 79.7 MB (83.43%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
@@ -1,6 +1,74 @@
 class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
-        return mySol(items, queries);
+        return mySol3(items, queries);
+    }
+
+    public int[] mySol3(int[][] items, int[] queries) {
+        Arrays.sort(items, (a, b) -> {
+            return a[0] != b[0] ? a[0] - b[0] : a[1] - b[1];
+        });
+
+        TreeMap<Integer, Integer> map = new TreeMap();
+        List<Integer> list = new ArrayList();
+
+        for (int[] item : items) {
+            int key = item[0];
+            if (!map.containsKey(key)) {
+                map.put(key, list.size());
+                list.add(0);
+            }
+
+            int index = map.get(key);
+
+            int value = item[1];
+
+            if (index > 0) {
+                int prev = list.get(index - 1);
+                list.set(index, Math.max(prev, value));
+            } else {
+                list.set(index, value);
+            }
+        }
+
+        // System.out.println(map);
+        // System.out.println(list);
+
+        for (int i = 0; i < queries.length; i++) {
+            Integer key = map.floorKey(queries[i]);
+            queries[i] = key == null ? 0 : list.get(map.get(key));
+        }
+
+        return queries;
+    }
+
+    public int[] mySol2_mle(int[][] items, int[] queries) {
+        int min = (int)1e9;
+        int max = 0;
+        
+        for (int[] item : items) {
+            min = Math.min(min, item[0]);
+            max = Math.max(max, item[0]);
+        }
+
+        int[] bucket = new int[max - min + 1];
+
+        for (int[] item : items) {
+            int key = item[0] - min;
+            int value = item[1];
+
+            bucket[key] = Math.max(bucket[key], value);
+        }
+
+        for (int i = 1; i <= max - min; i++) {
+            bucket[i] = Math.max(bucket[i], bucket[i - 1]);
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            int key = Math.min(queries[i] - min, max - min);
+            queries[i] = key < 0 ? 0 : bucket[key];
+        }
+
+        return queries;
     }
 
     public int[] mySol(int[][] items, int[] queries) {
