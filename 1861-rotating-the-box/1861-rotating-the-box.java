6a7e9faf9d518becc2b0801bd567b3368092fa58
team-hungrian <def.hunger@gmail.com>
2024-11-23 15:32:14 +0900

    Time: 6 ms (97.56%), Space: 79.6 MB (47.91%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -0,0 +1,33 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+        return mySol(box);
+    }
+
+    public char[][] mySol(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+        
+        char[][] ans = new char[n][m];
+
+        for (char[] row : ans) {
+            Arrays.fill(row, '.');
+        }
+
+        int[] pSum = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            int row = n - 1;
+            int col = m - i - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (box[i][j] == '*') {
+                    row = j;
+                    ans[row--][col] = '*';
+                } else if (box[i][j] == '#') {
+                    ans[row--][col] = '#';
+                }
+            }
+        }
+
+        return ans;
+    }
+}
