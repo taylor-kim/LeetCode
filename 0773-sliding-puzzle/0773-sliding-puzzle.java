d230a4932eba84f54bac16d1dbcbc5c5be2fd6f0
team-hungrian <def.hunger@gmail.com>
2024-11-26 00:19:41 +0900

    Time: 7 ms (84.75%), Space: 42.1 MB (98.98%) - LeetHub

--- 0773-sliding-puzzle/0773-sliding-puzzle.java
+++ 0773-sliding-puzzle/0773-sliding-puzzle.java
@@ -1,6 +1,102 @@
 class Solution {
     public int slidingPuzzle(int[][] board) {
-        return mySol2(board);
+        return try_faster(board);
+    }
+
+    public int try_faster(int[][] board) {
+        Map<Integer, List<Integer>> moves = new HashMap();
+
+        moves.put(0, Arrays.asList(1, 3));
+        moves.put(1, Arrays.asList(0, 2, 4));
+        moves.put(2, Arrays.asList(1, 5));
+        moves.put(3, Arrays.asList(0, 4));
+        moves.put(4, Arrays.asList(3, 1, 5));
+        moves.put(5, Arrays.asList(4, 2));
+
+        int value = 0;
+        int target = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                int index = i * 3 + j;
+                value = set(value, index, board[i][j]);
+                target = set(target, index, (index + 1) % 6);
+            }
+        }
+
+        // System.out.println(String.format("value:%s, target:%s", Integer.toBinaryString(value), Integer.toBinaryString(target)));
+
+        Queue<Integer> queue = new LinkedList();
+        Set<Integer> visit = new HashSet();
+
+        queue.add(value);
+        visit.add(value);
+
+        int turn = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            while (size-- > 0) {
+                int num = queue.poll();
+
+                if (num == target) {
+                    return turn;
+                }
+
+                int index = findIndex(num, 0);
+
+                // System.out.println(String.format("num:%s, index:%d", Integer.toBinaryString(num), index));
+
+                for (int nextIndex : moves.get(index)) {
+                    int nextValue = num;
+                    int digit = get(nextValue, nextIndex);
+                    nextValue = set(nextValue, nextIndex, 0);
+                    nextValue = set(nextValue, index, digit);
+
+                    if (visit.add(nextValue)) {
+                        queue.add(nextValue);
+                    }
+                }
+            }
+
+            turn++;
+        }
+
+        return -1;
+    }
+
+    private int get(int base, int index) {
+        return (base >> (index * 3)) & 7;
+    }
+
+    private int set(int base, int index, int number) {
+        base = erase(base, index);
+
+        number = number << (index * 3);
+
+        return base | number;
+    }
+
+    private int erase(int base, int index) {
+        int mask = 7 << (index * 3);
+        int erase = ~mask;
+
+        return base & erase;
+    }
+
+    private int findIndex(int base, int target) {
+        for (int i = 0; i < 6; i++) {
+            int mask = 7 << (i * 3);
+
+            if ((base & mask) == target) {
+                return i;
+            }
+        }
+
+        System.out.println(String.format("wtf! base:%s, target:%s", Integer.toBinaryString(base), Integer.toBinaryString(target)));
+
+        return -1;
     }
 
     public int mySol2(int[][] board) {
