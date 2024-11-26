e05e394d93bce4a81717c479f5e4574a42dd404c
team-hungrian <def.hunger@gmail.com>
2024-11-27 01:59:44 +0900

    Time: 130 ms (16.56%), Space: 57.3 MB (28.53%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,55 +1,98 @@
 class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        return try_bs(n, quantities);
+    }
 
-    public boolean canDistribute(int x, int[] quantities, int n) {
-        // Pointer to the first not fully distributed product type
-        int j = 0;
-        // Remaining quantity of the jth product type
-        int remaining = quantities[j];
+    public int try_bs(int n, int[] q) {
+        int max = 0;
 
-        // Loop through each store
-        for (int i = 0; i < n; i++) {
-            // Check if the remaining quantity of the jth product type
-            // can be fully distributed to the ith store
-            if (remaining <= x) {
-                // If yes, move the pointer to the next product type
-                j++;
-                // Check if all products have been distributed
-                if (j == quantities.length) {
-                    return true;
-                } else {
-                    remaining = quantities[j];
-                }
+        for (int count : q) {
+            max = Math.max(max, count);
+        }
+
+        int lo = 0;
+        int hi = max;
+
+        int ans = max;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (check(n, mid, q)) {
+                ans = mid;
+                hi = mid - 1;
             } else {
-                // Distribute the maximum possible quantity (x) to the ith store
-                remaining -= x;
+                lo = mid + 1;
             }
         }
+
+        return ans;
+    }
+
+    private boolean check(int n, int x, int[] q) {
+        int index = 0;
+        int remain = q[index];
+
+        for (int i = 0; i < n; i++) {
+            if (remain <= x) {
+                index++;
+
+                if (index == q.length) {
+                    return true;
+                } else {
+                    remain = q[index];
+                }
+            } else {
+                remain -= x;
+            }
+        }
+
         return false;
     }
 
-    public int minimizedMaximum(int n, int[] quantities) {
-        // Initialize the boundaries of the binary search
-        int left = 0;
-        int right = 0;
+    public int try_bf(int n, int[] q) {
+        return topdown(n, q, 0, 0, new Integer[n][q.length]);
+    }
 
-        // Find the maximum element in quantities
-        for (int quantity : quantities) {
-            if (quantity > right) {
-                right = quantity;
-            }
+    public int topdown(int n, int[] q, int i, int j, Integer[][] memo) {
+        if (j >= q.length) return 0;
+
+        if (i >= n) return -1;
+
+        // for (int i = 0;)
+        return 0;
+    }
+
+    public int mySol_fail(int n, int[] q) {
+        Arrays.sort(q);
+
+        int sum = 0;
+
+        int min = (int)1e6;
+
+        Map<Integer, Integer> counter = new HashMap();
+
+        for (int num : q) {
+            sum += num;
+            min = Math.min(min, num);
+
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
         }
 
-        // Perform binary search until the boundaries converge
-        while (left < right) {
-            int middle = (left + right) / 2;
-            if (canDistribute(middle, quantities, n)) {
-                // Try for a smaller maximum
-                right = middle;
-            } else {
-                // Increase the minimum possible maximum
-                left = middle + 1;
-            }
+        int base = sum / n;
+        int odd = sum % n;
+        
+        int ans = base + (odd == 0 ? 0 : 1);
+
+        System.out.println(Arrays.toString(q));
+
+        System.out.println(String.format("sum:%d, base:%d, odd:%d, ans:%d, min:%d", sum, base, odd, ans, min));
+
+        if (ans > min) {
+            int count = counter.get(min);
+            return ans + (ans - min) / count;
         }
-        return left;
+
+        return ans;
     }
 }
