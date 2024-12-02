c1ba89d440b5f0af7c2fa54e198c9a962d2f5f2e
team-hungrian <def.hunger@gmail.com>
2024-12-02 23:34:08 +0900

    Time: 0 ms (100%), Space: 41.5 MB (27.01%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -1,6 +1,22 @@
 class Solution {
     public int isPrefixOfWord(String sentence, String searchWord) {
-        return mySol(sentence, searchWord);
+        return mySol_trie(sentence, searchWord);
+    }
+
+    public int mySol_trie(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+
+        Trie t = new Trie();
+
+        for (int i = 0; i < words.length; i++) {
+            t.add(words[i]);
+
+            if (t.find(searchWord)) {
+                return i + 1;
+            }
+        }
+
+        return -1;
     }
 
     public int mySol(String sentence, String searchWord) {
@@ -14,5 +30,34 @@ class Solution {
         return -1;
     }
 
+    class Trie {
+        Trie[] children = new Trie[26];
+        
+        public void add(String word) {
+            Trie t = this;
+
+            for (char c : word.toCharArray()) {
+                if (t.children[c - 'a'] == null) {
+                    t.children[c - 'a'] = new Trie();
+                }
+
+                t = t.children[c - 'a'];
+            }
+        }
+
+        public boolean find(String search) {
+            Trie t = this;
+
+            for (char c : search.toCharArray()) {
+                if (t.children[c - 'a'] == null) {
+                    return false;
+                }
+
+                t = t.children[c - 'a'];
+            }
+
+            return true;
+        }
+    }
 
 }
