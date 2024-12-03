5e17a1dd2d224babe226962bfc762772e70f76d3
team-hungrian <def.hunger@gmail.com>
2024-12-03 14:17:34 +0900

    Time: 22 ms (85.57%), Space: 79.5 MB (73.74%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.java
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.java
@@ -1,6 +1,22 @@
 class Solution {
     public String addSpaces(String s, int[] spaces) {
-        return mySol(s, spaces);
+        return official_two_pointers(s, spaces);
+    }
+
+    public String official_two_pointers(String s, int[] spaces) {
+        StringBuilder ans = new StringBuilder();
+        int spaceIndex = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (spaceIndex < spaces.length && spaces[spaceIndex] == i) {
+                ans.append(' ');
+                spaceIndex++;
+            }
+
+            ans.append(s.charAt(i));
+        }
+
+        return ans.toString();
     }
 
     public String mySol(String s, int[] spaces) {
