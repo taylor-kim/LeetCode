34317094fe7b95bf72a2ae1650a4464f5540feff
team-hungrian <def.hunger@gmail.com>
2024-12-07 16:24:26 +0900

    Time: 24 ms (74.07%), Space: 57.4 MB (42.81%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -1,6 +1,31 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
-        return mySol2(nums, lower, upper);
+        return official_good_bs(nums, lower, upper);
+    }
+
+    public long official_good_bs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return lower_bound(nums, upper + 1) - lower_bound(nums, lower);
+    }
+
+    // Calculate the number of pairs with sum less than `value`.
+    private long lower_bound(int[] nums, int value) {
+        int left = 0, right = nums.length - 1;
+        long result = 0;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            // If sum is less than value, add the size of window to result and move to the
+            // next index.
+            if (sum < value) {
+                result += (right - left);
+                left++;
+            } else {
+                // Otherwise, shift the right pointer backwards, until we get a valid window.
+                right--;
+            }
+        }
+
+        return result;
     }
 
     public long mySol2(int[] nums, int lower, int upper) {
