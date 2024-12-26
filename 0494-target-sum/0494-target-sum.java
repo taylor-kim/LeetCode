d5b84cec17e092210ce286866baefb979e16b84e
team-hungrian <def.hunger@gmail.com>
2024-12-26 15:20:50 +0900

    Time: 7 ms (74.15%), Space: 45 MB (24.62%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -4,15 +4,21 @@ class Solution {
     }
 
     public int mySol_bf(int[] nums, int target) {
-        return topdown(nums, 0, target);
+        return topdown(nums, 0, target, new Integer[nums.length][3001]);
     }
 
-    private int topdown(int[] nums, int index, int target) {
+    private int topdown(int[] nums, int index, int target, Integer[][] memo) {
         if (index >= nums.length) return target == 0 ? 1 : 0;
 
-        int plus = topdown(nums, index + 1, target + nums[index]);
-        int minus = topdown(nums, index + 1, target - nums[index]);
+        int shift = 1000;
 
-        return plus + minus;
+        if (memo[index][target + shift] != null) {
+            return memo[index][target + shift];
+        }
+
+        int plus = topdown(nums, index + 1, target + nums[index], memo);
+        int minus = topdown(nums, index + 1, target - nums[index], memo);
+
+        return memo[index][target + shift] = plus + minus;
     }
 }
