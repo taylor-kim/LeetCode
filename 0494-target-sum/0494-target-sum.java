aae9759de60ff312e05e2c8cbe35b701fe142ee7
team-hungrian <def.hunger@gmail.com>
2024-12-26 17:48:50 +0900

    Time: 15 ms (56.16%), Space: 44.8 MB (31.17%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,6 +1,40 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return mySol_bottomup_fail_and_gpt(nums, target);
+        return try_space_opt(nums, target);
+    }
+
+    public int try_space_opt(int[] nums, int target) {
+        int max = Math.abs(target);
+
+        for (int num : nums) max += num;
+
+        int[] dp = new int[max * 2 + 1];
+
+        dp[max] = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int[] newDp = new int[max * 2 + 1];
+
+            for (int sum = -max; sum <= max; sum++) {
+                if (sum + nums[i] + max >= 0 && sum + nums[i] + max <= 2 * max) {
+                    newDp[sum + max] += dp[sum + nums[i] + max];
+                }
+
+                if (sum - nums[i] + max >= 0 && sum - nums[i] + max <= 2 * max) {
+                    newDp[sum + max] += dp[sum - nums[i] + max];
+                }
+            }
+
+            dp = newDp;
+        }
+
+        // for (int[] row : dp) {
+        //     System.out.println(Arrays.toString(row));
+        // }
+
+        // System.out.println(String.format("target:%d, max:%d, j:%d", target, max, target + max));
+
+        return dp[target + max];
     }
 
     public int mySol_bottomup_fail_and_gpt(int[] nums, int target) {
