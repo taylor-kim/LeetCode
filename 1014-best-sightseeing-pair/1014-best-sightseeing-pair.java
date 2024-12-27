737539a4d8532d0601d3c3b5c0096b48e9fffe4e
team-hungrian <def.hunger@gmail.com>
2024-12-27 17:32:38 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -0,0 +1,84 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+        return mySol2_bf(values);
+    }
+
+    public int mySol2_bf(int[] values) {
+        int[][] sorted = new int[values.length][2];
+
+        for (int i = 0; i < values.length; i++) {
+            sorted[i][0] = values[i];
+            sorted[i][1] = i;
+        }
+
+        Arrays.sort(sorted, (a, b) -> {
+            return a[0] - b[0];
+        });
+
+        int n = sorted.length;
+
+        return topdown(sorted, 0, n - 1, new Integer[n][n]);
+    }
+
+    private int topdown(int[][] sorted, int left, int right, Integer[][] memo) {
+        if (left >= right) return Integer.MIN_VALUE;
+
+        if (memo[left][right] != null) return memo[left][right];
+
+        int ans = getScore(sorted, left, right);
+
+        ans = Math.max(ans, topdown(sorted, left + 1, right, memo));
+        ans = Math.max(ans, topdown(sorted, left, right - 1, memo));
+
+        return memo[left][right] = ans;
+    }
+
+    public int mySol_fail(int[] values) {
+        // index : 1,3,2,4,0
+        // value : 1,2,5,6,8
+        //
+
+        // 1, 8 => 1 + 8 + 0 - 1 = 8
+        // 2, 8 => 2 + 8 + 0 - 3 = 7
+            // 1, 6 => 1 + 6 + 1 - 4 = 4
+        // 5 + 8 + 0 - 2 = 11
+            // 2 + 6 + 3 - 4 = 7
+        // 5 + 6 + 2 - 4 = 9
+        // 6 + 8 + 0 - 4 = 10
+
+        int[][] sorted = new int[values.length][2];
+
+        for (int i = 0; i < values.length; i++) {
+            sorted[i][0] = values[i];
+            sorted[i][1] = i;
+        }
+
+        Arrays.sort(sorted, (a, b) -> {
+            return a[0] - b[0];
+        });
+
+        int ans = Integer.MIN_VALUE;
+
+        int left = 0;
+        int right = sorted.length - 1;
+
+        while (left < right) {
+            int score = getScore(sorted, left, right);
+            ans = Math.max(ans, score);
+
+            if (left + 1 == right) break;
+
+            if (getScore(sorted, left + 1, right) < getScore(sorted, left, right - 1)) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return ans;
+    }
+
+    private int getScore(int[][] arr, int left, int right) {
+        return arr[left][0] + arr[right][0] - Math.abs(arr[left][1] - arr[right][1]);
+    }
+}
