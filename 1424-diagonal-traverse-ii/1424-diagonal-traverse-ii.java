ea5a9ea7e772bd152a14be38dbf3eb7cfbba7d9d
team-hungrian <def.hunger@gmail.com>
2025-01-02 01:14:12 +0900

    Time: 29 ms (65.29%), Space: 76.1 MB (19.91%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
@@ -1,22 +1,47 @@
 class Solution {
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-        return mySol(nums);
+        return gpt(nums);
     }
 
-    public int[] mySol(List<List<Integer>> nums) {
+    public int[] gpt(List<List<Integer>> nums) {
+        Map<Integer, List<Integer>> map = new HashMap();
+        int n = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.get(i).size(); j++) {
+                map.computeIfAbsent(i + j, k -> new ArrayList()).add(nums.get(i).get(j));
+                n++;
+            }
+        }
+
+        int[] ans = new int[n];
+        int index = 0;
+
+        for (int key = 0; map.containsKey(key); key++) {
+            List<Integer> diagonal = map.get(key);
+
+            for (int i = diagonal.size() - 1; i >= 0; i--) {
+                ans[index++] = diagonal.get(i);
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] mySol_fail(List<List<Integer>> nums) {
         int n = nums.size();
         List<Integer> list = new ArrayList();
 
         int maxLength = 0;
 
         for (int i = 0; i < n; i++) {
-            add(i, 0, nums, list);
+            add(i, 0, nums, list, i + 1);
             maxLength = Math.max(maxLength, nums.get(i).size());
         }
 
         // for (int j = 1; j < nums.get(n - 1).size(); j++) {
         for (int j = 1; j < maxLength; j++) {
-            add(n - 1, j, nums, list);
+            add(n - 1, j, nums, list, maxLength);
         }
 
         int[] ans = new int[list.size()];
@@ -28,8 +53,8 @@ class Solution {
         return ans;
     }
 
-    private void add(int row, int col, List<List<Integer>> nums, List<Integer> ans) {
-        while (row >= 0) {
+    private void add(int row, int col, List<List<Integer>> nums, List<Integer> ans, int max) {
+        while (row >= 0 && col <= max) {
             if (col < nums.get(row).size()) {
                 ans.add(nums.get(row).get(col));
             }
