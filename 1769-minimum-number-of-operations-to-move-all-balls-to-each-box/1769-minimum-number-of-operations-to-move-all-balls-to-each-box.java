f28c261ac3d16fb5ba8abc43466474100dd677ba
team-hungrian <def.hunger@gmail.com>
2025-01-06 17:08:49 +0900

    Time: 27 ms (54.34%), Space: 45 MB (20.83%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,6 +1,21 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        return mySol(boxes);
+        return official_bruteforce(boxes);
+    }
+
+    public int[] official_bruteforce(String boxes) {
+        int n = boxes.length();
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (boxes.charAt(i) == '1') {
+                for (int j = 0; j < n; j++) {
+                    ans[j] += Math.abs(j - i);
+                }
+            }
+        }
+
+        return ans;
     }
 
     public int[] mySol(String boxes) {
