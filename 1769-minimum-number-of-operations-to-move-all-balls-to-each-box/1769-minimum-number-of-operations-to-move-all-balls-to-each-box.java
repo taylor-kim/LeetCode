ad37b8e68f4c63fe83b050928e8e83b04b323c9f
team-hungrian <def.hunger@gmail.com>
2025-01-06 16:56:37 +0900

    Time: 8 ms (56.25%), Space: 45.2 MB (11.46%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        return mySol(boxes);
+    }
+
+    public int[] mySol(String boxes) {
+        int n = boxes.length();
+        int[] ans = new int[n];
+
+        char[] arr = boxes.toCharArray();
+
+        int[][] toRight = new int[n][2];
+        int[][] toLeft = new int[n][2];
+
+        toRight[0][0] = arr[0] - '0';
+        toLeft[n - 1][0] = arr[n - 1] - '0';
+
+        for (int i = 1; i < n; i++) {
+            toRight[i][0] = toRight[i - 1][0] + (arr[i] - '0');
+            toRight[i][1] = toRight[i - 1][0] + toRight[i - 1][1];
+
+            toLeft[n - i - 1][0] = toLeft[n - i][0] + (arr[n - i - 1] - '0');
+            toLeft[n - i - 1][1] = toLeft[n - i][0] + toLeft[n - i][1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = toRight[i][1] + toLeft[i][1];
+        }
+
+        return ans;
+    }
+}
