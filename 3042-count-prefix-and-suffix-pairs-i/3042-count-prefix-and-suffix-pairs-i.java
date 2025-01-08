334e74058ea14f455ae4be6224c8e2b322f5f081
team-hungrian <def.hunger@gmail.com>
2025-01-08 20:45:10 +0900

    Time: 6 ms (6.51%), Space: 45.5 MB (5.56%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
@@ -1,6 +1,65 @@
 class Solution {
     public int countPrefixSuffixPairs(String[] words) {
-        return mySol(words);
+        return official_dual_trie(words);
+    }
+
+    public int official_dual_trie(String[] words) {
+        int ans = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            String a = words[i];
+
+            Trie prefix = new Trie();
+            Trie suffix = new Trie();
+
+            prefix.build(a);
+            suffix.build(new StringBuilder(a).reverse().toString());
+
+            for (int j = 0; j < i; j++) {
+                String b = words[j];
+
+                if (a.length() >= b.length() 
+                    && prefix.find(b)
+                    && suffix.find(new StringBuilder(b).reverse().toString())) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    class Trie {
+        Trie[] children = new Trie[26];
+        boolean isWord = false;
+
+        public void build(String s) {
+            Trie t = this;
+
+            for (char c : s.toCharArray()) {
+                if (t.children[c - 'a'] == null) {
+                    t.children[c - 'a'] = new Trie();
+                }
+
+                t = t.children[c - 'a'];
+            }
+
+            t.isWord = true;
+        }
+
+        public boolean find(String s) {
+            Trie t = this;
+
+            for (char c : s.toCharArray()) {
+                if (t.children[c - 'a'] == null) {
+                    return false;
+                }
+
+                t = t.children[c - 'a'];
+            }
+
+            return true;
+        }
     }
 
     public int mySol(String[] words) {
