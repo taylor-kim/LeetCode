010ce95b5242b4d5a27c156e9449cab3493c89ac
team-hungrian <def.hunger@gmail.com>
2025-01-09 16:19:27 +0900

    Time: 5 ms (6.21%), Space: 45.5 MB (5.1%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.java
@@ -1,6 +1,49 @@
 class Solution {
     public int prefixCount(String[] words, String pref) {
-        return mySol(words, pref);
+        return mySol_trie(words, pref);
+    }
+
+    public int mySol_trie(String[] words, String pref) {
+        int ans = 0;
+
+        for (String word : words) {
+            Trie trie = new Trie();
+            trie.build(word);
+
+            if (trie.find(pref)) ans++;
+        }
+
+        return ans;
+    }
+
+    class Trie {
+        Trie[] children = new Trie[26];
+
+        public void build(String s) {
+            Trie t = this;
+
+            for (char c : s.toCharArray()) {
+                if (t.children[c - 'a'] == null) {
+                    t.children[c - 'a'] = new Trie();
+                }
+
+                t = t.children[c - 'a'];
+            }
+        }
+
+        public boolean find(String s) {
+            Trie t = this;
+
+            for (char c : s.toCharArray()) {
+                if (t.children[c - 'a'] == null) {
+                    return false;
+                }
+
+                t = t.children[c - 'a'];
+            }
+
+            return true;
+        }
     }
 
     public int mySol(String[] words, String pref) {
