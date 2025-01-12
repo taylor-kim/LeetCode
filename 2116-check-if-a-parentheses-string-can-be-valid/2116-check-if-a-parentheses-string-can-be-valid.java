bb02e204b6b8ec82b4bd9701e6744f9a79f75dee
team-hungrian <def.hunger@gmail.com>
2025-01-12 22:01:00 +0900

    Time: 15 ms (83.4%), Space: 45.6 MB (75.93%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -1,6 +1,80 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
-        return official_space_opt(s, locked);
+        return others_onepass(s, locked);
+    }
+
+    // https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/submissions/1506022118/
+    public boolean others_onepass(String s, String locked) {
+        int n = s.length();
+
+        if ((n & 1) == 1) return false;
+
+        int l = 0;
+        int r = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(' || locked.charAt(i) == '0') {
+                r++;
+            } else {
+                r--;
+            }
+
+            if (s.charAt(i) == ')' || locked.charAt(i) == '0') {
+                l--;
+            } else {
+                l++;
+            }
+
+            if (r < 0) return false;
+
+            l = Math.max(l, 0);
+        }
+
+        return l == 0;
+    }
+
+    public boolean try_space_opt(String s, String locked) {
+        int n = s.length();
+
+        if (n % 2 == 1) return false;
+
+        int open = 0;
+        int joker = 0;
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
+            if (locked.charAt(i) == '0') {
+                joker++;
+            } else if (c == '(') {
+                open++;
+            } else {
+                if (open > 0) {
+                    open--;
+                } else if (joker > 0) {
+                    joker--;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        open = 0;
+        int close = 0;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (locked.charAt(i) == '0') {
+                close++;
+            } else if (s.charAt(i) == '(') {
+                open++;
+            } else {
+                close++;
+            }
+
+            if (open > close) return false;
+        }
+
+        return true;
     }
 
     public boolean official_space_opt(String s, String locked) {
