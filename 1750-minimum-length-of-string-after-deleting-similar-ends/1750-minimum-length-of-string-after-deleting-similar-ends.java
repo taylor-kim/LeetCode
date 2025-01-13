63f8e5fb3a6b552280ffd43be2e175e68608e178
team-hungrian <def.hunger@gmail.com>
2025-01-13 11:49:18 +0900

    Time: 4 ms (78.61%), Space: 45.3 MB (58.8%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,6 +1,25 @@
 class Solution {
     public int minimumLength(String s) {
-        return try_20250113(s);
+        return official_two_pointer(s);
+    }
+
+    public int official_two_pointer(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            char c = s.charAt(left);
+            
+            while (left <= right && s.charAt(left) == c) {
+                left++;
+            }
+            
+            while (left <= right && s.charAt(right) == c) {
+                right--;
+            }
+        }
+        
+        return right - left + 1;
     }
 
     public int try_20250113(String s) {
@@ -9,11 +28,11 @@ class Solution {
         int right = n - 1;
 
         while (left < right && s.charAt(left) == s.charAt(right)) {
-            while (left + 1 < n && s.charAt(left) == s.charAt(left + 1)) {
+            while (left + 1 < right && s.charAt(left) == s.charAt(left + 1)) {
                 left++;
             }
 
-            while (right - 1 >= 0 && s.charAt(right - 1) == s.charAt(right)) {
+            while (right - 1 > left && s.charAt(right - 1) == s.charAt(right)) {
                 right--;
             }
 
