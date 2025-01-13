933bddd839d90c3d2ed9e89ee480ae73307c1e6e
team-hungrian <def.hunger@gmail.com>
2025-01-13 11:39:38 +0900

    Time: 8 ms (8.72%), Space: 45.4 MB (43.26%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -1,73 +1,26 @@
 class Solution {
     public int minimumLength(String s) {
-        return official_rec(s);
+        return try_20250113(s);
     }
-    
-    public int official_rec(String s) {
-        return official_rec(s, 0, s.length() - 1);
-    }
-    
-    public int official_rec(String s, int left, int right) {
-        if (left >= right || s.charAt(left) != s.charAt(right)) {
-            return right - left + 1;
-        }
-        
-        char c = s.charAt(left);
-        
-        while (left <= right && s.charAt(left) == c) {
-            left++;
-        }
-        
-        while (left < right && s.charAt(right) == c) {
-            right--;
-        }
-        
-        return official_rec(s, left, right);
-    }
-    
-    public int official_two_pointer(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-        
-        while (left < right && s.charAt(left) == s.charAt(right)) {
-            char c = s.charAt(left);
-            
-            while (left <= right && s.charAt(left) == c) {
-                left++;
-            }
-            
-            while (left < right && s.charAt(right) == c) {
-                right--;
-            }
-        }
-        
-        return right - left + 1;
-    }
-    
-    public int mySol(String s) {
+
+    public int try_20250113(String s) {
         int n = s.length();
-        int removed = 0;
-        
         int left = 0;
         int right = n - 1;
-        
-        while (left < right) {
-            char lc = s.charAt(left);
-            char rc = s.charAt(right);
-            
-            if (lc != rc) break;
-            
-            removed += 2;
-            
-            while (++left < right && lc == s.charAt(left)) {
-                removed++;
+
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            while (left + 1 < n && s.charAt(left) == s.charAt(left + 1)) {
+                left++;
             }
-            
-            while (--right > left && rc == s.charAt(right)) {
-                removed++;
+
+            while (right - 1 >= 0 && s.charAt(right - 1) == s.charAt(right)) {
+                right--;
             }
+
+            left++;
+            right--;
         }
-        
-        return n - removed;
+
+        return Math.max(0, right - left + 1);
     }
 }
