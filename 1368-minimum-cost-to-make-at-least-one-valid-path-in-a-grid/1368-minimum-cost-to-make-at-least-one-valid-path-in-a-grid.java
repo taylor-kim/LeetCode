ed0dbf386f5994075db48cf5a60bbd0159703e05
team-hungrian <def.hunger@gmail.com>
2025-01-19 01:31:19 +0900

    Time: 22 ms (55.11%), Space: 45 MB (66.06%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,6 +1,55 @@
 class Solution {
     public int minCost(int[][] grid) {
-        return mySol(grid);
+        return official_dijkstra(grid);
+    }
+
+    public int official_dijkstra(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        Queue<int[]> queue = new PriorityQueue<>((a, b) -> {
+            return a[2] - b[2];
+        });
+
+        int[][] minCosts = new int[m][n];
+
+        for (int[] row : minCosts) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+
+        minCosts[0][0] = 0;
+
+        queue.add(new int[] {0,0,0});
+
+        int[][] dirs = {
+            {0, 1},
+            {0, -1},
+            {1, 0},
+            {-1, 0}
+        };
+
+        while (!queue.isEmpty()) {
+            int[] data = queue.poll();
+
+            int y = data[0];
+            int x = data[1];
+            int cost = data[2];
+
+            // if (minCosts[y][x] < cost) continue;
+
+            for (int i = 0; i < dirs.length; i++) {
+                int ny = y + dirs[i][0];
+                int nx = x + dirs[i][1];
+                int newCost = cost + (grid[y][x] - 1 == i ? 0 : 1);
+
+                if (ny >= 0 && ny < grid.length && nx >= 0 && nx < grid[0].length && minCosts[ny][nx] > newCost) {
+                    minCosts[ny][nx] = newCost;
+                    queue.add(new int[] {ny, nx, newCost});
+                }
+            }
+        }
+
+        return minCosts[m - 1][n - 1];
     }
 
     public int mySol(int[][] grid) {
