a37d04fada83f326a7323f8bf44ffb595a7538bd
team-hungrian <def.hunger@gmail.com>
2025-01-20 23:07:23 +0900

    Time: 27 ms (49.25%), Space: 65.9 MB (64.18%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
@@ -1,6 +1,35 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
-        return mySol(arr, mat);
+        return official_counting(arr, mat);
+    }
+
+    public int official_counting(int[] arr, int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+
+        Map<Integer, int[]> map = new HashMap();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                map.put(mat[i][j], new int[] {i, j});
+            }
+        }
+
+        for (int i = 0; i < m * n; i++) {
+            int[] yx = map.get(arr[i]);
+
+            rowCount[yx[0]]++;
+            colCount[yx[1]]++;
+
+            if (rowCount[yx[0]] == n || colCount[yx[1]] == m) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     public int mySol(int[] arr, int[][] mat) {
