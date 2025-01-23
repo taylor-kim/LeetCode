bb5e3590ec0c24e3b04e92175415bc528d028fb6
team-hungrian <def.hunger@gmail.com>
2025-01-24 03:34:18 +0900

    Time: 5 ms (22.66%), Space: 51.4 MB (86.41%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -1,171 +1,66 @@
+class DSU {
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> rank = new ArrayList<>();
+    DSU(int n){ 
+        for(int i = 0;i <= n;i++){
+            rank.add(0);
+            parent.add(i);
+        }
+    }
+    public int findUPar(int node) {
+        if (node == parent.get(node)) {
+            return node;
+        }
+        parent.set(node,findUPar(parent.get(node)));
+        return parent.get(node);
+    }
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+}
 class Solution {
     public int countServers(int[][] grid) {
-        return official_server_grouping(grid);
-    }
-
-    public int try_disjointset_fail(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        UnionFind uf = new UnionFind(m * n);
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int index = i * n + j;
-
-                if (grid[i][j] == 1) {
-                    uf.merge(i, index);
-                    uf.merge(j, index);
+        int n = grid.length;
+        int m = grid[0].length;
+        DSU ds = new DSU(n + m);
+        for(int i=0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
+                if(grid[i][j] == 1){
+                int u = i;
+                int v = j + n;
+                ds.unionByRank(u,v);
                 }
             }
         }
-
-        return uf.getComponentCount();
-    }
-
-    public class UnionFind {
-        int[] parents;
-        // int componentCount;
-
-        public UnionFind(int n) {
-            parents = new int[n];
-            // componentCount = n;
-
-            for (int i = 0; i < n; i++) parents[i] = i;
+        int size[] = new int[n+m];
+        int cntservers = 0;
+        for(int i = 0;i < n;i++){
+           for(int j = 0;j < m;j++){
+              if(grid[i][j] == 1)
+              {
+                int par = ds.findUPar(i);
+                size[par]++;
+              }
+           }
         }
-
-        public int find(int n) {
-            if (parents[n] != n) {
-                parents[n] = find(parents[n]);
-            }
-
-            return parents[n];
-        }
-
-        public void merge(int p, int c) {
-            p = find(p);
-            c = find(c);
-
-            if (p != c) {
-                System.out.println(String.format("merge : %d <= %d", p, c));
-                parents[c] = p;
-                // componentCount--;
+        for(int i = 0;i < (n + m);i++){
+            if(size[i] > 1){
+                cntservers += size[i];
             }
         }
+        
 
-        public int getComponentCount() {
-            Map<Integer, Integer> map = new HashMap();
-
-            System.out.println(Arrays.toString(parents));
-
-            for (int i = 0; i < parents.length; i++) {
-                map.put(find(i), map.getOrDefault(find(i), 0) + 1);
-            }
-
-            System.out.println(map);
-
-            int componentCount = 0;
-
-            for (int count : map.values()) {
-                if (count > 1) componentCount++;
-            }
-
-            return componentCount;
-        }
-    }
-
-    public int official_server_grouping(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        int[] serverCountInCol = new int[n];
-        int[] lastColInRow = new int[m];
-        Arrays.fill(lastColInRow, -1);
-
-        int ans = 0;
-
-        for (int i = 0; i < m; i++) {
-            int serverCountInRow = 0;
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    serverCountInRow++;
-                    serverCountInCol[j]++;
-                    lastColInRow[i] = j;
-                }
-            }
-
-            if (serverCountInRow > 1) {
-                ans += serverCountInRow;
-                lastColInRow[i] = -1;
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            if (lastColInRow[i] != -1 && serverCountInCol[lastColInRow[i]] > 1) {
-                ans++;
-            }
-        }
-
-        return ans;
-    }
-
-    public int official_track_with_two_arrays(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        int[] numberOfRow = new int[m];
-        int[] numberOfCol = new int[n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    numberOfRow[i]++;
-                    numberOfCol[j]++;
-                }
-            }
-        }
-
-        int ans = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1
-                    && (numberOfRow[i] > 1 || numberOfCol[j] > 1)) {
-                    ans++;
-                }
-            }
-        }
-
-        return ans;
-    }
-
-    public int mySol(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        int[] numberOfRow = new int[m];
-        int[] numberOfCol = new int[n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    numberOfRow[i]++;
-                    numberOfCol[j]++;
-                }
-            }
-        }
-
-        Set<Integer> set = new HashSet();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1
-                    && (numberOfRow[i] > 1 || numberOfCol[j] > 1)) {
-                    set.add(i * n + j);
-                }
-            }
-        }
-
-        return set.size();
+        return cntservers;
     }
 }
