ea288f32f8c2a2c4b96dcddde59b930eeb0cf4cb
team-hungrian <def.hunger@gmail.com>
2025-01-24 01:25:41 +0900

    Time: 2 ms (100%), Space: 52.9 MB (12.99%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -1,6 +1,36 @@
 class Solution {
     public int countServers(int[][] grid) {
-        return mySol(grid);
+        return official_track_with_two_arrays(grid);
+    }
+
+    public int official_track_with_two_arrays(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] numberOfRow = new int[m];
+        int[] numberOfCol = new int[n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    numberOfRow[i]++;
+                    numberOfCol[j]++;
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1
+                    && (numberOfRow[i] > 1 || numberOfCol[j] > 1)) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
     }
 
     public int mySol(int[][] grid) {
