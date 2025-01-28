4511d897a7005bc9af479a14c64d77e94e682eb4
team-hungrian <def.hunger@gmail.com>
2025-01-28 19:18:43 +0900

    Time: 5 ms (65.83%), Space: 45.6 MB (7.56%) - LeetHub

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
@@ -0,0 +1,76 @@
+class Solution {
+    public int findMaxFish(int[][] grid) {
+        return mySol(grid);
+    }
+
+    public int mySol(int[][] grid) {
+        int[][] dirs = {
+            {0, 1},
+            {0, -1},
+            {1, 0},
+            {-1, 0}
+        };
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        UnionFind uf = new UnionFind(m * n);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] > 0) {
+                    int index = i * n + j;
+                    uf.scores[index] = grid[i][j];
+                    grid[i][j] = -grid[i][j];
+                    for (int[] d : dirs) {
+                        int ny = i + d[0];
+                        int nx = j + d[1];
+
+                        if (ny >= 0 && nx >= 0 && ny < m && nx < n && grid[ny][nx] < 0) {
+                            int adjIndex = ny * n + nx;
+                            uf.merge(adjIndex, index);
+                        }
+                    }
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for (int score : uf.scores) {
+            ans = Math.max(ans, score);
+        }
+
+        return ans;
+    }
+
+    class UnionFind {
+        int[] parents;
+        int[] scores;
+
+        public UnionFind(int n) {
+            parents = new int[n];
+            scores = new int[n];
+
+            for (int i = 0; i < n; i++) parents[i] = i;
+        }
+
+        public int find(int i) {
+            if (parents[i] != i) {
+                parents[i] = find(parents[i]);
+            }
+
+            return parents[i];
+        }
+
+        public void merge(int a, int b) {
+            a = find(a);
+            b = find(b);
+
+            if (a != b) {
+                parents[b] = a;
+                scores[a] += scores[b];
+            }
+        }
+    }
+}
