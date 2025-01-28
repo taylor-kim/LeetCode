36ac953eea341698f3cc164514a1290eea525a01
team-hungrian <def.hunger@gmail.com>
2025-01-28 18:46:53 +0900

    Time: 78 ms (25.38%), Space: 48 MB (69.1%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -1,6 +1,50 @@
 class Solution {
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
-        return mySol(numCourses, prerequisites, queries);
+        return official_topological_sort(numCourses, prerequisites, queries);
+    }
+
+    public List<Boolean> official_topological_sort(int numCourses, int[][] prerequisites, int[][] queries) {
+        Map<Integer, List<Integer>> graph = new HashMap();
+        int[] indegree = new int[numCourses];
+
+        for (int[] edge : prerequisites) {
+            graph.computeIfAbsent(edge[0], k -> new ArrayList()).add(edge[1]);
+            indegree[edge[1]]++;
+        }
+        
+        Queue<Integer> queue = new LinkedList();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        Map<Integer, Set<Integer>> nodePrerequisites = new HashMap();
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+
+            for (int next : graph.getOrDefault(node, new ArrayList<>())) {
+                nodePrerequisites.computeIfAbsent(next, k -> new HashSet()).add(node);
+
+                for (int preOfNode : nodePrerequisites.getOrDefault(node, new HashSet<>())) {
+                    nodePrerequisites.computeIfAbsent(next, k -> new HashSet()).add(preOfNode);
+                }
+
+                if (--indegree[next] == 0) {
+                    queue.add(next);
+                }
+            }
+        }
+
+        List<Boolean> ans = new ArrayList();
+
+        for (int[] query : queries) {
+            ans.add(nodePrerequisites.getOrDefault(query[1], new HashSet()).contains(query[0]));
+        }
+
+        return ans;
     }
 
     public List<Boolean> mySol(int numCourses, int[][] prerequisites, int[][] queries) {
