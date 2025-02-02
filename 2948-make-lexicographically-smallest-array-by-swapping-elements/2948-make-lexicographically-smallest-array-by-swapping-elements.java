bcc983601f39870435473a0c267ea9b4f10b8e75
team-hungrian <def.hunger@gmail.com>
2025-02-03 01:06:16 +0900

    Time: 127 ms (12.08%), Space: 79.2 MB (16.07%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,6 +1,44 @@
 class Solution {
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
-        return tryAgain_20250203(nums, limit);
+        return official_sort_and_bidir_mapping(nums, limit);
+    }
+
+    public int[] official_sort_and_bidir_mapping(int[] nums, int limit) {
+        int n = nums.length;
+        Integer[][] arr = new Integer[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            return a[0] - b[0];
+        });
+
+        Map<Integer, LinkedList<Integer[]>> groups = new HashMap();
+        Map<Integer, Integer> numToGroup = new HashMap();
+
+        int groupId = 0;
+
+        groups.computeIfAbsent(groupId, k -> new LinkedList()).add(arr[0]);
+        numToGroup.put(arr[0][0], groupId);
+
+        for (int i = 1; i < n; i++) {
+            if (Math.abs(arr[i - 1][0] - arr[i][0]) > limit) {
+                groupId++;
+            }
+            groups.computeIfAbsent(groupId, k -> new LinkedList()).add(arr[i]);
+            numToGroup.put(arr[i][0], groupId);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            groupId = numToGroup.get(num);
+            nums[i] = groups.get(groupId).pollFirst()[0];
+        }
+
+        return nums;
     }
 
     public int[] tryAgain_20250203(int[] nums, int limit) {
