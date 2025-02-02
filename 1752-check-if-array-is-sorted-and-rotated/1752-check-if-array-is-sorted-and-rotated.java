ee80e45b0d57d17d5cde7b79fc56840fe2090bb5
team-hungrian <def.hunger@gmail.com>
2025-02-02 16:38:27 +0900

    Time: 0 ms (100%), Space: 41.5 MB (27.27%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,6 +1,25 @@
 class Solution {
     public boolean check(int[] nums) {
-        return mySol2(nums);
+        return official_linear(nums);
+    }
+
+    public boolean official_linear(int[] nums) {
+        int n = nums.length;
+        if (n <= 1) return true;
+
+        int inversionCount = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                inversionCount++;
+            }
+        }
+
+        if (nums[0] < nums[n - 1]) {
+            inversionCount++;
+        }
+
+        return inversionCount <= 1;
     }
 
     public boolean mySol2(int[] nums) {
