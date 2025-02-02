ba3df5a1d5bef64c35f4d333476e86adc2f3917d
team-hungrian <def.hunger@gmail.com>
2025-02-02 16:23:30 +0900

    Time: 0 ms (100%), Space: 41.6 MB (27.27%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,6 +1,27 @@
 class Solution {
     public boolean check(int[] nums) {
-        return mySol(nums);
+        return mySol2(nums);
+    }
+
+    public boolean mySol2(int[] nums) {
+        int index = 0;
+        int min = nums[index++];
+
+        while (index < nums.length && nums[index - 1] <= nums[index]) {
+            index++;
+        }
+
+        if (index < nums.length && min < nums[index]) {
+            return false;
+        }
+
+        index++;
+
+        while (index < nums.length && nums[index - 1] <= nums[index] && nums[index] <= min) {
+            index++;
+        }
+
+        return index >= nums.length;
     }
 
     public boolean mySol(int[] nums) {
