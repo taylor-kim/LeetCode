385fc37f6935a08b74630b3d0849395c7ba62455
team-hungrian <def.hunger@gmail.com>
2025-02-13 19:28:54 +0900

    Time: 149 ms (62.14%), Space: 65.6 MB (100%) - LeetHub

--- 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.java
+++ 3066-minimum-operations-to-exceed-threshold-value-ii/3066-minimum-operations-to-exceed-threshold-value-ii.java
@@ -1,6 +1,29 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        return mySol(nums, k);
+        return others_int(nums, k);
+    }
+
+    public int others_int(int[] nums, int k) {
+        Queue<Integer> pq = new PriorityQueue();
+
+        for (int num : nums) pq.add(num);
+
+        int ans = 0;
+
+        while (pq.size() >= 2 && pq.peek() < k) {
+            int x = pq.poll();
+            int y = pq.poll();
+
+            if (x >= (Integer.MAX_VALUE  - y) / 2) {
+                pq.add(k);
+            } else {
+                pq.add(Math.min(x, y) * 2 + Math.max(x, y));
+            }
+
+            ans++;
+        }
+
+        return ans;
     }
 
     public int mySol(int[] nums, int k) {
