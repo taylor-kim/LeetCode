b7fefc600d00ca3b6657712edacc1f74506b6d1d
team-hungrian <def.hunger@gmail.com>
2025-02-17 20:51:41 +0900

    Time: 12 ms (54.19%), Space: 44.9 MB (53.52%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -7,34 +7,67 @@ class Solution {
         char[] arr = tiles.toCharArray();
         Arrays.sort(arr);
 
-        Set<String> set = new HashSet();
+        Set<Long> set = new HashSet();
 
-        topdown(arr, 0, new boolean[arr.length], new StringBuilder(), set);
+        topdown(arr, 0, new boolean[arr.length], 0L, set);
+
+        // System.out.println(set);
+
+        // StringBuilder sb = new StringBuilder();
+        // long mask = (int)Math.pow(2, 5) - 1;
+
+        // System.out.println(String.format("mask:%d", mask));
+
+        // for (long num : set) {
+            
+        //     while (num > 0) {
+        //         int digit = (int)(num & mask);
+        //         num = num >> 5;
+
+        //         if (digit == 0) continue;
+
+        //         char c = (char)(digit + 'A' - 1);
+        //         sb.append(c);
+        //     }
+
+        //     System.out.println(sb.reverse().toString());
+        //     sb.setLength(0);
+        // }
 
         return set.size();
     }
 
-    private void topdown(char[] arr, int index, boolean[] visit, StringBuilder sb, Set<String> set) {
+    private void topdown(char[] arr, int index, boolean[] visit, long seq, Set<Long> set) {
         if (index >= arr.length) return;
 
         if (visit[index]) return;
 
-        sb.append(arr[index]);
+        long mask = (arr[index] - 'A' + 1);
 
-        if (set.add(sb.toString())) {
+        long seqNext = seq;
+
+        if (seqNext != 0) {
+            seqNext <<= 5;
+        }
+
+        seqNext |= mask;
+
+        if (set.add(seqNext)) {
             visit[index] = true;
 
             for (int i = 0; i < arr.length; i++) {
                 if (visit[i]) continue;
 
-                topdown(arr, i, visit, sb, set);
+                topdown(arr, i, visit, seqNext, set);
             }
 
             visit[index] = false;
         }
 
-        sb.setLength(sb.length() - 1);
+        topdown(arr, index + 1, visit, seq, set);
+    }
 
-        topdown(arr, index + 1, visit, sb, set);
+    private String bs(long num) {
+        return Long.toBinaryString(num);
     }
 }
