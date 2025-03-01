f34b70247e9f49526c881581ca26a6d8e5b037b6
team-hungrian <def.hunger@gmail.com>
2025-03-01 15:38:07 +0900

    Time: 1 ms (96.37%), Space: 43.9 MB (34.48%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -1,6 +1,28 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
-        return try_onepass(nums);
+        return official_onepass(nums);
+    }
+
+    public int[] official_onepass(int[] nums) {
+        int writeIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 < nums.length && nums[i] == nums[i + 1] && nums[i] != 0) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+
+            if (nums[i] != 0) {
+                if (i != writeIndex) {
+                    int temp = nums[i];
+                    nums[i] = nums[writeIndex];
+                    nums[writeIndex] = temp;
+                }
+                writeIndex++;
+            }
+        }
+
+        return nums;
     }
 
     public int[] try_onepass(int[] nums) {
