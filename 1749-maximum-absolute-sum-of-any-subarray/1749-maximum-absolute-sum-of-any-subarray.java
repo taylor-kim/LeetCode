0d64d7e2253396170677cfd8f3ff347b7bb31f62
team-hungrian <def.hunger@gmail.com>
2025-03-03 18:34:25 +0900

    Time: 11 ms (10.29%), Space: 56 MB (96.32%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
@@ -1,103 +1,44 @@
 class Solution {
     public int maxAbsoluteSum(int[] nums) {
-        return official2(nums);
+        return tryAgain_20250303(nums);
     }
 
-    public int official2(int[] nums) {
-        int minPrefixSum = 0, maxPrefixSum = 0;
-        int prefixSum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            prefixSum += nums[i];
-
-            minPrefixSum = Math.min(minPrefixSum, prefixSum);
-            maxPrefixSum = Math.max(maxPrefixSum, prefixSum);
-        }
-
-        return maxPrefixSum - minPrefixSum;
-    }
-
-    public int official1(int[] nums) {
-        int minPrefixSum = Integer.MAX_VALUE, maxPrefixSum = Integer.MIN_VALUE;
-        int prefixSum = 0, maxAbsSum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            // Prefix sum from index 0 to i
-            prefixSum += nums[i];
-
-            // Minimum & Maximum prefix sum we have seen so far
-            minPrefixSum = Math.min(minPrefixSum, prefixSum);
-            maxPrefixSum = Math.max(maxPrefixSum, prefixSum);
-
-            if (prefixSum >= 0) {
-                // If the prefixSum is positive, we will get the difference between prefixSum &
-                // minPrefixSum
-                maxAbsSum = Math.max(
-                    maxAbsSum,
-                    Math.max(prefixSum, prefixSum - minPrefixSum)
-                );
-            } else if (prefixSum <= 0) {
-                // If the prefixSum is negative, we will get the absolute difference between
-                // prefixSum & maxPrefixSum
-                maxAbsSum = Math.max(
-                    maxAbsSum,
-                    Math.max(
-                        Math.abs(prefixSum),
-                        Math.abs(prefixSum - maxPrefixSum)
-                    )
-                );
-            }
-        }
-
-        return maxAbsSum;
-    }
-
-    public int mySol2_fail(int[] nums) {
+    public int tryAgain_20250303(int[] nums) {
         int n = nums.length;
+        int[] dpNeg = new int[n];
+        int[] dpPos = new int[n];
 
-        int[] pSum = new int[n + 1];
-        int[] negSum = new int[n + 1];
-        int[] posSum = new int[n + 1];
+        int sum = nums[0];
 
-        int ans = 0;
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-
-            pSum[i + 1] = pSum[i] + num;
-            negSum[i + 1] = negSum[i];
-            posSum[i + 1] = posSum[i];
-            
-            if (num < 0) {
-                negSum[i + 1] += num;
-            } else {
-                posSum[i + 1] += num;
-            }
+        if (nums[0] < 0) {
+            dpNeg[0] = nums[0];
+        } else {
+            dpPos[0] = nums[0];
         }
 
-        System.out.println(Arrays.toString(negSum));
-        System.out.println(Arrays.toString(posSum));
-        System.out.println(Arrays.toString(pSum));
+        int ans = Math.abs(nums[0]);
 
-        return ans;
-    }
-
-
-    public int mySol_fail(int[] nums) {
-        int ans = 0;
-        int maxSum = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-
-            maxSum += num;
-
-            if (Math.abs(maxSum) < Math.abs(num)) {
-                maxSum = num;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < 0) {
+                if (dpNeg[i - 1] != 0) {
+                    dpNeg[i] = nums[i] + dpNeg[i - 1];
+                } else {
+                    dpNeg[i] = nums[i];
+                }
+                dpPos[i] = Math.max(dpPos[i - 1] + nums[i], 0);
+            } else {
+                if (dpPos[i - 1] != 0) {
+                    dpPos[i] = nums[i] + dpPos[i - 1];
+                } else {
+                    dpPos[i] = nums[i];
+                }
+                dpNeg[i] = Math.min(dpNeg[i - 1] + nums[i], 0);
             }
 
-            ans = Math.max(ans, Math.abs(maxSum));
+            // System.out.println(Arrays.toString(dpNeg));
+            // System.out.println(Arrays.toString(dpPos) + "\n");
+
+            ans = Math.max(ans, Math.max(dpPos[i], Math.abs(dpNeg[i])));
         }
 
         return ans;
