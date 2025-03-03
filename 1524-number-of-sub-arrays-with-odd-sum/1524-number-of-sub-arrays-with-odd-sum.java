8592d9409f334fbd0c834b7fe0ab6220681edca8
team-hungrian <def.hunger@gmail.com>
2025-03-03 17:30:40 +0900

    Time: 11 ms (11.46%), Space: 60.2 MB (5.99%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.java
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.java
@@ -1,6 +1,37 @@
 class Solution {
     public int numOfSubarrays(int[] arr) {
-        return official_prefixsum(arr);
+        return tryAgain_20250303(arr);
+    }
+
+    public int tryAgain_20250303(int[] arr) {
+        int n = arr.length;
+        int[] oddDp = new int[n];
+        int[] evenDp = new int[n];
+        
+        if (arr[0] % 2 == 0) {
+            evenDp[0] = 1;
+        } else {
+            oddDp[0] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                evenDp[i] = Math.max(evenDp[i - 1] + 1, 1);
+                oddDp[i] = oddDp[i - 1];
+            } else {
+                oddDp[i] = Math.max(evenDp[i - 1] + 1, 1);
+                evenDp[i] = oddDp[i - 1];
+            }
+        }
+
+        int mod = (int)1e9 + 7;
+        int ans = 0;
+
+        for (int num : oddDp) {
+            ans = (ans + num) % mod;
+        }
+
+        return ans;
     }
 
     public int official_prefixsum(int[] arr) {
