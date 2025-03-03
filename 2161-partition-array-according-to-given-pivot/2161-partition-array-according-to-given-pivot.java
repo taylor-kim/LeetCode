77e20774dc9c0a19655dd144023828c590c60d32
team-hungrian <def.hunger@gmail.com>
2025-03-03 15:18:55 +0900

    Time: 5 ms (85.39%), Space: 68.4 MB (15.73%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,6 +1,29 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        return official_twopasses_with_fixed_array(nums, pivot);
+        return official_two_pointers(nums, pivot);
+    }
+
+    public int[] official_two_pointers(int[] nums, int pivot) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int[] ans = new int[n];
+
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
+            if (nums[i] < pivot) {
+                ans[left++] = nums[i];
+            }
+
+            if (nums[j] > pivot) {
+                ans[right--] = nums[j];
+            }
+        }
+
+        while (left <= right) {
+            ans[left++] = pivot;
+        }
+
+        return ans;
     }
 
     public int[] official_twopasses_with_fixed_array(int[] nums, int pivot) {
