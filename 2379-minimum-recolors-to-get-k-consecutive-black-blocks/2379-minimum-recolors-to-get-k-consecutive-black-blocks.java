b75014868cea7c0a3515794188b62ba43fa0d59c
team-hungrian <def.hunger@gmail.com>
2025-03-08 17:29:31 +0900

    Time: 1 ms (92.1%), Space: 41.6 MB (58.45%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,6 +1,28 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        return mySol(blocks, k);
+        return official_sw(blocks, k);
+    }
+
+    public int official_sw(String blocks, int k) {
+        int ans = k;
+
+        int left = 0;
+
+        char[] arr = blocks.toCharArray();
+
+        int count = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            count += arr[right] == 'W' ? 0 : 1;
+
+            if (right - left + 1 == k) {
+                ans = Math.min(ans, k - count);
+
+                count -= arr[left++] == 'W' ? 0 : 1;
+            }
+        }
+
+        return ans;
     }
 
     public int mySol(String blocks, int k) {
