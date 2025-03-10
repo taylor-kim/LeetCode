9668217df164444c1e4715e8b28306f694f93c90
team-hungrian <def.hunger@gmail.com>
2025-03-11 02:06:32 +0900

    Time: 188 ms (70.94%), Space: 55.5 MB (8.55%) - LeetHub

---
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -0,0 +1,106 @@
+class Solution {
+    public long countOfSubstrings(String word, int k) {
+        return official_sw(word, k);
+    }
+
+    public long official_sw(String word, int k) {
+        char[] arr = word.toCharArray();
+        
+        Map<Character, Integer> vowels = new HashMap();
+        int consonants = 0;
+        int[] nextConsonant = new int[arr.length];
+        int nextConsonantIndex = arr.length;
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            nextConsonant[i] = nextConsonantIndex;
+
+            if (!isVowel(arr[i])) {
+                nextConsonantIndex = i;
+            }
+        }
+
+        int left = 0;
+        long ans = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            char c = arr[right];
+
+            if (isVowel(c)) {
+                vowels.put(c, vowels.getOrDefault(c, 0) + 1);
+            } else {
+                consonants++;
+            }
+
+            while (consonants > k) {
+                char removed = arr[left++];
+
+                if (isVowel(removed)) {
+                    remove(vowels, removed);
+                } else {
+                    consonants--;
+                }
+            }
+
+            while (left < arr.length && vowels.size() == 5 && consonants == k) {
+                ans += nextConsonant[right] - right;
+
+                char removed = arr[left++];
+
+                if (isVowel(removed)) {
+                    remove(vowels, removed);
+                } else {
+                    consonants--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public long mySol_fail(String word, int k) {
+        char[] arr = word.toCharArray();
+        
+        Map<Character, Integer> vowels = new HashMap();
+        LinkedList<Integer> consonants = new LinkedList();
+
+        int left = 0;
+        int right = 0;
+        long ans = 0;
+
+        while (right < arr.length) {
+            while (right < arr.length && (consonants.size() < k || isVowel(arr[right]))) {
+                char c = arr[right++];
+
+                if (isVowel(c)) {
+                    vowels.put(c, vowels.getOrDefault(c, 0) + 1);
+                } else {
+                    consonants.add(right);
+                }
+            }
+
+            while (left < arr.length && vowels.size() == 5 && consonants.size() == k) {
+                ans += consonants.size();
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    private void remove(Map<Character, Integer> map, char c) {
+        map.put(c, map.get(c) - 1);
+
+        if (map.get(c) == 0) {
+            map.remove(c);
+        }
+    }
+
+    private int getCount(int left, int right, int k, Map<Character, Integer> map, LinkedList<Integer> consonants) {
+        if (map.size() < 5) return 0;
+
+        return 0;
+    }
+}
