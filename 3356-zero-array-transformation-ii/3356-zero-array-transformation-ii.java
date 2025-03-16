d2714069ad9eebc5187f0ee6a79b346e51eb8b87
team-hungrian <def.hunger@gmail.com>
2025-03-16 23:13:05 +0900

    Time: 29 ms (16.01%), Space: 119.5 MB (59.51%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
@@ -1,6 +1,48 @@
 class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
-        return mySol(nums, queries);
+        return official_diff_array(nums, queries);
+    }
+
+    public int official_diff_array(int[] nums, int[][] queries) {
+        int n = nums.length;
+
+        int lo = 0;
+        int hi = queries.length + 1;
+
+        int ans = -1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (currentIndexZero(nums, queries, mid)) {
+                ans = mid;
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean currentIndexZero(int[] nums, int[][] queries, int k) {
+        int[] diffArray = new int[nums.length + 1];
+
+        for (int i = 0; i < k; i++) {
+            int[] query = queries[i];
+            diffArray[query[0]] += query[2];
+            diffArray[query[1] + 1] -= query[2];
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += diffArray[i];
+
+            if (sum < nums[i]) return false;
+        }
+
+        return true;
     }
 
     public int mySol(int[] nums, int[][] queries) {
