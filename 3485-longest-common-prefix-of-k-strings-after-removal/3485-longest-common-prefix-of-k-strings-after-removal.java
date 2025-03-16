fa9444e1960ec76b9566aa31a2d8e207a79aff03
team-hungrian <def.hunger@gmail.com>
2025-03-17 02:09:22 +0900

    Time: 780 ms (100%), Space: 62.7 MB (100%) - LeetHub

--- 3485-longest-common-prefix-of-k-strings-after-removal/3485-longest-common-prefix-of-k-strings-after-removal.java
+++ 3485-longest-common-prefix-of-k-strings-after-removal/3485-longest-common-prefix-of-k-strings-after-removal.java
@@ -37,7 +37,7 @@ class Solution {
                 node = node.children[c - 'a'];
                 node.count++;
 
-                if (node.count == k) {
+                if (node.count >= k) {
                     map.put(i + 1, map.getOrDefault(i + 1, 0) + 1);
                 }
             }
@@ -51,7 +51,7 @@ class Solution {
 
                 node = node.children[c - 'a'];
 
-                if (node.count == k) {
+                if (node.count >= k) {
                     map.put(i + 1, map.get(i + 1) - 1);
 
                     if (map.get(i + 1) == 0) {
