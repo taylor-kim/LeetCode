8993b2d855aee3827ba1c9ccf46b17bee6ccea7b
team-hungrian <def.hunger@gmail.com>
2025-03-17 00:52:51 +0900

    Time: 3 ms (100%), Space: 99.5 MB (92.68%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.java
@@ -1,6 +1,64 @@
 class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
-        return official_diff_array(nums, queries);
+        return official_line_sweep(nums, queries);
+    }
+
+    public int official_line_sweep(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] diffArray = new int[n + 1];
+
+        int sum = 0;
+        int k = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (sum + diffArray[i] < nums[i]) {
+                k++;
+
+                if (k > queries.length) return -1;
+
+                int left = queries[k - 1][0];
+                int right = queries[k - 1][1];
+                int value = queries[k - 1][2];
+
+                if (i <= right) {
+                    diffArray[Math.max(left, i)] += value;
+                    diffArray[right + 1] -= value;
+                }
+            }
+
+            sum += diffArray[i];
+        }
+
+        return k;
+    }
+
+    public int try_line_sweep_fail(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] diffArray = new int[n + 1];
+
+        int sum = 0;
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            while (ans < queries.length && nums[i] > diffArray[i]) {
+                diffArray[queries[ans][0]] += queries[ans][2];
+                diffArray[queries[ans][1] + 1] -= queries[ans][2];
+
+                for (int j = queries[ans][0]; j < queries[ans][1]; j++) {
+                    diffArray[j + 1] += diffArray[j];
+                }
+                // System.out.println(String.format("ans:%d, %s", ans, Arrays.toString(diffArray)));
+                ans++;
+            }
+
+            // System.out.println(String.format("ans:%d, %s", ans, Arrays.toString(diffArray)));
+
+            // System.out.println("\n");
+
+            if (nums[i] > diffArray[i]) return -1;
+        }
+
+        return ans;
     }
 
     public int official_diff_array(int[] nums, int[][] queries) {
