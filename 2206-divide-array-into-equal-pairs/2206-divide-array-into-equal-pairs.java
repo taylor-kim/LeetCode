b615ea4ed73ea208cec007a985ef1578034208d5
team-hungrian <def.hunger@gmail.com>
2025-03-17 18:09:22 +0900

    Time: 1 ms (100%), Space: 44.1 MB (94.91%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        return mySol(nums);
+    }
+
+    public boolean mySol(int[] nums) {
+        int[] counts = new int[501];
+
+        for (int num : nums) counts[num]++;
+
+        for (int count : counts) {
+            if (count % 2 == 1) return false;
+        }
+
+        return true;
+    }
+}
