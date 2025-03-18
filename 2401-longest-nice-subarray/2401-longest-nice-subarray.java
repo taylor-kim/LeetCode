b3fdc30d8fcd8b5ec884ed7ebdd704610ebc486b
team-hungrian <def.hunger@gmail.com>
2025-03-18 22:29:03 +0900

    Time: 134 ms (8.72%), Space: 55.7 MB (57.77%) - LeetHub

---
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        return mySol(nums);
+    }
+
+    public int mySol(int[] nums) {
+        // 3, 8, 48
+        // 00000000011 : 3
+        // 00000000100 : 4
+        // 00000001000 : 8
+        // 00000110000 : 48
+        // 00000110100 : 52
+
+        int n = nums.length;
+
+        int ans = 1;
+        int left = 0;
+        int sum = 0;
+        int[] bits = new int[32];
+
+        for (int right = 0; right < n; right++) {
+            int num = nums[right];
+            
+            while (left < right && (sum & num) != 0) {
+                //sum 에서 nums[left++] unset
+                int leftNum = nums[left++];
+                unsetBits(leftNum, bits);
+
+                for (int i = 0; i < bits.length; i++) {
+                    if (bits[i] == 0) {
+                        int mask = ~(1 << i);
+                        sum &= mask;
+                    }
+                }
+            }
+
+            if ((sum & num) == 0) {
+                ans = Math.max(ans, right - left + 1);
+            }
+
+            sum |= num;
+            setBits(num, bits);
+        }
+
+        return ans;
+    }
+
+    private void setBits(int num, int[] bits) {
+        for (int i = 0; i < 32; i++) {
+            int mask = 1 << i;
+
+            if ((num & mask) != 0) {
+                bits[i]++;
+            }
+        }
+    }
+
+    private void unsetBits(int num, int[] bits) {
+        for (int i = 0; i < 32; i++) {
+            int mask = 1 << i;
+
+            if ((num & mask) != 0) {
+                bits[i]--;
+            }
+        }
+    }
+}
