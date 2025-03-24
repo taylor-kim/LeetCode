ae4af9eb31e12e6fda98c220cf8b1792b18c1110
team-hungrian <def.hunger@gmail.com>
2025-03-24 18:46:42 +0900

    Time: 41 ms (40.4%), Space: 110.3 MB (6.4%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.java
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.java
@@ -1,9 +1,43 @@
 class Solution {
     public int countDays(int days, int[][] meetings) {
-        return mySol(days, meetings);
+        return mySol2(days, meetings);
     }
 
-    public int mySol(int days, int[][] meetings) {
+    public int mySol2(int days, int[][] meetings) {
+        List<int[]> list = new ArrayList();
+
+        for (int[] meeting : meetings) {
+            list.add(new int[] {meeting[0], meeting[1]});
+        }
+
+        Collections.sort(list, (a, b) -> {
+            return a[0] - b[0];
+        });
+
+        int ans = 0;
+        int prevEnd = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            int[] current = list.get(i);
+
+            // System.out.println(String.format("prevEnd:%d, current:%s", prevEnd, Arrays.toString(current)));
+
+            if (prevEnd + 1 < current[0]) {
+                ans += current[0] - (prevEnd + 1);
+                prevEnd = current[1];
+            } else {
+                prevEnd = Math.max(prevEnd, current[1]);
+            }
+        }
+
+        if (prevEnd < days) {
+            ans += days - prevEnd;
+        }
+
+        return ans;
+    }
+
+    public int mySol_mle(int days, int[][] meetings) {
         int[] dp = new int[days + 2];
 
         for (int[] meeting : meetings) {
