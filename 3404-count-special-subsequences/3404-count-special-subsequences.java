ffbfe19fe81df4a316b554822f985dfc7ccb8672
team-hungrian <def.hunger@gmail.com>
2025-03-30 23:27:10 +0900

    Time: 365 ms (46.43%), Space: 54.7 MB (98.7%) - LeetHub

--- 3404-count-special-subsequences/3404-count-special-subsequences.java
+++ 3404-count-special-subsequences/3404-count-special-subsequences.java
@@ -1,19 +1,29 @@
 class Solution {
-    public long numberOfSubsequences(int[] A) {
-        int n = A.length;
-        long res = 0;
-        Map<Double, Integer> cnt = new HashMap<>();
-        for (int r = 4; r < n - 2; ++r) {
+    public long numberOfSubsequences(int[] nums) {
+        return mySol(nums);
+    }
+
+    public long mySol(int[] nums) {
+        int n = nums.length;
+        
+        Map<Double, Integer> ratioCount = new HashMap<>();
+
+        long ans = 0;
+
+        for (int r = 4; r < n - 2; r++) {
             int q = r - 2;
-            for (int p = 0; p < q - 1; ++p) {
-                double key = 1.0 * A[p] / A[q];
-                cnt.put(key, cnt.getOrDefault(key, 0) + 1);
+
+            for (int p = 0; p < q - 1; p++) {
+                double ratio = 1.0 * nums[p] / nums[q];
+                ratioCount.put(ratio, ratioCount.getOrDefault(ratio, 0) + 1);
             }
-            for (int s = r + 2; s < n; ++s) {
-                double key = 1.0 * A[s] / A[r];
-                res += cnt.getOrDefault(key, 0);
+
+            for (int s = r + 2; s < n; s++) {
+                double ratio = 1.0 * nums[s] / nums[r];
+                ans += ratioCount.getOrDefault(ratio, 0);
             }
         }
-        return res;
+
+        return ans;
     }
 }
