63132dbaa5a0c7db783d972508cecdc1717ab445
team-hungrian <def.hunger@gmail.com>
2025-03-30 23:20:52 +0900

    Time: 133 ms (18.49%), Space: 103.7 MB (27.7%) - LeetHub

--- 2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
@@ -1,30 +1,39 @@
 class StockPrice {
-    TreeMap<Integer, Integer> record = new TreeMap<>();
-    TreeMap<Integer, Set<Integer>> vals = new TreeMap<>();
+    private TreeMap<Integer, Integer> priceByTime = new TreeMap<>();
+    private TreeMap<Integer, Set<Integer>> timesByPrice = new TreeMap<>();
 
     public void update(int timestamp, int price) {
-        if (record.containsKey(timestamp)) {
-            int prevPrice = record.get(timestamp);
-            Set<Integer> book = vals.get(prevPrice);
-            book.remove(timestamp);
-            if (book.isEmpty()) {
-                vals.remove(prevPrice);
-            }
+        if (priceByTime.containsKey(timestamp)) {
+            removeOldPrice(timestamp);
         }
-        vals.putIfAbsent(price, new HashSet<>());
-        vals.get(price).add(timestamp);
-        record.put(timestamp, price);
+
+        addNewPrice(timestamp, price);
     }
 
     public int current() {
-        return record.lastEntry().getValue();
+        return priceByTime.lastEntry().getValue();
     }
 
     public int maximum() {
-        return vals.lastKey();
+        return timesByPrice.lastKey();
     }
 
     public int minimum() {
-        return vals.firstKey();
+        return timesByPrice.firstKey();
+    }
+
+    private void removeOldPrice(int timestamp) {
+        int prevPrice = priceByTime.get(timestamp);
+        Set<Integer> times = timesByPrice.get(prevPrice);
+        times.remove(timestamp);
+
+        if (times.isEmpty()) {
+            timesByPrice.remove(prevPrice);
+        }
+    }
+
+    private void addNewPrice(int timestamp, int price) {
+        timesByPrice.computeIfAbsent(price, k -> new HashSet<>()).add(timestamp);
+        priceByTime.put(timestamp, price);
     }
 }
