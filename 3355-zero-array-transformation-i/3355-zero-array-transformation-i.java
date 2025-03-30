ab74b8ae124806f352760e837e1bf11479ae3772
team-hungrian <def.hunger@gmail.com>
2025-03-30 23:05:31 +0900

    Time: 3 ms (79.14%), Space: 94.8 MB (76.87%) - LeetHub

--- 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
@@ -1,19 +1,29 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int[] pre = new int[nums.length+1];
-        for(int i=0;i<queries.length;i++)
-        {
-            pre[queries[i][0]]++;
-            pre[queries[i][1]+1]--;
+        return mySol(nums, queries);
+    }
+
+    public boolean mySol(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int[] diffArray = new int[n + 1];
+
+        for (int i = 0; i < queries.length; i++) {
+            diffArray[queries[i][0]]++;
+            diffArray[queries[i][1] + 1]--;
         }
-        if(pre[0]<nums[0])
+
+        if (diffArray[0] < nums[0]) {
             return false;
-        for(int i=1;i<nums.length;i++)
-        {
-            pre[i]+=pre[i-1];
-            if(pre[i]<nums[i])
-                return false;
         }
+
+        for (int i = 1; i < n; i++) {
+            diffArray[i] += diffArray[i - 1];
+
+            if (diffArray[i] < nums[i]) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
