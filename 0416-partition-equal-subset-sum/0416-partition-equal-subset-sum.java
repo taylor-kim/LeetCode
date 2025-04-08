9664a7a9ad8fa3a14d8a42f2187484ca7cd8c9ca
team-hungrian <def.hunger@gmail.com>
2025-04-08 20:39:31 +0900

    Time: 46 ms (45.44%), Space: 46.5 MB (65.59%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -3,6 +3,35 @@ class Solution {
         return try_bottomup(nums);
     }
 
+    public boolean try_bottomup_spaceopt(int[] nums) {
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if (sum % 2 == 1) return false;
+
+        sum /= 2;
+
+        int n = nums.length;
+
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+
+        for (int i = n - 1; i >= 0; i--) {
+            // for (int j = 0; j <= sum; j++) {
+            //     if (j >= nums[i]) {
+            //         dp[j] = dp[i + 1][j - nums[i]] || dp[i + 1][j];
+            //     } else {
+            //         dp[j] |= dp[i + 1][j];
+            //     }
+            // }
+        }
+
+        return dp[sum];
+    }
+
     public boolean try_bottomup(int[] nums) {
         int sum = 0;
 
@@ -20,9 +49,10 @@ class Solution {
         dp[n][0] = true;
 
         for (int i = n - 1; i >= 0; i--) {
-            for (int j = 0; j <= sum; j++) {
+            // dp[i][0] = true;
+            for (int j = 1; j <= sum; j++) {
                 if (j >= nums[i]) {
-                    dp[i][j] = dp[i + 1][j - nums[i]] || dp[i + 1][j];
+                    dp[i][j] |= dp[i + 1][j - nums[i]] || dp[i + 1][j];
                 } else {
                     dp[i][j] |= dp[i + 1][j];
                 }
