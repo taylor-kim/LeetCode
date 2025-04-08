f6fa007590779aa8a6cff4d0426d7ca53a8fb6b9
team-hungrian <def.hunger@gmail.com>
2025-04-08 20:21:23 +0900

    Time: 20 ms (91.99%), Space: 55.4 MB (40.13%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,99 +1,30 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        return niceDP(nums);
+        return try_20250408(nums);
     }
 
-    public boolean niceDP(int[] nums) {
+    public boolean try_20250408(int[] nums) {
         int sum = 0;
-        int n = nums.length;
-        
-        for(int i : nums) sum+=i;
-        
-        if(sum%2!=0) return false;
-        
-        sum /= 2;
-        
-        boolean[] dp = new boolean[sum + 1];
-        dp[0] = true;
 
         for (int num : nums) {
-            for (int i = sum; i > 0; i--) {
-                if (i >= num) {
-                    dp[i] = dp[i] || dp[i - num];
-                }
-            }
+            sum += num;
         }
-        
-        return dp[sum];
+
+        if (sum % 2 == 1) return false;
+
+        return topdown(nums, 0, sum / 2, new Boolean[nums.length][sum / 2 + 1]);
     }
 
-    Boolean mem[][];
-    public boolean othersMemoization(int[] nums) {
-        int sum = 0;
-        int n = nums.length;
-        
-        for(int i : nums) sum+=i;
-        
-        if(sum%2!=0) return false;
-        
-        sum /= 2;
-        
-        mem = new Boolean[n+1][sum+1];
-        
-        return subsetSum(nums,0,sum);
-    }
-    
-    boolean subsetSum(int[] nums, int pos, int sum){
-        if(sum==0) return true;
-        
-        else if(pos>=nums.length || sum<0) return false;
-        
-        if(mem[pos][sum]!=null) return mem[pos][sum];
-        
-        return mem[pos][sum] = subsetSum(nums,pos+1,sum-nums[pos]) ||
-                                subsetSum(nums,pos+1,sum);
-        
-        
-    }
+    private boolean topdown(int[] nums, int index, int remain, Boolean[][] memo) {
+        if (remain < 0) return false;
 
-    public boolean mySol(int[] nums) {
-        int total = 0;
+        if (remain == 0) return true;
 
-        for (int num : nums) {
-            total += num;
-        }
+        if (index >= nums.length) return remain == 0;
 
-        if (total % 2 == 1) {
-            return false;
-        }
+        if (memo[index][remain] != null) return memo[index][remain];
 
-        total /= 2;
-
-        int[][] memo = new int[nums.length][total + 1];
-
-        for (int i = 0; i < nums.length; i++) {
-            Arrays.fill(memo[i], -1);
-        }
-
-        return mySol(nums, total, 0, memo);
-    }
-
-    public boolean mySol(int[] nums, int total, int index, int[][] memo) {
-        if (total == 0) {
-            return true;
-        }
-
-        if (index >= nums.length || total < 0) {
-            return false;
-        }
-
-        if (memo[index][total] != -1) {
-            return memo[index][total] == 1;
-        }
-
-        memo[index][total] = (mySol(nums, total - nums[index], index + 1, memo) 
-                                || mySol(nums, total, index + 1, memo)) ? 1 : 0;
-
-        return memo[index][total] == 1;
+        return memo[index][remain] = topdown(nums, index + 1, remain - nums[index], memo)
+            || topdown(nums, index + 1, remain, memo);
     }
 }
