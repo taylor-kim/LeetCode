a1896a2fbeda9748b1e89e78367d8fba43ff24ff
team-hungrian <def.hunger@gmail.com>
2025-04-13 22:21:14 +0900

    Time: 0 ms (100%), Space: 40.5 MB (65.84%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.java
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -9,26 +9,22 @@ class Solution {
         long evenCount = n / 2 + (n % 2);
         long oddCount = n / 2;
 
-        long evenTotal = pow(5l, evenCount, mod, new HashMap());
-        long oddTotal = pow(4l, oddCount, mod, new HashMap());
+        long evenTotal = pow(5l, evenCount, mod);
+        long oddTotal = pow(4l, oddCount, mod);
 
         return (int)((evenTotal * oddTotal) % mod);
     }
 
-    private long pow(long base, long exp, int mod, Map<Long, Long> memo) {
+    private long pow(long base, long exp, int mod) {
         if (exp == 0) return 1l;
 
-        // if (memo.containsKey(exp)) return memo.get(exp);
-
-        long sqrt = pow(base, exp / 2, mod, memo) % mod;
+        long sqrt = pow(base, exp / 2, mod) % mod;
 
         long ret = (sqrt * sqrt) % mod;
 
         if (exp % 2 == 1) {
             ret = (ret * base) % mod;
         }
-
-        // memo.put(exp, ret);
 
         return ret;
     }
