89c984cf62326450c46a87b433dbbf0bf0321135
team-hungrian <def.hunger@gmail.com>
2025-04-13 23:41:28 +0900

    Time: 10 ms (97.96%), Space: 41.3 MB (88.92%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.java
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.java
@@ -1,6 +1,23 @@
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
-        return mySol(low, high);
+        return official(low, high);
+    }
+
+    public int official(int low, int high) {
+        int ans = 0;
+
+        for (int num = low; num <= high; num++) {
+            if (num < 100 && num % 11 == 0) {
+                ans++;
+            } else if (num >= 1001 && num < 10000) {
+                int left = num / 1000 + ((num / 100) % 10);
+                int right = (num % 100) / 10 + (num % 10);
+
+                if (left == right) ans++;
+            }
+        }
+
+        return ans;
     }
 
     public int mySol(int low, int high) {
