0558d5f79057303cfa58b3a9c3ba33afa4201820
team-hungrian <def.hunger@gmail.com>
2025-04-26 22:25:17 +0900

    Time: 132 ms (5.04%), Space: 58.5 MB (98.22%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.java
@@ -1,21 +1,56 @@
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
-        return others(nums, minK, maxK);
+        return mySol(nums, minK, maxK);
     }
 
-    public long others(int[] nums, int minK, int maxK) {
+    public long mySol(int[] nums, int minK, int maxK) {
         int n = nums.length;
+        int[] next = new int[n + 1];
+        next[n] = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int num = nums[i];
+
+            if (num < minK || num > maxK) {
+                next[i] = -1;
+            } else if (next[i + 1] >= 0) {
+                next[i] = next[i + 1];
+            } else {
+                next[i] = i;
+            }
+        }
+
         long ans = 0;
-        int bad = -1;
-        int min = -1;
-        int max = -1;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < minK || nums[i] > maxK) bad = i;
-            if (nums[i] == minK) min = i;
-            if (nums[i] == maxK) max = i;
+        int left = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
-            ans += Math.max(0l, Math.min(min, max) - bad);
+        TreeMap<Integer, Integer> map = new TreeMap();
+        
+        for (int right = 0; right < n; right++) {
+            while (left <= right && !map.isEmpty() && (map.firstKey() < minK || map.lastKey() > maxK)) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+
+            int num = nums[right];
+
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            while (!map.isEmpty() && map.firstKey() == minK && map.lastKey() == maxK) {
+                ans += next[right] - right + 1;
+                map.put(nums[left], map.get(nums[left]) - 1);
+
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
         }
 
         return ans;
