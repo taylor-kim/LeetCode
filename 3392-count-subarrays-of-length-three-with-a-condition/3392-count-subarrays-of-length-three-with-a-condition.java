01ad745fbaec507641213312429327f8da79bf84
team-hungrian <def.hunger@gmail.com>
2025-04-28 19:27:41 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.java
+++ 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.java
@@ -5,19 +5,57 @@ class Solution {
 
     public int mySol(int[] nums) {
         int ans = 0;
-        
-        for (int i = 0; i < nums.length - 2; i++) {
-            int one = nums[i];
-            int two = nums[i + 1];
-            int three = nums[i + 2];
 
-            if (two % 2 == 1) continue;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] % 2 == 1) continue;
 
-            int halfOfSecond = two / 2;
-
-            if (one + three == halfOfSecond) ans++;
+            if (nums[i - 1] + nums[i + 1] == nums[i] / 2) {
+                ans++;
+            }
         }
 
         return ans;
+    }
+
+    public int mySol_fail(int[] nums) {
+        int ans = 0;
+
+        Map<Integer, Integer> map = new HashMap();
+        Map<Integer, Integer> remains = new HashMap();
+
+        for (int num : nums) {
+            add(remains, num);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            remove(remains, num);
+
+            if (num % 2 == 0) {
+                int left = map.getOrDefault(num / 4, 0);
+                int right = remains.getOrDefault(num / 4, 0);
+
+                ans += left * right;
+
+                // System.out.println(String.format("num:%d, left:%d, right:%d, ans:%d", num, left, right, ans));
+            }
+
+            add(map, num);
+        }
+        
+        return ans;
+    }
+
+    private void add(Map<Integer, Integer> map, int num) {
+        map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+    private void remove(Map<Integer, Integer> map, int num) {
+        map.put(num, map.get(num) - 1);
+
+        if (map.get(num) == 0) {
+            map.remove(num);
+        }
     }
 }
