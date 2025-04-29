d37eee9eab33383bc7f8589d702db483bbdc5d0c
team-hungrian <def.hunger@gmail.com>
2025-04-30 00:30:41 +0900

    Time: 9 ms (28.68%), Space: 57.4 MB (96.79%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,91 +1,40 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        return official_sw2(nums, k);
+        return mySol(nums, k);
     }
 
-    public long official_sw2(int[] nums, int k) {
-        long ans = 0;
-
-        int max = 0;
-
-        for (int num : nums) max = Math.max(max, num);
-
-        List<Integer> list = new ArrayList();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == max) list.add(i);
-
-            if (list.size() >= k) {
-                ans += list.get(list.size() - k) + 1;
-            }
-        }
-
-        return ans;
-    }
-
-    public long official_sw(int[] nums, int k) {
+    public long mySol(int[] nums, int k) {
         int n = nums.length;
         int max = 0;
 
         for (int num : nums) max = Math.max(max, num);
 
-        int left = 0;
-        int count = 0;
         long ans = 0;
+        List<Integer> indices = new ArrayList();
 
-        for (int right = 0; right < n; right++) {
-            count += nums[right] == max ? 1 : 0;
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
 
-            while (k == count) {
-                if (nums[left++] == max) {
-                    count--;
-                }
-            }
-            ans += left;
+            if (max != num) continue;
+
+            indices.add(i);
         }
 
-        return ans;
-    }
+        int prev = -1;
 
-    public long mySol_misunderstood(int[] nums, int k) {
-        int n = nums.length;
+        for (int i = 0; i < indices.size() - k + 1; i++) {
+            int left = indices.get(i);
+            int right = indices.get(i + k - 1);
 
-        TreeMap<Integer, Integer> map = new TreeMap();
+            long leftCount = left - prev;
+            long rightCount = n - right;
 
-        int left = 0;
-        int ans = 0;
+            ans += (leftCount * rightCount);
 
-        int[] max = new int[n + 1];
-        int[] dp = new int[n + 1];
+            prev = left;
 
-        for (int right = 0; right < n; right++) {
-            int rv = nums[right];
-            map.put(rv, map.getOrDefault(rv, 0) + 1);
-
-            int local = 0;
-
-            max[right + 1] = Math.max(max[right], rv);
-
-            while (left <= right && map.get(map.lastKey()) >= k) {
-                System.out.println(String.format("left:%d, right:%d", left, right));
-                local++;
-                int lv = nums[left++];
-                map.put(lv, map.get(lv) - 1);
-
-                if (map.get(lv) == 0) {
-                    map.remove(lv);
-                }
-            }
-
-            if (max[right + 1] == max[right]) {
-                dp[right + 1] += dp[right];
-            }
-
-            dp[right + 1] += local;
-        }
-
-        for (int num : dp) {
-            ans += num;
+            // System.out.println(String.format("i:%d, left:%d, right:%d, leftCount:%d, rightCount:%d, ans:%d"
+            //                                 , i, left, right, leftCount, rightCount, ans));
         }
 
         return ans;
