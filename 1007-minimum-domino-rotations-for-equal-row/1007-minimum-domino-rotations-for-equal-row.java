6dd724e677588d6c0c552948067004ec9b734d42
team-hungrian <def.hunger@gmail.com>
2025-05-05 14:22:43 +0900

    Time: 3 ms (86.73%), Space: 50.8 MB (12.41%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,21 +1,66 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int res = getRotation(tops, bottoms, tops[0]);
-        if (bottoms[0] != tops[0]) {
-            res = Math.min(res, getRotation(tops, bottoms, bottoms[0]));
-        }
-        return res == Integer.MAX_VALUE ? -1 : res;
+        return mySol3(tops, bottoms);
     }
 
-    private int getRotation(int[] tops, int[] bottoms, int target) {
-        int rotateTop = 0, rotateBottom = 0;
-        for (int i = 0; i < tops.length; i++) {
-            if (tops[i] != target && bottoms[i] != target) {
-                return Integer.MAX_VALUE;
+    public int mySol3(int[] tops, int[] bottoms) {
+        int top = mySol2(tops, bottoms);
+        int bot = mySol2(bottoms, tops);
+
+        return top == -1 && bot == -1 ? -1 : Math.min(top, bot);
+    }
+
+    public int mySol2(int[] tops, int[] bottoms) {
+        int n = tops.length;
+
+        int ans = n;
+
+        for (int num = 1; num <= 6; num++) {
+            int swap = 0;
+            boolean success = true;
+
+            for (int i = 0; i < n; i++) {
+                if (tops[i] == num) continue;
+
+                if (bottoms[i] == num) {
+                    swap++;
+                } else {
+                    success = false;
+                    break;
+                }
             }
-            if (tops[i] != target) rotateTop++;
-            if (bottoms[i] != target) rotateBottom++;
+
+            if (success) {
+                ans = Math.min(ans, swap);
+            }
         }
-        return Math.min(rotateTop, rotateBottom);
+
+        return ans == n ? -1 : ans;
+    }
+
+    public int mySol_fail(int[] tops, int[] bottoms) {
+        int n = tops.length;
+        Map<Integer, Integer> topCount = new HashMap();
+        Map<Integer, Integer> bottomCount = new HashMap();
+
+        for (int i = 0; i < n; i++) {
+            topCount.put(tops[i], topCount.getOrDefault(tops[i], 0) + 1);
+            bottomCount.put(bottoms[i], bottomCount.getOrDefault(bottoms[i], 0) + 1);
+        }
+
+        int ans = n;
+
+        for (int number = 1; number <= 6; number++) {
+            int tc = topCount.getOrDefault(number, 0);
+            int bc = bottomCount.getOrDefault(number, 0);
+
+            System.out.println(String.format("num:%d, tc:%d, bc:%d", number, tc, bc));
+
+            if (tc + bc == n) {
+                ans = Math.min(tc, bc);
+            }
+        }
+
+        return ans == n ? -1 : ans;
     }
 }
