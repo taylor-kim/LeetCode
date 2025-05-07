582a4c943da8c1ac8bd84a7654d65469425dcfe6
team-hungrian <def.hunger@gmail.com>
2025-05-07 15:11:20 +0900

    Time: 2 ms (7.44%), Space: 45.4 MB (63.49%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,6 +1,25 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        return mySol(nums);
+        return try_inplace(nums);
+    }
+
+    public int[] try_inplace(int[] nums) {
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            int newIndex = nums[i] % 1000;
+            int newValue = (nums[newIndex] % 1000) * 1000 + nums[i];
+
+            nums[i] = newValue;
+
+            // System.out.println(String.format("newIndex:%d, newValue:%d", newIndex, newValue));
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] /= 1000;
+        }
+
+        return nums;
     }
 
     public int[] mySol(int[] nums) {
