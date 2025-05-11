38116c0a85ac502c03147c537585b7a27f54537f
team-hungrian <def.hunger@gmail.com>
2025-05-11 16:10:41 +0900

    Time: 0 ms (100%), Space: 42.2 MB (70.92%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.java
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -1,6 +1,18 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        return mySol(arr);
+        return try_20250511(arr);
+    }
+
+    public boolean try_20250511(int[] arr) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            count = arr[i] % 2 == 0 ? 0 : count + 1;
+
+            if (count == 3) return true;
+        }
+
+        return false;
     }
 
     public boolean mySol(int[] arr) {
