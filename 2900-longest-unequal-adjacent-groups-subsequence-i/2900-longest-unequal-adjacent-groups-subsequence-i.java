4ae277a8f2a4890a667ad8f1232a9e59b5a736f5
team-hungrian <def.hunger@gmail.com>
2025-05-16 19:02:51 +0900

    Time: 1 ms (96%), Space: 44.9 MB (59.6%) - LeetHub

--- 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.java
+++ 2900-longest-unequal-adjacent-groups-subsequence-i/2900-longest-unequal-adjacent-groups-subsequence-i.java
@@ -6,12 +6,9 @@ class Solution {
     public List<String> official_greedy(String[] words, int[] groups) {
         List<String> ans = new ArrayList();
 
-        int prev = -1;
-
         for (int i = 0; i < words.length; i++) {
-            if (prev == -1 || groups[prev] != groups[i]) {
+            if (i == 0 || groups[i - 1] != groups[i]) {
                 ans.add(words[i]);
-                prev = i;
             }
         }
 
