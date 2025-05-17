7709af0d5c19a2b61a56cf66da9e709d21cf7542
team-hungrian <def.hunger@gmail.com>
2025-05-17 20:13:29 +0900

    Time: 0 ms (100%), Space: 42.2 MB (30.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,72 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        retry_fail_20240612(nums);
+        mySol(nums);
     }
 
-    public void retry_fail_20240612(int[] nums) {
+    public void mySol(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
 
-        int index = 0;
-
-        while (index <= right) {
-            if (nums[index] == 0) {
-                nums[index] = nums[left];
-                nums[left++] = 0;
-            } else if (nums[index] == 2) {
-                nums[index--] = nums[right];
-                nums[right--] = 2;
+        for (int i = 0; i < nums.length && i <= right; i++) {
+            if (nums[i] == 0) {
+                swap(nums, left++, i);
+            } else if (nums[i] == 2) {
+                swap(nums, right--, i--);
             }
-            index++;
         }
     }
 
-    public void othersOnePass(int[] nums) {
-        int lo = 0;
-        int hi = nums.length - 1;
-        int index = 0;
-
-        while (index <= hi) {
-            if (nums[index] == 0) {
-                nums[index] = nums[lo];
-                nums[lo++] = 0;
-            } else if (nums[index] == 2) {
-                nums[index--] = nums[hi];
-                nums[hi--] = 2;
-            }
-            index++;
-        }
-    }
-
-    public void mySolTowPass(int[] nums) {
-        int[] count = new int[3];
-
-        for (int num : nums) {
-            count[num]++;
-        }
-
-        int index = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            while (count[index] == 0) {
-                index++;
-            }
-
-            count[index]--;
-
-            nums[i] = index;
-        }
-    }
-
-    public void bruteForce(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] > nums[j]) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
-            }
-        }
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
