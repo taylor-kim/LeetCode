7492ff3e44dd671ec4877856dae5d70c17b44e6c
team-hungrian <def.hunger@gmail.com>
2025-05-20 10:15:54 +0900

    Time: 3 ms (79.52%), Space: 96.2 MB (17.22%) - LeetHub

--- 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.java
@@ -5,23 +5,20 @@ class Solution {
 
     public boolean mySol(int[] nums, int[][] queries) {
         int n = nums.length;
-        int[] diffArray = new int[n + 1];
+        int[] arr = new int[n + 1];
 
-        for (int i = 0; i < queries.length; i++) {
-            diffArray[queries[i][0]]++;
-            diffArray[queries[i][1] + 1]--;
+        for (int[] query : queries) {
+            arr[query[0]]++;
+            arr[query[1] + 1]--;
         }
 
-        if (diffArray[0] < nums[0]) {
-            return false;
-        }
+        int sum = 0;
 
-        for (int i = 1; i < n; i++) {
-            diffArray[i] += diffArray[i - 1];
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            nums[i] -= sum;
 
-            if (diffArray[i] < nums[i]) {
-                return false;
-            }
+            if (nums[i] > 0) return false;
         }
 
         return true;
