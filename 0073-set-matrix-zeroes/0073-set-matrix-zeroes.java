15c2e426bbfc635ea565fdd190f8543ab68f5414
team-hungrian <def.hunger@gmail.com>
2025-05-21 18:16:36 +0900

    Time: 1 ms (72.83%), Space: 45.6 MB (66.55%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,6 +1,39 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        mySol(matrix);
+        others(matrix);
+    }
+
+    public void others(int[][] matrix) {
+        boolean fc = false;
+        boolean fr = false;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
+
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; fc && i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+
+        for (int j = 0; fr && j < matrix[0].length; j++) {
+            matrix[0][j] = 0;
+        }
     }
 
     public void mySol(int[][] matrix) {
