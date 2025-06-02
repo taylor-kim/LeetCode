658ebc127f357377c22a3e9988e347aa5702761c
team-hungrian <def.hunger@gmail.com>
2025-06-02 16:50:48 +0900

    Time: 11 ms (8.29%), Space: 49.9 MB (6.03%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,6 +1,58 @@
 class Solution {
     public int candy(int[] ratings) {
-        return mySol(ratings);
+        return try_linear(ratings);
+    }
+
+    public int try_linear(int[] ratings) {
+        int n = ratings.length;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int r : ratings) {
+            min = Math.min(min, r);
+            max = Math.max(max, r);
+        }
+
+        List<Integer>[] data = new List[max - min + 1];
+
+        for (int i = 0; i < n; i++) {
+            if (data[ratings[i] - min] == null) {
+                data[ratings[i] - min] = new ArrayList();
+            }
+
+            data[ratings[i] - min].add(i);
+        }
+
+        int r = 0;
+
+        int[] given = new int[n];
+
+        int ans = 0;
+
+        while (r <= max - min) {
+            if (data[r] == null) {
+                r++;
+                continue;
+            }
+
+            for (int index : data[r]) {
+                int count = 1;
+
+                if (index - 1 >= 0 && ratings[index - 1] < ratings[index]) {
+                    count = given[index - 1] + 1;
+                }
+
+                if (index + 1 < n && ratings[index] > ratings[index + 1]) {
+                    count = Math.max(count, given[index + 1] + 1);
+                }
+
+                ans += given[index] = count;
+            }
+
+            r++;
+        }
+
+        return ans;
     }
 
     public int mySol(int[] ratings) {
