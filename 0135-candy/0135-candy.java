d83de9f839bd13df3adc4dbff5338786e99d0973
team-hungrian <def.hunger@gmail.com>
2025-06-02 17:29:14 +0900

    Time: 15 ms (7.82%), Space: 45.7 MB (70.42%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -3,6 +3,43 @@ class Solution {
         return try_linear2(ratings);
     }
 
+    public int try_linear3(int[] ratings) {
+        int n = ratings.length;
+
+        int[] leftToRight = new int[n];
+        int[] rightToLeft = new int[n];
+
+        leftToRight[0] = 1;
+        rightToLeft[n - 1] = 1;
+
+        for (int i = 1; i < n; i++) {
+            int j = n - i - 1;
+
+            if (ratings[i - 1] < ratings[i]) {
+                leftToRight[i] = leftToRight[i - 1] + 1;
+            } else {
+                leftToRight[i] = 1;
+            }
+
+            if (ratings[j] > ratings[j + 1]) {
+                rightToLeft[j] = rightToLeft[j] + 1;
+            } else {
+                rightToLeft[j] = 1;
+            }
+        }
+
+        int ans = 0;
+
+        System.out.println(Arrays.toString(leftToRight));
+        System.out.println(Arrays.toString(rightToLeft));
+
+        for (int i = 0; i < n; i++) {
+            ans += Math.max(leftToRight[i], rightToLeft[i]);
+        }
+
+        return ans;
+    }
+
     public int try_linear2(int[] ratings) {
         int n = ratings.length;
         int[] given = new int[n];
