def7578e169fa02db84b634b629b94b9ddd2ff86
team-hungrian <def.hunger@gmail.com>
2025-06-02 16:21:10 +0900

    Time: 32 ms (5.87%), Space: 46.1 MB (24.2%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int candy(int[] ratings) {
+        return mySol(ratings);
+    }
+
+    public int mySol(int[] ratings) {
+        int n = ratings.length;
+
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return a[0] != b[0] ? a[0] - b[0] : a[1] - b[1];
+        });
+
+        for (int i = 0; i < n; i++) {
+            pq.add(new int[] {ratings[i], i});
+        }
+
+        int[] given = new int[n];
+
+        int ans = 0;
+
+        while (!pq.isEmpty()) {
+            int[] data = pq.poll();
+
+            int rating = data[0];
+            int index = data[1];
+
+            int count = 1;
+
+            if (index - 1 >= 0 && ratings[index - 1] < ratings[index]) {
+                count = given[index - 1] + 1;
+            }
+
+            if (index + 1 < n && ratings[index] > ratings[index + 1]) {
+                count = Math.max(count, given[index + 1] + 1);
+            }
+
+            ans += given[index] = count;
+        }
+
+        return ans;
+    }
+}
