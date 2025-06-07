6b9d094230f5b97d3f6d09d1b308e7138c03c085
team-hungrian <def.hunger@gmail.com>
2025-06-07 12:38:33 +0900

    Time: 45 ms (97.58%), Space: 55 MB (21.82%) - LeetHub

---
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.java
@@ -0,0 +1,50 @@
+class Solution {
+    public String clearStars(String s) {
+        return mySol(s);
+    }
+
+    public String mySol(String s) {
+        LinkedList<Integer>[] freq = new LinkedList[26];
+
+        for (int i = 0; i < 26; i++) {
+            freq[i] = new LinkedList();
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '*') {
+                for (int j = 0; j < 26; j++) {
+                    if (freq[j].size() > 0) {
+                        freq[j].pollLast();
+                        break;
+                    }
+                }
+            } else {
+                freq[c - 'a'].add(i);
+            }
+        }
+
+        List<int[]> data = new ArrayList();
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i].size() > 0) {
+                for (int index : freq[i]) {
+                    data.add(new int[] {i, index});
+                }
+            }
+        }
+
+        Collections.sort(data, (a, b) -> {
+            return a[1] - b[1];
+        });
+
+        for (int[] d : data) {
+            sb.append((char)(d[0] + 'a'));
+        }
+
+        return sb.toString();
+    }
+}
