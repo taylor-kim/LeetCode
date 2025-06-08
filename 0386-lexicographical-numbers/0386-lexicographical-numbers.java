280006c4875fb4a34e8cb2356809ca722a526a3c
team-hungrian <def.hunger@gmail.com>
2025-06-08 17:40:58 +0900

    Time: 1 ms (100%), Space: 48.8 MB (34.13%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,42 +1,30 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        return mySol2(n);
+        return mySol(n);
     }
 
-    public List<Integer> mySol2(int n) {
+    public List<Integer> mySol(int n) {
         List<Integer> ans = new ArrayList();
 
-        for (int i = 1; i < 10; i++) {
-            mySol2_dfs(i, n, ans);
-        }
+        topdown(1, n, ans);
 
         return ans;
     }
 
-    public void mySol2_dfs(int num, int n, List<Integer> ans) {
-        if (num > n) return;
+    private void topdown(int start, int n, List<Integer> ans) {
+        if (start > n) return;
 
-        ans.add(num);
+        ans.add(start);
+        topdown(start * 10, n, ans);
 
-        for (int i = 0; i < 10; i++) {
-            mySol2_dfs(num * 10 + i, n, ans);
-        }
-    }    
-
-    public List<Integer> mySol_fail(int n) {
-        List<Integer> ans = new ArrayList();
-
-        for (int i = 1; i < 10; i++) {
-            int num = i;
-            int power = 10;
-            ans.add(num);
-            for (int digit = 0; digit < 10; digit++) {
-                int next = num * power;
-
-                // if (next)
-            }
+        if (start % 10 < 9) {
+            topdown(start + 1, n, ans);
         }
 
-        return ans;
+        // int limit = Math.min((start * 10) - 1, n);
+        
+        // for (int i = start + 1; i <= limit; i++) {
+        //     topdown(i * 10, n, ans);
+        // }
     }
 }
