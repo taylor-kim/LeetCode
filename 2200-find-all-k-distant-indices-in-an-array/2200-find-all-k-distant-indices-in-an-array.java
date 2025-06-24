25b72102ae9cb88fe3b9dad08d5f7dac641c34d2
team-hungrian <def.hunger@gmail.com>
2025-06-24 15:00:15 +0900

    Time: 106 ms (11.29%), Space: 45.4 MB (28.1%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.java
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.java
@@ -3,7 +3,7 @@ class Solution {
         return mySol(nums, key, k);
     }
 
-    public List<Integer> mySol(int[] nums, int key, int k) {
+    public List<Integer> ref_official(int[] nums, int key, int k) {
         List<Integer> keyIndices = new ArrayList();
 
         for (int j = 0; j < nums.length; j++) {
@@ -26,6 +26,24 @@ class Solution {
             //     ans.add(i);
             //     last = i;
             // }
+        }
+
+        return new ArrayList(ans);
+    }
+
+    public List<Integer> mySol(int[] nums, int key, int k) {
+        List<Integer> keyIndices = new ArrayList();
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] == key) keyIndices.add(j);
+        }
+
+        Set<Integer> ans = new LinkedHashSet();
+
+        for (int j : keyIndices) {
+            for (int i = Math.max(0, j - k); i < Math.min(nums.length, j + k + 1); i++) {
+                ans.add(i);
+            }
         }
 
         return new ArrayList(ans);
