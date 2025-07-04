c4730343fc0c74db053e58a492b42e00784fb5c6
team-hungrian <def.hunger@gmail.com>
2025-07-04 19:54:49 +0900

    Time: 1 ms (100%), Space: 43.8 MB (7.32%) - LeetHub

--- 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.java
+++ 3307-find-the-k-th-character-in-string-game-ii/3307-find-the-k-th-character-in-string-game-ii.java
@@ -1,50 +1,18 @@
 class Solution {
+
     public char kthCharacter(long k, int[] operations) {
-        return byVersionOneSol(k, operations);
-    }
-
-    public char byVersionOneSol(long k, int[] operations) {
         int ans = 0;
-        int index = 0;
-
+        int t;
         while (k != 1) {
-            int t = 63 - Long.numberOfLeadingZeros(k);
-
-            if ((1 << t) == k) {
+            t = 63 - Long.numberOfLeadingZeros(k);
+            if ((1L << t) == k) {
                 t--;
             }
-
-            k = k - (1 << t);
-
-            ans += operations[t];
-        }
-
-        return (char)('a' + (ans % 26));
-    }
-
-    public char mySol_tle(long k, int[] operations) {
-        String s = "a";
-
-        int index = 0;
-        
-        while (s.length() < k) {
-            if (operations[index++] == 0) {
-                s += s;
-            } else {
-                s += next(s);
+            k = k - (1L << t);
+            if (operations[t] != 0) {
+                ans++;
             }
         }
-
-        return s.charAt((int)k - 1);
+        return (char) ('a' + (ans % 26));
     }
-
-    private String next(String s) {
-		StringBuilder sb = new StringBuilder();
-
-		for (char c : s.toCharArray()) {
-			sb.append(c == 'z' ? 'a' : (char)(c + 1));
-		}
-
-		return sb.toString();
-	}
 }
