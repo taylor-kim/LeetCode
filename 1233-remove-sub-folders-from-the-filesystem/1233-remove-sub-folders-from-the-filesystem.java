0bc864904159063f43751f13cda2f4a4b2cb0b95
team-hungrian <def.hunger@gmail.com>
2025-07-19 16:27:41 +0900

    Time: 118 ms (7.03%), Space: 69.5 MB (12.5%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,6 +1,55 @@
 class Solution {
     public List<String> removeSubfolders(String[] folder) {
-        return mySol(folder);
+        return try_trie(folder);
+    }
+
+    public List<String> try_trie(String[] folder) {
+        Trie dic = new Trie();
+
+        for (String s : folder) dic.build(s);
+
+        List<String> ans = new ArrayList();
+
+        for (String s : folder) {
+            if (dic.isNotSubFolder(s)) {
+                ans.add(s);
+            }
+        }
+
+        return ans;
+    }
+
+    public class Trie {
+        boolean isFolder;
+        Map<String, Trie> map = new HashMap();
+
+        public void build(String folder) {
+            Trie t = this;
+
+            String[] paths = folder.split("/");
+
+            for (int i = 1; i < paths.length; i++) {
+                t = t.map.computeIfAbsent(paths[i], k -> new Trie());
+            }
+
+            t.isFolder = true;
+        }
+
+        boolean isNotSubFolder(String folder) {
+            Trie t = this;
+
+            String[] paths = folder.split("/");
+            
+            for (int i = 1; i < paths.length; i++) {
+                t = t.map.computeIfAbsent(paths[i], k -> new Trie());
+
+                if (t.isFolder && i < paths.length - 1) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 
     public List<String> mySol(String[] folder) {
