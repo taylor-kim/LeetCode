6be10e97b686be0c95975d4bd4cd62e29cec603b
team-hungrian <def.hunger@gmail.com>
2025-07-21 19:32:30 +0900

    Time: 43 ms (30.05%), Space: 45.7 MB (78.54%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
@@ -2,18 +2,22 @@ class Solution {
     public String makeFancyString(String s) {
         return mySol(s);
     }
-    
-    public String mySol(String s) {
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < s.length(); i++) {
+    public String mySol(String s) {
+        if (s.length() < 3) return s;
+
+        StringBuilder ans = new StringBuilder();
+        ans.append(s.charAt(0));
+        ans.append(s.charAt(1));
+
+        for (int i = 2; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            if (sb.length() < 2 || sb.charAt(sb.length() - 1) != c || sb.charAt(sb.length() - 2) != c) {
-                sb.append(c);
+            if (ans.charAt(ans.length() - 1) != c || ans.charAt(ans.length() - 2) != c) {
+                ans.append(c);
             }
         }
 
-        return sb.toString();
+        return ans.toString();
     }
 }
