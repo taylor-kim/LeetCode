262ccf4b671ab8e50bf09d3ca8b2a27582994eeb
team-hungrian <def.hunger@gmail.com>
2025-07-27 00:43:28 +0900

    Time: 14 ms (100%), Space: 157.3 MB (5.13%) - LeetHub

--- 3480-maximize-subarrays-after-removing-one-conflicting-pair/3480-maximize-subarrays-after-removing-one-conflicting-pair.java
+++ 3480-maximize-subarrays-after-removing-one-conflicting-pair/3480-maximize-subarrays-after-removing-one-conflicting-pair.java
@@ -1,6 +1,55 @@
 class Solution {
     public long maxSubarrays(int n, int[][] conflictingPairs) {
-        return others(n, conflictingPairs);
+        return official(n, conflictingPairs);
+    }
+
+    public long official(int n, int[][] conflictingPairs) {
+        int[] rightMin1 = new int[n + 1];
+        int[] rightMin2 = new int[n + 1];
+
+        Arrays.fill(rightMin1, Integer.MAX_VALUE);
+        Arrays.fill(rightMin2, Integer.MAX_VALUE);
+
+        for (int[] pair : conflictingPairs) {
+            int a = Math.min(pair[0], pair[1]);
+            int b = Math.max(pair[0], pair[1]);
+
+            if (rightMin1[a] > b) {
+                rightMin2[a] = rightMin1[a];
+                rightMin1[a] = b;
+            } else if (rightMin2[a] > b) {
+                rightMin2[a] = b;
+            }
+        }
+
+        long ans = 0;
+
+        long[] delCount = new long[n + 1];
+        int ri = n;
+        int right2 = Integer.MAX_VALUE;
+
+        for (int left = n; left > 0; left--) {
+            if (rightMin1[ri] > rightMin1[left]) {
+                right2 = Math.min(right2, rightMin1[ri]);
+                ri = left;
+            } else {
+                right2 = Math.min(right2, rightMin1[left]);
+            }
+
+            ans += Math.min(rightMin1[ri], n + 1) - left;
+
+            delCount[ri] += 
+                Math.min(Math.min(right2, rightMin2[ri]), n + 1)
+                - Math.min(rightMin1[ri], n + 1);
+        }
+
+        long max = 0;
+
+        for (long count : delCount) {
+            max = Math.max(max, count);
+        }
+
+        return ans + max;
     }
 
     // https://www.youtube.com/watch?v=UmshxbwMmrk
