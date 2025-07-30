d7faa795e3438969abdb991e88e5cfc1d3ac909a
team-hungrian <def.hunger@gmail.com>
2025-07-30 20:09:52 +0900

    Time: 4 ms (22.41%), Space: 61.1 MB (29.46%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.java
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.java
@@ -1,22 +1,94 @@
 class Solution {
-
     public int longestSubarray(int[] nums) {
-        int maxVal = 0, ans = 0, currentStreak = 0;
+        return try_20250730(nums);
+    }
+
+    public int try_20250730(int[] nums) {
+        int n = nums.length;
+        int ans = 1;
+        int max = nums[0];
+        int and = nums[0];
+
+        int left = 0;
+
+        for (int right = 1; right < n; right++) {
+            int num = nums[right];
+
+            and &= num;
+
+            if (and < max || max < num) {
+                if (max < num) {
+                    ans = 1;
+                }
+                max = Math.max(max, num);
+                left = right;
+                and = num;
+            } else if (and == max) {
+                ans = Math.max(ans, right - left + 1);
+            }
+        }
+
+        return ans;
+    }
+
+    private int getNumer(int[] bits) {
+        int num = 0;
+
+        for (int i = 0; i < bits.length; i++) {
+            int bit = 1 << i;
+
+            if (bits[bit] != 0) {
+                num |= bit;
+            }
+        }
+
+        return num;
+    }
+
+    public int official_oneway(int[] nums) {
+        int ans = 0;
+        int max = 0;
+        int count = 0;
 
         for (int num : nums) {
-            if (maxVal < num) {
-                maxVal = num;
-                ans = currentStreak = 0;
+            if (max < num) {
+                max = num;
+                ans = 0;
+                count = 0;
             }
 
-            if (maxVal == num) {
-                currentStreak++;
+            if (max == num) {
+                count++;
             } else {
-                currentStreak = 0;
+                count = 0;
             }
 
-            ans = Math.max(ans, currentStreak);
+            ans = Math.max(ans, count);
         }
+
+        return ans;
+    }
+
+    public int mySol(int[] nums) {
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) max = nums[i];
+        }
+
+        int ans = 1;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == max) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            ans = Math.max(ans, count);
+        }
+
         return ans;
     }
 }
