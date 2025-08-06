72d5f472635905bddf134191aec5deab01240371
team-hungrian <def.hunger@gmail.com>
2025-08-07 01:57:05 +0900

    Time: 217 ms (26.13%), Space: 58.8 MB (81.08%) - LeetHub

--- 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.java
+++ 3479-fruits-into-baskets-iii/3479-fruits-into-baskets-iii.java
@@ -1,6 +1,50 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        return official(fruits, baskets);
+        return official_sqaure_root_decomposition(fruits, baskets);
+    }
+
+    public int official_sqaure_root_decomposition(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        int m = (int)Math.sqrt(n);
+        int section = (n + m - 1) / m;
+
+        int[] sections = new int[section];
+
+        for (int i = 0; i < n; i++) {
+            sections[i / m] = Math.max(sections[i / m], baskets[i]);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            for (int s = 0; s < section; s++) {
+                if (sections[s] < fruits[i]) continue;
+
+                int max = 0;
+
+                for (int j = 0; j < m; j++) {
+                    int index = s * m + j;
+
+                    if (index < n) {
+                        if (baskets[index] >= fruits[i] && count == 1) {
+                            baskets[index] = 0;
+                            count = 0;
+                        }
+
+                        max = Math.max(max, baskets[index]);
+                    }
+                }
+
+                sections[s] = max;
+
+                if (count == 0) break;
+            }
+
+            ans += count;
+        }
+
+        return ans;
     }
 
     public int official(int[] fruits, int[] baskets) {
@@ -77,6 +121,8 @@ class Solution {
         }
 
         private void update(int node, int left, int right, int pos, int val) {
+            if (pos < left || pos > right) return;
+            
             if (left == right) {
                 tree[node] = val;
                 return;
@@ -84,13 +130,20 @@ class Solution {
 
             int mid = left + (right - left) / 2;
 
-            if (pos <= mid) {
-                update(node * 2, left, mid, pos, val);
-            } else {
-                update(node * 2 + 1, mid + 1, right, pos, val);
-            }
+            update(node * 2, left, mid, pos, val);
+            update(node * 2 + 1, mid + 1, right, pos, val);
 
             tree[node] = Math.max(tree[node * 2], tree[node * 2 + 1]);
+
+            // int mid = left + (right - left) / 2;
+
+            // if (pos <= mid) {
+            //     update(node * 2, left, mid, pos, val);
+            // } else {
+            //     update(node * 2 + 1, mid + 1, right, pos, val);
+            // }
+
+            // tree[node] = Math.max(tree[node * 2], tree[node * 2 + 1]);
         }
     }
 }
