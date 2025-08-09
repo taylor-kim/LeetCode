60655237526926c1442a5845f5346cd445391684
team-hungrian <def.hunger@gmail.com>
2025-08-09 14:56:22 +0900

    Time: 1 ms (35.75%), Space: 41.3 MB (17.04%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,35 +1,21 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return others_bit(n);
+        return mySol(n);
     }
-    
-    public boolean others_bit(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
-    }
-    
-    public boolean rec(int n) {
-        if (n <= 0) return false;
-        
-        if (n == 1 || n == 2) return true;
-        
-        if (n % 2 == 1) return false;
-        
-        return rec(n / 2);
-    }
-    
+
     public boolean mySol(int n) {
-        if (n < 0) return false;
-        
-        int bitCount = 0;
-        
-        for (int i = 0; i < 31; i++) {
-            int bit = 1 << i;
-            
-            if ((n & bit) != 0) {
-                bitCount++;
-            }
+        int count = 0;
+
+        if (n <= 0) {
+            return false;
         }
-        
-        return bitCount == 1;
+
+        for (int i = 0; i < 31; i++) {
+            if ((n & (1 << i)) != 0) count++;
+
+            if (count > 1) return false;
+        }
+
+        return true;
     }
 }
