25c48cb8c86ab94018ad2a7281510bc9292d6f85
team-hungrian <def.hunger@gmail.com>
2025-08-12 02:11:18 +0900

    Time: 16 ms (84.95%), Space: 106.4 MB (5.38%) - LeetHub

---
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.java
@@ -0,0 +1,82 @@
+class Solution {
+    public int[] productQueries(int n, int[][] queries) {
+        return mySol(n, queries);
+    }
+
+    public int[] mySol(int n, int[][] queries) {
+        List<Integer> powers = getPowers(n);
+        SegmentTree st = new SegmentTree(powers);
+        st.init(1, 0, powers.size() - 1);
+
+        // System.out.println(Arrays.toString(st.data));
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
+
+            ans[i] = st.query(1, 0, powers.size() - 1, query[0], query[1]);
+        }
+
+        return ans;
+    }
+
+    public List<Integer> getPowers(int n) {
+        List<Integer> list = new LinkedList();
+
+        for (int i = 0; i < 32; i++) {
+            int bit = 1 << i;
+
+            if ((n & bit) != 0) {
+                list.add(bit);
+            }
+        }
+
+        return list;
+    }
+
+    public class SegmentTree {
+        private int mod = (int)1e9 + 7;
+        private List<Integer> powers;
+
+        private int[] data;
+
+        public SegmentTree(List<Integer> powers) {
+            this.data = new int[powers.size() * 4 + 1];
+            this.powers = powers;
+        }
+
+        public int init(int node, int left, int right) {
+            if (left == right) {
+                return data[node] = powers.get(left);
+            }
+
+            if (left > right) return 1;
+
+            int mid = left + (right - left) / 2;
+
+            long lv = (long)init(node * 2, left, mid);
+            long rv = (long)init(node * 2 + 1, mid + 1, right);
+
+            return this.data[node] = (int)((lv * rv) % mod);
+        }
+
+        public int query(int node, int left, int right, int qLeft, int qRight) {
+            if (qRight < left || right < qLeft) return 1;
+
+            if (qLeft <= left && right <= qRight) {
+                return data[node];
+            }
+
+            int mid = left + (right - left) / 2;
+
+            long lv = (long)query(node * 2, left, mid, qLeft, qRight);
+            long rv = (long)query(node * 2 + 1, mid + 1, right, qLeft, qRight);
+
+            // System.out.println(String.format("node:%d, left:%d, right:%d, qLeft:%d, qRight:%d",
+            //                     node, left, right, qLeft, qRight));
+
+            return (int)((lv * rv) % mod);
+        }
+    }
+}
