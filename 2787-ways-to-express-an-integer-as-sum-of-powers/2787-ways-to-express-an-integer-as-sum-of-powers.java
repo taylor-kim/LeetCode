5265187c411a16d48dfab9df68668ed9a6f8d531
team-hungrian <def.hunger@gmail.com>
2025-08-13 01:27:29 +0900

    Time: 63 ms (66.32%), Space: 45 MB (51.58%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,6 +1,26 @@
 class Solution {
     public int numberOfWays(int n, int x) {
-        return mySol(n, x);
+        return official_bottomup(n, x);
+    }
+
+    public int official_bottomup(int n, int x) {
+        int[][] dp = new int[n + 1][n + 1];
+        dp[0][0] = 1;
+        int mod = (int)1e9 + 7;
+
+        for (int i = 1; i <= n; i++) {
+            int power = (int)Math.pow(i, x);
+
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j];
+
+                if (j >= power) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - power]) % mod;
+                }
+            }
+        }
+
+        return dp[n][n];
     }
 
     public int mySol(int n, int x) {
