6b9d91644d91d2fdeddade2da19128d2704d3599
team-hungrian <def.hunger@gmail.com>
2025-08-16 16:31:58 +0900

    Time: 5 ms (78.9%), Space: 62.1 MB (77.93%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.java
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,17 +1,58 @@
 class Solution {
-
     public int maximumLength(int[] nums) {
-        int res = 0;
-        int[][] patterns = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } };
-        for (int[] pattern : patterns) {
-            int cnt = 0;
-            for (int num : nums) {
-                if (num % 2 == pattern[cnt % 2]) {
-                    cnt++;
-                }
+        return try_20250816_2(nums);
+    }
+
+    public int try_20250816_2(int[] nums) {
+        int zeros = 0;
+        int ones = 0;
+        int pingPong = 1;
+        int prev = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int mod = num % 2;
+
+            if (mod == 0) {
+                zeros++;
+            } else {
+                ones++;
             }
-            res = Math.max(res, cnt);
+
+            if (nums[prev] % 2 != mod) {
+                pingPong++;
+                prev = i;
+            }
         }
-        return res;
+
+        return Math.max(pingPong, Math.max(zeros, ones));
+    }
+
+    public int try_20250816_fail(int[] nums) {
+        return Math.max(
+            topdown(nums, 0, 0, 0),
+            // topdown(nums, 1, 0, 0)
+            0
+        );
+    }
+
+    public int topdown(int[] nums, int mod, int prev, int index) {
+        if (index >= nums.length) return 0;
+
+        int ans = 0;
+
+        if (prev == index) {
+            int include = 1 + topdown(nums, mod, index, index + 1);
+            int exclude = topdown(nums, mod, prev, index + 1);
+
+            ans = Math.max(include, exclude);
+        } else if ((nums[prev] + nums[index]) % 2 == mod) {
+            ans = 1 + topdown(nums, mod, index, index + 1);
+        } else {
+            ans = Math.max(ans, topdown(nums, mod, prev, index + 1));
+            ans = Math.max(ans, topdown(nums, mod, index, index + 1));
+        }
+
+        return ans;
     }
 }
