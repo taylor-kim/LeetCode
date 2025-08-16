5630b0110340949798572e2af30094fa10942368
team-hungrian <def.hunger@gmail.com>
2025-08-16 18:54:31 +0900

    Time: 5 ms (78.9%), Space: 59.6 MB (99.95%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.java
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.java
@@ -1,6 +1,61 @@
 class Solution {
     public int maximumLength(int[] nums) {
-        return try_20250816_2(nums);
+        return tryAgain_lis(nums);
+    }
+
+    public int tryAgain_lis(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[2][n];
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            // dp[0][i] = 1;
+            // dp[1][i] = 1;
+            // for (int j = 0; j < i; j++) {
+            //     int mod = (nums[j] + nums[i]) % 2;
+            //     dp[mod][i] = Math.max(dp[mod][i], dp[mod][j] + 1);
+            //     ans = Math.max(ans, dp[mod][i]);
+            // }
+            for (int mod = 0; mod < 2; mod++) {
+                dp[mod][num % 2] = dp[num % 2][mod] + 1;
+                ans = Math.max(ans, dp[mod][num % 2]);
+            }
+        }
+
+        return ans;
+    }
+
+    public int origin_mySol(int[] nums) {
+        int n = nums.length;
+        int[] oddDp = new int[n];
+        int[] evenDp = new int[n];
+        oddDp[0] = 1;
+        evenDp[0] = 1;
+
+        int ans = 1;
+
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 0; j < i; j++) {
+                if ((nums[j] + nums[i]) % 2 == 0) {
+                    evenDp[i] = Math.max(evenDp[i], evenDp[j] + 1);
+                    // oddDp[i] = 1;
+                } else {
+                    oddDp[i] = Math.max(oddDp[i], oddDp[j] + 1);
+                    // evenDp[i] = 1;
+                }
+            }
+
+            ans = Math.max(ans, Math.max(evenDp[i], oddDp[i]));
+        }
+
+        // System.out.println(Arrays.toString(evenDp));
+        // System.out.println(Arrays.toString(oddDp));
+
+        // return Math.max(oddDp[n - 1], evenDp[n - 1]);
+        return ans;
     }
 
     public int try_20250816_2(int[] nums) {
