063c8f5661199298fb4c8536ff68b63cb54fba83
team-hungrian <def.hunger@gmail.com>
2025-08-16 18:14:45 +0900

    Time: 66 ms (10.65%), Space: 55.3 MB (7.39%) - LeetHub

--- 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.java
+++ 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.java
@@ -1,15 +1,50 @@
 class Solution {
-
     public int maximumLength(int[] nums, int k) {
+        return mySol(nums, k);
+    }
+
+    public int mySol(int[] nums, int k) {
+        // k == 3
+        // 0,1,2,0,1,2
+        // 3,4,5,3,4,5
+
+        // k == 2
+        // 1,2,3,4,5
+        // 1,0,1,0,1
+        //
         int[][] dp = new int[k][k];
-        int res = 0;
+        int[] same = new int[k];
+        int[] set = new int[k];
+
+        int ans = 0;
+
         for (int num : nums) {
-            num %= k;
-            for (int prev = 0; prev < k; prev++) {
-                dp[prev][num] = dp[num][prev] + 1;
-                res = Math.max(res, dp[prev][num]);
+            int b = num % k;
+            
+            for (int mod = 0; mod < k; mod++) {
+                int a = (mod - b + k) % k;
+
+                if (dp[mod][a] > 0) {
+                    dp[mod][b] = Math.max(dp[mod][b], dp[mod][a] + 1);
+                    ans = Math.max(ans, dp[mod][b]);
+                }
             }
+
+            for (int mod = 0; mod < k; mod++) {
+                if (dp[mod][b] == 0) {
+                    dp[mod][b] = 1;
+                }
+            }
+            
+            same[b]++;
+
+            // System.out.println("\n");
         }
-        return res;
+
+        for (int i = 0; i < k; i++) {
+            // ans = Math.max(ans, same[i]);
+        }
+
+        return ans;
     }
 }
