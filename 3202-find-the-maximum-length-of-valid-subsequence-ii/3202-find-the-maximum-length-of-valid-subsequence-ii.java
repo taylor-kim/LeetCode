d153de3b2e1d7e6c230417a833c51bace4c48a12
team-hungrian <def.hunger@gmail.com>
2025-08-16 18:27:15 +0900

    Time: 37 ms (59.37%), Space: 55.9 MB (5.15%) - LeetHub

--- 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.java
+++ 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.java
@@ -1,6 +1,21 @@
 class Solution {
     public int maximumLength(int[] nums, int k) {
-        return mySol(nums, k);
+        return official_dp(nums, k);
+    }
+
+    public int official_dp(int[] nums, int k) {
+        int[][] dp = new int[k][k];
+        int ans = 0;
+
+        for (int num : nums) {
+            num %= k;
+            for (int prev = 0; prev < k; prev++) {
+                dp[prev][num] = dp[num][prev] + 1;
+                ans = Math.max(ans, dp[prev][num]);
+            }
+        }
+
+        return ans;
     }
 
     public int mySol(int[] nums, int k) {
