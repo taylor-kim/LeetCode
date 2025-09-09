ab03be9d1d69a960cec8557fd3a633d428a51aae
team-hungrian <def.hunger@gmail.com>
2025-09-09 10:19:51 +0900

    Add solution for Convert Integer to the Sum of Two No-Zero Integers

--- LeetCode/Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers/Solution.java
+++ LeetCode/Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers/Solution.java
@@ -1,33 +1,11 @@
-public class Solution {
-    public int[] getNoZeroIntegers(int n) {
-        return mySol(n);
-    }
+x1 = n, x2 = 0, mult = 1
+while n > 1:             // Why not n > 0? See Explanations below
+    steal = 1
+    if (n - steal) % 10 == 0: // if n % 10 == 1:
+        steal += 1       // Observe: steal amount at each digit is either 1 or 2
+    x1 -= steal * m
+    x2 += steal * m
+    n = (n - steal) / 10 // Move digits one to the right
+    m *= 10              // Advance the multiplier
 
-    public int[] mySol(int n) {
-        return topdown(0, n);
-    }
-
-    private int[] topdown(int a, int b) {
-        if (b <= 0) return null;
-
-        if (isOk(a) && isOk(b)) {
-            return new int[] {a, b};
-        }
-
-        return topdown(a + 1, b - 1);
-    }
-
-    private boolean isOk(int n) {
-        if (n <= 0) return false;
-
-        while (n > 0) {
-            int digit = n % 10;
-
-            if (digit == 0) return false;
-
-            n /= 10; 
-        }
-
-        return true;
-    }
-}
+return [x1, x2]
