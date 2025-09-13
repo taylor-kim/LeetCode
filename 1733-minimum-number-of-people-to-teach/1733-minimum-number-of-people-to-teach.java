d46ec3646a83be6ff62290e0bab8e3f6ce212884
team-hungrian <def.hunger@gmail.com>
2025-09-13 18:08:40 +0900

    Time: 179 ms (5.06%), Space: 65.9 MB (5.05%) - LeetHub

--- 1733-minimum-number-of-people-to-teach/1733-minimum-number-of-people-to-teach.java
+++ 1733-minimum-number-of-people-to-teach/1733-minimum-number-of-people-to-teach.java
@@ -1,6 +1,69 @@
 class Solution {
     public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
-        return mySol(n, languages, friendships);
+        return mySol_improve(n, languages, friendships);
+    }
+
+    public int mySol_improve(int n, int[][] languages, int[][] friendships) {
+        int ans = languages.length;
+
+        Set<Integer>[] langs = new Set[languages.length];
+        Set<Integer>[] peopleCanSpeak = new Set[n + 1];
+
+        for (int lang = 1; lang <= n; lang++) {
+            peopleCanSpeak[lang] = new HashSet();
+        }
+
+        for (int i = 0; i < languages.length; i++) {
+            langs[i] = new HashSet();
+
+            for (int lang : languages[i]) {
+                langs[i].add(lang);
+                peopleCanSpeak[lang].add(i);
+            }
+        }
+
+        Map<Integer, Set<Integer>> canComm = new HashMap();
+
+        for (int[] friend : friendships) {
+            int a = friend[0] - 1;
+            int b = friend[1] - 1;
+
+            canComm.computeIfAbsent(a, key -> new HashSet());
+            canComm.computeIfAbsent(b, key -> new HashSet());
+
+            for (int lang = 1; lang <= n; lang++) {
+                if (peopleCanSpeak[lang].contains(a) && peopleCanSpeak[lang].contains(b)) {
+                    canComm.get(a).add(b);
+                    canComm.get(b).add(a);
+                    break;
+                }
+            }
+        }
+
+        for (int lang = 1; lang <= n; lang++) {
+            Set<Integer> teach = new HashSet();
+
+            for (int[] friend : friendships) {
+                int a = friend[0] - 1;
+                int b = friend[1] - 1;
+
+                if (canComm.get(a).contains(b)) {
+                    continue;
+                }
+
+                if (!langs[a].contains(lang)) {
+                    teach.add(a);
+                }
+
+                if (!langs[b].contains(lang)) {
+                    teach.add(b);
+                }
+            }
+
+            ans = Math.min(ans, teach.size());
+        }
+
+        return ans;
     }
 
     public int mySol(int n, int[][] languages, int[][] friendships) {
