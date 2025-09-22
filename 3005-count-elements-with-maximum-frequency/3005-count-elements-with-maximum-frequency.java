52b19ca3c3a3b3f0b58f6ee709bbded9a32b0f65
team-hungrian <def.hunger@gmail.com>
2025-09-22 15:33:38 +0900

    Time: 1 ms (99.44%), Space: 42.6 MB (23.68%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -1,77 +1,24 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        return official_one_pass(nums);
+        return mySol(nums);
     }
-    
-    public int official_one_pass(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap();
-        
-        int maxFreq = 0;
-        int ans = 0;
-        
-        for (int num : nums) {
-            freq.computeIfAbsent(num, k -> 0);
-            
-            int nextFreq = freq.get(num) + 1;
-            
-            freq.put(num, nextFreq);
-            
-            if (maxFreq < nextFreq) {
-                maxFreq = nextFreq;
-                ans = maxFreq;
-            } else if (maxFreq == nextFreq) {
-                ans += maxFreq;
-            }
-        }
-        
-        return ans;
-    }
-    
-    public int official_sort(int[] nums) {
-        int[] freq = new int[101];
-        
-        int maxFreq = 0;
-        
-        for (int num : nums) {
-            freq[num]++;
-        }
-        
-        Arrays.sort(freq);
-        
-        int maxFreqIndex = freq.length - 1;
-        int ans = freq[maxFreqIndex];
-        
-        while (maxFreqIndex > 0 && freq[maxFreqIndex] == freq[maxFreqIndex - 1]) {
-            ans += freq[maxFreqIndex];
-            maxFreqIndex--;
-        }
-        
-        return ans;
-    }
-    
+
     public int mySol(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap();
-        
-        int maxFreq = 0;
-        
-        for (int num : nums) {
-            freq.computeIfAbsent(num, k -> 0);
-            
-            int nextFreq = freq.get(num) + 1;
-            
-            freq.put(num, nextFreq);
-            
-            maxFreq = Math.max(maxFreq, nextFreq);
-        }
-        
-        int ans = 0;
-        
-        for (int key : freq.keySet()) {
-            if (freq.get(key) == maxFreq) {
-                ans += maxFreq;
+        int[] freq = new int[101];
+
+        for (int num : nums) freq[num]++;
+
+        int max = 0;
+        int totalCount = 0;
+
+        for (int count : freq) {
+            if (max < count) {
+                totalCount = max = count;
+            } else if (max == count) {
+                totalCount += count;
             }
         }
-        
-        return ans;
+
+        return totalCount;
     }
 }
