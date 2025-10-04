a21ca4bb24bbd6107b6a6c95ef5e2b02c67a2d76
team-hungrian <def.hunger@gmail.com>
2025-10-04 17:43:09 +0900

    Time: 3 ms (96.25%), Space: 40.4 MB (100%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,59 +1,28 @@
 class Solution {
-    public int maxArea(int[] height) {
-        return mySol2(height);
-    }
+    public int maxArea(int[] heights) {
+        int maxArea = 0;
 
-    public int mySol2(int[] height) {
-        int n = height.length;
         int left = 0;
-        int right = n - 1;
-
-        int ans = 0;
+        int right = heights.length - 1;
 
         while (left < right) {
-            ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
+            int leftHeight = heights[left];
+            int rightHeight = heights[right];
 
-            if (height[left] < height[right]) {
+            int width = right - left;
+            int height = Math.min(leftHeight, rightHeight);
+
+            int area = width * height;
+
+            maxArea = Math.max(area, maxArea);
+
+            if (leftHeight < rightHeight) {
                 left++;
-            } else if (height[left] > height[right]) {
-                right--;
-            } else {
-                left++;
-            }
-        }
-
-        left = 0;
-        right = n - 1;
-
-        while (left < right) {
-            ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
-
-            if (height[left] < height[right]) {
-                left++;
-            } else if (height[left] > height[right]) {
-                right--;
             } else {
                 right--;
             }
         }
 
-        return ans;
-    }
-
-    public int mySol_fail(int[] height) {
-        int ans = 0;
-        int heighest = 0;
-        int left = 0;
-
-        for (int right = 0; right < height.length; right++) {
-            ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
-
-            if (heighest < height[right]) {
-                heighest = height[right];
-                left = right;
-            }
-        }
-
-        return ans;
+        return maxArea;
     }
 }
