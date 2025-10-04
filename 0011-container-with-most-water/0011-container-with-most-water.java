0c76bffa442b8989ad2c33d1d32771cc6fa803f7
team-hungrian <def.hunger@gmail.com>
2025-10-04 17:43:20 +0900

    Time: 5 ms (77.27%), Space: 57.6 MB (81.89%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,28 +1,42 @@
 class Solution {
-    public int maxArea(int[] heights) {
-        int maxArea = 0;
+    public int maxArea(int[] height) {
+        return mySol2(height);
+    }
 
+    public int mySol2(int[] height) {
+        int n = height.length;
         int left = 0;
-        int right = heights.length - 1;
+        int right = n - 1;
+
+        int ans = 0;
 
         while (left < right) {
-            int leftHeight = heights[left];
-            int rightHeight = heights[right];
+            ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
 
-            int width = right - left;
-            int height = Math.min(leftHeight, rightHeight);
-
-            int area = width * height;
-
-            maxArea = Math.max(area, maxArea);
-
-            if (leftHeight < rightHeight) {
+            if (height[left] <= height[right]) {
                 left++;
             } else {
                 right--;
             }
         }
 
-        return maxArea;
+        return ans;
+    }
+
+    public int mySol_fail(int[] height) {
+        int ans = 0;
+        int heighest = 0;
+        int left = 0;
+
+        for (int right = 0; right < height.length; right++) {
+            ans = Math.max(ans, (right - left) * Math.min(height[left], height[right]));
+
+            if (heighest < height[right]) {
+                heighest = height[right];
+                left = right;
+            }
+        }
+
+        return ans;
     }
 }
