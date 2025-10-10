51b771259755418e7763c5f5e46aa6c235682292
team-hungrian <def.hunger@gmail.com>
2025-10-10 18:18:47 +0900

    Time: 29 ms (5.52%), Space: 77.8 MB (5.52%) - LeetHub

---
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maximumEnergy(int[] energy, int k) {
+        return mySol2(energy, k);
+    }
+
+    public int mySol2(int[] energy, int k) {
+        int ans = Integer.MIN_VALUE;
+
+        Integer[] memo = new Integer[energy.length];
+
+        for (int i = 0; i < energy.length; i++) {
+            ans = Math.max(ans, topdown(i, energy, k, memo));
+        }
+
+        return ans;
+    }
+
+    public int topdown(int index, int[] energy, int k, Integer[] memo) {
+        if (index >= energy.length) return 0;
+
+        if (memo[index] != null) return memo[index];
+
+        return memo[index] = energy[index] + topdown(index + k, energy, k, memo);
+    }
+
+    public int mySol_fail(int[] energy, int k) {
+        int n = energy.length;
+        int[] dp = new int[n + k];
+
+        for (int i = 0; i < n; i++) {
+            dp[i + k] = energy[i] + dp[i];
+        }
+
+        return dp[n + k - 1];
+    }
+}
