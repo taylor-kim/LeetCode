374a044baa124fd81953bc878b39e5ac64b8817a
team-hungrian <def.hunger@gmail.com>
2025-10-16 02:32:25 +0900

    Time: 138 ms (9.57%), Space: 88.6 MB (52.13%) - LeetHub

--- 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.java
+++ 3350-adjacent-increasing-subarrays-detection-ii/3350-adjacent-increasing-subarrays-detection-ii.java
@@ -1,6 +1,65 @@
 class Solution {
     public int maxIncreasingSubarrays(List<Integer> nums) {
-        return mySol(nums);
+        return mySol2_with_bs(nums);
+    }
+
+    public int mySol2_with_bs(List<Integer> nums) {
+        int n = nums.size();
+        int max = n / 2;
+        int lo = 1;
+        int hi = max;
+        int ans = 0;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (mySol2(nums, mid)) {
+                lo = mid + 1;
+                ans = mid;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean mySol2(List<Integer> nums, int k) {
+        int n = nums.size();
+        int a = 0;
+        int b = k;
+
+        int ans = 0;
+
+        while (b < n) {
+            int count = 1;
+            int prevAv = nums.get(a);
+            int prevBv = nums.get(b);
+            while (a + count < n && b + count < n) {
+                int av = nums.get(a + count);
+
+                if (prevAv >= av) break;
+
+                prevAv = av;
+
+                int bv = nums.get(b + count);
+
+                if (prevBv >= bv) break;
+
+                prevBv = bv;
+
+                count++;
+
+                if (count == k) return true;
+            }
+
+            if (count >= k) return true;
+
+            a += count;
+            b += count;
+        }
+
+        return false;
     }
 
     public int mySol(List<Integer> nums) {
