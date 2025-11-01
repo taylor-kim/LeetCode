24c9e5258c02779bd6643c5bf8e85cca6d94a544
team-hungrian <def.hunger@gmail.com>
2025-11-01 18:37:38 +0900

    Time: 22 ms (26.6%), Space: 178.4 MB (5.52%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
@@ -10,45 +10,22 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        return mySol_improved(nums, head);
-    }
-
-    public ListNode mySol_improved(int[] nums, ListNode head) {
-        ListNode dummy = new ListNode(0, head);
-        ListNode node = dummy;
-
-        Set<Integer> set = new HashSet();
-
-        for (int num : nums) set.add(num);
-
-        while (node.next != null) {
-            if (set.contains(node.next.val)) {
-                node.next = node.next.next;
-            } else {
-                node = node.next;
-            }
-        }
-
-        return dummy.next;
+        return mySol(nums, head);
     }
 
     public ListNode mySol(int[] nums, ListNode head) {
-        ListNode dummy = new ListNode(0, head);
-        ListNode prev = dummy;
-        ListNode node = dummy.next;
-
         Set<Integer> set = new HashSet();
-
         for (int num : nums) set.add(num);
 
-        while (node != null) {
-            if (set.contains(node.val)) {
-                prev.next = node.next;
-            } else {
-                prev = node;
-            }
+        ListNode dummy = new ListNode(0, head);
+        head = dummy;
 
-            node = prev.next;
+        while (head.next != null) {
+            if (set.contains(head.next.val)) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
         }
 
         return dummy.next;
