0c7664ee72eb803da0507ad0bcb340c897a05719
team-hungrian <def.hunger@gmail.com>
2025-11-03 18:33:33 +0900

    Time: 7 ms (93.01%), Space: 93 MB (5.68%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,6 +1,31 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        return mySol(colors, neededTime);
+        return mySol_iter(colors, neededTime);
+    }
+
+    public int mySol_iter(String colors, int[] neededTime) {
+        int ans = 0;
+        int left = 0;
+        char prev = colors.charAt(0);
+        
+        for (int right = 1; right < colors.length(); right++) {
+            char cur = colors.charAt(right);
+
+            if (prev != cur) {
+                left = right;
+                prev = cur;
+            } else {
+                if (neededTime[left] < neededTime[right]) {
+                    ans += neededTime[left];
+                    left = right;
+                    prev = cur;
+                } else {
+                    ans += neededTime[right];
+                }
+            }
+        }
+
+        return ans;
     }
 
     public int mySol(String colors, int[] neededTime) {
