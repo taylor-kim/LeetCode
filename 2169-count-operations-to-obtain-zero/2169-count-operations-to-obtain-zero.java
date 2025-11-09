745e2b7d842785f83a73fb8cb2175e73e42692df
team-hungrian <def.hunger@gmail.com>
2025-11-09 19:03:11 +0900

    Time: 0 ms (100%), Space: 42.3 MB (11.81%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -1,6 +1,22 @@
 class Solution {
     public int countOperations(int num1, int num2) {
-        return mySol2(num1, num2);
+        return official(num1, num2);
+    }
+
+    public int official(int num1, int num2) {
+        int ans = 0;
+
+        while (num1 > 0 && num2 > 0) {
+            ans += num1 / num2;
+
+            num1 %= num2;
+
+            num1 += num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+        }
+
+        return ans;
     }
 
     public int mySol2(int num1, int num2) {
