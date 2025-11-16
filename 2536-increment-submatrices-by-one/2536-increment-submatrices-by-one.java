a976c8f2abd512e1289f788c6cac0331ff27eae7
team-hungrian <def.hunger@gmail.com>
2025-11-16 18:52:43 +0900

    Time: 12 ms (61.12%), Space: 87.1 MB (8.25%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
@@ -1,6 +1,36 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        return mySol(n, queries);
+        return editorial(n, queries);
+    }
+
+    public int[][] editorial(int n, int[][] queries) {
+        int[][] diffArray = new int[n + 1][n + 1];
+
+        for (int[] q : queries) {
+            int r1 = q[0];
+            int c1 = q[1];
+            int r2 = q[2];
+            int c2 = q[3];
+
+            diffArray[r1][c1] += 1;
+            diffArray[r2 + 1][c1] -= 1;
+            diffArray[r1][c2 + 1] -= 1;
+            diffArray[r2 + 1][c2 + 1] += 1;
+        }
+
+        int[][] mat = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int top = i == 0 ? 0 : mat[i - 1][j];
+                int left = j == 0 ? 0 : mat[i][j - 1];
+                int topLeft = (i == 0 || j == 0) ? 0 : mat[i - 1][j - 1];
+
+                mat[i][j] = diffArray[i][j] + top + left - topLeft;
+            }
+        }
+
+        return mat;
     }
 
     public int[][] mySol(int n, int[][] queries) {
