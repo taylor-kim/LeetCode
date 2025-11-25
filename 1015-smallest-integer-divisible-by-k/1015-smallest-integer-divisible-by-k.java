1cf4d775a944b0e88d5461743296ce4e1b7da695
team-hungrian <def.hunger@gmail.com>
2025-11-26 03:23:38 +0900

    Time: 3 ms (17%), Space: 42.7 MB (8.42%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
@@ -1,6 +1,33 @@
 class Solution {
     public int smallestRepunitDivByK(int k) {
-        return mySol(k);
+        return mySol_improved(k);
+    }
+
+    public int editorial(int k) {
+        for (int odd = 1; odd < k; odd++) {
+            // odd = 
+        }
+
+        return 0;
+    }
+
+    public int mySol_improved(int k) {
+        int n = 1;
+        int ans = 1;
+
+        // if (k % 2 == 0) return -1;
+
+        boolean[] visit = new boolean[k + 1];
+        visit[n] = true;
+
+        while (n % k != 0) {
+            n = (n * 10 + 1) % k;
+            if (visit[n]) return -1;
+            visit[n] = true;
+            ans++;
+        }
+
+        return ans;
     }
 
     public int mySol(int k) {
