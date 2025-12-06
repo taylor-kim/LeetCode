c513428ebabc29cfa3894e1c7426b48d4402af9a
team-hungrian <def.hunger@gmail.com>
2025-12-06 23:41:14 +0900

    Time: 206 ms (9.88%), Space: 86.6 MB (7.41%) - LeetHub

--- 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
+++ 3578-count-partitions-with-max-min-difference-at-most-k/3578-count-partitions-with-max-min-difference-at-most-k.java
@@ -1,9 +1,75 @@
 class Solution {
     public int countPartitions(int[] nums, int k) {
-        return mySol(nums, k);
+        return editorial(nums, k);
     }
 
-    public int mySol(int[] nums, int k) {
+    public int editorial(int[] nums, int k) {
+        int n = nums.length;
+        int mod = (int)1e9 + 7;
+        long[] dp = new long[n + 1];
+        long[] pSum = new long[n + 1];
+        dp[0] = 1;
+        pSum[0] = 1;
+
+        TreeMap<Integer, Integer> treeMap = new TreeMap();
+
+        for (int i = 0, j = 0; i < n; i++) {
+            treeMap.put(nums[i], treeMap.getOrDefault(nums[i], 0) + 1);
+
+            while (j <= i && treeMap.lastKey() - treeMap.firstKey() > k) {
+                treeMap.put(nums[j], treeMap.get(nums[j]) - 1);
+                if (treeMap.get(nums[j]) == 0) {
+                    treeMap.remove(nums[j]);
+                }
+                j++;
+            }
+
+            dp[i + 1] = (pSum[i] - (j > 0 ? pSum[j - 1] : 0) + mod) % mod;
+            pSum[i + 1] = (pSum[i] + dp[i + 1]) % mod;
+        }
+
+        return (int)dp[n];
+    }
+
+    public int mySol2_fail(int[] nums, int k) {
+        Deque<Integer> minHeap = new LinkedList();
+        Deque<Integer> maxHeap = new LinkedList();
+        int mod = (int)1e9 + 7;
+        long[] dp = new long[nums.length];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            while (!minHeap.isEmpty() && nums[minHeap.peekLast()] > nums[i]) {
+                minHeap.pollLast();
+            }
+
+            while (!maxHeap.isEmpty() && nums[maxHeap.peekLast()] < nums[i]) {
+                maxHeap.pollLast();
+            }
+
+            minHeap.add(i);
+            maxHeap.add(i);
+
+            int minIndex = minHeap.peekFirst();
+            int maxIndex = maxHeap.peekFirst();
+
+            int min = nums[minIndex];
+            int max = nums[maxIndex];
+
+            System.out.println("i:%d, min:%d, max:%d, minIndex:%d, maxIndex:%d".formatted(i, min, max, minIndex, maxIndex));
+
+            if (max - min <= k) {
+                int left = Math.min(minHeap.peekFirst(), maxHeap.peekFirst());
+                dp[i] = (dp[i] * dp[left]) % mod;
+            } 
+        }
+
+        return (int)dp[nums.length - 1];
+    }
+
+    public int mySol_fail(int[] nums, int k) {
         return topdown(nums, k, 0, new Integer[nums.length]);
     }
 
