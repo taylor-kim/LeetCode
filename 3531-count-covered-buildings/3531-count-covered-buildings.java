8ff4294802f76385ff112ff58aa0de63137dfc3b
team-hungrian <def.hunger@gmail.com>
2025-12-11 23:40:16 +0900

    Time: 12 ms (100%), Space: 215.3 MB (65.26%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.java
+++ 3531-count-covered-buildings/3531-count-covered-buildings.java
@@ -1,6 +1,40 @@
 class Solution {
     public int countCoveredBuildings(int n, int[][] buildings) {
-        return mySol(n, buildings);
+        return editorial(n, buildings);
+    }
+
+    public int editorial(int n, int[][] buildings) {
+        int[] minRow = new int[n + 1];
+        int[] maxRow = new int[n + 1];
+        int[] minCol = new int[n + 1];
+        int[] maxCol = new int[n + 1];
+
+        Arrays.fill(minRow, n + 1);
+        Arrays.fill(minCol, n + 1);
+
+        for (int[] pos : buildings) {
+            int x = pos[0];
+            int y = pos[1];
+
+            minRow[y] = Math.min(minRow[y], x);
+            maxRow[y] = Math.max(maxRow[y], x);
+
+            minCol[x] = Math.min(minCol[x], y);
+            maxCol[x] = Math.max(maxCol[x], y);
+        }
+
+        int ans = 0;
+
+        for (int[] pos : buildings) {
+            int x = pos[0];
+            int y = pos[1];
+
+            if (minRow[y] < x && x < maxRow[y] && minCol[x] < y && y < maxCol[x]) {
+                ans++;
+            }
+        }
+
+        return ans;
     }
 
     public int mySol(int n, int[][] buildings) {
