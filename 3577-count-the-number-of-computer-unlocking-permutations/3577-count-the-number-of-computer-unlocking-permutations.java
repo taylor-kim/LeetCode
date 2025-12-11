81f0eb72c1ded629b8566b6c0f11fa8acfa56fca
team-hungrian <def.hunger@gmail.com>
2025-12-11 16:50:08 +0900

    Time: 2 ms (75.32%), Space: 92.1 MB (12.99%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.java
@@ -1,18 +1,82 @@
 class Solution {
-
     public int countPermutations(int[] complexity) {
-        int n = complexity.length;
+        return mySol2(complexity);
+    }
+
+    public int mySol2(int[] arr) {
+        int n = arr.length;
+        int min = arr[0];
+
         for (int i = 1; i < n; i++) {
-            if (complexity[i] <= complexity[0]) {
-                return 0;
+            if (min >= arr[i]) return 0;
+        }
+
+        int mod = (int)1e9 + 7;
+        long fact = 1;
+
+        for (int i = 1; i < n; i++) {
+            fact = (fact * i) % mod;
+        }
+
+        return (int)fact;
+    }
+
+    public int trigger(int[] arr) {
+        int ans = 0;
+        int mod = (int)1e9 + 7;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                ans = (ans + topdown2(arr, i)) % mod;
             }
         }
 
-        int ans = 1;
-        int mod = 1000000007;
-        for (int i = 2; i < n; i++) {
-            ans = (int) (((long) ans * i) % mod);
+        return ans;
+    }
+
+    public int topdown2(int[] arr, int index) {
+        for (int j = 0; j < index; j++) {
+
         }
+
+        return 0;
+    }
+
+    public int mySol_fail(int[] complexity) {
+        boolean[] openned = new boolean[complexity.length];
+        openned[0] = true;
+
+        int ans = 0;
+        int mod = (int)1e9 + 7;
+
+        for (int i = 1; i < complexity.length; i++) {
+            ans = (ans + topdown(complexity, 1, i, openned)) % mod;
+        }
+
+        return ans;
+    }
+
+    public int topdown(int[] arr, int index, int count, boolean[] openned) {
+        if (count == arr.length) return 1;
+        if (index >= arr.length) return 0;
+
+        int ans = 0;
+        int mod = (int)1e9 + 7;
+
+        // System.out.println("index:%d, count:%d".formatted(index, count));
+
+        for (int j = 0; j < index; j++) {
+            if ((j != index && openned[j] && arr[j] < arr[index])) {
+                openned[index] = true;
+
+                ans = (ans + topdown(arr, index + 1, count + 1, openned)) % mod;
+
+                openned[index] = false;
+            }
+        }
+
+        // ans = (ans + topdown(arr, index + 1, count, openned)) % mod;
+
         return ans;
     }
 }
