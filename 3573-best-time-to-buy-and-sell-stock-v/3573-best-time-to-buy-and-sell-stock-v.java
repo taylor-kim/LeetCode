c2001f49bd20129f8010192cbebe009fcacf6008
team-hungrian <def.hunger@gmail.com>
2025-12-18 03:39:44 +0900

    Time: 269 ms (10.64%), Space: 276.1 MB (7.45%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.java
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.java
@@ -1,46 +1,94 @@
 class Solution {
-
-    private int[] prices;
-    private long[][][] memo;
-
     public long maximumProfit(int[] prices, int k) {
-        this.prices = prices;
-        int n = prices.length;
-        memo = new long[n][k + 1][3];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                for (int s = 0; s < 3; s++) {
-                    memo[i][j][s] = -1;
-                }
-            }
-        }
-        return dfs(n - 1, k, 0);
+        return after_sol(prices, k);
     }
 
-    private long dfs(int i, int j, int state) {
-        if (j == 0) {
-            return 0;
-        }
-        if (i == 0) {
-            return state == 0 ? 0 : (state == 1 ? -prices[0] : prices[0]);
-        }
-        if (memo[i][j][state] != -1) {
-            return memo[i][j][state];
+    public long after_sol(int[] prices, int k) {
+        return topdown3(prices, prices.length - 1, k, 0, new Long[prices.length][k + 1][3]);
+    }
+
+    public long topdown3(int[] prices, int index, int k, int state, Long[][][] memo) {
+        if (k == 0) return 0;
+
+        if (index == 0) {
+            return state == 0 ? 0 : state == 1 ? -prices[index] : prices[index];
         }
 
-        int p = prices[i];
-        long res;
+        if (memo[index][k][state] != null) return memo[index][k][state];
+
+        long ans = 0;
+
+        int p = prices[index];
+
         if (state == 0) {
-            res = Math.max(
-                dfs(i - 1, j, 0),
-                Math.max(dfs(i - 1, j, 1) + p, dfs(i - 1, j, 2) - p)
+            ans = Math.max(
+                topdown3(prices, index - 1, k, state, memo),
+                Math.max(
+                    topdown3(prices, index - 1, k, 1, memo) + p,
+                    topdown3(prices, index - 1, k, 2, memo) - p
+                )
             );
         } else if (state == 1) {
-            res = Math.max(dfs(i - 1, j, 1), dfs(i - 1, j - 1, 0) - p);
+            ans = Math.max(
+                topdown3(prices, index - 1, k, state, memo),
+                topdown3(prices, index - 1, k - 1, 0, memo) - p
+            );
         } else {
-            res = Math.max(dfs(i - 1, j, 2), dfs(i - 1, j - 1, 0) + p);
+            ans = Math.max(
+                topdown3(prices, index - 1, k, state, memo),
+                topdown3(prices, index - 1, k - 1, 0, memo) + p
+            );
         }
-        memo[i][j][state] = res;
-        return res;
+
+        return memo[index][k][state] = ans;
+    }
+
+    public long hint_fail(int[] prices, int k) {
+        return topdown2(prices, k, 0, 0, 0);
+    }
+
+    public long topdown2(int[] prices, int k, int index, int isBuy, int isRunning) {
+        if (index >= prices.length || k == 0) {
+            return -isRunning;
+        }
+
+        long ans = topdown2(prices, k, index + 1, isBuy, isRunning);
+
+        long sub = topdown2(prices, k - isRunning, index + 1, (isBuy + 1) % 2, (isRunning + 1) % 2);
+
+        if (sub >= 0) {
+            ans = Math.max(ans, sub + (prices[index] * (isBuy == 0 ? -1 : 1)));
+        }
+
+        return ans;
+    }
+
+    public long mySol_fail(int[] prices, int k) {
+        return topdown(prices, k, 0, new Long[prices.length][k + 1]);
+    }
+
+    public long topdown(int[] prices, int k, int index, Long[][] memo) {
+        if (index >= prices.length || k == 0) return 0;
+
+        if (memo[index][k] != null) return memo[index][k];
+
+        long ans = 0;
+
+        for (int i = index + 1; i < prices.length; i++) {
+            long buyAndSell = prices[i] - prices[index] + topdown(prices, k - 1, i + 1, memo);
+            long sellAndBuy = prices[index] - prices[i] + topdown(prices, k - 1, i + 1, memo);
+
+            ans = Math.max(ans, Math.max(buyAndSell, sellAndBuy));
+        }
+
+        for (int i = index + 1; i < prices.length; i++) {
+            long sub = prices[index] - prices[i] + topdown(prices, k - 1, i + 1, memo);
+
+            ans = Math.max(ans, sub);
+        }
+
+        ans = Math.max(ans, topdown(prices, k, index + 1, memo));
+
+        return memo[index][k] = ans;
     }
 }
