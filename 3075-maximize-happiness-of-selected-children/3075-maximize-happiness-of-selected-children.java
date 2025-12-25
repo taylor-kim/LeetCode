00259bbda188d3ab2cd627c38e30e51e3b661b70
team-hungrian <def.hunger@gmail.com>
2025-12-25 20:36:05 +0900

    Time: 43 ms (43.55%), Space: 109.4 MB (22.58%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -1,70 +1,21 @@
 class Solution {
     public long maximumHappinessSum(int[] happiness, int k) {
-        return official_sort(happiness, k);
-    }
-
-    public long official_heap(int[] happiness, int k) {
-        Queue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-
-        for (int h : happiness) {
-            pq.add(h);
-        }
-
-        long ans = 0;
-        int turn = 0;
-
-        for (int i = 0; i < k; i++) {
-            ans += Math.max(pq.poll() - turn, 0);
-
-            turn++;
-        }
-
-        return ans;
-    }
-
-    public long official_sort(int[] happiness, int k) {
-        int n = happiness.length;
-
-        Integer[] happinessArray = new Integer[n];
-        for(int i = 0; i < n; i++) {
-            happinessArray[i] = happiness[i];
-        }
-        
-        Arrays.sort(happinessArray, Collections.reverseOrder());
-
-        long ans = 0;
-        int turns = 0;
-
-        for(int i = 0; i < k; i++) {
-            // Adjust happiness and ensure it's not negative
-            ans += Math.max(happinessArray[i] - turns, 0);  
-
-             // Increment turns for the next iteration
-            turns++; 
-        }
-        
-        return ans;
+        return mySol(happiness, k);
     }
 
     public long mySol(int[] happiness, int k) {
-        int n = happiness.length;
-        happiness = IntStream.of(happiness).boxed().sorted(Comparator.reverseOrder()).mapToInt(i -> i).toArray();
-
-        int reduce = 0;
-
         long ans = 0;
 
-        for (int i = 0; i < n && k > 0; i++) {
-            int sum = 0;
+        Arrays.sort(happiness);
 
-            sum = Math.max(0, happiness[i] - reduce);
+        int dec = 0;
 
-            if (sum == 0) break;
+        for (int i = happiness.length - 1; i >= 0 && k-- > 0; i--) {
+            int h = happiness[i] - dec++;
 
-            ans += sum;
+            if (h <= 0) break;
 
-            k--;
-            reduce++;
+            ans += h;
         }
 
         return ans;
