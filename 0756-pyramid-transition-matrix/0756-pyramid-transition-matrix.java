479dd1a3f541a186ab5246f159496a1516065e84
team-hungrian <def.hunger@gmail.com>
2025-12-30 03:03:04 +0900

    Time: 234 ms (62.42%), Space: 49.9 MB (10.83%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
@@ -1,56 +1,96 @@
 class Solution {
-
     public boolean pyramidTransition(String bottom, List<String> allowed) {
-        return mySol(bottom, allowed);
+        return mySol_fail_and_fix_with_gpt(bottom, allowed);
     }
 
-    public boolean mySol(String bottom, List<String> allowed) {
-        Map<String, Boolean> memo = new HashMap<>();
-        return dfs(bottom, allowed, memo);
+    public boolean mySolWithGodtin(String bottom, List<String> allowed) {
+        List<StringBuilder> pyramid = new ArrayList();
+
+        for (int i = 0; i < bottom.length(); i++) {
+            pyramid.add(new StringBuilder());
+        }
+
+        pyramid.get(0).append(bottom);
+
+        Map<String, List<Character>> map = new HashMap();
+
+        for (String allow : allowed) {
+            map.computeIfAbsent(allow.substring(0, 2), k -> new ArrayList()).add(allow.charAt(2));
+        }
+
+        return topdown(1, 0, map, pyramid);
     }
 
-    private boolean dfs(String cur, List<String> allowed, Map<String, Boolean> memo) {
-        if (cur.length() == 1) {
-            return true;
+    public boolean topdown(int row, int col, Map<String, List<Character>> map, List<StringBuilder> pyramid) {
+        if (row == pyramid.size()) return true;
+
+        if (col == pyramid.size() - row) {
+            return topdown(row + 1, 0, map, pyramid);
         }
 
-        if (memo.containsKey(cur)) {
-            return memo.get(cur);
+        StringBuilder bottom = pyramid.get(row - 1);
+
+        String prefix = bottom.substring(col, col + 2);
+
+        boolean ans = false;
+
+        for (char next : map.getOrDefault(prefix, new ArrayList<>())) {
+            pyramid.get(row).append(next);
+
+            if (topdown(row, col + 1, map, pyramid)) {
+                return true;
+            }
+
+            pyramid.get(row).deleteCharAt(col);
         }
 
-        List<String> nextRows = new ArrayList<>();
-        buildNextRows(cur, 0, new StringBuilder(), allowed, nextRows);
+        return false;
+    }
 
-        for (String next : nextRows) {
-            if (dfs(next, allowed, memo)) {
-                memo.put(cur, true);
+    public boolean mySol_fail_and_fix_with_gpt(String bottom, List<String> allowed) {
+        return topdown(bottom, new HashSet(allowed), new HashMap());
+    }
+
+    public boolean topdown(String bottom, Set<String> allowed, Map<String, Boolean> memo) {
+        if (bottom.length() == 1) return true;
+
+        if (memo.containsKey(bottom)) {
+            return memo.get(bottom);
+        }
+
+        List<String> nextRows = new ArrayList();
+
+        buildNextRows(bottom, 0, new StringBuilder(), nextRows, allowed);
+
+        for (String nextRow : nextRows) {
+            if (topdown(nextRow, allowed, memo)) {
+                memo.put(bottom, true);
                 return true;
             }
         }
 
-        memo.put(cur, false);
+        memo.put(bottom, false);
+
         return false;
     }
 
-    private void buildNextRows(
-        String cur,
-        int idx,
-        StringBuilder sb,
-        List<String> allowed,
-        List<String> nextRows
-    ) {
-        if (idx == cur.length() - 1) {
-            nextRows.add(sb.toString());
+    void buildNextRows(String bottom, int col, StringBuilder nextRow, List<String> nextRows, Set<String> allowed) {
+        if (col == bottom.length() - 1) {
+            nextRows.add(nextRow.toString());
             return;
         }
 
-        String prefix = cur.substring(idx, idx + 2);
+        String prefix = bottom.substring(col, col + 2);
 
-        for (String a : allowed) {
-            if (a.startsWith(prefix)) {
-                sb.append(a.charAt(2));
-                buildNextRows(cur, idx + 1, sb, allowed, nextRows);
-                sb.deleteCharAt(sb.length() - 1);
+        for (int i = 0; i < 6; i++) {
+            char next = (char)(i + 'A');
+
+            if (allowed.contains(prefix + next)) {
+                nextRow.append(next);
+
+                buildNextRows(bottom, col + 1, nextRow, nextRows, allowed);
+
+                nextRow.deleteCharAt(col);
             }
         }
     }
