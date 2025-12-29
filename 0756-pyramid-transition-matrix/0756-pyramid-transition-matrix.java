2595c489c337abf8f5efc6a7ddfb3424bf968982
team-hungrian <def.hunger@gmail.com>
2025-12-30 03:07:34 +0900

    Time: 1458 ms (24.21%), Space: 47 MB (60.51%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.java
@@ -1,6 +1,31 @@
 class Solution {
     public boolean pyramidTransition(String bottom, List<String> allowed) {
-        return mySol_fail_and_fix_with_gpt(bottom, allowed);
+        return mySolWithGodtin(bottom, allowed);
+    }
+
+    public boolean try_editorial2(String bottom, List<String> allowed) {
+        return false;
+    }
+
+    public boolean editorial1_wrong_answer(String bottom, List<String> allowed) {
+        int[][] T = new int[1 << 7][1 << 7];
+        for (String triple: allowed) {
+            int u = 1 << (triple.charAt(0) - 'A');
+            int v = 1 << (triple.charAt(1) - 'A');
+            int w = 1 << (triple.charAt(2) - 'A');
+            for (int b1 = 0; b1 < (1 << 7); ++b1) if ((u & b1) > 0)
+                for (int b2 = 0; b2 < (1 << 7); ++b2) if ((v & b2) > 0)
+                    T[b1][b2] |= w;
+        }
+
+        int[] state = new int[bottom.length()];
+        int t = 0;
+        for (char c: bottom.toCharArray())
+            state[t++] = 1 << (c - 'A');
+        while (t-- > 1)
+            for (int i = 0; i < t; ++i)
+                state[i] = T[state[i]][state[i+1]];
+        return state[0] > 0;
     }
 
     public boolean mySolWithGodtin(String bottom, List<String> allowed) {
