2387c894b59b03643387353097ebe3670b953016
team-hungrian <def.hunger@gmail.com>
2025-12-30 13:39:27 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -1,119 +1,50 @@
 class Solution {
     public int numMagicSquaresInside(int[][] grid) {
-        return official_abracadabra(grid);
+        return mySol(grid);
     }
 
-    public int official_abracadabra(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        
-        int count = 0;
+    public int mySol(int[][] grid) {
+        int ans = 0;
 
-        for (int i = 0; i + 2 < rows; i++) {
-            for (int j = 0; j + 2 < cols; j++) {
-                if (abracadabra(grid, i, j)) {
-                    count++;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] pSumRows = new int[m][n + 1];
+        int[][] pSumCols = new int[n][m + 1];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pSumRows[i][j + 1] = pSumRows[i][j] + grid[i][j];
+                pSumCols[j][i + 1] = pSumCols[j][i] + grid[i][j];
+            }
+        }
+
+        for (int i = 0; i < m - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                int row1 = pSumRows[i][j + 3] - pSumRows[i][j];
+                int row2 = pSumRows[i + 1][j + 3] - pSumRows[i + 1][j];
+                int row3 = pSumRows[i + 2][j + 3] - pSumRows[i + 2][j];
+
+                int col1 = pSumCols[j][i + 3] - pSumCols[j][i];
+                int col2 = pSumCols[j + 1][i + 3] - pSumCols[j + 1][i];
+                int col3 = pSumCols[j + 2][i + 3] - pSumCols[j + 2][i];
+
+                int dig1 = grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2];
+                int dig2 = grid[i + 2][j] + grid[i + 1][j + 1] + grid[i][j + 2];
+
+                if (row1 == row2 
+                    && row1 == row3
+                    && row1 == col1
+                    && row1 == col2
+                    && row1 == col3
+                    && row1 == dig1
+                    && row1 == dig2
+                ) {
+                    ans++;
                 }
             }
         }
 
-        return count;
-    }
-
-    private boolean abracadabra(int[][] grid, int row, int col) {
-        String seq = "2943816729438167";
-        String seqReversed = "7618349276183492";
-
-        StringBuilder sb = new StringBuilder();
-        int[] borderIndices = {0,1,2,5,8,7,6,3};
-
-        for (int i : borderIndices) {
-            int num = grid[row + i / 3][col + (i % 3)];
-
-            sb.append(num);
-        }
-
-        String border = sb.toString();
-
-        return grid[row][col] % 2 == 0
-                && grid[row + 1][col + 1] == 5
-                && (
-                    seq.contains(border)
-                    || seqReversed.contains(border)
-                );
-    }
-
-    public int mySol_bruteForce(int[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        
-        int count = 0;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (isMagic(grid, i, j)) {
-                    count++;
-                }
-            }
-        }
-
-        // for (int[] row : grid) {
-        //     System.out.println(Arrays.toString(row));
-        // }
-
-        return count;
-    }
-
-    private boolean isMagic(int[][] grid, int y, int x) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        if (y + 2 >= rows || x + 2 >= cols) {
-            return false;
-        }
-
-        int[] freq = new int[16];
-
-        for (int i = y; i < y + 3; i++) {
-            for (int j = x; j < x + 3; j++) {
-                int num = grid[i][j];
-
-                if (freq[num]++ > 0) {
-                    return false;
-                }
-            }
-        }
-
-        for (int i = 1; i <= 9; i++) {
-            if (freq[i] != 1) {
-                return false;
-            }
-        }
-
-        int targetSum = grid[y][x] + grid[y][x + 1] + grid[y][x + 2];
-
-        for (int i = 0; i < 3; i++) {
-            int rowSum = grid[y + i][x] + grid[y + i][x + 1] + grid[y + i][x + 2];
-
-            if (rowSum != targetSum) {
-                return false;
-            }
-
-            int colSum = grid[y][x + i] + grid[y + 1][x + i] + grid[y + 2][x + i];
-
-            if (colSum != targetSum) {
-                return false;
-            }
-        }
-
-        int d1 = grid[y][x] + grid[y + 1][x + 1] + grid[y + 2][x + 2];
-
-        if (d1 != targetSum) return false;
-
-        int d2 = grid[y][x + 2] + grid[y + 1][x + 1] + grid[y + 2][x];
-
-        if (d2 != targetSum) return false;
-
-        return true;
+        return ans;
     }
 }
