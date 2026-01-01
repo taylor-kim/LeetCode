09390d6cfac104c957de3d6f6637bfcec236c936
team-hungrian <def.hunger@gmail.com>
2026-01-01 20:44:24 +0900

    Time: 0 ms (100%), Space: 43.1 MB (89.12%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,22 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        return mySol(digits);
+        return improve(digits);
+    }
+
+    public int[] improve(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i] += 1;
+
+            int carry = digits[i] / 10;
+            digits[i] %= 10;
+
+            if (carry == 0) return digits;
+        }
+
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+
+        return ans;
     }
 
     public int[] mySol(int[] digits) {
