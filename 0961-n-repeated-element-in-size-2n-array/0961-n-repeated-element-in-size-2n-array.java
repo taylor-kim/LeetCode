d9e38bbe969ef2be6dac7de58b8fe43310cddfed
team-hungrian <def.hunger@gmail.com>
2026-01-03 21:12:39 +0900

    Time: 0 ms (100%), Space: 47.3 MB (75.44%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,6 +1,16 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        return others_probability(nums);
+        return others_pigeonhole(nums);
+    }
+
+    public int others_pigeonhole(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) {
+                return nums[i];
+            }
+        }
+
+        return nums[nums.length - 1];
     }
 
     public int others_probability(int[] nums) {
