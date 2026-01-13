602628867518e4b3b394c9410b453a0abec9a99d
team-hungrian <def.hunger@gmail.com>
2026-01-13 19:44:27 +0900

    Time: 1376 ms (5.26%), Space: 146.3 MB (65.79%) - LeetHub

---
+++ 3453-separate-squares-i/3453-separate-squares-i.java
@@ -0,0 +1,60 @@
+class Solution {
+    public double separateSquares(int[][] squares) {
+        return mySol(squares);
+    }
+
+    public double mySol(int[][] squares) {
+        Arrays.sort(squares, (a, b) -> {
+            return a[1] - b[1];
+        });
+
+        double sum = 0;
+
+        for (int[] square : squares) {
+            sum += square[2] * square[2];
+        }
+
+        double lo = 0;
+        double hi = Double.MAX_VALUE;
+        double ans = Double.MAX_VALUE;
+
+        while (lo < hi) {
+            double mid = lo + (hi - lo) / 2;
+
+            double[] sumOfTopAndBot = getSumOfTopAndBot(squares, mid);
+
+            // System.out.println("lo:%f, hi:%f, mid:%f, top:%f, bot:%f"
+            //     .formatted(lo, hi, mid, sumOfTopAndBot[0], sumOfTopAndBot[1]));
+
+            if (sumOfTopAndBot[0] <= sumOfTopAndBot[1]) {
+                hi = mid - 0.000001d;;
+                ans = mid;
+            } else {
+                lo = mid + 0.000001d;
+            }
+        }
+
+        return ans;
+    }
+
+    private double[] getSumOfTopAndBot(int[][] squares, double y) {
+        double sumOfTop = 0d;
+        double sumOfBot = 0d;
+
+        for (int[] square : squares) {
+            double top = (double)square[1] + square[2];;
+            double bot = (double)square[1];
+
+            if (y <= bot) {
+                sumOfTop += (top - bot) * square[2];
+            } else if (top <= y) {
+                sumOfBot += (top - bot) * square[2];
+            } else {
+                sumOfTop += (top - y) * square[2];
+                sumOfBot += (y - bot) * square[2];
+            }
+        }
+
+        return new double[] {sumOfTop, sumOfBot};
+    }
+}
