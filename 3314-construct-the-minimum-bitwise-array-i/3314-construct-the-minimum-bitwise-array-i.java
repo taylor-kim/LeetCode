2173e084d6f94fb65ed168ca0e9c278a6bf035ba
team-hungrian <def.hunger@gmail.com>
2026-01-20 19:54:40 +0900

    Time: 3 ms (57.02%), Space: 46.6 MB (65.79%) - LeetHub

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        return mySol2(nums);
+    }
+
+    public int[] mySol2(List<Integer> nums) {
+        int n = nums.size();
+
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+
+        for (int i = 0; i < n; i++) {
+            int num = nums.get(i);
+            
+            for (int cand = 0; cand < num; cand++) {
+                if ((cand | (cand + 1)) == num) {
+                    ans[i] = cand;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] mySol_fail(List<Integer> nums) {
+        int n = nums.size();
+
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int num = nums.get(i);
+            int found = 0;
+
+            for (int bit = 0; bit < 32; bit++) {
+                int mask = 1 << bit;
+                if ((num & mask) != 0) {
+                    found |= mask;
+                } else {
+                    break;
+                }
+            }
+
+            if (found == 0) {
+                ans[i] = -1;
+            } else {
+                ans[i] = found;
+            }
+        }
+
+        return ans;
+    }
+}
