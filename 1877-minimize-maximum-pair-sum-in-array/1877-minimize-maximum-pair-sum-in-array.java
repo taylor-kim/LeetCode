f52394fc20f80b562ce04331b4f1a209e3f803dd
team-hungrian <def.hunger@gmail.com>
2026-01-24 19:58:18 +0900

    Time: 58 ms (63.1%), Space: 97.2 MB (11.55%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minPairSum(int[] nums) {
+        return mySol(nums);
+    }
+
+    public int mySol(int[] nums) {
+        Arrays.sort(nums);
+
+        int max = 0;
+
+        for (int i = 0; i < nums.length / 2; i++) {
+            max = Math.max(max, nums[i] + nums[nums.length - i - 1]);
+        }
+
+        return max;
+    }
+}
