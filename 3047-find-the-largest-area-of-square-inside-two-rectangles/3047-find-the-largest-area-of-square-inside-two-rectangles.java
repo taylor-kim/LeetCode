10484f419dd92df1166c1ff670b45913bd1ece4d
team-hungrian <def.hunger@gmail.com>
2026-01-25 13:32:26 +0900

    Time: 86 ms (24.4%), Space: 47.7 MB (75.4%) - LeetHub

--- 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.java
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.java
@@ -1,23 +1,63 @@
 class Solution {
-
     public long largestSquareArea(int[][] bottomLeft, int[][] topRight) {
+        return mySolBf(bottomLeft, topRight);
+    }
+
+    public long mySolBf(int[][] bottomLeft, int[][] topRight) {
         int n = bottomLeft.length;
-        long maxSide = 0;
+        long ans = 0;
 
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int w =
-                    Math.min(topRight[i][0], topRight[j][0]) -
-                    Math.max(bottomLeft[i][0], bottomLeft[j][0]);
-                int h =
-                    Math.min(topRight[i][1], topRight[j][1]) -
-                    Math.max(bottomLeft[i][1], bottomLeft[j][1]);
-                int side = Math.min(w, h);
+            int left1 = bottomLeft[i][0];
+            int right1 = topRight[i][0];
+            int bot1 = bottomLeft[i][1];
+            int top1 = topRight[i][1];
 
-                maxSide = Math.max(maxSide, side);
+            for (int j = i + 1; j < n; j++) {
+                int left2 = bottomLeft[j][0];
+                int right2 = topRight[j][0];
+                int bot2 = bottomLeft[j][1];
+                int top2 = topRight[j][1];
+
+                int xLength = getIntersectLength(left1, right1, left2, right2);
+                int yLength = getIntersectLength(bot1, top1, bot2, top2);
+
+                // System.out.println("xLength:%d, yLength:%d".formatted(xLength, yLength));
+
+                int min = Math.min(xLength, yLength);
+
+                ans = Math.max(ans, 1l * min * min);
             }
         }
 
-        return maxSide * maxSide;
+        return ans;
+    }
+
+    private int getIntersectLength(int lo1, int hi1, int lo2, int hi2) {
+        int lo = Math.max(lo1, lo2);
+        int hi = Math.min(hi1, hi2);
+        return Math.max(0, hi - lo);
+    }
+
+    private int getIntersectLength_false(int lo1, int hi1, int lo2, int hi2) {
+        // [3,4], [1, 5]
+        // [1, 5], [3,4]
+
+        if (lo2 <= lo1 && hi2 <= hi1) {
+            System.out.println("wtf1 - lo1:%d, hi1:%d, lo2:%d, hi2:%d".formatted(lo1, hi1, lo2, hi2));
+            return hi2 - lo1;
+        }
+        if (lo1 <= lo2 && hi1 <= hi2) {
+            System.out.println("wtf2 - lo1:%d, hi1:%d, lo2:%d, hi2:%d".formatted(lo1, hi1, lo2, hi2));
+            return hi1 - lo2;
+        }
+        if (lo1 <= lo2 && hi2 <= hi1) {
+            System.out.println("wtf3 - lo1:%d, hi1:%d, lo2:%d, hi2:%d".formatted(lo1, hi1, lo2, hi2));
+            return hi2 - lo2;
+        }
+
+        System.out.println("wtf4");
+
+        return 0;
     }
 }
