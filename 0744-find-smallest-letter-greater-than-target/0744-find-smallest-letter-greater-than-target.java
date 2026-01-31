d23e4f1d79de0100596253d1e0c9fc38e5ccecc0
team-hungrian <def.hunger@gmail.com>
2026-01-31 17:24:03 +0900

    Time: 0 ms (100%), Space: 46.4 MB (43.82%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -1,6 +1,23 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        return mySol(letters, target);
+        return mySol2(letters, target);
+    }
+
+    public char mySol2(char[] letters, char target) {
+        int lo = 0;
+        int hi = letters.length;
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (letters[mid] <= target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return letters[lo % letters.length];
     }
 
     public char mySol(char[] letters, char target) {
