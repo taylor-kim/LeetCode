f6022138a580f4e53bbe10c4ed9a7bd8c2ab5422
team-hungrian <def.hunger@gmail.com>
2026-02-01 15:48:24 +0900

    Time: 9 ms (97.85%), Space: 43.1 MB (98.3%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,6 +1,43 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        return mySol2_bottomup(s1, s2);
+        return mySol2_bottomup_n_space(s1, s2);
+    }
+
+    public int mySol2_bottomup_n_space(String s1, String s2) {
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        int[] dp = new int[c2.length + 1];
+
+        Arrays.fill(dp, 1000 * ((int)'z' + 1));
+
+        dp[c2.length] = 0;
+
+        for (int i1 = c1.length; i1 >= 0; i1--) {
+            int botRight = dp[c2.length];
+            for (int i2 = c2.length; i2 >= 0; i2--) {
+                int val = 0;
+                if (i1 >= c1.length && i2 >= c2.length) {
+                    continue;
+                } else if (i1 >= c1.length) {
+                    val = c2[i2] + dp[i2 + 1];
+                } else if (i2 >= c2.length) {
+                    val = c1[i1] + dp[i2];
+                } else if (c1[i1] == c2[i2]) {
+                    // dp[i1][i2] = dp[i1 + 1][i2 + 1];
+                    val = botRight;
+                } else {
+                    val = Math.min(
+                        c1[i1] + dp[i2],
+                        c2[i2] + dp[i2 + 1]
+                    );
+                }
+
+                botRight = dp[i2];
+                dp[i2] = val;
+            }
+        }
+
+        return dp[0];
     }
 
     public int mySol2_bottomup(String s1, String s2) {
