910c5d6e09dc816f6601e42446e777c821ed4dc8
team-hungrian <def.hunger@gmail.com>
2026-02-07 18:47:08 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,6 +1,76 @@
 class Solution {
     public int minimumDeletions(String s) {
-        return others(s);
+        return try_20260207_2(s);
+    }
+    
+    public int try_20260207_2(String s) {
+        int n = s.length();
+        int ans = 0;
+
+        boolean foundB = false;
+
+        int removeA = 0;
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
+            if (foundB && c == 'a') {
+                removeA++;
+            } else if (!foundB && c == 'b') {
+                foundB = true;
+            }
+        }
+
+        boolean foundA = false;
+
+        int removeB = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+
+            if (foundA && c == 'b') {
+                removeB++;
+            } else if (!foundA && c == 'a') {
+                foundA = true;
+            }
+        }
+
+        return Math.min(removeA, removeB);
+    }
+
+    public int try_20260207_fail(String s) {
+        int n = s.length();
+
+        int countA = 0;
+        int countB = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'a') {
+                countA++;
+            } else {
+                countB++;
+            }
+        }
+
+        int ans = Math.min(countA, countB);
+
+        int i = 0;
+        int j = 1;
+
+        while (i < n) {
+            while (j < n && s.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+
+            char prev = s.charAt(i);
+            char cur = j < n ? s.charAt(j) : prev;
+
+            if (prev > cur) {
+
+            }
+        }
+
+        return 0;
     }
 
     public int others(String s) {
