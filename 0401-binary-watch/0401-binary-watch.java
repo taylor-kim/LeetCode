355870f47078f33e247f96757b67ba29b176662f
team-hungrian <def.hunger@gmail.com>
2026-02-17 13:24:58 +0900

    Time: 29 ms (8.18%), Space: 47.1 MB (58.92%) - LeetHub

--- 0401-binary-watch/0401-binary-watch.java
+++ 0401-binary-watch/0401-binary-watch.java
@@ -1,14 +1,62 @@
 class Solution {
-
     public List<String> readBinaryWatch(int turnedOn) {
-        List<String> ans = new ArrayList<String>();
-        for (int h = 0; h < 12; ++h) {
-            for (int m = 0; m < 60; ++m) {
-                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
-                    ans.add(h + ":" + (m < 10 ? "0" : "") + m);
-                }
+        return mySol(turnedOn);
+    }
+
+    public List<String> mySol(int turnedOn) {
+        Set<String> set = new HashSet();
+
+        topdown(set, turnedOn, 0, new boolean[10]);
+
+        return new ArrayList(set);
+    }
+
+    private void topdown(Set<String> set, int turnedOn, int index, boolean[] arr) {
+        if (turnedOn == 0 || index >= arr.length) {
+            String s = time(arr);
+
+            if (s != null) set.add(s);
+
+            return;
+        }
+
+        for (int i = index; i < arr.length; i++) {
+            if (!arr[i]) {
+                arr[i] = true;
+                topdown(set, turnedOn - 1, index + 1, arr);
+                arr[i] = false;
             }
         }
-        return ans;
+    }
+
+    private String time(boolean[] arr) {
+        StringBuilder sb = new StringBuilder();
+
+        int h = getNum(arr, 0, 4);
+
+        if (h > 11) return null;
+
+        int m = getNum(arr, 4, arr.length);
+
+        if (m > 59) return null;
+
+        sb.append(h).append(":").append("%02d".formatted(m));
+
+        return sb.toString();
+    }
+
+    private int getNum(boolean[] arr, int start, int end) {
+        int n = 0;
+
+        int pos = 0;
+
+        for (int i = start; i < end; i++) {
+            if (arr[i]) {
+                n |= (1 << pos);
+            }
+            pos++;
+        }
+
+        return n;
     }
 }
