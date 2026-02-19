90b542d370d2c7f8f0e90ce4b5405e9eb8cc7fe1
team-hungrian <def.hunger@gmail.com>
2026-02-19 14:22:46 +0900

    Time: 8 ms (96.2%), Space: 46.6 MB (28.97%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.java
+++ 0696-count-binary-substrings/0696-count-binary-substrings.java
@@ -1,6 +1,32 @@
 class Solution {
     public int countBinarySubstrings(String s) {
-        return mySol(s);
+        return mySol_improved(s);
+    }
+
+    public int mySol_improved(String s) {
+        int ans = 0;
+        int n = s.length();
+
+        char prev = s.charAt(0) == '0' ? '1' : '0';
+        int prevCount = 0;
+
+        int i = 0;
+
+        while (i < n) {
+            int count = 0;
+
+            while (i + count < n && prev != s.charAt(i + count)) {
+                count++;
+            }
+
+            ans += Math.min(prevCount, count);
+            prevCount = count;
+            prev = s.charAt(i);
+
+            i += count;
+        }
+
+        return ans;
     }
 
     public int mySol(String s) {
