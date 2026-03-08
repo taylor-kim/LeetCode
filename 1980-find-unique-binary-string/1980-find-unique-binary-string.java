ce3ff1750179474c947d72912de62a0b4bf13ce8
team-hungrian <def.hunger@gmail.com>
2026-03-08 19:14:10 +0900

    Time: 3 ms (29.39%), Space: 43.5 MB (37.32%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,27 +1,45 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        return mySol(nums);
+        return try_20260308(nums);
     }
 
-    public String mySol(String[] nums) {
-        int n = nums.length;
-        int max = (int)Math.pow(2, n);
+    public String try_20260308(String[] nums) {
+        Set<String> set = new HashSet();
+        for (String num : nums) set.add(num);
 
-        Set<Integer> set = new HashSet();
+        return topdown(set, "");
+    }
 
-        for (String bin : nums) {
-            set.add(Integer.parseInt(bin, 2));
+    public String topdown(Set<String> nums, String s) {
+        if (nums.size() == s.length()) {
+            return !nums.contains(s) ? s : null;
         }
 
-        int missing = -1;
-        Random random = new Random();
+        String a = topdown(nums, s + "0");
 
-        do {
-            missing = random.nextInt(max);
-        } while (set.contains(missing));
+        if (a != null) return a;
 
-        String s = Integer.toBinaryString(missing);
+        return topdown(nums, s + "1");
+    }
 
-        return s.length() == n ? s : "0".repeat(n - s.length()) + s;
+    public String try_20250215(String[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        int max = (int)Math.pow(2, n) - 1;
+
+        for (int i = 0; i <= max; i++) {
+            String target = Integer.toBinaryString(i);
+
+            if (target.length() < n) {
+                target = "0".repeat(n - target.length()) + target;
+            }
+
+            if (Arrays.binarySearch(nums, target) < 0) {
+                return target;
+            }
+        }
+
+        return "";
     }
 }
