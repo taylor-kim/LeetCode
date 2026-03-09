bc34681235e100a5ef5a3950eeedac8991c532a0
team-hungrian <def.hunger@gmail.com>
2026-03-09 20:53:01 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
@@ -1,36 +1,116 @@
 class Solution {
-
     public int numberOfStableArrays(int zero, int one, int limit) {
-        final long MOD = 1000000007;
-        long[][][] dp = new long[zero + 1][one + 1][2];
-        for (int i = 0; i <= Math.min(zero, limit); i++) {
-            dp[i][0][0] = 1;
+        return mySol2(zero, one, limit);
+    }
+
+    public int mySol2(int zero, int one, int limit) {
+        int[][] pSum = new int[2][zero + one + 1];
+
+        return topdown2(zero, one, limit, 0, pSum);
+    }
+
+    public int topdown2(int zero, int one, int limit, int index, int[][] pSum) {
+        // System.out.println("zero:%d, one:%d, index:%d".formatted(zero, one, index));
+
+        if (zero < 0 || one < 0) return 0;
+
+        if (index > limit && 
+            (
+                pSum[0][index] - pSum[0][Math.max(index - limit - 1, 0)] == 0 ||
+                pSum[1][index] - pSum[1][Math.max(index - limit - 1, 0)] == 0
+            )
+        ) {
+            return 0;
         }
-        for (int j = 0; j <= Math.min(one, limit); j++) {
-            dp[0][j][1] = 1;
+
+        if (zero == 0 && one == 0) return 1;
+
+        int mod = (int)1e9 + 7;
+
+        int a = 0;
+        int b = 0;
+
+        pSum[0][index + 1] = pSum[0][index] + 1;
+        pSum[1][index + 1] = pSum[1][index];
+        a = topdown2(zero - 1, one, limit, index + 1, pSum);
+        pSum[0][index + 1] = 0;
+        pSum[1][index + 1] = 0;
+
+        pSum[0][index + 1] = pSum[0][index];
+        pSum[1][index + 1] = pSum[1][index] + 1;
+        b = topdown2(zero, one - 1, limit, index + 1, pSum);
+        pSum[0][index + 1] = 0;
+        pSum[1][index + 1] = 0;
+
+        return (a + b) % mod;
+    }
+
+    public int topdown_fail(int zero, int one, int limit, int index, int[][] pSum) {
+        // System.out.println("zero:%d, one:%d, index:%d".formatted(zero, one, index));
+
+        if (zero < 0 || one < 0) return 0;
+
+        if (zero == 0 && one == 0) return 1;
+
+        int mod = (int)1e9 + 7;
+
+        int a = 0;
+        int b = 0;
+
+        if (index < limit || pSum[1][index] - pSum[1][Math.max(index - limit, 0)] > 0) {
+            pSum[0][index + 1] = pSum[0][index] + 1;
+            a = topdown_fail(zero - 1, one, limit, index + 1, pSum);
+            pSum[0][index + 1] = pSum[0][index] - 1;
         }
-        for (int i = 1; i <= zero; i++) {
-            for (int j = 1; j <= one; j++) {
-                if (i > limit) {
-                    dp[i][j][0] =
-                        dp[i - 1][j][0] +
-                        dp[i - 1][j][1] -
-                        dp[i - limit - 1][j][1];
-                } else {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
-                }
-                dp[i][j][0] = ((dp[i][j][0] % MOD) + MOD) % MOD;
-                if (j > limit) {
-                    dp[i][j][1] =
-                        dp[i][j - 1][1] +
-                        dp[i][j - 1][0] -
-                        dp[i][j - limit - 1][0];
-                } else {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
-                }
-                dp[i][j][1] = ((dp[i][j][1] % MOD) + MOD) % MOD;
+
+        if (index < limit || pSum[0][index] - pSum[0][Math.max(index - limit, 0)] > 0) {
+            pSum[1][index + 1] = pSum[1][index] + 1;
+            b = topdown_fail(zero, one - 1, limit, index + 1, pSum);
+            pSum[1][index + 1] = pSum[1][index] - 1;
+        }
+
+        return (a + b) % mod;
+    }
+
+    public int mySol_tle(int zero, int one, int limit) {
+        return topdown(zero, one, limit, "");
+    }
+
+    public int topdown(int zero, int one, int limit, String s) {
+        if (zero < 0 || one < 0) return 0;
+
+        if (zero == 0 && one == 0) {
+            return isOk(s, limit) ? 1 : 0;
+        }
+
+        int a = topdown(zero - 1, one, limit, s + "0");
+        int b = topdown(zero, one - 1, limit, s + "1");
+
+        int mod = (int)1e9 + 7;
+
+        return (a + b) % mod;
+    }
+
+    private boolean isOk(String s, int limit) {
+        if (s.length() <= limit) return true;
+
+        int left = 0;
+        int[] counter = new int[2];
+
+        for (int right = 0; right < s.length(); right++) {
+            int digit = s.charAt(right) - '0';
+
+            counter[digit]++;
+
+            if (right - left + 1 > limit) {
+                if (counter[0] == 0 || counter[1] == 0) return false;
+
+                int remove = s.charAt(left++) - '0';
+
+                counter[remove]--;
             }
         }
-        return (int) ((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+
+        return true;
     }
 }
