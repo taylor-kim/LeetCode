c6e3437b17c392ea41d00554c0ba9d1c8ba0b4cd
team-hungrian <def.hunger@gmail.com>
2026-03-13 16:35:07 +0900

    Time: 227 ms (7.82%), Space: 46.6 MB (92.59%) - LeetHub

---
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/3496-minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -0,0 +1,74 @@
+class Solution {
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        return mySol(mountainHeight, workerTimes);
+    }
+
+    public long mySol(int height, int[] times) {
+        // t * 1 + t * 2 + t * 3... t * n => t * (1 + 2 + 3 + ... + n) => t * (n * (1 + n) / 2) <= ans
+
+        // ans / t * 2 == n + n^2 == some. => n * (1 + n) == some
+
+        // t * (h * (1 + h) / 2) == secs
+        // (h * (1 + h)) <= ans / t * 2
+
+        // 1 * (2 * (1 + 2) / 2) == 10
+        // (h * (1 + h)) <= 10 / 1 * 2 <= 20
+
+        long lo = 0;
+        long hi = Long.MAX_VALUE;
+
+        while (lo < hi) {
+            long mid = lo + (hi - lo) / 2;
+
+            // System.out.println("lo:%d, hi:%d, mid:%d".formatted(lo, hi, mid));
+
+            if (isPossible(times, height, mid)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+
+            // System.out.println("\n");
+        }
+
+        return lo;
+    }
+
+    private boolean isPossible(int[] times, int height, long seconds) {
+        for (int time : times) {
+            int reduced = getReducedAmount(time, seconds);
+
+            // System.out.println("time:%d, reduced:%d, height:%d".formatted(time, reduced, height));
+
+            height -= reduced;
+
+            if (height <= 0) return true;
+        }
+
+        return false;
+    }
+
+    private int getReducedAmount(int time, long seconds) {
+        int lo = 1;
+        int hi = Integer.MAX_VALUE;
+
+        // time * (h * (1 + h) / 2)
+        // (n * (1 + n)) <= ans / t * 2
+
+        long target = (seconds * 2) / time;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            long x = mid * (1l + mid);
+
+            if (x <= target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return lo - 1;
+    }
+}
