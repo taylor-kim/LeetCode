d178780d2da0e51ea47b56fc925dc3192ce47be9
team-hungrian <def.hunger@gmail.com>
2026-03-18 09:05:27 +0900

    Time: 1 ms (100%), Space: 46.1 MB (16.15%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -1,38 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     int maxSum = Integer.MIN_VALUE;
-    public int maxPath(TreeNode root){
-         if(root == null){
-            return 0;
-        }
-    
-        int leftSum = Math.max(0, maxPath(root.left));
-        int rightSum = Math.max(0, maxPath(root.right));
-
-        maxSum = Math.max(maxSum, leftSum + rightSum + root.val);
-        return Math.max(leftSum, rightSum) + root.val;
-    }
 
     public int maxPathSum(TreeNode root) {
-       if(root == null){
-        return 0;
-       }
+        maxGain(root);
+        return maxSum;
+    }
 
-       int path = maxPath(root);
-       return maxSum;
+    public int maxGain(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        
+        // 递归计算左右子节点的最大贡献值
+        // 只有在最大贡献值大于 0 时，才会选取对应子节点
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        // 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
+        int priceNewpath = node.val + leftGain + rightGain;
+
+        // 更新答案
+        maxSum = Math.max(maxSum, priceNewpath);
+
+        // 返回节点的最大贡献值
+        return node.val + Math.max(leftGain, rightGain);
     }
 }
