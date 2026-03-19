b3dccc56fcc32e3cb1d178f6b1d35363dfb5ec5f
team-hungrian <def.hunger@gmail.com>
2026-03-19 14:21:23 +0900

    Time: 25 ms (10.61%), Space: 120.6 MB (5.38%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -1,6 +1,44 @@
 class Solution {
     public int largestSubmatrix(int[][] matrix) {
-        return try_20260317(matrix);
+        return official_no_sort(matrix);
+    }
+
+    public int official_no_sort(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int ans = 0;
+
+        List<int[]> prevHeights = new ArrayList();
+
+        for (int i = 0; i < m; i++) {
+            List<int[]> heights = new ArrayList();
+            boolean[] seen = new boolean[n];
+
+            for (int[] data : prevHeights) {
+                int col = data[0];
+                int h = data[1];
+
+                if (matrix[i][col] == 1) {
+                    heights.add(new int[] {col, h + 1});
+                    seen[col] = true;
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (!seen[j] && matrix[i][j] == 1) {
+                    heights.add(new int[] {j, 1});
+                }
+            }
+
+            for (int j = 0; j < heights.size(); j++) {
+                ans = Math.max(ans, heights.get(j)[1] * (j + 1));
+            }
+
+            prevHeights = heights;
+        }
+
+        return ans;
     }
 
     public int try_20260317(int[][] matrix) {
