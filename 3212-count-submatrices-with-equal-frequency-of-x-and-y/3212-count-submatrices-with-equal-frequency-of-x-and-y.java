d9ef4185b27e23dbb490921dbbbf4c2716593a0d
team-hungrian <def.hunger@gmail.com>
2026-03-19 16:39:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,6 +1,55 @@
 class Solution {
     public int numberOfSubmatrices(char[][] grid) {
-        return mySol(grid);
+        return mySol_space_opt(grid);
+    }
+
+    public int mySol_space_opt(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] counters = new int[2][n + 1];
+        int ans = 0;
+
+        for (int i = 0; i < m; i++) {
+            int[] topLeft = {0, 0};
+
+            for (int j = 0; j < n; j++) {
+                char c = grid[i][j];
+
+                int[] nextTopLeft = {0, 0};
+
+                for (int k = 0; k < counters.length; k++) {
+                    int[] counter = counters[k];
+
+                    counter[j + 1] = counter[j + 1] + counter[j] - topLeft[i];
+                    nextTopLeft[k] = counter[j + 1];
+                }
+
+                if (c == 'X') {
+                    counters[0][j + 1]++;
+                } else if (c == 'Y') {
+                    counters[1][j + 1]++;
+                }
+
+                if (counters[0][j + 1] > 0 && (counters[0][j + 1] == counters[1][j + 1])) {
+                    ans++;
+                }
+
+                topLeft = nextTopLeft;
+            }
+        }
+
+        // for (int[] row : counters[0]) {
+        //     System.out.println(Arrays.toString(row));
+        // }
+
+        // System.out.println("");
+
+        // for (int[] row : counters[1]) {
+        //     System.out.println(Arrays.toString(row));
+        // }
+
+        return ans;
     }
 
     public int mySol(char[][] grid) {
