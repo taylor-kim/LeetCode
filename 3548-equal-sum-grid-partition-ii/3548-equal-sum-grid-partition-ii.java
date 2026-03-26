94f03e4f9008e547941bd9bb85970e016d6c7823
team-hungrian <def.hunger@gmail.com>
2026-03-26 23:48:12 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.java
+++ 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.java
@@ -1,67 +1,108 @@
 class Solution {
-
     public boolean canPartitionGrid(int[][] grid) {
+        return mySol(grid);
+    }
+
+    public boolean mySol(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
         long total = 0;
-        int m = grid.length;
-        int n = grid[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                total += grid[i][j];
+        long[] rowSum = new long[n];
+        long[] colSum = new long[m];
+
+        Map<Long, Integer> rowMap1 = new HashMap();
+        Map<Long, Integer> colMap1 = new HashMap();
+
+        Map<Long, Integer> rowMap2 = new HashMap();
+        Map<Long, Integer> colMap2 = new HashMap();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int num = grid[i][j];
+
+                total += num;
+                rowSum[i] += num;
+                colSum[j] += num;
+
+                modify(rowMap2, num, 1);
+                modify(colMap2, num, 1);
             }
         }
-        for (int k = 0; k < 4; k++) {
-            Set<Long> exist = new HashSet<>();
-            exist.add(0L);
-            long sum = 0;
-            m = grid.length;
-            n = grid[0].length;
-            if (m < 2) {
-                grid = rotation(grid);
-                continue;
+
+        for (int i = 0; i < n; i++) {
+            long diff = (total - rowSum[i]) - rowSum[i];
+
+            if (diff == 0) return true;
+
+            for (int col : grid[i]) {
+                modify(rowMap1, col, 1);
+                modify(rowMap2, col, -1);
             }
-            if (n == 1) {
-                for (int i = 0; i < m - 1; i++) {
-                    sum += grid[i][0];
-                    long tag = sum * 2 - total;
-                    if (tag == 0 || tag == grid[0][0] || tag == grid[i][0]) {
-                        return true;
-                    }
-                }
-                grid = rotation(grid);
-                continue;
+
+            // System.out.println("diff:%d".formatted(diff));
+            // System.out.println(rowMap1);
+            // System.out.println(rowMap2 + "\n");
+
+            if (diff > 0) {
+                if (rowMap2.containsKey(diff) && 
+                    (i + 1 != n - 1 || 
+                        (grid[i + 1][0] == diff || grid[i + 1][m - 1] == diff)
+                    )
+                ) return true;
+            } else {
+                if (rowMap1.containsKey(-diff) && 
+                    (i > 0 ||
+                        (grid[i][0] == -diff || grid[i][m - 1] == -diff)
+                    )) return true;
             }
-            for (int i = 0; i < m - 1; i++) {
-                for (int j = 0; j < n; j++) {
-                    exist.add((long) grid[i][j]);
-                    sum += grid[i][j];
-                }
-                long tag = sum * 2 - total;
-                if (i == 0) {
-                    if (
-                        tag == 0 || tag == grid[0][0] || tag == grid[0][n - 1]
-                    ) {
-                        return true;
-                    }
-                    continue;
-                }
-                if (exist.contains(tag)) {
-                    return true;
-                }
+
+            if (i + 1 < n) {
+                rowSum[i + 1] += rowSum[i];
             }
-            grid = rotation(grid);
         }
+
+        for (int j = 0; j < m; j++) {
+            long diff = (total - colSum[j]) - colSum[j];
+
+            System.out.println("total:%d, colSum[%d]:%d, diff:%d".formatted(total, j, colSum[j], diff));
+
+            if (diff == 0) return true;
+
+            for (int i = 0; i < n; i++) {
+                int row = grid[i][j];
+
+                modify(colMap1, row, 1);
+                modify(colMap2, row, -1);
+            }
+
+            if (diff > 0) {
+                if (colMap2.containsKey(diff) &&
+                    (j + 1 != m - 1 ||
+                        (grid[0][j + 1] == -diff || grid[n - 1][j] == -diff)
+                    )
+                ) return true;
+            } else {
+                if (colMap1.containsKey(-diff) && 
+                    (j > 0 ||
+                        (grid[0][j] == diff || grid[n - 1][j + 1] == diff)
+                    )
+                ) return true;
+            }
+
+            if (j + 1 < m) {
+                colSum[j + 1] += colSum[j];
+            }
+        }
+
         return false;
     }
 
-    public int[][] rotation(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] tmp = new int[n][m];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                tmp[j][m - 1 - i] = grid[i][j];
-            }
+    private void modify(Map<Long, Integer> map, long num, int delta) {
+        map.put(num, map.getOrDefault(num, 0) + delta);
+
+        if (map.get(num) == 0) {
+            map.remove(num);
         }
-        return tmp;
     }
 }
