2bff48412a7e419af7c67c515af34f1c9103b63a
team-hungrian <def.hunger@gmail.com>
2026-03-28 17:22:24 +0900

    Time: 4 ms (8.92%), Space: 49.6 MB (10.77%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,88 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public void reorderList(ListNode head) {
-        retry_20240324(head);
-    }
-
-    public void retry_20240324(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if (head == null) {
+            return;
         }
-
-        ListNode prev = null;
-
-        while (slow != null) {
-            ListNode next = slow.next;
-            
-            slow.next = prev;
-            prev = slow;
-            slow = next;
+        List<ListNode> list = new ArrayList<ListNode>();
+        ListNode node = head;
+        while (node != null) {
+            list.add(node);
+            node = node.next;
         }
-
-        ListNode back = prev;
-
-        while (head.next != back && head != back) {
-            prev = back.next;
-
-            back.next = head.next;
-            head.next = back;
-            head = head.next.next;
-            back = prev;
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            list.get(i).next = list.get(j);
+            i++;
+            if (i == j) {
+                break;
+            }
+            list.get(j).next = list.get(i);
+            j--;
         }
-    }
-
-    public void mySol(ListNode head) {
-        if (head.next == null) return;
-
-        ListNode slow = head;
-        ListNode fast = head.next;
-        
-        int mid = 0;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            mid++;
-        }
-
-        if (fast != null) {
-            //even
-            slow = slow.next;
-        } else {
-            //odd
-        }
-
-        ListNode back = null;
-
-        while (slow != null) {
-            ListNode temp = slow;
-            slow = slow.next;
-            temp.next = back;
-            back = temp;
-        }
-
-        while (mid-- > 0) {
-            ListNode remove = back;
-            back = back.next;
-
-            ListNode next = head.next;
-
-            head.next = remove;
-            remove.next = next;
-            head = next;
-        }
+        list.get(i).next = null;
     }
 }
