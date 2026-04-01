6419ff83979f8ae3a64cc5c942fa95e2b4d1e74d
team-hungrian <def.hunger@gmail.com>
2026-04-02 00:47:00 +0900

    Time: 42 ms (65.22%), Space: 114.3 MB (80.87%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.java
+++ 2751-robot-collisions/2751-robot-collisions.java
@@ -1,6 +1,52 @@
 class Solution {
     public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
-        return mySol(positions, healths, directions);
+        return official(positions, healths, directions);
+    }
+
+    public List<Integer> official(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        Integer[] indices = new Integer[n];
+
+        for (int i = 0; i < n; i++) indices[i] = i;
+
+        Arrays.sort(indices, (a, b) -> {
+            return positions[a] - positions[b];
+        });
+
+        Stack<Integer> stack = new Stack();
+
+        for (int index : indices) {
+            char c = directions.charAt(index);
+
+            if (c == 'R') {
+                stack.push(index);
+            } else {
+                while (!stack.isEmpty()) {
+                    if (healths[stack.peek()] == healths[index]) {
+                        healths[stack.pop()] = 0;
+                        healths[index] = 0;
+                        break;
+                    } else if (healths[stack.peek()] > healths[index]) {
+                        healths[stack.peek()]--;
+                        healths[index] = 0;
+                        break;
+                    } else {
+                        healths[stack.pop()] = 0;
+                        healths[index]--;
+                    }
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList();
+
+        for (int i = 0; i < n; i++) {
+            if (healths[i] > 0) {
+                ans.add(healths[i]);
+            }
+        }
+
+        return ans;
     }
 
     public List<Integer> mySol(int[] positions, int[] healths, String directions) {
