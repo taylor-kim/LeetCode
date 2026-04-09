ac4c1967158a522e94d05aa1b3c41936f6a02483
team-hungrian <def.hunger@gmail.com>
2026-04-10 01:51:57 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,6 +1,7 @@
 class Solution {
         public String largestNumber(int[] nums) {
         String s = Arrays.stream(nums).mapToObj(String::valueOf).sorted((v1, v2) ->  (v2+v1).compareTo(v1+v2)).collect(Collectors.joining());
-        return s.charAt(0) == '0' ? "0" : s;
+        // return s.charAt(0) == '0' ? "0" : s;
+        return s;
     }
 }
