5f733c7b3451e81db110e37ef249bffaca4bc37a
team-hungrian <def.hunger@gmail.com>
2026-04-11 19:50:08 +0900

    Time: 74 ms (93.85%), Space: 208 MB (93.3%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
@@ -1,7 +1,46 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-        return mySol(nums);
+        return official_index_track(nums);
     }
+
+    public int official_index_track(int[] nums) {
+        int n = nums.length;
+        int[] indices = new int[n];
+        Arrays.fill(indices, -1);
+        Map<Integer, Integer> lastIndices = new HashMap();
+        
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (lastIndices.containsKey(nums[i])) {
+                indices[i] = lastIndices.get(nums[i]);
+            }
+
+            lastIndices.put(nums[i], i);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int prev = indices[i];
+            if (prev != -1) {
+                int prevOfPrev = indices[prev];
+
+                if (prevOfPrev != -1) {
+                    ans = Math.min(ans, i - prevOfPrev);
+                }
+            }
+        }
+
+        return ans == Integer.MAX_VALUE ? -1 : ans * 2;
+    }
+
+    /**
+    n = 10
+    idx : 0,1,2,3,4,5,6,7,8,9
+    nums: 4,3,7,1,2,7,1,9,7,8
+    
+    map={8:9, 7:2, 9:7, 1:6, 2:4, 1:3, 3:1, 4:0}
+    next={5:8, 2:5}
+     */
 
     public int mySol(int[] nums) {
         Map<Integer, List<Integer>> map = new HashMap();
