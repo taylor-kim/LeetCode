d58cf8bbe3f739ef5d8c52bf038792f1e7b47be3
team-hungrian <def.hunger@gmail.com>
2026-04-15 19:36:46 +0900

    Time: 0 ms (100%), Space: 47.2 MB (7.79%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,6 +1,20 @@
 class Solution {
     public int closestTarget(String[] words, String target, int startIndex) {
-        return mySol(words, target, startIndex);
+        return official_one_pass(words, target, startIndex);
+    }
+
+    public int official_one_pass(String[] words, String target, int startIndex) {
+        int n = words.length;
+        int ans = n;
+
+        for (int i = 0; i < n; i++) {
+            if (words[i].equals(target)) {
+                int dist = Math.abs(i - startIndex);
+                ans = Math.min(ans, Math.min(dist, n - dist));
+            }
+        }
+
+        return ans == n ? -1 : ans;
     }
 
     public int mySol(String[] words, String target, int startIndex) {
