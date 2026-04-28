3ad1497784f892194ebe25a7b9c7d515a09e130c
team-hungrian <def.hunger@gmail.com>
2026-04-28 18:25:29 +0900

    Time: 43 ms (64.89%), Space: 110.4 MB (67.94%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,6 +1,38 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        return mySol(grid, x);
+        return mySol_improved(grid, x);
+    }
+
+    public int mySol_improved(int[][] grid, int x) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[] arr = new int[n * m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i * m + j] = grid[i][j];
+            }
+        }
+
+        Arrays.sort(arr);
+
+        int mid = arr.length / 2;
+        int ans = 0;
+
+        for (int i = 0; i < mid; i++) {
+            int diff1 = arr[mid] - arr[i];
+
+            if (diff1 % x != 0) return -1;
+
+            int diff2 = arr[arr.length - i - 1] - arr[mid];
+
+            if (diff2 % x != 0) return -1;
+
+            ans += diff1 / x;
+            ans += diff2 / x;
+        }
+
+        return ans;
     }
 
     public int mySol(int[][] grid, int x) {
