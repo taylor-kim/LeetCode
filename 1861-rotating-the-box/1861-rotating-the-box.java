344f3396d2f6c63bd494d26d461078f79d1085b9
team-hungrian <def.hunger@gmail.com>
2026-05-06 17:43:13 +0900

    Time: 298 ms (5.05%), Space: 124.8 MB (43.27%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -1,6 +1,59 @@
 class Solution {
     public char[][] rotateTheBox(char[][] box) {
-        return mySol(box);
+        return try_20260506(box);
+    }
+
+    public char[][] try_20260506(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+
+        char[][] ans = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][m - i - 1] = box[i][j];
+            }
+        }
+
+        // for (char[] row : ans) {
+        //     System.out.println(Arrays.toString(row));
+        // }
+
+        for (int j = 0; j < m; j++) {
+            int start = 0;
+            int stone = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (ans[i][j] == '*') {
+                    int empty = (i - start) - stone;
+
+                    while (empty-- > 0 && start < i) {
+                        ans[start++][j] = '.';
+                    }
+
+                    while (stone-- > 0 && start < i) {
+                        ans[start++][j] = '#';
+                    }
+
+                    start = i + 1;
+                    stone = 0;
+                } else if (ans[i][j] == '#') {
+                    stone++;
+                }
+            }
+
+            int empty = (n - start) - stone;
+
+            while (empty-- > 0 && start < n) {
+                ans[start++][j] = '.';
+            }
+
+            while (stone-- > 0 && start < n) {
+                ans[start++][j] = '#';
+            }
+        }
+
+        return ans;
     }
 
     public char[][] mySol(char[][] box) {
