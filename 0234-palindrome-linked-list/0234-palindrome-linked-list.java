fdde951813df22dc94871688bbfe8986415675a6
team-hungrian <def.hunger@gmail.com>
2026-05-07 20:20:37 +0900

    Time: 8 ms (31.84%), Space: 99.4 MB (14.77%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,73 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        return others_good(head);
-    }
+        List<Integer> vals = new ArrayList<Integer>();
 
-    public boolean others_good(ListNode head) {
-        if (head == null) return false;
-        if (head.next == null) return true;
-
-        ListNode slow = head;
-        ListNode fast = head.next;
-        ListNode reverse = null;
-
-        while (fast != null && fast.next != null) {
-            ListNode temp = slow;
-
-            slow = slow.next;
-            fast = fast.next.next;
-
-            temp.next = reverse;
-            reverse = temp;
+        // 将链表的值复制到数组中
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
         }
 
-        if (fast == null) {
-            //odd, slow is mid
-            slow = slow.next;
-        } else {
-            //even, slow id mid - 1
-            ListNode temp = slow;
-            slow = slow.next;
-
-            temp.next = reverse;
-            reverse = temp;
-
+        // 使用双指针判断是否回文
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
         }
-
-        while (reverse != null && slow != null) {
-            if (reverse.val != slow.val) return false;
-            reverse = reverse.next;
-            slow = slow.next;
-        }
-
-        return true;
-    }
-
-    public boolean mySol(ListNode head) {
-        List<Integer> list = new ArrayList();
-
-        while (head != null) {
-            list.add(head.val);
-            head = head.next;
-        }
-
-        int left = 0;
-        int right = list.size() - 1;
-
-        while (left < right) {
-            if (list.get(left++) != list.get(right--)) return false;
-        }
-
         return true;
     }
 }
