e7a2d38dc6b7d929db17b004639e76a5cf84274e
team-hungrian <def.hunger@gmail.com>
2026-05-12 19:24:59 +0900

    Time: 6 ms (10.91%), Space: 47.2 MB (43.47%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,58 +1,16 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        return try_20240601(nums);
-    }
-
-    public int[] try_20240601(int[] nums) {
-        int xor = 0;
-
-        for (int num : nums) xor ^= num;
-
-        xor = findLeftmost(xor);
-        // xor = xor & (-xor);
-
-        int[] ans = new int[2];
-
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
         for (int num : nums) {
-            if ((xor & num) == 0) {
-                ans[0] ^= num;
-            } else {
-                ans[1] ^= num;
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        int[] ans = new int[2];
+        int index = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() == 1) {
+                ans[index++] = entry.getKey();
             }
         }
-
         return ans;
-    }
-
-    private int findLeftmost(int num) {
-        int pos = 0;
-
-        while (pos < 32) {
-            int mask = 1 << pos;
-
-            if ((mask & num) != 0) {
-                return mask;
-            }
-
-            pos++;
-        }
-
-        return -1;
-    }
-
-    private int findRightmost(int num) {
-        int pos = 31;
-
-        while (pos >= 0) {
-            int mask = 1 << pos;
-
-            if ((mask & num) != 0) {
-                return mask;
-            }
-
-            pos--;
-        }
-
-        return -1;
     }
 }
