a7becd1e272c809ad89fbce31c08232c15e8d389
team-hungrian <def.hunger@gmail.com>
2026-05-15 14:26:47 +0900

    Time: 0 ms (100%), Space: 43.7 MB (80.63%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,18 +1,21 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return retry_20241003(nums, target);
+        return try_20260515(nums, target);
     }
 
-    public int retry_20241003(int[] nums, int target) {
+    public int try_20260515(int[] nums, int target) {
+        int n = nums.length;
         int lo = 0;
-        int hi = nums.length - 1;
+        int hi = n - 1;
 
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[lo] <= nums[mid]) {
+            // System.out.println("lo:%d, mid:%d, hi:%d".formatted(lo, mid, hi));
+
+            if (nums[mid] == target) return mid;
+
+            if (nums[lo] <= nums[mid]) {
                 if (nums[lo] <= target && target < nums[mid]) {
                     hi = mid - 1;
                 } else {
@@ -23,6 +26,35 @@ class Solution {
                     lo = mid + 1;
                 } else {
                     hi = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public int try_20240410(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else {
+                if (nums[left] <= nums[mid]) {
+                    if (nums[left] <= target && target <= nums[mid]) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                } else {
+                    if (nums[mid] < target && target <= nums[right]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
                 }
             }
         }
