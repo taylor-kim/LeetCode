74eeb7982cd57ab3692f5e47e8f9f89a5484f846
team-hungrian <def.hunger@gmail.com>
2026-05-16 15:58:12 +0900

    Time: 0 ms (100%), Space: 44.2 MB (46.36%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -1,88 +1,16 @@
 class Solution {
     public int findMin(int[] nums) {
-        return others_better(nums);
-    }
-
-    public int others_better(int[] nums) {
-        int n = nums.length;
-        int lo = 0;
-        int hi = n - 1;
-
-        while (lo < hi) {
-            while (lo < hi && nums[lo] == nums[hi]) {
-                lo++;
+        int low = 0, high = nums.length - 1;
+        while(low < high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] > nums[high])
+                low = mid + 1;
+            else if(nums[mid] == nums[high]) {
+                high--;
             }
-
-            int mid = lo + (hi - lo) / 2;
-
-            if (nums[lo] <= nums[hi]) {
-                hi = mid;
-            } else {
-                if (nums[mid] > nums[hi]) {
-                    lo = mid + 1;
-                } else {
-                    hi = mid;
-                }
-            }
+            else
+                high = mid;
         }
-
-        return nums[lo];
-    }
-
-    public int mySol_fail(int[] nums) {
-        int n = nums.length;
-        int lo = 0;
-        int hi = n - 1;
-
-        long[] pSum = new long[n + 1];
-
-        int min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            pSum[i + 1] = pSum[i] + nums[i];
-
-            min = Math.min(min, nums[i]);
-        }
-
-        return min;
-
-        // System.out.println("pSum:%s".formatted(Arrays.toString(pSum)));
-
-        // while (lo < hi) {
-        //     int mid = lo + (hi - lo) / 2;
-
-        //     if (nums[lo] < nums[hi]) {
-        //         hi = mid;
-        //     } else if (nums[lo] == nums[hi]) {
-        //         int leftLength = mid - lo + 1;
-        //         int rightLength = hi - mid;
-
-        //         long leftSum = pSum[mid + 1] - pSum[lo];
-        //         long rightSum = pSum[hi + 1] - pSum[mid + 1];
-
-        //         if (leftSum == rightSum && leftLength == rightLength) {
-        //             break;
-        //         } else {
-        //             double leftAvg = (double)leftSum / leftLength;
-        //             double rightAvg = (double)rightSum / rightLength;
-
-        //             if (leftAvg < rightAvg) {
-        //                 hi = mid;
-        //             } else {
-        //                 lo = mid + 1;
-        //             }
-        //         }
-        //     } else {
-        //         if (nums[mid] > nums[hi]) {
-        //             lo = mid + 1;
-        //         } else {
-        //             hi = mid;
-        //         }
-        //     }
-        // }
-
-        // // System.out.println("result index : %d".formatted(lo));
-
-        // return nums[lo];
+        return nums[low];
     }
 }
