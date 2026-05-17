36d46cbd43825b17ce811b8a0593a2eb4ba263d7
team-hungrian <def.hunger@gmail.com>
2026-05-17 18:56:47 +0900

    Time: 26 ms (14.64%), Space: 81.3 MB (48%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,90 +1,22 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        return studyAgain_slow_fast(nums);
-    }
-
-    // https://www.youtube.com/watch?v=wjYnzkAhcNk
-    public int studyAgain_slow_fast(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        slow = nums[0];
-
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-
-        return slow;
-    }
-
-    public int mySol(int[] nums) {
-        int ans = -1;
-
-        for (int i = 0; i < nums.length; i++) {
-            int num = Math.abs(nums[i]);
-
-            if (nums[num] < 0) {
-                ans = num;
-                break;
-            } else {
-                nums[num] *= -1;
-            }
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = Math.abs(nums[i]);
-        }
-
-        return ans;
-    }
-
-    public int officialTortoiseAndHare(int[] nums) {
-        int tor = nums[0];
-        int hare = nums[0];
-
-        do {
-            tor = nums[tor];
-            hare = nums[nums[hare]];
-        } while (tor != hare);
-
-        tor = nums[0];
-
-        while (tor != hare) {
-            tor = nums[tor];
-            hare = nums[hare];
-        }
-
-        return tor;
-    }
-
-    public int officialBinarySearch(int[] nums) {
-        int lo = 1;
-        int hi = nums.length - 1;
-
-        while (lo <= hi) {
-            int cand = lo + (hi - lo) / 2;
-
-            int count = 0;
-
-            for (int num : nums) {
-                if (num <= cand) {
-                    count++;
+        int n = nums.length;
+        int l = 1, r = n - 1, ans = -1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] <= mid) {
+                    cnt++;
                 }
             }
-
-            if (count > cand) {
-                hi = cand - 1;
+            if (cnt <= mid) {
+                l = mid + 1;
             } else {
-                lo = cand + 1;
+                r = mid - 1;
+                ans = mid;
             }
         }
-
-        return lo;
+        return ans;
     }
 }
