03f3c21d13fe7476ebd98a2984e57f342d81b16d
team-hungrian <def.hunger@gmail.com>
2026-05-17 18:52:09 +0900

    Time: 7 ms (48.48%), Space: 56.6 MB (18.53%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -4,21 +4,19 @@ class Solution {
     }
 
     public boolean mySol(int[] arr, int start) {
-        return topdown(arr, start, new Boolean[arr.length], new HashSet());
+        return topdown(arr, start, new HashSet());
     }
 
-    public boolean topdown(int[] arr, int index, Boolean[] memo, Set<Integer> visit) {
+    public boolean topdown(int[] arr, int index, Set<Integer> visit) {
         if (index >= arr.length || index < 0) return false;
 
         if (arr[index] == 0) return true;
 
-        // if (memo[index] != null) return memo[index];
-
         if (!visit.add(index)) return false;
 
-        boolean plus = topdown(arr, index + arr[index], memo, visit);
-        boolean minus = topdown(arr, index - arr[index], memo, visit);
+        if (topdown(arr, index + arr[index], visit)) return true;
+        if (topdown(arr, index - arr[index], visit)) return true;
 
-        return memo[index] = plus || minus;
+        return false;
     }
 }
