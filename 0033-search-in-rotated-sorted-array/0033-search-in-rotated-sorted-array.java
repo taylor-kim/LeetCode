999bb750880769dadd6a9bc84b870c7d4e6e7075
team-hungrian <def.hunger@gmail.com>
2026-05-22 18:00:44 +0900

    Time: 0 ms (100%), Space: 43 MB (73.58%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,64 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return try_20260515(nums, target);
-    }
-
-    public int try_20260515(int[] nums, int target) {
         int n = nums.length;
-        int lo = 0;
-        int hi = n - 1;
-
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-
-            // System.out.println("lo:%d, mid:%d, hi:%d".formatted(lo, mid, hi));
-
-            if (nums[mid] == target) return mid;
-
-            if (nums[lo] <= nums[mid]) {
-                if (nums[lo] <= target && target < nums[mid]) {
-                    hi = mid - 1;
-                } else {
-                    lo = mid + 1;
-                }
-            } else {
-                if (nums[mid] < target && target <= nums[hi]) {
-                    lo = mid + 1;
-                } else {
-                    hi = mid - 1;
-                }
-            }
+        if (n == 0) {
+            return -1;
         }
-
-        return -1;
-    }
-
-    public int try_20240410(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } else {
-                if (nums[left] <= nums[mid]) {
-                    if (nums[left] <= target && target <= nums[mid]) {
-                        right = mid - 1;
-                    } else {
-                        left = mid + 1;
-                    }
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
                 } else {
-                    if (nums[mid] < target && target <= nums[right]) {
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
-                    }
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
                 }
             }
         }
-
         return -1;
     }
 }
