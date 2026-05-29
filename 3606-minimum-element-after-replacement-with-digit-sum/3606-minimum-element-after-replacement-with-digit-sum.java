62716613c233b9f22a9a0f34a3833a55a0f715a8
team-hungrian <def.hunger@gmail.com>
2026-05-29 13:30:36 +0900

    Time: 1 ms (100%), Space: 44 MB (86.11%) - LeetHub

---
+++ 3606-minimum-element-after-replacement-with-digit-sum/3606-minimum-element-after-replacement-with-digit-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minElement(int[] nums) {
+        return mySol(nums);
+    }
+
+    public int mySol(int[] nums) {
+        int ans = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            ans = Math.min(ans, sum);
+        }
+
+        return ans;
+    }
+}
