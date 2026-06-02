857568c4db661b9daab2a979c1be799593cc9fab
team-hungrian <def.hunger@gmail.com>
2026-06-02 20:05:48 +0900

    Time: 4 ms (26.19%), Space: 46.8 MB (77.78%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,9 +1,25 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
         return Math.min(
-            solution(landStartTime, landDuration, waterStartTime, waterDuration)
-            , solution(waterStartTime, waterDuration, landStartTime, landDuration)
+            mySol(landStartTime, landDuration, waterStartTime, waterDuration),
+            mySol(waterStartTime, waterDuration, landStartTime, landDuration)
         );
+    }
+
+    public int mySol(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int n = landStartTime.length;
+        int m = waterStartTime.length;
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int end = landStartTime[i] + landDuration[i];
+            for (int j = 0; j < m; j++) {
+                ans = Math.min(ans, Math.max(end, waterStartTime[j]) + waterDuration[j]);
+            }
+        }
+
+        return ans;
     }
 
     public int solution(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
