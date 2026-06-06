bef2b983bfb25bf0d107ae9784a29129d9493c0e
team-hungrian <def.hunger@gmail.com>
2026-06-06 17:49:32 +0900

    Time: 2 ms (98.84%), Space: 46.3 MB (92.55%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,6 +1,23 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
-        return mySol(nums);
+        return try_20260606(nums);
+    }
+
+    public int[] try_20260606(int[] nums) {
+        int n = nums.length;
+        int[] pSum = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            pSum[i + 1] = pSum[i] + nums[i];
+        }
+
+        int[] ans = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            ans[i] = Math.abs(pSum[i] - (pSum[n] - pSum[i + 1]));
+        }
+
+        return ans;
     }
 
     public int[] mySol(int[] nums) {
