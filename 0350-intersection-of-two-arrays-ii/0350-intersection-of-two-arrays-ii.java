8411c639367915abc95d06134efd9806550271d3
team-hungrian <def.hunger@gmail.com>
2026-06-07 17:59:42 +0900

    Time: 6 ms (38.82%), Space: 45.1 MB (37.14%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,27 +1,47 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return intersect(nums2, nums1);
-        }
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        return othersMap(nums1, nums2);
+    }
+
+    public int[] othersMap(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> freq = new HashMap();
+
         for (int num : nums1) {
-            int count = map.getOrDefault(num, 0) + 1;
-            map.put(num, count);
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
-        int[] intersection = new int[nums1.length];
-        int index = 0;
+
+        List<Integer> list = new ArrayList();
+
         for (int num : nums2) {
-            int count = map.getOrDefault(num, 0);
-            if (count > 0) {
-                intersection[index++] = num;
-                count--;
-                if (count > 0) {
-                    map.put(num, count);
-                } else {
-                    map.remove(num);
-                }
+            if (freq.getOrDefault(num, 0) > 0) {
+                freq.put(num, freq.get(num) - 1);
+                list.add(num);
             }
         }
-        return Arrays.copyOfRange(intersection, 0, index);
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] mySol(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        List<Integer> list = new ArrayList();
+
+        int i = 0, j = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
