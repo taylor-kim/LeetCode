a14a15912a8df21a4e2548c73161e234cf00ee51
team-hungrian <def.hunger@gmail.com>
2026-06-07 17:57:49 +0900

    Time: 3 ms (89.59%), Space: 44.2 MB (75.97%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,28 +1,27 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        return mySol(nums1, nums2);
-    }
-
-    public int[] mySol(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-
-        List<Integer> list = new ArrayList();
-
-        int i = 0, j = 0;
-
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                list.add(nums1[i]);
-                i++;
-                j++;
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums1) {
+            int count = map.getOrDefault(num, 0) + 1;
+            map.put(num, count);
+        }
+        int[] intersection = new int[nums1.length];
+        int index = 0;
+        for (int num : nums2) {
+            int count = map.getOrDefault(num, 0);
+            if (count > 0) {
+                intersection[index++] = num;
+                count--;
+                if (count > 0) {
+                    map.put(num, count);
+                } else {
+                    map.remove(num);
+                }
             }
         }
-
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.copyOfRange(intersection, 0, index);
     }
 }
