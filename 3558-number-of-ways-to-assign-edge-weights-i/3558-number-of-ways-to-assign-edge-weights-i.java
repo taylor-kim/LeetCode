78fd6afca498fe533d63f26bf8fb3fdf661d51f7
team-hungrian <def.hunger@gmail.com>
2026-06-12 02:08:53 +0900

    Time: 210 ms (23.81%), Space: 304.5 MB (11.9%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.java
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.java
@@ -1,42 +1,49 @@
 class Solution {
-
-    private static final int MOD = 1_000_000_007;
-
-    private int qpow(int x, int y) {
-        long res = 1;
-        long base = x;
-        while (y > 0) {
-            if ((y & 1) == 1) {
-                res = (res * base) % MOD;
-            }
-            base = (base * base) % MOD;
-            y >>= 1;
-        }
-        return (int) res;
-    }
-
-    private int dfs(List<List<Integer>> g, int x, int f) {
-        int maxDep = 0;
-        for (int y : g.get(x)) {
-            if (y == f) continue;
-            maxDep = Math.max(maxDep, dfs(g, y, x) + 1);
-        }
-        return maxDep;
-    }
-
     public int assignEdgeWeights(int[][] edges) {
-        int n = edges.length + 1;
-        List<List<Integer>> g = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            g.add(new ArrayList<>());
+        return mySol(edges);
+    }
+
+    public int mySol(int[][] edges) {
+        Map<Integer, List<Integer>> graph = getGraph(edges);
+
+        int max = getMax(graph, 1, 0);
+
+        return topdown(max)[0];
+    }
+
+    private int[] topdown(int depth) {
+        if (depth == 1) return new int[] {1, 1};
+
+        int[] oddEven = topdown(depth - 1);
+
+        int mod = (int)1e9 + 7;
+
+        int odd = (oddEven[0] + oddEven[1]) % mod;
+        int even = (oddEven[0] + oddEven[1]) % mod;
+
+        return new int[] {odd, even};
+    }
+
+    private int getMax(Map<Integer, List<Integer>> graph, int node, int prev) {
+        int max = 0;
+
+        for (int next : graph.get(node)) {
+            if (next == prev) continue;
+
+            max = Math.max(max, 1 + getMax(graph, next, node));
         }
-        for (int[] e : edges) {
-            int u = e[0];
-            int v = e[1];
-            g.get(u).add(v);
-            g.get(v).add(u);
+
+        return max;
+    }
+
+    private Map<Integer, List<Integer>> getGraph(int[][] edges) {
+        Map<Integer, List<Integer>> graph = new HashMap();
+
+        for (int[] edge : edges) {
+            graph.computeIfAbsent(edge[0], k -> new ArrayList()).add(edge[1]);
+            graph.computeIfAbsent(edge[1], k -> new ArrayList()).add(edge[0]);
         }
-        int maxDep = dfs(g, 1, 0);
-        return qpow(2, maxDep - 1);
+
+        return graph;
     }
 }
