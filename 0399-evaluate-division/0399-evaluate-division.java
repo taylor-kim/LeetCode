006d1b1e678f6f2b08f24fcd8ef8bd1e17e16696
team-hungrian <def.hunger@gmail.com>
2026-06-24 18:55:37 +0900

    Time: 1 ms (99.77%), Space: 45.3 MB (96.01%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,59 +1,105 @@
-class Solution {
-    private void dfs(HashMap<String,HashMap<String,Double>> gr, String src, String dst, double[] cur, double temp, HashSet<String> vis) {
-    if (vis.contains(src)) return; // \U0001f504 Avoid cycles
-    vis.add(src);
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    if (src.equals(dst)) { // \U0001f3af Found target
-        cur[0] = temp;
-        return;
+public class Solution {
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        int equationsSize = equations.size();
+
+        UnionFind unionFind = new UnionFind(2 * equationsSize);
+        // 第 1 步：预处理，将变量的值与 id 进行映射，使得并查集的底层使用数组实现，方便编码
+        Map<String, Integer> hashMap = new HashMap<>(2 * equationsSize);
+        int id = 0;
+        for (int i = 0; i < equationsSize; i++) {
+            List<String> equation = equations.get(i);
+            String var1 = equation.get(0);
+            String var2 = equation.get(1);
+
+            if (!hashMap.containsKey(var1)) {
+                hashMap.put(var1, id);
+                id++;
+            }
+            if (!hashMap.containsKey(var2)) {
+                hashMap.put(var2, id);
+                id++;
+            }
+            unionFind.union(hashMap.get(var1), hashMap.get(var2), values[i]);
+        }
+
+        // 第 2 步：做查询
+        int queriesSize = queries.size();
+        double[] res = new double[queriesSize];
+        for (int i = 0; i < queriesSize; i++) {
+            String var1 = queries.get(i).get(0);
+            String var2 = queries.get(i).get(1);
+
+            Integer id1 = hashMap.get(var1);
+            Integer id2 = hashMap.get(var2);
+
+            if (id1 == null || id2 == null) {
+                res[i] = -1.0d;
+            } else {
+                res[i] = unionFind.isConnected(id1, id2);
+            }
+        }
+        return res;
     }
 
-    for (Map.Entry<String, Double> edge : gr.get(src).entrySet()) { // \U0001f504 Visit neighbors
-        String curVal = edge.getKey();
-        double val = edge.getValue();
-        dfs(gr, curVal, dst, cur, temp * val, vis);
-    }
-}
+    private class UnionFind {
 
-private HashMap<String, HashMap<String, Double>> createGraph(List<List<String>> equations, double[] values) {
-    HashMap<String, HashMap<String, Double>> gr = new HashMap<>();
-    int n = equations.size();
+        private int[] parent;
 
-    for (int i = 0; i < n; i++) {
-        String src = equations.get(i).get(0);
-        String dst = equations.get(i).get(1);
-        double ans = values[i];
+        /**
+         * 指向的父结点的权值
+         */
+        private double[] weight;
 
-        gr.putIfAbsent(src, new HashMap<>());
-        gr.putIfAbsent(dst, new HashMap<>());
 
-        gr.get(src).put(dst, ans);         // ➗ Forward relation
-        gr.get(dst).put(src, 1.0 / ans);   // \U0001f504 Reverse relation
-    }
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            this.weight = new double[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                weight[i] = 1.0d;
+            }
+        }
 
-    return gr;
-}
+        public void union(int x, int y, double value) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
 
-public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-    HashMap<String, HashMap<String, Double>> gr = createGraph(equations, values);
-    int n = queries.size();
-    double[] ans = new double[n];
+            parent[rootX] = rootY;
+          	// 关系式的推导请见「参考代码」下方的示意图
+            weight[rootX] = weight[y] * value / weight[x];
+        }
 
-    for (int i = 0; i < n; i++) {
-        String src = queries.get(i).get(0);
-        String dst = queries.get(i).get(1);
+        /**
+         * 路径压缩
+         *
+         * @param x
+         * @return 根结点的 id
+         */
+        public int find(int x) {
+            if (x != parent[x]) {
+                int origin = parent[x];
+                parent[x] = find(parent[x]);
+                weight[x] *= weight[origin];
+            }
+            return parent[x];
+        }
 
-        if (!gr.containsKey(src) || !gr.containsKey(dst)) { // ⚠️ Variables not present
-            ans[i] = -1.0;
-        } else {
-            HashSet<String> vis = new HashSet<>();
-            double[] cur = { -1.0 };
-            double temp = 1.0;
-            dfs(gr, src, dst, cur, temp, vis);
-            ans[i] = cur[0];
+        public double isConnected(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return weight[x] / weight[y];
+            } else {
+                return -1.0d;
+            }
         }
     }
-
-    return ans;
-}
 }
