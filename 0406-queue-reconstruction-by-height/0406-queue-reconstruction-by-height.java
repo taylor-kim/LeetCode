ca669b1802972c6e5106527924893abc6bce92c5
team-hungrian <def.hunger@gmail.com>
2026-06-27 23:36:43 +0900

    Time: 18 ms (7.11%), Space: 46.5 MB (56.07%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] person1, int[] person2) {
+                if (person1[0] != person2[0]) {
+                    return person1[0] - person2[0];
+                } else {
+                    return person2[1] - person1[1];
+                }
+            }
+        });
+        int n = people.length;
+        int[][] ans = new int[n][];
+        for (int[] person : people) {
+            int spaces = person[1] + 1;
+            for (int i = 0; i < n; ++i) {
+                if (ans[i] == null) {
+                    --spaces;
+                    if (spaces == 0) {
+                        ans[i] = person;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
