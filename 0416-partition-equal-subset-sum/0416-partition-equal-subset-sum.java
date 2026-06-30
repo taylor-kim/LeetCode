7cc4bf7203bd2ff48b0e0e8b92528303c0b21704
team-hungrian <def.hunger@gmail.com>
2026-06-30 15:44:01 +0900

    Time: 44 ms (26.36%), Space: 46.9 MB (20.84%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,87 +1,36 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        return try_bottomup_spaceopt(nums);
-    }
-
-    public boolean try_bottomup_spaceopt(int[] nums) {
-        int sum = 0;
-
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
         for (int num : nums) {
             sum += num;
+            maxNum = Math.max(maxNum, num);
         }
-
-        if (sum % 2 == 1) return false;
-
-        sum /= 2;
-
-        int n = nums.length;
-
-        boolean[] dp = new boolean[sum + 1];
-        dp[0] = true;
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = sum; j >= 0; j--) {
-                if (j >= nums[i]) {
-                    dp[j] = dp[j - nums[i]] || dp[j];
-                }
-            }
+        if (sum % 2 != 0) {
+            return false;
         }
-
-        return dp[sum];
-    }
-
-    public boolean try_bottomup(int[] nums) {
-        int sum = 0;
-
-        for (int num : nums) {
-            sum += num;
+        int target = sum / 2;
+        if (maxNum > target) {
+            return false;
         }
-
-        if (sum % 2 == 1) return false;
-
-        sum /= 2;
-
-        int n = nums.length;
-
-        boolean[][] dp = new boolean[n + 1][sum + 1];
-        dp[n][0] = true;
-
-        for (int i = n - 1; i >= 0; i--) {
-            // dp[i][0] = true;
-            for (int j = 1; j <= sum; j++) {
-                if (j >= nums[i]) {
-                    dp[i][j] |= dp[i + 1][j - nums[i]] || dp[i + 1][j];
+        boolean[][] dp = new boolean[n][target + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 1; j <= target; j++) {
+                if (j >= num) {
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
                 } else {
-                    dp[i][j] |= dp[i + 1][j];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-
-        return dp[0][sum];
-    }
-
-    public boolean try_20250408(int[] nums) {
-        int sum = 0;
-
-        for (int num : nums) {
-            sum += num;
-        }
-
-        if (sum % 2 == 1) return false;
-
-        return topdown(nums, 0, sum / 2, new Boolean[nums.length][sum / 2 + 1]);
-    }
-
-    private boolean topdown(int[] nums, int index, int remain, Boolean[][] memo) {
-        if (remain < 0) return false;
-
-        if (remain == 0) return true;
-
-        if (index >= nums.length) return remain == 0;
-
-        if (memo[index][remain] != null) return memo[index][remain];
-
-        return memo[index][remain] = topdown(nums, index + 1, remain - nums[index], memo)
-            || topdown(nums, index + 1, remain, memo);
+        return dp[n - 1][target];
     }
 }
