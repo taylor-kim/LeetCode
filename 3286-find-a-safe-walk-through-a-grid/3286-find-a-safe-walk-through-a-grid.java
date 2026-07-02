ab1588dd409b791fcddf435c563f27c54ec5721e
team-hungrian <def.hunger@gmail.com>
2026-07-02 20:38:45 +0900

    Time: 9 ms (91.39%), Space: 47.1 MB (42.62%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
@@ -1,6 +1,60 @@
 class Solution {
     public boolean findSafeWalk(List<List<Integer>> grid, int health) {
-        return mySol(grid, health);
+        return official_binaryBfs(grid, health);
+    }
+
+    public boolean official_binaryBfs(List<List<Integer>> grid, int health) {
+        int m = grid.size();
+        int n = grid.get(0).size();
+
+        int[][] costs = new int[m][n];
+
+        for (int[] row : costs) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+
+        costs[0][0] = grid.get(0).get(0);
+
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.offerFirst(new int[] {costs[0][0], 0, 0});
+
+        int[][] dirs = {
+            {0, 1}, {0, -1}, {1, 0}, {-1 , 0}
+        };
+
+        while (!deque.isEmpty()) {
+            int[] data = deque.pollFirst();
+            int cost = data[0];
+            int y = data[1];
+            int x = data[2];
+
+            if (cost >= health) {
+                return false;
+            }
+
+            if (y == m - 1 && x == n - 1) return true;
+
+            for (int[] dir : dirs) {
+                int ny = y + dir[0];
+                int nx = x + dir[1];
+                
+                if (ny >= 0 && ny < m && nx >= 0 && nx < n) {
+                    int nextCost = cost + grid.get(ny).get(nx);
+
+                    if (costs[ny][nx] <= nextCost) continue;
+
+                    costs[ny][nx] = nextCost;
+
+                    if (nextCost - cost == 0) {
+                        deque.offerFirst(new int[] {nextCost, ny, nx});
+                    } else {
+                        deque.offerLast(new int[] {nextCost, ny, nx});
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 
     public boolean mySol(List<List<Integer>> grid, int health) {
