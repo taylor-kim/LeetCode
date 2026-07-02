7398940a3ee93da6f3548f291f79ae9b809afbfd
team-hungrian <def.hunger@gmail.com>
2026-07-02 19:41:41 +0900

    Time: 405 ms (7.19%), Space: 185.1 MB (5%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -1,181 +1,91 @@
 class Solution {
     public int maximumSafenessFactor(List<List<Integer>> grid) {
-        return official_greedy_dijkstra(grid);
+        return mySol(grid);
     }
 
-    private int[][] dirs = {
-        {0, 1}
-        , {0, -1}
-        , {1, 0}
-        , {-1, 0}
+    int[][] dirs = {
+        {1, 0},
+        {-1, 0},
+        {0, 1},
+        {0, -1},
     };
 
-    public int official_greedy_dijkstra(List<List<Integer>> grid) {
-        int n = grid.size();
+    public int mySol(List<List<Integer>> grid) {
+        Queue<Integer> queue = new LinkedList();
+        Map<Integer, Integer> points = new HashMap();
 
-        Queue<int[]> queue = new LinkedList();
+        int n = grid.size();
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid.get(i).get(j) == 1) {
-                    grid.get(i).set(j, 0);
-                    queue.add(new int[] {i, j});
-                } else {
-                    grid.get(i).set(j, -1);
+                    int index = i * n + j;
+                    queue.add(index);
+                    points.put(index, 0);
                 }
             }
         }
 
         while (!queue.isEmpty()) {
-            int size = queue.size();
+            int index = queue.poll();
+            int y = index / n;
+            int x = index % n;
+            int p = points.get(index);
 
-            while (size-- > 0) {
-                int[] pos = queue.poll();
-                int r = pos[0];
-                int c = pos[1];
-                int safeness = grid.get(r).get(c);
+            for (int[] dir : dirs) {
+                int ny = y + dir[0];
+                int nx = x + dir[1];
+                int nIndex = ny * n + nx;
 
-                for (int[] d : dirs) {
-                    int nr = r + d[0];
-                    int nc = c + d[1];
-
-                    if (isValidCell(n, nr, nc) && grid.get(nr).get(nc) == -1) {
-                        queue.add(new int[] {nr, nc});
-                        grid.get(nr).set(nc, safeness + 1);
-                    }
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && !points.containsKey(nIndex)) {
+                    queue.add(nIndex);
+                    points.put(nIndex, p + 1);
                 }
             }
         }
 
-        if (grid.get(0).get(0) == 0 || grid.get(n - 1).get(n - 1) == 0) {
-            return 0;
-        }
+        // System.out.println(points);
 
         Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            return b[2] - a[2];
+            return b[1] - a[1];
         });
 
-        pq.add(new int[] {0, 0, grid.get(0).get(0)});
-        boolean[][] visit = new boolean[n][n];
-        visit[0][0] = true;
+        pq.add(new int[] {0, points.remove(0)});
+
+        int finish = (n * n) - 1;
 
         while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
+            int[] data = pq.poll();
+            int index = data[0];
+            int p = data[1];
 
-            if (cur[0] == n - 1 && cur[1] == n - 1) {
-                return cur[2];
+            int y = index / n;
+            int x = index % n;
+
+            // System.out.println("index:%d, y:%d, x:%d, p:%d".formatted(index, y, x, p));
+
+            if (index == finish) {
+                return p;
             }
 
-            for (int[] d : dirs) {
-                int nr = cur[0] + d[0];
-                int nc = cur[1] + d[1];
+            for (int[] dir : dirs) {
+                int ny = y + dir[0];
+                int nx = x + dir[1];
+                int nIndex = ny * n + nx;
 
-                if (isValidCell(n, nr, nc) && !visit[nr][nc]) {
-                    visit[nr][nc] = true;
-                    pq.add(new int[] {nr, nc, Math.min(grid.get(nr).get(nc), cur[2])});
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && points.containsKey(nIndex)) {
+                    pq.add(new int[] {nIndex, Math.min(p, points.remove(nIndex))});
                 }
             }
         }
+
+        /**
+        [3,2,1,0]
+        [2,3,2,1]
+        [1,2,3,2]
+        [0,1,2,3]
+         */
 
         return -1;
-    }
-
-    public int official_bfs_bs(List<List<Integer>> grid) {
-        int n = grid.size();
-
-        Queue<int[]> queue = new LinkedList();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid.get(i).get(j) == 1) {
-                    grid.get(i).set(j, 0);
-                    queue.add(new int[] {i, j});
-                } else {
-                    grid.get(i).set(j, -1);
-                }
-            }
-        }
-
-        int maxSafeness = 0;
-
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-
-            while (size-- > 0) {
-                int[] pos = queue.poll();
-                int r = pos[0];
-                int c = pos[1];
-                int safeness = grid.get(r).get(c);
-
-                maxSafeness = Math.max(maxSafeness, safeness);
-
-                for (int[] d : dirs) {
-                    int nr = r + d[0];
-                    int nc = c + d[1];
-
-                    if (isValidCell(n, nr, nc) && grid.get(nr).get(nc) == -1) {
-                        queue.add(new int[] {nr, nc});
-                        grid.get(nr).set(nc, safeness + 1);
-                    }
-                }
-            }
-        }
-
-        int lo = 0;
-        int hi = maxSafeness;
-        int ans = 0;
-
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-
-            if (isValidSafeness(grid, mid)) {
-                ans = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
-        }
-
-        return ans;
-    }
-
-    private boolean isValidCell(int n, int r, int c) {
-        return r >= 0 && r < n && c >= 0 && c < n;
-    }
-
-    private boolean isValidSafeness(List<List<Integer>> grid, int minSafeness) {
-        int n = grid.size();
-
-        if (grid.get(0).get(0) < minSafeness || grid.get(n - 1).get(n - 1) < minSafeness) {
-            return false;
-        }
-
-        Queue<int[]> queue = new LinkedList();
-
-        boolean[][] visit = new boolean[n][n];
-        visit[0][0] = true;
-
-        queue.add(new int[] {0, 0});
-
-        while (!queue.isEmpty()) {
-            int r = queue.peek()[0];
-            int c = queue.poll()[1];
-
-            if (r == n - 1 && c == n - 1) {
-                return true;
-            }
-
-            for (int[] d : dirs) {
-                int nr = r + d[0];
-                int nc = c + d[1];
-
-                if (isValidCell(n, nr, nc) && !visit[nr][nc] && grid.get(nr).get(nc) >= minSafeness) {
-                    visit[nr][nc] = true;
-                    queue.add(new int[] {nr, nc});
-                }
-            }
-        }
-
-        return false;
     }
 }
