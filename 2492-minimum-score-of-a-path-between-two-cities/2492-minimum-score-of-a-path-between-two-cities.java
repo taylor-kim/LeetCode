ce58e8f2aa1b15696fdf129acbfe6b28315822c2
team-hungrian <def.hunger@gmail.com>
2026-07-04 18:11:20 +0900

    Time: 71 ms (13.82%), Space: 174.3 MB (22.59%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -3,7 +3,42 @@ class Solution {
         return mySol(n, roads);
     }
 
-    public int mySol(int n, int[][] roads) {     
+    public int mySol(int n, int[][] roads) {
+        return dfs(n, roads);
+    }
+
+    public int dfs(int n, int[][] roads) {
+        Map<Integer, List<int[]>> graph = new HashMap();
+
+        for (int[] road : roads) {
+            graph.computeIfAbsent(road[0], k -> new ArrayList()).add(new int[] {road[1], road[2]});
+            graph.computeIfAbsent(road[1], k -> new ArrayList()).add(new int[] {road[0], road[2]});
+        }
+
+        int[] costs = new int[n + 1];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+
+        return topdown(graph, 1, costs, costs[1]);
+    }
+
+    public int topdown(Map<Integer, List<int[]>> graph, int node, int[] costs, int cost) {
+        int ans = cost;
+
+        for (int[] next : graph.get(node)) {
+            int nextNode = next[0];
+            int nextCost = next[1];
+
+            if (costs[nextNode] > nextCost) {
+                costs[nextNode] = nextCost;
+
+                ans = Math.min(ans, topdown(graph, nextNode, costs, nextCost));
+            }
+        }
+
+        return ans;
+    }
+
+    public int bfs(int n, int[][] roads) {
         Map<Integer, List<int[]>> graph = new HashMap();
 
         for (int[] road : roads) {
