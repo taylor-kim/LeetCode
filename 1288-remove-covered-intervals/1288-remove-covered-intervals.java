189e288ed0fd2c7afa53574124239dd66ce40b20
team-hungrian <def.hunger@gmail.com>
2026-07-06 19:54:28 +0900

    Time: 4 ms (97.38%), Space: 46.3 MB (82.87%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,6 +1,29 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        return mySol(intervals);
+        return mySol2(intervals);
+    }
+
+    public int mySol2(int[][] intervals) {
+        int ans = 0;
+        int n = intervals.length;
+
+        for (int i = 0; i < n; i++) {
+            int a = intervals[i][0];
+            int b = intervals[i][1];
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
+                int c = intervals[j][0];
+                int d = intervals[j][1];
+                
+                if (c <= a && b <= d) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+
+        return n - ans;
     }
 
     public int mySol(int[][] intervals) {
