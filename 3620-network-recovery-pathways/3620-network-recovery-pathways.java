51837398981837a60bc9549d889d512f7f3c8121
team-hungrian <def.hunger@gmail.com>
2026-07-11 17:48:41 +0900

    Time: 163 ms (56.87%), Space: 183.2 MB (48.88%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.java
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.java
@@ -1,20 +1,17 @@
 class Solution {
     public int findMaxPathScore(int[][] edges, boolean[] online, long k) {
-        return mySol_after_topic(edges, online, k);
+        return try_20260711(edges, online, k);
     }
 
-    public int mySol_after_topic(int[][] edges, boolean[] online, long k) {
-        int n = online.length;
-
-        Queue<long[]> pq = new PriorityQueue<>((a, b) -> {
-            return b[2] != a[2] ? Long.compare(b[2], a[2]) : Long.compare(a[1], b[1]);
-        });
-
+    public int try_20260711(int[][] edges, boolean[] online, long k) {
         Map<Integer, List<int[]>> graph = new HashMap();
+        int max = 0;
 
         for (int[] edge : edges) {
             if (online[edge[0]] && online[edge[1]]) {
                 graph.computeIfAbsent(edge[0], key -> new ArrayList()).add(new int[] {edge[1], edge[2]});
+
+                max = Math.max(max, edge[2]);
             }
         }
 
@@ -25,114 +22,53 @@ class Solution {
         }
 
         int lo = 0;
-        int hi = Integer.MAX_VALUE;
-        int ans = -1;
+        int hi = max + 1;
 
-        while (lo <= hi) {
+        while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
 
-            if (find(graph, k, mid, n)) {
-                ans = mid;
+            if (find(graph, online.length, k, mid)) {
                 lo = mid + 1;
             } else {
-                hi = mid - 1;
+                hi = mid;
             }
         }
 
-        return ans;
+        return lo - 1;
     }
 
-    private boolean find(Map<Integer, List<int[]>> graph, long k, int min, int n) {
-        Queue<long[]> pq = new PriorityQueue<>((a, b) -> {
-            // return b[2] != a[2] ? Long.compare(b[2], a[2]) : Long.compare(a[1], b[1]);
+    private boolean find(Map<Integer, List<int[]>> graph, int n, long k, int min) {
+        long[] costs = new long[n];
+
+        Arrays.fill(costs, Long.MAX_VALUE);
+        costs[0] = 0;
+
+        Queue<long[]> queue = new PriorityQueue<>((a, b) -> {
             return Long.compare(a[1], b[1]);
         });
+        queue.add(new long[] {0, 0});
 
-        pq.add(new long[] {0, 0});
-        long[] minCost = new long[n];
-        Arrays.fill(minCost, Long.MAX_VALUE);
-        minCost[0] = 0;
+        while (!queue.isEmpty()) {
+            int node = (int)queue.peek()[0];
+            long cost = queue.poll()[1];
 
-        while (!pq.isEmpty()) {
-            long[] data = pq.poll();
-            int node = (int)data[0];
-            long totalCost = data[1];
-
-            // System.out.println("n:%d, node:%d, totalCost:%d, minCost:%d, min:%d".formatted(n, node, totalCost, minCost, min));
-
-            if (node == n - 1) {
-                // System.out.println("n:%d, node:%d, totalCost:%d, minCost:%d, min:%d".formatted(n, node, totalCost, minCost, min));
-            }
-
-            if (node == n - 1 && totalCost <= k) {
-                // System.out.println("node:%d, totalCost:%d, minCost:%d, min:%d".formatted(node, totalCost, minCost, min));
+            if (node == n - 1 && cost <= k) {
                 return true;
             }
 
-            if (totalCost > k) continue;
-
             if (!graph.containsKey(node)) continue;
-            
+
             for (int[] next : graph.get(node)) {
                 int nextNode = next[0];
-                int addingCost = next[1];
-                long nextTotalCost = totalCost + addingCost;
+                long nextCost = cost + next[1];
 
-                if (nextTotalCost > k || addingCost < min) continue;
-
-                if (minCost[nextNode] > nextTotalCost) {
-                    minCost[nextNode] = nextTotalCost;
-                    pq.add(new long[] {nextNode, nextTotalCost});
+                if (costs[nextNode] > nextCost && k >= nextCost && next[1] >= min) {
+                    costs[nextNode] = nextCost;
+                    queue.add(new long[] {nextNode, nextCost});
                 }
             }
         }
 
         return false;
-    }
-
-    public int mySol_fail_mle(int[][] edges, boolean[] online, long k) {
-        int n = online.length;
-
-        Queue<long[]> pq = new PriorityQueue<>((a, b) -> {
-            return b[2] != a[2] ? Long.compare(b[2], a[2]) : Long.compare(a[1], b[1]);
-        });
-
-        Map<Integer, List<int[]>> graph = new HashMap();
-        Map<Integer, Map<Integer, Integer>> costs = new HashMap();
-
-        for (int[] edge : edges) {
-            if (online[edge[0]] && online[edge[1]]) {
-                graph.computeIfAbsent(edge[0], key -> new ArrayList()).add(new int[] {edge[1], edge[2]});
-            }
-        }
-
-        pq.add(new long[] {0, 0, Integer.MAX_VALUE});
-
-        while (!pq.isEmpty()) {
-            long[] data = pq.poll();
-            int node = (int)data[0];
-            long totalCost = data[1];
-            int minCost = (int)data[2];
-
-            if (node == n - 1 && totalCost <= k) {
-                return minCost;
-            }
-
-            if (totalCost > k) continue;
-
-            if (!graph.containsKey(node)) continue;
-            
-            for (int[] next : graph.get(node)) {
-                int nextNode = next[0];
-                if (!online[nextNode]) continue;
-                int addingCost = next[1];
-
-                if (totalCost + addingCost > k) continue;
-
-                pq.add(new long[] {nextNode, totalCost + addingCost, Math.min(minCost, addingCost)});
-            }
-        }
-
-        return -1;
     }
 }
