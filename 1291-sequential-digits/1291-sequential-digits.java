d69eb233e512f5c7b06dfe23968485793919e226
team-hungrian <def.hunger@gmail.com>
2026-07-13 20:23:35 +0900

    Time: 1 ms (28.06%), Space: 42.2 MB (73.12%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        return try_bf(low, high);
+    }
+
+    public List<Integer> try_bf(int low, int high) {
+        Map<Integer, List<Integer>> map = new HashMap();
+
+        for (int i = 1; i < 10; i++) {
+            int num = i;
+            int length = 1;
+
+            while (num <= high) {
+                if (low <= num && num <= high) {
+                    map.computeIfAbsent(length, k -> new ArrayList()).add(num);
+                }
+
+                int digit = num % 10;
+
+                if (digit == 9) break;
+
+                num = num * 10 + digit + 1;
+                length++;
+            }
+        }
+
+        List<Integer> list = new ArrayList();
+
+        for (int i = 1; i < 10; i++) {
+            if (map.containsKey(i)) {
+                list.addAll(map.get(i));
+            }
+        }
+
+        return list;
+    }
+
+    public List<Integer> mySol_hold(int low, int high) {
+        List<Integer> list = new ArrayList();
+
+        backtrack("" + low, "" + high, false, 0, 0, list);
+
+        return list;
+    }
+
+    private void backtrack(String lo, String hi, boolean carry, int index, int num, List<Integer> list) {
+        if (index >= hi.length()) return;
+
+        int prev = num % 10;
+        int plusOne = prev + 1;
+
+        int from = index >= lo.length() ? 0 : lo.charAt(index) - '0';
+        int to = hi.charAt(index) - '0';
+
+        // 400
+        // 1234
+
+        // 1
+    }
+}
