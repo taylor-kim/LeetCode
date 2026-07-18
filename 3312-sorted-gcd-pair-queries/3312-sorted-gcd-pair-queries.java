03d7e6d39cfde02e5aec3cb7d95f05b4ce9c02f2
team-hungrian <def.hunger@gmail.com>
2026-07-18 15:52:39 +0900

    Time: 31 ms (58.82%), Space: 99.4 MB (79.41%) - LeetHub

--- 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
@@ -1,45 +1,58 @@
 class Solution {
-
     public int[] gcdValues(int[] nums, long[] queries) {
-        int m = 0;
+        return try_after_editorial(nums, queries);
+    }
+
+    public int[] try_after_editorial(int[] nums, long[] queries) {
+        int max = 0;
+        for (int num : nums) max = Math.max(max, num);
+
+        long[] counter = new long[max + 1];
+
         for (int num : nums) {
-            m = Math.max(m, num);
+            counter[num]++;
         }
-        long[] cnt = new long[m + 1];
-        for (int num : nums) {
-            cnt[num]++;
-        }
-        for (int i = 1; i <= m; i++) {
-            for (int j = i * 2; j <= m; j += i) {
-                cnt[i] += cnt[j];
+
+        for (int gcd = 1; gcd <= max; gcd++) {
+            for (int multi = gcd * 2; multi <= max; multi += gcd) {
+                counter[gcd] += counter[multi];
             }
         }
-        for (int i = 1; i <= m; i++) {
-            cnt[i] = (cnt[i] * (cnt[i] - 1)) / 2;
+
+        for (int gcd = 1; gcd <= max; gcd++) {
+            counter[gcd] = (counter[gcd] * (counter[gcd] - 1)) / 2;
         }
-        for (int i = m; i >= 1; i--) {
-            for (int j = i * 2; j <= m; j += i) {
-                cnt[i] -= cnt[j];
+
+        for (int gcd = max; gcd >= 1; gcd--) {
+            for (int multi = gcd * 2; multi <= max; multi += gcd) {
+                counter[gcd] -= counter[multi];
             }
         }
-        for (int i = 1; i <= m; i++) {
-            cnt[i] += cnt[i - 1];
+
+        for (int i = 1; i < counter.length; i++) {
+            counter[i] += counter[i - 1];
         }
+
         int[] ans = new int[queries.length];
-        for (int k = 0; k < queries.length; k++) {
-            long q = queries[k] + 1;
-            int left = 1,
-                right = m;
-            while (left < right) {
-                int mid = (left + right) / 2;
-                if (cnt[mid] >= q) {
-                    right = mid;
+
+        for (int i = 0; i < queries.length; i++) {
+            long q = queries[i] + 1;
+            int lo = 1;
+            int hi = max + 1;
+
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+                
+                if (counter[mid] < q) {
+                    lo = mid + 1;
                 } else {
-                    left = mid + 1;
+                    hi = mid;
                 }
             }
-            ans[k] = left;
+
+            ans[i] = lo;
         }
+
         return ans;
     }
 }
