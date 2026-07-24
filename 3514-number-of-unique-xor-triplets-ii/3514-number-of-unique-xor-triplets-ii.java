f82c2c0401f80780460ff1c937a23ddb8d8f8111
team-hungrian <def.hunger@gmail.com>
2026-07-24 19:09:37 +0900

    Time: 258 ms (56.36%), Space: 46.9 MB (85.45%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -23,7 +23,7 @@ class Solution {
         boolean[] xorThree = new boolean[xorMax + 1];
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
+            for (int j = i; j < nums.length; j++) {
                 int xor = nums[i] ^ nums[j];
                 xorTwo[xor] = true;
             }
