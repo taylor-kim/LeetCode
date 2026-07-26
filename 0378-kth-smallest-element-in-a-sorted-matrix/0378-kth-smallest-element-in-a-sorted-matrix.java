5b3e3bc42dc67d515c229081c06c7a22a4c0a6b5
team-hungrian <def.hunger@gmail.com>
2026-07-26 19:02:11 +0900

    Time: 0 ms (100%), Space: 51.3 MB (5.95%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,21 +1,33 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
         int n = matrix.length;
-        for (int i = 0; i < n; i++) {
-            pq.offer(new int[]{matrix[0][i], 0, i});
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (check(matrix, mid, k, n)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        for (int i = 0; i < k - 1; i++) {
-            int[] now = pq.poll();
-            if (now[1] != n - 1) {
-                pq.offer(new int[]{matrix[now[1] + 1][now[2]], now[1] + 1, now[2]});
+        return left;
+    }
+
+    public boolean check(int[][] matrix, int mid, int k, int n) {
+        int i = n - 1;
+        int j = 0;
+        int count = 0;
+
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] <= mid) {
+                count += (i + 1);
+                j++;
+            } else {
+                i--;
             }
         }
 
-        return pq.poll()[0];
+        return count >= k;
     }
 }
