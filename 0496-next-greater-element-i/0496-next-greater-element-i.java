377e1fcfc5566de35df847f55fe58bf1786989fb
team-hungrian <def.hunger@gmail.com>
2026-08-01 17:19:20 +0900

    Time: 2 ms (99.05%), Space: 44.5 MB (66.89%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -4,21 +4,28 @@ class Solution {
     }
 
     public int[] mySol(int[] nums1, int[] nums2) {
-        Stack<Integer> stack = new Stack();
+        int n = nums1.length;
+        int[] ans = new int[n];
+
         Map<Integer, Integer> map = new HashMap();
 
         for (int i = 0; i < nums2.length; i++) {
-            while (!stack.isEmpty() && nums2[stack.peek()] < nums2[i]) {
-                map.put(nums2[stack.pop()], nums2[i]);
+            map.put(nums2[i], i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int j = map.get(nums1[i]);
+
+            ans[i] = -1;
+
+            for (int k = j + 1; k < nums2.length; k++) {
+                if (nums2[j] < nums2[k]) {
+                    ans[i] = nums2[k];
+                    break;
+                }
             }
-
-            stack.push(i);
         }
 
-        for (int i = 0; i < nums1.length; i++) {
-            nums1[i] = map.getOrDefault(nums1[i], -1);
-        }
-
-        return nums1;
+        return ans;
     }
 }
