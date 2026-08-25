2db1f5b2e683760634980cd1b543bdbd799a516b
team-hungrian <def.hunger@gmail.com>
2026-08-25 17:32:06 +0900

    Time: 1 ms (35.08%), Space: 44.7 MB (45.9%) - LeetHub

---
+++ 0774-maximum-depth-of-n-ary-tree/0774-maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int maxChildDepth = 0;
+        List<Node> children = root.children;
+        for (Node child : children) {
+            int childDepth = maxDepth(child);
+            maxChildDepth = Math.max(maxChildDepth, childDepth);
+        }
+        return maxChildDepth + 1;
+    }
+}
