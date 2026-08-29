42a0b6a38c849a9641bcde19f260ace5b4bb6fcd
team-hungrian <def.hunger@gmail.com>
2026-08-29 19:08:14 +0900

    Time: 1223 ms (5.3%), Space: 167 MB (58.33%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,41 +1,166 @@
 class Solution {
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
-        return official_sort_and_bidir_mapping(nums, limit);
+        return mySol2_20260829(nums, limit);
     }
+
+    public int[] mySol2_20260829(int[] nums, int limit) {
+        int n = nums.length;
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+        
+        List<List<Integer>> groups = new ArrayList();
+
+        int prev = sorted[0];
+        List<Integer> group = new ArrayList();
+        group.add(prev);
+        
+        for (int i = 1; i < n; i++) {
+            int num = sorted[i];
+
+            if (prev + limit >= num) {
+                group.add(num);
+            } else {
+                groups.add(group);
+                group = new ArrayList();
+                group.add(num);
+            }
+
+            prev = num;
+        }
+
+        groups.add(group);
+
+        int[] ans = new int[n];
+        int index = 0;
+
+        for (int num : nums) {
+            int lo = 0;
+            int hi = groups.size();
+            
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+
+                group = groups.get(mid);
+
+                int min = group.get(0);
+                int max = group.get(group.size() - 1);
+
+                if (max < num) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid;
+                }
+            }
+
+            // System.out.println("groups:%s, lo:%d, num:%d".formatted(groups, lo, num));
+
+            group = groups.get(lo);
+
+            int smaller = group.remove(0);
+
+            ans[index++] = smaller;
+
+            if (group.size() == 0) {
+                groups.remove(lo);
+            }
+        }
+
+        return ans;
+    }
+
+    public int[] mySol_20260829_fail(int[] nums, int limit) {
+        int n = nums.length;
+
+        TreeMap<Integer, Integer> map = new TreeMap();
+
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int[] ans = new int[n];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = map.containsKey(nums[i]) ? nums[i] : map.higherKey(nums[i]);
+
+            int smallest = num - limit;
+
+            // Integer target = map.ceilingKey(smallest);
+            Integer target = findSmallest(map, num, limit);
+
+            int result = target != null ? target : num;
+
+            ans[index++] = result;
+
+            // System.out.println(result + ", " + map);
+
+            map.put(result, map.get(result) - 1);
+
+            if (map.get(result) == 0) {
+                map.remove(result);
+            }
+        }
+
+        return ans;
+    }
+
+    private Integer findSmallest(TreeMap<Integer, Integer> map, int num, int limit) {
+        Integer found = null;
+        Integer ans = num;
+
+        while (true) {
+            found = map.ceilingKey(ans - limit);
+
+            if (found != null && found < ans) {
+                ans = found;
+            } else {
+                break;
+            }
+        }
+
+        return ans == num ? null : ans;
+    }
+
+
+
+
+
+
+
+
+
+
 
     public int[] official_sort_and_bidir_mapping(int[] nums, int limit) {
         int n = nums.length;
-        Integer[][] arr = new Integer[n][2];
+        int[] arr = nums.clone();
 
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = nums[i];
-            arr[i][1] = i;
-        }
+        Arrays.sort(arr);
 
-        Arrays.sort(arr, (a, b) -> {
-            return a[0] - b[0];
-        });
-
-        Map<Integer, LinkedList<Integer[]>> groups = new HashMap();
+        Map<Integer, LinkedList<Integer>> groups = new HashMap();
         Map<Integer, Integer> numToGroup = new HashMap();
 
         int groupId = 0;
 
         groups.computeIfAbsent(groupId, k -> new LinkedList()).add(arr[0]);
-        numToGroup.put(arr[0][0], groupId);
+        numToGroup.put(arr[0], groupId);
 
         for (int i = 1; i < n; i++) {
-            if (Math.abs(arr[i - 1][0] - arr[i][0]) > limit) {
+            if (Math.abs(arr[i - 1] - arr[i]) > limit) {
                 groupId++;
             }
             groups.computeIfAbsent(groupId, k -> new LinkedList()).add(arr[i]);
-            numToGroup.put(arr[i][0], groupId);
+            numToGroup.put(arr[i], groupId);
+        }
+
+        for (List<Integer> group : groups.values()) {
+            System.out.println(group);
         }
 
         for (int i = 0; i < n; i++) {
             int num = nums[i];
             groupId = numToGroup.get(num);
-            nums[i] = groups.get(groupId).pop()[0];
+            nums[i] = groups.get(groupId).pop();
         }
 
         return nums;
