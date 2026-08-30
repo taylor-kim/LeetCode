cbc35c6de588094422dbb65aeefd97232abe1ac8
team-hungrian <def.hunger@gmail.com>
2026-08-30 20:46:52 +0900

    Time: 33 ms (90.14%), Space: 48 MB (9.39%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int candy : candyType) {
+            set.add(candy);
+        }
+        return Math.min(set.size(), candyType.length / 2);
+    }
+}
