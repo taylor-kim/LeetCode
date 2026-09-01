c310a11057398f7cc332e19897f588069b67e218
team-hungrian <def.hunger@gmail.com>
2026-09-02 03:55:55 +0900

    Time: 0 ms (100%), Space: 43.1 MB (64.62%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -6,23 +6,22 @@ class Solution {
     public void mySol(int[][] board) {
         int m = board.length;
         int n = board[0].length;
-        int[][] ans = new int[m][n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int lives = getLives(board, i, j);
 
-                if (board[i][j] == 1 && lives >= 2 && lives <= 3) {
-                    ans[i][j] = 1;
-                } else if (board[i][j] == 0 && lives == 3) {
-                    ans[i][j] = 1;
+                if ((board[i][j] & 1) == 1 && lives >= 2 && lives <= 3) {
+                    board[i][j] = 3;
+                } else if ((board[i][j] & 1) == 0 && lives == 3) {
+                    board[i][j] = 2;
                 }
             }
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] = ans[i][j];
+                board[i][j] >>= 1;
             }
         }
     }
@@ -36,7 +35,7 @@ class Solution {
             for (int j = Math.max(0, x - 1); j <= Math.min(x + 1, board[0].length - 1); j++) {
                 if (i == y && j == x) continue;
 
-                lives += board[i][j];
+                lives += (board[i][j] & 1);
             }
         }
 
