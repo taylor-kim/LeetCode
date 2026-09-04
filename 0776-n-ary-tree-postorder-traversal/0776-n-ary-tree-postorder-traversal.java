dcb882acbe1c045f26cfac77527b6b107ef004a4
team-hungrian <def.hunger@gmail.com>
2026-09-04 10:35:53 +0900

    Time: 1 ms (66.39%), Space: 45.7 MB (46.22%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(Node root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        for (Node ch : root.children) {
+            helper(ch, res);
+        }
+        res.add(root.val);
+    }
+}
