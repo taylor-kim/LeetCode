dd59b2df7cd2887b38fea0cd8516214385c35ab1
team-hungrian <def.hunger@gmail.com>
2026-09-06 18:07:32 +0900

    Time: 15 ms (87.02%), Space: 54.3 MB (50.85%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -4,7 +4,33 @@ class Solution {
     }
 
     public int mySol(String s, String t) {
-        return topdown(s, t, 0, 0, new Integer[s.length()][t.length()]);
+        // return topdown(s, t, 0, 0, new Integer[s.length()][t.length()]);
+        return try_bottomup(s, t);
+    }
+
+    public int try_bottomup(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+
+        for (int i = m - 1; i >= 0; i--) {
+            char c1 = s.charAt(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char c2 = t.charAt(j);
+
+                dp[i][j] = dp[i + 1][j];
+
+                if (c1 == c2) {
+                    dp[i][j] += dp[i + 1][j + 1];
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 
     public int topdown(String s, String t, int i, int j, Integer[][] memo) {
