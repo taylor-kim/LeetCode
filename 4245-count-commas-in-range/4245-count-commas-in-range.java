c950189abc65981204b3976455363ccd1b2f3081
team-hungrian <def.hunger@gmail.com>
2026-09-08 01:45:33 +0900

    Time: 1 ms (36.67%), Space: 42 MB (36.67%) - LeetHub

---
+++ 4245-count-commas-in-range/4245-count-commas-in-range.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int countCommas(int n) {
+        return mySol(n);
+    }
+
+    public int mySol(int n) {
+        //aa,bbb,ccc
+        //21,234,567
+        //34,567
+        int[] counter = new int[32];
+
+        int num = n;
+        int index = 0;
+
+        while (num > 0) {
+            int d = 0;
+            int pos = 1;
+            for (int i = 0; i < 3 && num > 0; i++) {
+                d += pos * (num % 10);
+                num /= 10;
+                pos *= 10;
+            }
+
+            counter[index++] = d;
+        }
+
+        if (index == 1) return 0;
+
+        int highest3 = counter[index - 1];
+        int others = 0;
+
+        int pos = 1;
+
+        for (int i = 0; i < index - 1; i++) {
+            others += pos * counter[i];
+            pos *= 1000;
+        }
+
+        // System.out.println(Arrays.toString(counter));
+
+        return (highest3 - 1) * 1000 + (others + 1);
+    }
+}
