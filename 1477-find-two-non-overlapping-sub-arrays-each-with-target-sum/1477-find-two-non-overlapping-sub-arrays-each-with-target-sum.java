7d43f4c002a84236e769436869946efefa881fd8
team-hungrian <def.hunger@gmail.com>
2026-09-18 02:38:17 +0900

    Time: 8 ms (67.26%), Space: 91 MB (60.54%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,9 +1,37 @@
 class Solution {
     public int minSumOfLengths(int[] arr, int target) {
-        return official_psum(arr, target);
+        return official_sw_dp(arr, target);
+    }
+    
+    public int official_sw_dp(int[] arr, int target) {
+        int n = arr.length;
+        int sum = 0;
+        int ans = n + 1;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n);
+
+        for (int l = 0, r = 0; r < n; r++) {
+            sum += arr[r];
+
+            while (sum > target) {
+                sum -= arr[l++];
+            }
+
+            if (sum == target) {
+                int length = r - l + 1;
+
+                dp[r + 1] = Math.min(dp[r], length);
+
+                ans = Math.min(ans, dp[l] + length);
+            }
+
+            dp[r + 1] = Math.min(dp[r], dp[r + 1]);
+        }
+
+        return ans == n + 1 ? -1 : ans;
     }
 
-    public int official_psum(int[] arr, int target) {
+    public int official_psum_dp(int[] arr, int target) {
         int n = arr.length;
         Map<Integer, Integer> pSum = new HashMap();
         pSum.put(0, -1);
