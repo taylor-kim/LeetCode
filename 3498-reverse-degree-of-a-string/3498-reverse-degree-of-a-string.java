7ce2644674a755f4001b5347e3844404b8d39202
team-hungrian <def.hunger@gmail.com>
2026-09-20 19:21:34 +0900

    Time: 1 ms (100%), Space: 43.9 MB (67.44%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,5 +1,20 @@
 class Solution {
     public int reverseDegree(String s) {
+        return mySol(s);
+    }
+
+    public int mySol(String s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            ans += (i + 1) * (26 - (c - 'a'));
+        }
+
+        return ans;
+    }
+
+    public int mySol_old(String s) {
         int ans = 0;
 
         for (int i = 0; i < s.length(); i++) {
