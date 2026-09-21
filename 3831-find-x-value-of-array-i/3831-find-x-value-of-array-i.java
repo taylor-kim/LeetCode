797d50f59e68cd1dbdbd39532356802afb70c9f6
team-hungrian <def.hunger@gmail.com>
2026-09-21 17:03:53 +0900

    Time: 47 ms (8.33%), Space: 99 MB (41.67%) - LeetHub

---
+++ 3831-find-x-value-of-array-i/3831-find-x-value-of-array-i.java
@@ -0,0 +1,66 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        return mySol2_with_hint_and_gemini(nums, k);
+    }
+
+    public long[] mySol2_with_hint_and_gemini(int[] nums, int k) {
+        int n = nums.length;
+        long[][] prefix = new long[n + 1][k];
+        // prefix[0][0] = 1;
+
+        // long[][] suffix = new long[n + 1][k];
+        // suffix[n][0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            int j = n - i - 1;
+            for (int x = 0; x < k; x++) {
+                prefix[i + 1][(int)(1l * x * nums[i] % k)] += prefix[i][x];
+                // suffix[j][x * nums[j] % k] += 1 + suffix[j + 1][x];
+            }
+            prefix[i + 1][nums[i] % k]++;
+        }
+
+        // for (long[] d : prefix) {
+        //     System.out.println(Arrays.toString(d) + ", ");
+        // }
+
+        // System.out.println("\n");
+
+        // for (long[] d : suffix) {
+        //     System.out.println(Arrays.toString(d) + ", ");
+        // }
+
+        long[] ans = new long[k];
+
+        for (int i = 0; i <= n; i++) {
+            for (int x = 0; x < k; x++) {
+                ans[x] += prefix[i][x];
+            }
+        }
+
+        return ans;
+    }
+
+    public long[] mySol_hold(int[] nums, int k) {
+        int n = nums.length;
+        long[] pMul = new long[n + 1];
+        pMul[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            pMul[i + 1] = pMul[i] * nums[i];
+        }
+
+        int[] ans = new int[k];
+
+        for (int x = 0; x < k; x++) {
+            long mul = 1;
+            for (int left = 0, right = 0; right < n; right++) {
+                mul *= nums[right];
+
+                // if ()
+            }
+        }
+
+        return null;
+    }
+}
