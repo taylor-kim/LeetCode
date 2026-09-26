33cfd8823146dab68f9ac74d3a3ad3646fb0522f
team-hungrian <def.hunger@gmail.com>
2026-09-26 20:09:54 +0900

    Time: 34 ms (79.64%), Space: 90.6 MB (86.42%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,6 +1,34 @@
 class Solution {
     public String evaluate(String s, List<List<String>> knowledge) {
-        return mySol(s, knowledge);
+        return editorial(s, knowledge);
+    }
+
+    public String editorial(String s, List<List<String>> knowledge) {
+        Map<String, String> map = new HashMap();
+
+        for (List<String> each : knowledge) {
+            map.put(each.get(0), each.get(1));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder key = new StringBuilder();
+
+        for (int i = -1, j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            if (c == '(') {
+                i = j;
+            } else if (c == ')') {
+                sb.append(map.getOrDefault(key.toString(), "?"));
+                i = -1;
+                key.setLength(0);
+            } else if (i < 0) {
+                sb.append(c);
+            } else {
+                key.append(c);
+            }
+        }
+
+        return sb.toString();
     }
 
     public String mySol(String s, List<List<String>> knowledge) {
